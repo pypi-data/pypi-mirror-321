df2d79133260c809647cc65bba1cc3cd11adfdbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:53:34 +0000

    Add package pyitc/1.1.0/pyitc-1.1.0-pp38-pypy38_pp73-win_amd64.whl

---
+++ packages/pyitc/pyitc-1.1.0-pp38-pypy38_pp73-win_amd64.whl/pyitc/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2024-2025 pyitc project. Released under AGPL-3.0
+# license. Refer to the LICENSE file for details or visit:
+# https://www.gnu.org/licenses/agpl-3.0.en.html
+"""Python bindings for the libitc library."""
+
+from __future__ import annotations
+
+from types import MappingProxyType
+
+from pyitc.pyitc import Stamp
+from pyitc.util import StampComparisonResult
+
+from ._internals import _lib
+
+__all__ = [
+    "SUPPORTED_FEATURES",
+    "Stamp",
+    "StampComparisonResult",
+]
+
+SUPPORTED_FEATURES: MappingProxyType[str, bool] = MappingProxyType(
+    {
+        "EXTENDED_API": bool(_lib.ITC_CONFIG_ENABLE_EXTENDED_API),
+        "64_BIT_EVENT_COUNTERS": bool(_lib.ITC_CONFIG_USE_64BIT_EVENT_COUNTERS),
+        "SERIALISE_TO_STRING_API": bool(_lib.ITC_CONFIG_ENABLE_SERIALISE_TO_STRING_API),
+    }
+)
+"""A mapping describing the supported features by the underlying C library"""
