c3080324f6bfb0568a1cb232d62a3fa2cbcb3e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:56:22 +0000

    Add package hanzi-pinyin/0.2.5/hanzi_pinyin-0.2.5.tar.gz

---
+++ packages/hanzi-pinyin/hanzi_pinyin-0.2.5.tar.gz/hanzi_pinyin-0.2.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name='hanzi_pinyin',
+    version='0.2.5',
+    packages=find_packages(),
+    description='Query the pinyin of Chinese characters.',
+    long_description=Path('README.md').read_text(encoding='utf-8'),
+    long_description_content_type='text/markdown',
+    author='neooier',
+    author_email='53945377+neooier@users.noreply.github.com',
+    url='https://github.com/neooier/hanzi_pinyin',
+    install_requires=[
+        'unidecode',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
