a31072fc52d247c4db30f1b0fd1ffc241fbe54aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:01:05 +0000

    Add package prefect/3.1.13.dev4/prefect-3.1.13.dev4-py3-none-any.whl

---
+++ packages/prefect/prefect-3.1.13.dev4-py3-none-any.whl/prefect/server/ui/assets/404-Dx1nJzW2.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,l as a,o as t,dC as r}from"./index-Dient852.js";import{u as s}from"./usePageTitle-DTgp16PY.js";const m=e({__name:"404",setup(n){return s("Page Not Found"),(c,p)=>(t(),o(a(r)))}});export{m as default};
+//# sourceMappingURL=404-Dx1nJzW2.js.map
---
+++ packages/prefect/prefect-3.1.13.dev4-py3-none-any.whl/prefect/server/ui/assets/useCan-fcXCjlwF.js
@@ -0,0 +1,2 @@
+import{cD as n}from"./index-Dient852.js";const c=Symbol("canInjectionKey");function a(){return n(c)}export{c,a as u};
+//# sourceMappingURL=useCan-fcXCjlwF.js.map
