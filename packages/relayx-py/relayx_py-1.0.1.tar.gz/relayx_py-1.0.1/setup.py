64267294a6c09b57920a5ff525ad3ffc25918c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:56 +0000

    Add package relayx-py/1.0.1/relayx_py-1.0.1.tar.gz

---
+++ packages/relayx-py/relayx_py-1.0.1.tar.gz/relayx_py-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="relayx_py",
+    version="1.0.1",
+    packages=["relayx_py"],
+    install_requires=["nats-py", "pytest-asyncio"],
+    author="Relay",
+    description="A SDK to connect to the Relay Network",
+    license="Apache 2.0"
+)
