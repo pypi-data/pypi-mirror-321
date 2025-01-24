3374f67f339cd3512bf800da37455ae8aac4f578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 22:04:58 +0000

    Add package kframework/7.1.199/kframework-7.1.199-py3-none-any.whl

---
+++ packages/kframework/kframework-7.1.199-py3-none-any.whl/pyk/k2lean4/Prelude.lean
@@ -0,0 +1,6 @@
+abbrev SortBool : Type := Int
+abbrev SortBytes: Type := ByteArray
+abbrev SortId : Type := String
+abbrev SortInt : Type := Int
+abbrev SortString : Type := String
+abbrev SortStringBuffer : Type := String
