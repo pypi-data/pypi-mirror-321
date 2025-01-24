1b8934258c073095aff0565e969b95feb1b0ee7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:19 +0000

    Add package yellowcard-business/0.1.0/yellowcard_business-0.1.0.tar.gz

---
+++ packages/yellowcard-business/yellowcard_business-0.1.0.tar.gz/yellowcard_business-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+def read_requirements(filename):
+    with open(filename) as f:
+        return f.read().splitlines()
+
+setup(
+    name='yellowcard_business',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=read_requirements('requirements.txt'),  # Use requirements.txt
+    description='Python SDK for the YellowCard B2B API',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/b2b-api-sdk',
+)
