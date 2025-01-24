cefbcf70a7fca7558df2f06cfff83520781b396f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:25 +0000

    Add package draive/0.39.0/draive-0.39.0-py3-none-any.whl

---
+++ packages/draive/draive-0.39.0-py3-none-any.whl/draive/mistral/tokenization.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from draive.sentencepiece import sentencepiece_processor, sentencepiece_tokenizer
+from draive.tokenization import Tokenization
+
+__all__ = [
+    "mistral_text_tokenizer",
+]
+
+
+async def mistral_text_tokenizer(
+    model_name: str,
+    /,
+) -> Tokenization:
+    return Tokenization(
+        tokenize_text=sentencepiece_tokenizer(
+            await sentencepiece_processor(model_path=_model_path(model_name))
+        )
+    )
+
+
+def _model_path(
+    model_name: str,
+    /,
+) -> str:
+    model_file: str = _mapping.get(model_name, "mistral_v3_tokenizer.model")
+    return str(Path(__file__).parent / "tokens" / model_file)
+
+
+_mapping: dict[str, str] = {
+    "open-mistral-7b": "mistral_v1_tokenizer.model",
+    "open-mixtral-8x7b": "mistral_v1_tokenizer.model",
+    "mistral-embed": "mistral_v1_tokenizer.model",
+    "mistral-small": "mistral_v2_tokenizer.model",
+    "mistral-large": "mistral_v2_tokenizer.model",
+    "open-mixtral-8x22b": "mistral_v3_tokenizer.model",
+    "codestral-22b": "mistral_v3_tokenizer.model",
+}
---
+++ packages/draive/draive-0.39.0-py3-none-any.whl/draive/tokenization/call.py
@@ -0,0 +1,40 @@
+from typing import Any
+
+from haiway import ctx
+
+from draive.tokenization.state import Tokenization
+
+__all__ = [
+    "count_text_tokens",
+    "tokenize_text",
+]
+
+
+def tokenize_text(
+    text: str,
+    **extra: Any,
+) -> list[int]:
+    """
+    Tokenize input text using current Tokenization state.
+    """
+
+    return ctx.state(Tokenization).tokenize_text(
+        text=text,
+        **extra,
+    )
+
+
+def count_text_tokens(
+    text: str,
+    **extra: Any,
+) -> int:
+    """
+    Count input text tokens using current Tokenization state.
+    """
+
+    return len(
+        ctx.state(Tokenization).tokenize_text(
+            text=text,
+            **extra,
+        )
+    )
