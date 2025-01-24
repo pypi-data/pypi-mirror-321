465a5be476aee139b2530b2b981a9155bfefc9d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:03:30 +0000

    Add package eba-file-tracker/0.1.4/eba_file_tracker-0.1.4.tar.gz

---
+++ packages/eba-file-tracker/eba_file_tracker-0.1.4.tar.gz/eba_file_tracker-0.1.4/eba_file_tracker/client.py
@@ -0,0 +1,204 @@
+import os
+import asyncio
+import signal
+import click
+from dotenv import load_dotenv
+from pathlib import Path
+from .core.models.base import DictJsonData
+from .core.models.command import Command, AddCommand, RemoveCommand, InfoCommand, SimpleCommand, CommandType
+from .core.models.result import CommandResult, ListTrackingInfoResult, ListTrackedInfoResult, PingResult, InfoResult, \
+    parse_result
+from .core.communication.json_transfer import read_json, write_json, read_json_from_file, write_json_to_file
+from .core.communication.system import get_pid, is_process_running
+from .response import ResponseFormatter
+
+load_dotenv(Path("eba_file_tracker") / "var" / ".env")
+PID_FILE = os.getenv("PID_FILE")
+SOCKET_FILE = os.getenv("SOCKET_FILE")
+HOST_NAME = os.getenv("HOST_NAME")
+HOST_PORT = int(os.getenv("HOST_PORT"))
+CLIENT_STATE_FILE = os.getenv("CLIENT_STATE_FILE")
+
+
+class State:
+    def __init__(self, json_data: DictJsonData):
+        self.state = json_data
+
+    @property
+    def use_unix_optimization(self) -> bool:
+        if 'use_unix_optimization' not in self.state:
+            self.use_unix_optimization = os.name == 'posix'
+        return self.state['use_unix_optimization']
+
+    @use_unix_optimization.setter
+    def use_unix_optimization(self, value: bool) -> None:
+        self.state['use_unix_optimization'] = value
+
+    def clear(self) -> None:
+        self.state = dict()
+
+    @staticmethod
+    def read() -> 'State':
+        if not os.path.exists(CLIENT_STATE_FILE):
+            write_json_to_file(CLIENT_STATE_FILE, dict())
+
+        return State(read_json_from_file(CLIENT_STATE_FILE))
+
+    def write(self) -> None:
+        write_json_to_file(CLIENT_STATE_FILE, self.state)
+
+
+state: State = None
+
+
+async def send_command(command: Command) -> CommandResult:
+    reader, writer = await asyncio.open_unix_connection(SOCKET_FILE) \
+        if state.use_unix_optimization \
+        else await asyncio.open_connection(HOST_NAME, HOST_PORT)
+
+    try:
+        await write_json(writer, command.to_json_data())
+        json_data = await read_json(reader)
+        result = parse_result(command.type, json_data)
+        return result
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+
+def send_ping() -> PingResult:
+    """Send ping command to the server."""
+    return asyncio.run(send_command(SimpleCommand(CommandType.PING)))
+
+
+def slim_ping() -> bool:
+    """Simplified server work check."""
+    return os.path.exists(PID_FILE) and is_process_running(get_pid(PID_FILE))
+
+
+@click.group()
+def cli():
+    """A client for managing a file tracking server."""
+    pass
+
+
+@cli.command()
+@click.option(
+    '-no', '--no-optimization', is_flag=True,
+    help="Don't use UNIX optimizations, even if they are available."
+)
+def start(no_optimization: bool) -> None:
+    """Start the file tracking server."""
+    if slim_ping():
+        send_ping()
+        click.echo("Server is already working")
+        return
+
+    if no_optimization:
+        state.use_unix_optimization = False
+
+    server_path = Path("eba_file_tracker.server")
+    start_cmd = f"python3 -m {server_path} -rl"
+    if state.use_unix_optimization:
+        start_cmd += " -ux"
+
+    os.system(start_cmd)
+
+    ping_result = send_ping()
+    click.echo("Server start work with configuration:")
+    click.echo(ResponseFormatter.make_from_configuration(ping_result.configuration))
+
+
+@cli.command()
+def stop() -> None:
+    """Stop the file tracking server."""
+    if not slim_ping():
+        click.echo("Server is not running")
+        return
+
+    send_ping()
+    state.clear()
+    pid = get_pid(PID_FILE)
+    os.kill(pid, signal.SIGTERM)
+    click.echo("Server stopped")
+
+
+@cli.command()
+def status() -> None:
+    """The status of the file tracking server."""
+    if not slim_ping():
+        click.echo("Server is not running")
+        return
+
+    ping_result = send_ping()
+    click.echo("Server is working with configuration:")
+    click.echo(ResponseFormatter.make_from_configuration(ping_result.configuration))
+
+
+@cli.command()
+@click.argument('file_path', type=click.Path(exists=True, dir_okay=False))
+@click.argument('file_id', type=int)
+def add(file_path: str, file_id: int) -> None:
+    """
+    Add files to tracking.
+    :param file_path: full path to the file
+    :param file_id: id of the file from the main serve.
+    """
+    if not slim_ping():
+        click.echo("Server is not running")
+        return
+
+    results: ListTrackingInfoResult = asyncio.run(send_command(AddCommand(file_path, file_id)))
+    click.echo(ResponseFormatter.make_from_add(results))
+
+
+@cli.command()
+@click.argument('file_path', type=click.Path(exists=True, dir_okay=False))
+def info(file_path: str) -> None:
+    """Information about the tracked file."""
+    if not slim_ping():
+        click.echo("Server is not running")
+        return
+
+    results: InfoResult = asyncio.run(send_command(InfoCommand(file_path)))
+    click.echo(ResponseFormatter.make_from_info(results))
+
+
+@cli.command()
+@click.argument("file_paths", nargs=-1, type=click.Path())
+def remove(file_paths: tuple) -> None:
+    """Remove files from tracking."""
+    if not slim_ping():
+        click.echo("Server is not running")
+        return
+
+    file_paths = list(file_paths)
+    results: ListTrackingInfoResult = asyncio.run(send_command(RemoveCommand(file_paths)))
+    click.echo(ResponseFormatter.make_from_remove(results))
+
+
+@cli.command(name="list")
+def get_list() -> None:
+    """List all tracked files."""
+    if not slim_ping():
+        click.echo("Server is not running")
+        return
+
+    results: ListTrackedInfoResult = asyncio.run(send_command(SimpleCommand(CommandType.LIST)))
+    click.echo(ResponseFormatter.make_from_list(results))
+
+
+def main() -> None:
+    global state
+    state = State.read()
+
+    try:
+        cli()
+    except Exception as e:
+        click.echo(f"Error: {e}", err=True)
+    finally:
+        state.write()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/eba-file-tracker/eba_file_tracker-0.1.4.tar.gz/eba_file_tracker-0.1.4/eba_file_tracker/core/tracker.py
@@ -0,0 +1,170 @@
+import os
+import socket
+import logging
+import requests
+from datetime import datetime, timezone
+from watchdog.events import FileSystemEventHandler, DirModifiedEvent, FileModifiedEvent, DirDeletedEvent, \
+    FileDeletedEvent
+from watchdog.observers import Observer
+from .models.tracker import File, FileMetadata
+from .models.result import CommandResultType, TrackingStatus, TrackingInfoResult, TrackedInfoResult, \
+    ListTrackedInfoResult, InfoResult
+
+
+def format_timestamp_to_iso8601(timestamp) -> datetime:
+    return datetime.fromtimestamp(timestamp, tz=timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+
+
+def send_metadata_to_server(metadata: FileMetadata) -> None:
+    try:
+        if not metadata:
+            raise ValueError("Empty metadata")
+        response = requests.post("http://127.0.0.1:8000/client/add_event", json=metadata.to_json_data())
+        if response.status_code == 200:
+            logging.info(f"Metadata has been sent successfully: {metadata.file_path}")
+        else:
+            logging.error(f"Error while sending metadata: {response.status_code}")
+    except Exception as e:
+        logging.error(f"Couldn't send metadata: {e}")
+
+
+class DirectoryEventHandler(FileSystemEventHandler):
+    def __init__(self) -> None:
+        super().__init__()
+        self.files: dict[str, File] = dict()
+
+    def add_file(self, file: File) -> bool:
+        if file.file_path in self.files:
+            return False
+
+        self.files[file.file_path] = file
+        return True
+
+    def remove_file(self, file_path: str) -> bool:
+        if file_path not in self.files:
+            return False
+
+        self.files.pop(file_path)
+        return True
+
+    def get_metadata(self, file_path: str) -> FileMetadata | None:
+        if file_path not in self.files:
+            return None
+
+        stats = os.stat(file_path)
+        return FileMetadata(
+            hostname=socket.gethostname(),
+            file_path=file_path,
+            file_id=self.files[file_path].file_id,
+            age=format_timestamp_to_iso8601(stats.st_ctime),
+            access_rights=oct(stats.st_mode)[-3:],
+            last_access_date=format_timestamp_to_iso8601(stats.st_atime),
+            last_modification_date=format_timestamp_to_iso8601(stats.st_mtime),
+            size=stats.st_size,
+        )
+
+    def on_modified(self, event: DirModifiedEvent | FileModifiedEvent) -> None:
+        if event.src_path in self.files:
+            logging.info(f"File modified: {event.src_path}")
+            metadata = self.get_metadata(event.src_path)
+            send_metadata_to_server(metadata)
+
+    def on_deleted(self, event: DirDeletedEvent | FileDeletedEvent) -> None:
+        if event.src_path in self.files:
+            if self.remove_file(event.src_path):
+                logging.info(f"File deleted: {event.src_path}")
+
+
+class SingleDirectoryTracker:
+    def __init__(self, dir_path: str) -> None:
+        super().__init__()
+        self._handler = DirectoryEventHandler()
+        self._observer = Observer()
+
+        self._observer.schedule(self._handler, dir_path, recursive=False)
+        self._observer.start()
+
+    def add_file(self, file: File) -> bool:
+        return self._handler.add_file(file)
+
+    def remove_file(self, file_path: str) -> bool:
+        return self._handler.remove_file(file_path)
+
+    def get_file_info(self, file_path: str) -> FileMetadata | None:
+        return self._handler.get_metadata(file_path)
+
+    def empty(self) -> bool:
+        return len(self._handler.files) == 0
+
+    def list(self) -> list[str]:
+        return list(self._handler.files.keys())
+
+    def stop(self) -> None:
+        self._observer.stop()
+        self._observer.join()
+
+
+class DirectoryTrackerManager:
+    def __init__(self):
+        self.tracker: dict[str, SingleDirectoryTracker] = dict()
+
+    def start_watching(self, file: File) -> TrackingInfoResult:
+        if not os.path.exists(file.file_path):
+            return TrackingInfoResult(CommandResultType.ADD, TrackingStatus.NOT_FOUND, file.file_path)
+
+        dir_path = os.path.dirname(file.file_path)
+        if dir_path not in self.tracker:
+            self.tracker[dir_path] = SingleDirectoryTracker(dir_path)
+
+        if self.tracker[dir_path].add_file(file):
+            return TrackingInfoResult(CommandResultType.ADD, TrackingStatus.IN_PROGRESS, file.file_path)
+        else:
+            return TrackingInfoResult(CommandResultType.ADD, TrackingStatus.ALREADY, file.file_path)
+
+    def stop_watching(self, file_path: str) -> TrackingInfoResult:
+        if not os.path.exists(file_path):
+            return TrackingInfoResult(CommandResultType.REMOVE, TrackingStatus.NOT_FOUND, file_path)
+
+        dir_path = os.path.dirname(file_path)
+        if dir_path not in self.tracker:
+            return TrackingInfoResult(CommandResultType.REMOVE, TrackingStatus.ALREADY, file_path)
+
+        tracker = self.tracker[dir_path]
+        is_success = tracker.remove_file(file_path)
+
+        if tracker.empty():
+            self._remove_tracker(dir_path)
+
+        return TrackingInfoResult(CommandResultType.REMOVE, TrackingStatus.COMPLETED, file_path) if is_success \
+            else TrackingInfoResult(CommandResultType.REMOVE, TrackingStatus.ALREADY, file_path)
+
+    def get_file_info(self, file_path: str) -> InfoResult:
+        if not os.path.exists(file_path):
+            return InfoResult(None)
+
+        dir_path = os.path.dirname(file_path)
+        if dir_path not in self.tracker:
+            return InfoResult(None)
+
+        tracker = self.tracker[dir_path]
+        return InfoResult(tracker.get_file_info(file_path))
+
+    def list_watched_files(self) -> ListTrackedInfoResult:
+        watched_files: list[TrackedInfoResult] = []
+        for tracker in self.tracker.values():
+            watched_files.extend(
+                map(
+                    lambda tracker_file: TrackedInfoResult(tracker_file),
+                    tracker.list()
+                )
+            )
+        return ListTrackedInfoResult(watched_files)
+
+    def stop_all_watching(self) -> None:
+        for dir_path in list(self.tracker.keys()):
+            self._remove_tracker(dir_path)
+
+    def _remove_tracker(self, dir_path: str) -> None:
+        tracker = self.tracker.pop(dir_path)
+        tracker.stop()
+        del tracker
