dc18734e8a464d89b631e9a6801c68a5a25ec5be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 18:06:21 +0000

    Add package pytwin/0.8.0/pytwin-0.8.0-py3-none-any.whl

---
+++ packages/pytwin/pytwin-0.8.0-py3-none-any.whl/pytwin/twin_runtime/log_level.py
@@ -0,0 +1,12 @@
+#------------------------------------------------------------------------------
+# (c) 2020-2024 ANSYS, Inc. All rights reserved.
+#------------------------------------------------------------------------------
+from enum import Enum
+
+
+class LogLevel(Enum):
+    TWIN_LOG_ALL = 0
+    TWIN_LOG_WARNING = 1
+    TWIN_LOG_ERROR = 2
+    TWIN_LOG_FATAL = 3
+    TWIN_NO_LOG = 4
