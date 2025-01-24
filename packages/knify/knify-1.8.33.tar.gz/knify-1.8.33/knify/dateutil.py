80711ebac81c44b3c102e5ead57d5f18aebe2054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:02:01 +0000

    Add package knify/1.8.33/knify-1.8.33.tar.gz

---
+++ packages/knify/knify-1.8.33.tar.gz/knify-1.8.33/knify/dateutil.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+import datetime
+import time
+
+FORMAT_DATE_YYMMDD = "%Y-%m-%d"
+FORMAT_DATE_HMS = "%H:%M:%S"
+FORMAT_DATE_YYMMDDHMS = "%Y-%m-%d %H:%M:%S"
+FORMAT_DATE_YYMMDDHMSF = "%Y-%m-%d %H:%M:%S.%f"
+
+
+def now() -> datetime:
+    return datetime.datetime.now()
+
+
+def date_to_str(date_obj: datetime, format: str = FORMAT_DATE_YYMMDDHMS) -> str:
+    if type(date_obj) == datetime.timedelta:
+        return (datetime.datetime(1970, 1, 1) + date_obj).strftime(format)
+    return date_obj.strftime(format)
+
+
+def str_to_date(str_obj: str, format: str) -> datetime:
+    return datetime.datetime.strptime(str_obj, format)
+
+
+def date_to_timestamp(date_obj: datetime):
+    return time.mktime(date_obj.timetuple())
