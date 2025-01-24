7649bdacda5ab7ce814fd720ff946f6cc908ded9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:05 +0000

    Add package hellosailb/2.0.0b70/hellosailb-2.0.0b70.tar.gz

---
+++ packages/hellosailb/hellosailb-2.0.0b70.tar.gz/hellosailb-2.0.0b70/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+import sys
+
+
+if sys.version_info < (3, 8):
+    raise RuntimeError("requires Python 3.8+")
+current_version = '2.0.0b70'
+
+setup(
+    name='hellosailb',
+    version=current_version,
+    author='Sailesh',
+    author_email='test@gmail.com',
+    packages=find_packages(where='.', exclude=['test*']),
+    license='LICENSE',
+    description=' Python programming language',
+    install_requires=[
+],
+    python_requires=">=3.8"
+)
