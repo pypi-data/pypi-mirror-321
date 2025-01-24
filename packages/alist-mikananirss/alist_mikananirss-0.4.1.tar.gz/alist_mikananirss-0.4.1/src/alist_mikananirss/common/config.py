6122125964a4606c57e9c59c827dff8c60c66e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 19:05:16 +0000

    Add package alist-mikananirss/0.4.1/alist_mikananirss-0.4.1.tar.gz

---
+++ packages/alist-mikananirss/alist_mikananirss-0.4.1.tar.gz/alist_mikananirss-0.4.1/src/alist_mikananirss/common/config.py
@@ -0,0 +1,347 @@
+from dataclasses import dataclass
+from typing import Optional
+
+import yaml
+
+from alist_mikananirss.alist.api import AlistDownloaderType
+from alist_mikananirss.bot.pushplus_bot import PushPlusChannel
+
+from ..utils import Singleton
+
+
+class ConfigLoader:
+    """A class for loading and accessing YAML configuration files.
+
+    This class provides methods to load a YAML config file and retrieve values
+    using dot notation paths.
+
+    Attributes:
+        config_path (str): The path to the YAML configuration file.
+        config (dict): The loaded configuration data.
+
+    Example:
+        >>> config_loader = ConfigLoader('config.yaml')
+        >>> database_url = config_loader.get('database.url')
+        >>> port = config_loader.get('server.port', default=8080)
+    """
+
+    _MISSING = object()
+
+    def __init__(self, config_path):
+        """Initializes the ConfigLoader with the given config file path.
+
+        Args:
+            config_path (str): The path to the YAML configuration file.
+        """
+        self.config_path = config_path
+        self.config = self.load_config()
+
+    def load_config(self):
+        """Loads the YAML configuration file.
+
+        Returns:
+            dict: The loaded configuration data.
+
+        Raises:
+            yaml.YAMLError: If there's an error parsing the YAML file.
+        """
+        with open(self.config_path, "r", encoding="utf-8") as file:
+            return yaml.safe_load(file)
+
+    def get(self, path, default=_MISSING):
+        """Retrieves a value from the configuration using a dot notation path.
+
+        Args:
+            path (str): The dot notation path to the desired configuration value.
+            default: The default value to return if the path is not found.
+                     If not provided, a KeyError will be raised.
+
+        Returns:
+            The value at the specified path in the configuration.
+
+        Raises:
+            KeyError: If the path is not found and no default value is provided.
+        """
+        keys = path.split(".")
+        value = self.config
+        for key in keys:
+            if value.get(key) is None:
+                if default is not self._MISSING:
+                    return default
+                else:
+                    raise KeyError(
+                        f"{path} is not found in config file {self.config_path}"
+                    )
+            value = value[key]
+        return value
+
+
+@dataclass
+class AppConfig:
+    common_interval_time: int
+    common_proxies: Optional[dict]
+
+    alist_base_url: str
+    alist_token: str
+    alist_downloader: AlistDownloaderType
+    alist_download_path: str
+
+    mikan_subscribe_url: list
+    mikan_regex_pattern: dict
+    mikan_filters: list
+
+    notification_enable: bool
+    notification_telegram_enable: bool
+    notification_telegram_bot_token: str
+    notification_telegram_user_id: str
+    notification_pushplus_enable: bool
+    notification_pushplus_token: str
+    notification_pushplus_channel: PushPlusChannel
+    notification_interval_time: int
+
+    rename_enable: bool
+    rename_chatgpt_api_key: str
+    rename_chatgpt_base_url: str
+    rename_chatgpt_model: str
+    rename_format: str
+    rename_remap_enable: bool
+    rename_remap_cfg_path: str
+
+    bot_assistant_enable: bool
+    bot_assistant_telegram_enable: bool
+    bot_assistant_telegram_bot_token: str
+
+    dev_log_level: str
+
+    def __post_init__(self):
+        """Check the validity of the configuration."""
+        if self.notification_enable:
+            assert (
+                self.notification_telegram_enable or self.notification_pushplus_enable
+            ), "At least one notification method should be enabled."
+        if self.notification_telegram_enable:
+            assert (
+                self.notification_telegram_bot_token
+                and self.notification_telegram_user_id
+            ), "Telegram bot token and user id should be provided."
+        if self.notification_pushplus_enable:
+            assert (
+                self.notification_pushplus_token
+            ), "Pushplus token should be provided."
+        if self.rename_enable:
+            assert self.rename_chatgpt_api_key, "ChatGPT API key should be provided."
+            self.__check_rename_format(self.rename_format)
+        if self.bot_assistant_enable:
+            assert (
+                self.bot_assistant_telegram_enable
+            ), "Telegram config should be provided."
+            assert (
+                self.bot_assistant_telegram_bot_token
+            ), "Telegram bot token should be provided."
+
+        assert self.common_interval_time >= 0, "Invalid interval time."
+        assert self.dev_log_level in [
+            "DEBUG",
+            "INFO",
+            "WARNING",
+            "ERROR",
+            "CRITICAL",
+        ], "Invalid log level."
+
+    def __repr__(self) -> str:
+        sections = {
+            "common": {
+                "interval_time": self.common_interval_time,
+                "proxies": self.common_proxies,
+            },
+            "alist": {
+                "base_url": self.alist_base_url,
+                "token": self.alist_token,
+                "downloader": self.alist_downloader,
+                "download_path": self.alist_download_path,
+            },
+            "mikan": {
+                "subscribe_url": self.mikan_subscribe_url,
+                "regex_pattern": self.mikan_regex_pattern,
+                "filters": self.mikan_filters,
+            },
+            "notification": {
+                "telegram": {
+                    "bot_token": self.notification_telegram_bot_token,
+                    "user_id": self.notification_telegram_user_id,
+                },
+                "pushplus": {"token": self.notification_pushplus_token},
+                "interval_time": self.notification_interval_time,
+            },
+            "rename": {
+                "chatgpt": {
+                    "api_key": self.rename_chatgpt_api_key,
+                    "base_url": self.rename_chatgpt_base_url,
+                    "model": self.rename_chatgpt_model,
+                },
+                "rename_format": self.rename_format,
+                "remap": {
+                    "enable": self.rename_remap_enable,
+                    "cfg_path": self.rename_remap_cfg_path,
+                },
+            },
+            "bot_assistant": {
+                "telegram": {
+                    "bot_token": self.bot_assistant_telegram_bot_token,
+                },
+            },
+            "dev": {"log_level": self.dev_log_level},
+        }
+
+        def format_dict(d: dict, indent: int = 0) -> str:
+            result = []
+            for key, value in d.items():
+                spaces = " " * indent
+                if isinstance(value, dict):
+                    result.append(f"{spaces}{key}:")
+                    result.append(format_dict(value, indent + 2))
+                elif isinstance(value, list):
+                    result.append(f"{spaces}{key}:")
+                    for item in value:
+                        result.append(f"{spaces}  - {item}")
+                else:
+                    result.append(f"{spaces}{key}: {value}")
+            return "\n".join(result)
+
+        return format_dict(sections)
+
+    def __check_rename_format(self, rename_format: str):
+        # Check if there are unknown keys in rename format
+        from collections import defaultdict
+
+        all_key_test_data = {
+            "name": "test",
+            "season": 1,
+            "episode": 1,
+            "fansub": "fansub",
+            "quality": "1080p",
+            "language": "简体中文",
+        }
+        # if the key in rename_format is not in all_key_test_data, it will be replaced by "undefined"
+        safe_dict = defaultdict(lambda: "undefined", all_key_test_data)
+        res = rename_format.format_map(safe_dict)
+        if "undefined" in res:
+            unknown_keys = [
+                key for key, value in safe_dict.items() if value == "undefined"
+            ]
+            raise KeyError(f"Error keys in rename format: {', '.join(unknown_keys)}")
+
+
+class ConfigManager(metaclass=Singleton):
+    def __init__(self, config_path):
+        self.config: AppConfig = self.__load_config(config_path)
+
+    def __load_config(self, path):
+        config_loader = ConfigLoader(path)
+        # ----------common----------
+        common_interval_time = config_loader.get("common.interval_time", 300)
+        common_proxies = config_loader.get("common.proxies", None)
+        # ----------alist----------
+        alist_base_url = config_loader.get("alist.base_url")
+        alist_token = config_loader.get("alist.token")
+        alist_downloader = AlistDownloaderType(
+            config_loader.get("alist.downloader", AlistDownloaderType.ARIA.value)
+        )
+        alist_download_path = config_loader.get("alist.download_path")
+        # ----------mikan----------
+        urls = config_loader.get("mikan.subscribe_url")
+        if isinstance(urls, str):
+            urls = [urls]
+        mikan_subscribe_url = urls
+        default_regex_pattern = {
+            "简体": "(简体|简中|简日|CHS)",
+            "繁体": "(繁体|繁中|繁日|CHT|Baha)",
+            "1080p": "(X1080|1080P)",
+            "非合集": "^(?!.*(\\d{2}-\\d{2}|合集)).*",
+        }
+        default_regex_pattern.update(config_loader.get("mikan.regex_pattern", {}))
+        mikan_regex_pattern = default_regex_pattern
+        mikan_filters = config_loader.get("mikan.filters", ["1080p", "非合集"])
+        # ----------notification----------
+        notification_enable = (
+            False if not config_loader.get("notification", {}) else True
+        )
+        notification_telegram_enable = (
+            False if not config_loader.get("notification.telegram", {}) else True
+        )
+        notification_telegram_bot_token = config_loader.get(
+            "notification.telegram.bot_token", ""
+        )
+        notification_telegram_user_id = config_loader.get(
+            "notification.telegram.user_id", ""
+        )
+        notification_pushplus_enable = (
+            False if not config_loader.get("notification.pushplus", {}) else True
+        )
+        notification_pushplus_token = config_loader.get(
+            "notification.pushplus.token", ""
+        )
+        notification_pushplus_channel = PushPlusChannel(
+            config_loader.get(
+                "notification.pushplus.channel", PushPlusChannel.WECHAT.value
+            )
+        )
+        notification_interval_time = config_loader.get(
+            "notification.interval_time", 300
+        )
+        # ----------rename----------
+        rename_enable = False if not config_loader.get("rename", {}) else True
+        rename_chatgpt_api_key = config_loader.get("rename.chatgpt.api_key", "")
+        rename_chatgpt_base_url = config_loader.get("rename.chatgpt.base_url", "")
+        rename_chatgpt_model = config_loader.get("rename.chatgpt.model", "gpt-4o-mini")
+        rename_format = config_loader.get(
+            "rename.rename_format", "{name} S{season:02d}E{episode:02d}"
+        )
+        rename_remap_enable = config_loader.get("rename.remap.enable", False)
+        rename_remap_cfg_path = config_loader.get("rename.remap.cfg_path", "remap.yaml")
+        # ----------bot_assistant----------
+        bot_assistant_enable = (
+            False if not config_loader.get("bot_assistant", {}) else True
+        )
+        bot_assistant_telegram_enable = (
+            False if not config_loader.get("bot_assistant.telegram", {}) else True
+        )
+        bot_assistant_telegram_bot_token = config_loader.get(
+            "bot_assistant.telegram.bot_token", ""
+        )
+        # ----------dev----------
+        dev_log_level = config_loader.get("dev.log_level", "INFO")
+
+        return AppConfig(
+            common_interval_time=common_interval_time,
+            common_proxies=common_proxies,
+            alist_base_url=alist_base_url,
+            alist_token=alist_token,
+            alist_downloader=alist_downloader,
+            alist_download_path=alist_download_path,
+            mikan_subscribe_url=mikan_subscribe_url,
+            mikan_regex_pattern=mikan_regex_pattern,
+            mikan_filters=mikan_filters,
+            notification_enable=notification_enable,
+            notification_telegram_enable=notification_telegram_enable,
+            notification_telegram_bot_token=notification_telegram_bot_token,
+            notification_telegram_user_id=notification_telegram_user_id,
+            notification_pushplus_enable=notification_pushplus_enable,
+            notification_pushplus_token=notification_pushplus_token,
+            notification_pushplus_channel=notification_pushplus_channel,
+            notification_interval_time=notification_interval_time,
+            rename_enable=rename_enable,
+            rename_chatgpt_api_key=rename_chatgpt_api_key,
+            rename_chatgpt_base_url=rename_chatgpt_base_url,
+            rename_chatgpt_model=rename_chatgpt_model,
+            rename_format=rename_format,
+            rename_remap_enable=rename_remap_enable,
+            rename_remap_cfg_path=rename_remap_cfg_path,
+            bot_assistant_enable=bot_assistant_enable,
+            bot_assistant_telegram_enable=bot_assistant_telegram_enable,
+            bot_assistant_telegram_bot_token=bot_assistant_telegram_bot_token,
+            dev_log_level=dev_log_level,
+        )
+
+    def get_config(self):
+        return self.config
---
+++ packages/alist-mikananirss/alist_mikananirss-0.4.1.tar.gz/alist_mikananirss-0.4.1/tests/download_manager/test_download_manager.py
@@ -0,0 +1,280 @@
+from unittest.mock import AsyncMock, MagicMock, patch
+
+import pytest
+from loguru import logger
+from tenacity import RetryError, wait_none
+
+from alist_mikananirss.alist import Alist
+from alist_mikananirss.alist.tasks import (
+    AlistDownloadTask,
+    AlistTaskCollection,
+    AlistTaskStatus,
+    AlistTransferTask,
+)
+from alist_mikananirss.common.database import SubscribeDatabase
+from alist_mikananirss.core import DownloadManager
+from alist_mikananirss.core.download_manager import AnimeDownloadTaskInfo, TaskMonitor
+from alist_mikananirss.websites import ResourceInfo
+
+
+@pytest.fixture
+def test_rousources():
+    resources = [
+        ResourceInfo(
+            resource_title="title1",
+            torrent_url="https://test1.torrent",
+            published_date="1",
+            anime_name="Test Anime",
+            season=1,
+            episode=5,
+            fansub="TestSub",
+            quality="1080p",
+            language="JP",
+        ),
+        ResourceInfo(
+            resource_title="title2",
+            torrent_url="https://test2.torrent",
+            published_date="1",
+            anime_name="Test Anime2",
+            season=1,
+            episode=5,
+            fansub="TestSub",
+            quality="1080p",
+            language="JP",
+        ),
+    ]
+    return resources
+
+
+@pytest.fixture
+def download_manager():
+    mock_alist = AsyncMock(spec=Alist)
+    mock_db = AsyncMock(spec=SubscribeDatabase)
+    DownloadManager.initialize(
+        mock_alist, "/base/path", use_renamer=True, need_notification=True, db=mock_db
+    )
+    return DownloadManager()
+
+
+@pytest.mark.asyncio
+async def test_download(download_manager, test_rousources):
+    download_manager.alist_client.add_offline_download_task.side_effect = [
+        AlistTaskCollection(
+            [
+                AlistDownloadTask(
+                    tid=1,
+                    description="Download task 1",
+                    status="success",
+                    progress=100,
+                    error_msg=None,
+                    url=test_rousources[0].torrent_url,
+                ),
+            ]
+        ),
+        TimeoutError,
+    ]
+    with patch.object(logger, "error") as logger_error_mock:
+        success_task_info = await download_manager.download(test_rousources)
+        logger_error_mock.assert_called_once()
+        assert len(success_task_info) == 1
+        assert success_task_info[0].resource == test_rousources[0]
+
+
+@pytest.mark.asyncio
+async def test_monitor_success(download_manager):
+    test_task = AnimeDownloadTaskInfo(
+        resource=ResourceInfo(
+            resource_title="title",
+            torrent_url="https://test1.torrent",
+            published_date="1",
+            anime_name="Test Anime",
+            season=1,
+            episode=5,
+            fansub="TestSub",
+            quality="1080p",
+            language="JP",
+        ),
+        download_path="test/path",
+        download_task=MagicMock(),
+    )
+
+    with (
+        patch.object(download_manager, "_wait_finished") as wait_finished_mock,
+        patch.object(download_manager, "_post_process") as post_process_mock,
+    ):
+        wait_finished_mock.return_value = MagicMock()
+        await download_manager.monitor(test_task)
+        wait_finished_mock.assert_awaited_once_with(test_task)
+        post_process_mock.assert_called_once()
+        assert download_manager.db.delete_by_resource_title.call_count == 0
+
+
+@pytest.mark.asyncio
+async def test_monitor_failed(download_manager):
+    test_task = AnimeDownloadTaskInfo(
+        resource=ResourceInfo(
+            resource_title="title",
+            torrent_url="https://test1.torrent",
+            published_date="1",
+            anime_name="Test Anime",
+            season=1,
+            episode=5,
+            fansub="TestSub",
+            quality="1080p",
+            language="JP",
+        ),
+        download_path="test/path",
+        download_task=MagicMock(),
+    )
+
+    with (
+        patch.object(download_manager, "_wait_finished") as wait_finished_mock,
+        patch.object(download_manager, "_post_process") as post_process_mock,
+    ):
+        wait_finished_mock.return_value = None
+        await download_manager.monitor(test_task)
+        wait_finished_mock.assert_awaited_once_with(test_task)
+        assert post_process_mock.call_count == 0
+        download_manager.db.delete_by_resource_title.assert_called_once_with(
+            test_task.resource.resource_title
+        )
+
+
+@pytest.mark.asyncio
+async def test_add_download_task(download_manager, test_rousources):
+    download_tasks_info = [
+        AnimeDownloadTaskInfo(
+            resource=test_rousources[0],
+            download_path="test/path",
+            download_task=MagicMock(),
+        ),
+        AnimeDownloadTaskInfo(
+            resource=test_rousources[1],
+            download_path="test/path",
+            download_task=MagicMock(),
+        ),
+    ]
+
+    with patch.object(download_manager, "download") as download_mock:
+        with patch.object(download_manager, "monitor") as monitor_mock:
+            download_mock.return_value = download_tasks_info
+            await DownloadManager.add_download_tasks(test_rousources)
+            download_mock.assert_called_once()
+            assert download_manager.db.insert_resource_info.call_count == 2
+            assert monitor_mock.call_count == 2
+
+
+@pytest.mark.asyncio
+async def test_find_transfer_task(download_manager):
+    resource = ResourceInfo(
+        anime_name="Test Anime",
+        resource_title="Episode 1",
+        torrent_url="https://example.com/test.torrent",
+    )
+    transfer_task = AlistTransferTask(
+        tid="123",
+        description="transfer Test Anime/file.mp4 to [/path]",
+        status=AlistTaskStatus.Running,
+        progress=50,
+        uuid="123",
+        file_name="file.mp4",
+    )
+
+    download_manager.alist_client.get_task_list.return_value = [transfer_task]
+
+    download_manager._find_transfer_task.retry.wait = wait_none()
+    result = await download_manager._find_transfer_task(resource)
+
+    assert result == transfer_task
+    assert transfer_task.uuid in download_manager.uuid_set
+
+
+@pytest.mark.asyncio
+async def test_find_transfer_task_not_found(download_manager):
+    resource = ResourceInfo(
+        anime_name="Test Anime",
+        resource_title="Episode 1",
+        torrent_url="https://example.com/test.torrent",
+    )
+    transfer_task = AlistTransferTask(
+        tid="123",
+        description="transfer WrongAnime/file.mp4 to [/path]",
+        status=AlistTaskStatus.Running,
+        progress=50,
+        uuid="123",
+        file_name="file.mp4",
+    )
+
+    download_manager.alist_client.get_task_list.return_value = [transfer_task]
+
+    with (
+        patch("asyncio.sleep", new_callable=AsyncMock),
+        patch("tenacity.nap.sleep", return_value=None),
+    ):
+        with pytest.raises(RetryError):
+            await download_manager._find_transfer_task(resource)
+
+
+@pytest.mark.asyncio
+async def test_wait_finished_success(download_manager):
+    resource = ResourceInfo(
+        anime_name="Test Anime",
+        resource_title="Episode 1",
+        torrent_url="https://example.com/test.torrent",
+    )
+    download_task = AlistDownloadTask(
+        tid="123",
+        description="download https://example.com/test.torrent to",
+        status=AlistTaskStatus.Running,
+        progress=0.5,
+        url="https://example.com/test.torrent",
+    )
+    transfer_task = AlistTransferTask(
+        tid="456",
+        description="transfer Test Anime/file.mp4 to [/path]",
+        status=AlistTaskStatus.Running,
+        progress=0.5,
+        uuid="uuid",
+        file_name="file.mp4",
+    )
+
+    task_info = AnimeDownloadTaskInfo(
+        resource=resource,
+        download_path="/base/path/Test Anime",
+        download_task=download_task,
+    )
+
+    with (
+        patch(
+            "alist_mikananirss.core.download_manager.TaskMonitor"
+        ) as mock_task_monitor,
+        patch.object(
+            download_manager,
+            "_find_transfer_task",
+            AsyncMock(return_value=transfer_task),
+        ),
+    ):
+        mock_download_task_monitor = AsyncMock(spec=TaskMonitor)
+        mock_download_task_monitor.wait_finished.return_value = AlistDownloadTask(
+            tid="123",
+            description="",
+            status=AlistTaskStatus.Succeeded,
+            progress=1.0,
+            url="https://example.com/test.torrent",
+        )
+        mock_transfer_task_monitor = MagicMock(spec=TaskMonitor)
+        mock_transfer_task_monitor.wait_finished.return_value = AlistTransferTask(
+            tid="456",
+            description="",
+            status=AlistTaskStatus.Succeeded,
+            progress=1.0,
+            uuid="uuid",
+            file_name="file.mp4",
+        )
+        mock_task_monitor.side_effect = [
+            mock_download_task_monitor,
+            mock_transfer_task_monitor,
+        ]
+        result = await download_manager._wait_finished(task_info)
+        assert result is not None
+        assert result.transfer_task.tid == transfer_task.tid
