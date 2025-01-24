23343ff4cca066bfd55603b3ed10f20758bb2945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:15 +0000

    Add package swcgeom/0.18.3/swcgeom-0.18.3.tar.gz

---
+++ packages/swcgeom/swcgeom-0.18.3.tar.gz/swcgeom-0.18.3/swcgeom/utils/solid_geometry.py
@@ -0,0 +1,109 @@
+# Copyright 2022-2025 Zexin Yuan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Solid Geometry."""
+
+import numpy as np
+import numpy.typing as npt
+
+__all__ = [
+    "find_unit_vector_on_plane",
+    "find_sphere_line_intersection",
+    "project_point_on_line",
+    "project_vector_on_vector",
+    "project_vector_on_plane",
+]
+
+
+def find_unit_vector_on_plane(normal_vec3: npt.NDArray) -> npt.NDArray:
+    r = np.random.rand(3)
+    r /= np.linalg.norm(r)
+    while np.allclose(r, normal_vec3) or np.allclose(r, -normal_vec3):
+        r = np.random.rand(3)
+        r /= np.linalg.norm(r)
+
+    u = np.cross(r, normal_vec3)
+    u /= np.linalg.norm(u)
+    return u
+
+
+def find_sphere_line_intersection(
+    sphere_center: npt.NDArray,
+    sphere_radius: float,
+    line_point_a: npt.NDArray,
+    line_point_b: npt.NDArray,
+) -> list[tuple[float, npt.NDArray[np.float64]]]:
+    A = np.array(line_point_a)
+    B = np.array(line_point_b)
+    C = np.array(sphere_center)
+
+    D = B - A  # line direction vector
+    f = A - C  # line to sphere center
+
+    # solve: a*t^2 + b*t + c = 0
+    a = np.dot(D, D)
+    b = 2 * np.dot(f, D)
+    c = np.dot(f, f) - sphere_radius**2
+    discriminant = b**2 - 4 * a * c
+
+    if discriminant < 0:
+        return []  # no intersection
+
+    if discriminant == 0:
+        t = -b / (2 * a)
+        p = A + t * D
+        return [(t, p)]  # single intersection, a tangent
+
+    t1 = (-b - np.sqrt(discriminant)) / (2 * a)
+    t2 = (-b + np.sqrt(discriminant)) / (2 * a)
+    p1 = A + t1 * D
+    p2 = A + t2 * D
+    return [(t1, p1), (t2, p2)]
+
+
+def project_point_on_line(
+    point_a: npt.ArrayLike, direction_vector: npt.ArrayLike, point_p: npt.ArrayLike
+) -> npt.NDArray:
+    A = np.array(point_a)
+    n = np.array(direction_vector)
+    P = np.array(point_p)
+
+    AP = P - A
+    projection = A + np.dot(AP, n) / np.dot(n, n) * n
+    return projection
+
+
+def project_vector_on_vector(vec: npt.ArrayLike, target: npt.ArrayLike) -> npt.NDArray:
+    v = np.array(vec)
+    n = np.array(target)
+
+    n_normalized = n / np.linalg.norm(n)
+    projection_on_n = np.dot(v, n_normalized) * n_normalized
+    return projection_on_n
+
+
+def project_vector_on_plane(
+    vec: npt.ArrayLike, plane_normal_vec: npt.ArrayLike
+) -> npt.NDArray:
+    v = np.array(vec)
+    n = np.array(plane_normal_vec)
+
+    # project v to n
+    projection_on_n = project_vector_on_vector(vec, n)
+
+    # project v to plane
+    projection_on_plane = v - projection_on_n
+
+    return projection_on_plane
---
+++ packages/swcgeom/swcgeom-0.18.3.tar.gz/swcgeom-0.18.3/swcgeom/utils/transforms.py
@@ -0,0 +1,313 @@
+# Copyright 2022-2025 Zexin Yuan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""3D geometry transformations."""
+
+import numpy as np
+import numpy.typing as npt
+
+__all__ = [
+    "Vec3f",
+    "angle",
+    "scale3d",
+    "translate3d",
+    "rotate3d",
+    "rotate3d_x",
+    "rotate3d_y",
+    "rotate3d_z",
+    "to_homogeneous",
+    "model_view_transformation",
+    "orthographic_projection_simple",
+]
+
+Vec3f = tuple[float, float, float]
+
+
+def angle(a: npt.ArrayLike, b: npt.ArrayLike) -> float:
+    """Get the angle of vectors.
+
+    Returns
+    -------
+    angle : float
+        Angle in radians.
+    """
+    a, b = np.array(a), np.array(b)
+    costheta = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+    theta = np.arccos(costheta)
+    return theta if np.cross(a, b) > 0 else -theta
+
+
+def scale3d(sx: float, sy: float, sz: float) -> npt.NDArray[np.float32]:
+    """Get the 3D scale transfomation matrix.
+
+    Returns
+    -------
+    T : np.NDArray
+        The homogeneous transfomation matrix, shape (4, 4).
+    """
+    return np.array(
+        [
+            [sx, 0, 0, 0],
+            [0, sy, 0, 0],
+            [0, 0, sz, 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=np.float32,
+    )
+
+
+def translate3d(tx: float, ty: float, tz: float) -> npt.NDArray[np.float32]:
+    """Get the 3D translate transfomation matrix.
+
+    Returns
+    -------
+    T : np.NDArray
+        The homogeneous transfomation matrix, shape (4, 4).
+    """
+    return np.array(
+        [
+            [1, 0, 0, tx],
+            [0, 1, 0, ty],
+            [0, 0, 1, tz],
+            [0, 0, 0, 1],
+        ],
+        dtype=np.float32,
+    )
+
+
+def rotate3d(n: npt.ArrayLike, theta: float) -> npt.NDArray[np.float32]:
+    r"""Get the 3D rotation transfomation matrix.
+
+    Rotate v with axis n by an angle theta according to the right hand rule,
+    follow rodrigues' rotaion formula.
+
+    .. math::
+
+        R(\mathbf{n}, \alpha) = \cos{\alpha} \cdot \mathbf{I}
+        + (1-\cos{\alpha}) \cdot \mathbf{n} \cdot \mathbf{n^T}
+        + \sin{\alpha} \cdot \mathbf{N} \\
+
+        N  = \begin{pmatrix}
+        0 & -n_z & n_y \\
+        n_z & 0 & -n_x \\
+        -n_y & n_x & 0
+        \end{pmatrix}
+
+    Parameters
+    ----------
+    n : ArrayLike
+        Rotation axis.
+    theta : float
+        Rotation angle in radians.
+
+    Returns
+    -------
+    T : np.NDArray
+        The homogeneous transfomation matrix, shape (4, 4).
+    """
+
+    n = np.array(n)
+    nx, ny, nz = n[0:3]
+    # pylint: disable-next=invalid-name
+    N = np.array(
+        [
+            [0, -nz, ny],
+            [nz, 0, -nx],
+            [-ny, nx, 0],
+        ],
+        dtype=np.float32,
+    )
+
+    return (
+        np.cos(theta) * np.identity(4)
+        + (1 - np.cos(theta)) * n * n[:, None]
+        + np.sin(theta) * N
+    )
+
+
+def rotate3d_x(theta: float) -> npt.NDArray[np.float32]:
+    """Get the 3D rotation transfomation matrix.
+
+    Rotate 3D vector `v` with `x`-axis by an angle theta according to the right
+    hand rule.
+
+    Parameters
+    ----------
+    theta : float
+        Rotation angle in radians.
+
+    Returns
+    -------
+    T : np.NDArray
+        The homogeneous transfomation matrix, shape (4, 4).
+    """
+
+    return np.array(
+        [
+            [1, 0, 0, 0],
+            [0, np.cos(theta), -np.sin(theta), 0],
+            [0, np.sin(theta), np.cos(theta), 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=np.float32,
+    )
+
+
+def rotate3d_y(theta: float) -> npt.NDArray[np.float32]:
+    """Get the 3D rotation transfomation matrix.
+
+    Rotate 3D vector `v` with `y`-axis by an angle theta according to the right
+    hand rule.
+
+    Parameters
+    ----------
+    theta : float
+        Rotation angle in radians.
+
+    Returns
+    -------
+    T : np.NDArray
+        The homogeneous transfomation matrix, shape (4, 4).
+    """
+    return np.array(
+        [
+            [np.cos(theta), 0, np.sin(theta), 0],
+            [0, 1, 0, 0],
+            [-np.sin(theta), 0, np.cos(theta), 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=np.float32,
+    )
+
+
+def rotate3d_z(theta: float) -> npt.NDArray[np.float32]:
+    """Get the 3D rotation transfomation matrix.
+
+    Rotate 3D vector `v` with `z`-axis by an angle theta according to the right
+    hand rule.
+
+    Parameters
+    ----------
+    theta : float
+        Rotation angle in radians.
+
+    Returns
+    -------
+    T : np.NDArray
+        The homogeneous transfomation matrix, shape (4, 4).
+    """
+    return np.array(
+        [
+            [np.cos(theta), -np.sin(theta), 0, 0],
+            [np.sin(theta), np.cos(theta), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=np.float32,
+    )
+
+
+def to_homogeneous(xyz: npt.ArrayLike, w: float) -> npt.NDArray[np.float32]:
+    """Fill xyz to homogeneous coordinates.
+
+    Parameters
+    ----------
+    xyz : ArrayLike
+        Coordinate of shape (..., 3)
+    w : float
+        w of homogeneous coordinate, 1 for dot, 0 for vector.
+
+    Returns
+    -------
+    xyz4 : npt.NDArray[np.float32]
+        Array of shape (..., 4)
+    """
+    xyz = np.array(xyz)
+    if xyz.ndim == 1:
+        return _to_homogeneous(xyz[None, ...], w)[0]
+
+    shape = xyz.shape[:-1]
+    xyz = xyz.reshape(-1, xyz.shape[-1])
+    xyz4 = _to_homogeneous(xyz, w).reshape(*shape, 4)
+    return xyz4
+
+
+def _to_homogeneous(xyz: npt.NDArray, w: float) -> npt.NDArray[np.float32]:
+    """Fill xyz to homogeneous coordinates.
+
+    Parameters
+    ----------
+    xyz : npt.NDArray
+        Coordinate of shape (N, 3)
+    w : float
+        w of homogeneous coordinate, 1 for dot, 0 for vector.
+
+    Returns
+    -------
+    xyz4 : npt.NDArray[np.float32]
+        Array of shape (N, 4)
+    """
+    if xyz.shape[1] == 4:
+        return xyz
+
+    assert xyz.shape[1] == 3
+    filled = np.full((xyz.shape[0], 1), fill_value=w)
+    xyz4 = np.concatenate([xyz, filled], axis=1)
+    return xyz4
+
+
+def model_view_transformation(
+    position: Vec3f, look_at: Vec3f, up: Vec3f
+) -> npt.NDArray[np.float32]:
+    r"""Play model/view transformation.
+
+    Parameters
+    ----------
+    position: Tuple[float, float, float]
+        Camera position \vec{e}.
+    look_at: Tuple[float, float, float]
+        Camera look-at \vec{g}.
+    up: Tuple[float, float, float]
+        Camera up direction \vec{t}.
+    """
+
+    e = np.array(position, dtype=np.float32)
+    g = np.array(look_at, dtype=np.float32) / np.linalg.norm(look_at)
+    t = np.array(up, dtype=np.float32) / np.linalg.norm(up)
+
+    t_view = translate3d(*(-1 * e))
+    r_view = np.array(
+        [
+            [*np.cross(g, t), 0],
+            [*t, 0],
+            [*(-1 * g), 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=np.float32,
+    )
+    return np.dot(r_view, t_view)
+
+
+def orthographic_projection_simple() -> npt.NDArray[np.float32]:
+    """Simple orthographic projection by drop z-axis"""
+    return np.array(
+        [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],  # drop z-axis
+            [0, 0, 0, 0],  # drop w
+        ],
+        dtype=np.float32,
+    )
