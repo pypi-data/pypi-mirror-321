d30d3c10a8c93bbaae5d4ca6d99cd0a4e7d1527b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:04:55 +0000

    Add package quadraturerules/0.3.3/quadraturerules-0.3.3.tar.gz

---
+++ packages/quadraturerules/quadraturerules-0.3.3.tar.gz/quadraturerules-0.3.3/quadraturerules/rules/__init__.py
@@ -0,0 +1,5 @@
+"""Definitions of quadrature rules."""
+
+from quadraturerules.rules.gauss_legendre import gauss_legendre
+from quadraturerules.rules.gauss_lobatto_legendre import gauss_lobatto_legendre
+from quadraturerules.rules.xiao_gimbutas import xiao_gimbutas
