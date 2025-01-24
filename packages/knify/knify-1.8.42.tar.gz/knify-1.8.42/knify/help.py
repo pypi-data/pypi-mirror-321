9ac71ffc375afd423f5847867616f05bf5dc1988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:04:00 +0000

    Add package knify/1.8.42/knify-1.8.42.tar.gz

---
+++ packages/knify/knify-1.8.42.tar.gz/knify-1.8.42/knify/help.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+
+def get_pip_name():
+    return 'knify'
+
+
+def get_version():
+    return '1.8.42'
+
+
+def print_version():
+    print('Knify %s\r\nDevelopment tools for python.' % get_version())
