f091ae912fedb8f754f87e34d9193f3916a3e26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:03:04 +0000

    Add package speech-engine/0.1.1/speech_engine-0.1.1-py3-none-any.whl

---
+++ packages/speech-engine/speech_engine-0.1.1-py3-none-any.whl/speech_engine/tts_google.py
@@ -0,0 +1,111 @@
+from gtts import gTTS
+from playsound import playsound
+import os
+from .exceptions import FileExtensionError
+from time import sleep
+
+class TTS_Google:
+    """
+    The TTS_Google class provides functionality to synthesize text into speech using the gTTS library.
+    """
+
+    def __init__(self):
+        self._lang = 'en'
+        self._tld = ''
+        self._slow = False
+
+    def get_language(self) -> str:
+        """
+        Returns the current language.
+
+        Returns:
+            str: The current language.
+        """
+        return self._lang
+
+    def set_language(self, lang : str):
+        """
+        Set the language for text synthesis.
+
+        Args:
+            lang (str): The language code.
+
+        """
+        self._lang = lang
+
+    def get_tld(self) -> str:
+        """
+        Returns the current top-level domain (TLD).
+
+        Returns:
+            str: The current top-level domain (TLD).
+        """
+        return self._tld
+
+    def set_tld(self, tld : str):
+        """
+        Set the top-level domain (TLD) for regional language accents.
+
+        Args:
+            tld (str): The top-level domain (TLD) code.
+
+        """
+        self._tld = tld
+
+    def get_slow(self) -> bool:
+        """
+        Returns the current speech speed setting.
+
+        Returns:
+            bool: The current speech speed setting.
+        """
+        return self._slow
+
+    def set_slow(self, slow : bool):
+        """
+        Set the speech speed.
+
+        Args:
+            slow (bool): True to enable slow speech, False otherwise.
+
+        """
+        self._slow = slow
+        
+    def speak(self, text : str):
+        """
+        Synthesizes the given text into speech and plays it.
+
+        Args:
+            text (str): The text to be synthesized into speech.
+        """
+        if self._tld:
+            gtts = gTTS(text=text, tld=self._tld, lang=self._lang, slow=self._slow)
+        else:
+            gtts = gTTS(text=text, lang=self._lang, slow=self._slow)
+
+        gtts.save("speech.mp3")
+        sleep(0.5)
+        playsound("speech.mp3")
+        os.remove("speech.mp3")
+
+    def save(self, text : str, filename : str = "output.mp3"):
+        """
+        Synthesizes the given text into speech and saves it as an audio file.
+
+        Args:
+            text (str): The text to be synthesized into speech.
+            filename (str): The filename to save the audio file (should have a .mp3 extension).
+
+        Raises:
+            FileExtensionError: If the provided filename doesn't have a .mp3 extension.
+
+        """
+        if not filename.endswith('.mp3'):
+            raise FileExtensionError()
+
+        if self._tld:
+            gtts = gTTS(text=text, tld=self._tld, lang=self._lang, slow=self._slow)
+        else:
+            gtts = gTTS(text=text, lang=self._lang, slow=self._slow)
+
+        gtts.save(filename)
---
+++ packages/speech-engine/speech_engine-0.1.1-py3-none-any.whl/speech_engine/tts_witai.py
@@ -0,0 +1,185 @@
+from .exceptions import InvalidTokenError, FileExtensionError
+import winsound
+import requests
+import os
+import json
+from time import sleep
+
+class TTS_Witai:
+    """
+    The TTS_Witai class provides functionality to synthesize text into speech using the wit.ai
+
+    Args:
+        authToken (str): The Wit.ai auth token.
+    """
+
+    def __init__(self, authToken : str):
+        if not authToken:
+            raise ValueError("Auth Token cannot be empty")
+        
+        self._auth_token = authToken
+        self.is_valid_token = self._validate_token()
+
+        if not self.is_valid_token:
+            raise InvalidTokenError()
+        
+        self._voice = 'Colin'
+        self._speed = None
+        self._pitch = None
+
+    def get_voice(self) -> str:
+        """
+        Returns the current voice.
+
+        Returns:
+            str: The current voice.
+        """
+        return self._voice
+
+    def set_voice(self, voice : str):
+        """
+        Sets the voice to be used for synthesis.
+
+        Args:
+            voice (str): The voice to be set.
+        """
+        self._voice = voice 
+
+    def get_speed(self) -> int:
+        """
+        Returns the current speed.
+
+        Returns:
+            int: The current speed.
+        """
+        return self._speed
+
+    def set_speed(self, speed : int):
+        """
+        Sets the speed of speech synthesis.
+
+        Args:
+            speed (int): The speed to be set.
+        """
+        self._speed = speed
+
+    def get_pitch(self) -> int:
+        """
+        Returns the current pitch.
+
+        Returns:
+            int: The current pitch.
+        """
+        return self._pitch
+
+    def set_pitch(self, pitch : int):
+        """
+        Sets the pitch of speech synthesis.
+
+        Args:
+            pitch (int): The pitch to be set.
+        """
+        self._pitch = pitch
+
+    def _validate_token(self):
+        """
+        Validate the Wit.ai auth token.
+
+        Returns:
+            bool: True if the token is valid, False otherwise.
+        """
+        headers = {
+            'Authorization': f'Bearer {self._auth_token}',
+        }
+        response = requests.get('https://api.wit.ai/voices?v=20220622', headers=headers)
+        return response.status_code == 200
+
+    def speak(self, text : str):
+        """
+        Synthesizes the given text into speech and plays it.
+
+        Args:
+            text (str): The text to be synthesized into speech.
+        """
+        
+        data = { 'q': text, 'voice': self._voice }
+        if self._speed:
+            data['speed'] = self._speed
+        
+        if self._pitch:
+            data['speed'] = self._pitch
+        
+        audio= requests.post(
+        'https://api.wit.ai/synthesize',
+        params={
+            'v': '20220622',
+        },
+        headers={
+            'Authorization': f'Bearer {self._auth_token}',
+        },
+        json=data,
+        )
+
+        with open("speech.mp3", "wb") as f:
+            f.write(audio.content)
+        f.close()
+        sleep(0.3)
+        winsound.PlaySound("speech.mp3", winsound.SND_FILENAME)
+        os.remove("speech.mp3")
+
+    def save(self, text : str, filename : str = "outupt.mp3"):
+        """
+        Synthesizes the given text into speech and saves it as an audio file.
+
+        Args:
+            text (str): The text to be synthesized into speech.
+            filename (str): The filename to save the audio file (should have a .mp3 extension).
+
+        Raises:
+            FileExtensionError: If the provided filename doesn't have a .mp3 extension.
+
+        """
+        if filename.split(".")[-1] != "mp3": raise FileExtensionError
+
+        data = { 'q': text, 'voice': self._voice }
+        if self._speed:
+            data['speed'] = self._speed
+        
+        if self._pitch:
+            data['speed'] = self._pitch
+        
+        audio= requests.post(
+        'https://api.wit.ai/synthesize',
+        params={
+            'v': '20220622',
+        },
+        headers={
+            'Authorization': f'Bearer {self._auth_token}',
+        },
+        json=data,
+        )
+
+        with open(filename, "wb") as f:
+            f.write(audio.content)
+        f.close()
+
+
+    def get_voices(self):
+        """
+        Fetches available voices from the Wit.ai API.
+
+        Returns:
+            list: A list of available voices.
+        """ 
+        headers = {
+            'Authorization': f'Bearer {self._auth_token}',
+        }
+        response = requests.get('https://api.wit.ai/voices?v=20220622', headers=headers)
+        resp = json.loads(response.content)
+
+        voices = []
+        for locale_voices in resp.values():
+            for voice in locale_voices:
+                voices.append(voice['name'].replace("wit$", ""))
+
+        return voices
