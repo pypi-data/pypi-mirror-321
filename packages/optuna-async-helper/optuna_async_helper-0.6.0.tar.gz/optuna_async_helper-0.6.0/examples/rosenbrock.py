55dcce726feac24536b92dd3d650f7c7705686d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:59:35 +0000

    Add package optuna-async-helper/0.6.0/optuna_async_helper-0.6.0.tar.gz

---
+++ packages/optuna-async-helper/optuna_async_helper-0.6.0.tar.gz/optuna_async_helper-0.6.0/examples/rosenbrock.py
@@ -0,0 +1,41 @@
+import pathlib
+import tempfile
+
+from optuna_async_helper import (
+    SearchSpace,
+    SearchSpec,
+    create_journal_storage,
+    create_study,
+    optimize,
+)
+
+
+def rosenbrock(x: float, y: float, z: float) -> float:
+    return (z - x) ** 2 + 100 * (y - x**2) ** 2
+
+
+if __name__ == "__main__":
+    search_space: SearchSpace = [
+        SearchSpec(var_name="x", domain_type="float", low=-5, high=5),
+        SearchSpec(var_name="y", domain_type="float", low=-5, high=5),
+    ]
+    z = -2.0
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        storage_path = pathlib.Path(tempdir, "example.db")
+        storage = create_journal_storage(str(storage_path))
+        study = create_study(
+            study_name="rosenbrock",
+            storage=storage,
+        )
+        study = optimize(
+            study,
+            objective_func=rosenbrock,
+            search_space=search_space,
+            n_trials=50,
+            batch_size=8,
+            z=z,
+        )
+
+        print(study.best_value)
+        print(study.best_params)
