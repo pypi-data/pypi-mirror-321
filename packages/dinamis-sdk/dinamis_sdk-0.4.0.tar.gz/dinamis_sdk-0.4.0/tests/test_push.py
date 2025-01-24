1ec052d9e3551939684557e0b6ba7c19c83fef1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:02:18 +0000

    Add package dinamis-sdk/0.4.0/dinamis_sdk-0.4.0.tar.gz

---
+++ packages/dinamis-sdk/dinamis_sdk-0.4.0.tar.gz/dinamis_sdk-0.4.0/tests/test_push.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import dinamis_sdk
+
+local_filename = "/tmp/toto.txt"
+
+with open(local_filename, "w") as f:
+    f.write("hello world")
+
+pushed = dinamis_sdk.push(
+    local_filename=local_filename,
+    target_url="https://s3-data.meso.umontpellier.fr/sm1-gdc-tests/titi.txt",
+)
+print("Done")
