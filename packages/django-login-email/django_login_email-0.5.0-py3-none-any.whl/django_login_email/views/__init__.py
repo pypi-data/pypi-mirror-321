cf2f7a6cba01855162576bb084362786c1de7555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:59:29 +0000

    Add package django-login-email/0.5.0/django_login_email-0.5.0-py3-none-any.whl

---
+++ packages/django-login-email/django_login_email-0.5.0-py3-none-any.whl/django_login_email/views/__init__.py
@@ -0,0 +1,54 @@
+# Create your views here.
+import logging
+from typing import Any
+
+from django.http import Http404, HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.views.generic import TemplateView
+
+from django_login_email import email, errors
+
+from . import limit
+from .login import EmailLoginView  # noqa
+from .mixin import MailRecordModelMixin
+
+logger = logging.getLogger(__name__)
+
+
+class EmailVerifyView(TemplateView, email.EmailVerifyMixin, MailRecordModelMixin):
+  """verify token in url"""
+
+  tl = limit.LoginTimeLimit()
+
+  def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+    token = request.GET.get("token", None)
+    if token is None:
+      raise Http404("Invalid Request")
+    try:
+      self.verify_login_mail(request=request, token_v=token)
+    except errors.ValidatedError as e:
+      return render(self.request, "login_email/error.html", {"error": e})
+    except Exception as e:
+      logger.error(e)
+      raise Http404("Invalid Request")
+    return redirect(self.get_success_url())
+
+  def get_success_url(self):
+    return reverse("login_email:home")
+
+
+class EmailLogoutView(TemplateView, email.EmailLogoutMixin):
+  def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+    self.logout(request=request)
+    return redirect("login_email:login")
+
+
+class HomeView(TemplateView):
+  template_name = "login_email/home.html"
+
+  def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+    if not self.request.user.is_anonymous and self.request.user.is_authenticated:
+      return super().get(request, *args, **kwargs)
+    else:
+      return redirect("login_email:login")
