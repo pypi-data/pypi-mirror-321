08689d75a6a3b376b3df4be7019c76072204136f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 20:04:39 +0000

    Add package adalflow/0.2.7/adalflow-0.2.7-py3-none-any.whl

---
+++ packages/adalflow/adalflow-0.2.7-py3-none-any.whl/adalflow/core/retriever.py
@@ -0,0 +1,163 @@
+r"""The base class for all retrievers who in particular retrieve documents from a given database."""
+
+from typing import List, Optional, Generic, Any, Callable, TYPE_CHECKING, Union
+import logging
+
+from adalflow.core.types import (
+    RetrieverQueriesType,
+    RetrieverQueryType,
+    RetrieverDocumentType,
+    RetrieverDocumentsType,
+    RetrieverOutputType,
+)
+from adalflow.optim.grad_component import GradComponent
+
+if TYPE_CHECKING:
+    from adalflow.core.generator import Generator
+from adalflow.optim.parameter import Parameter
+from adalflow.optim.types import ParameterType
+
+log = logging.getLogger(__name__)
+
+
+# TODO: tracing retriever in the diagnose files using callback manager
+class Retriever(GradComponent, Generic[RetrieverDocumentType, RetrieverQueryType]):
+    __doc__ = r"""The base class for all retrievers.
+
+    Retriever will manage its own index and retrieve in format of RetrieverOutput
+
+    Args:
+        indexed (bool, optional): whether the retriever has an index. Defaults to False.
+        index_keys (List[str], optional): attributes that define the index that can be used to restore the retriever. Defaults to [].
+
+    The key method :meth:`build_index_from_documents` is the method to build the index from the documents.
+    ``documents`` is a sequence of any type of document. With ``document_map_func``, you can map the document
+    of Any type to the specific type ``RetrieverDocumentType`` that the retriever expects.
+
+    note:
+    To get the state of the retriever, leverage the :methd: "from_dict" and "to_dict" methods of the base class Component.
+
+
+    """
+
+    indexed: bool = False
+    index_keys: List[str] = []  # attributes that define the index
+    name: str = "Retriever"
+    top_k: int
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+    def reset_index(self):
+        r"""Initialize/reset any attributes/states for the index."""
+        raise NotImplementedError("reset_index is not implemented")
+
+    def build_index_from_documents(
+        self,
+        documents: RetrieverDocumentsType,
+        document_map_func: Optional[Callable[[Any], RetrieverDocumentType]] = None,
+        **kwargs,
+    ):
+        r"""Built index from the [document_map_func(doc) for doc in documents]."""
+        raise NotImplementedError(
+            "build_index_from_documents and input_field_map_func is not implemented"
+        )
+
+    def save_to_file(self, path: str):
+        r"""Save the state, including the index to a file.
+
+        Optional for subclass to implement a default persistence method.
+        Subclass can leverge component's `to_dict` method to get the states and choose to save them in any file format.
+        """
+        pass
+
+    @classmethod
+    def load_from_file(cls, path: str):
+        r"""Load the state, including index from a file to restore the retriever.
+
+        Subclass can leverge component's `from_dict` method to restore the states from the file.
+        """
+        pass
+
+    def call(
+        self,
+        input: RetrieverQueriesType,
+        top_k: Optional[int] = None,
+        id: str = None,  # for tracing, diagnosing, and training
+        **kwargs,
+    ) -> RetrieverOutputType:
+        raise NotImplementedError("retrieve is not implemented")
+
+    async def acall(
+        self,
+        input: RetrieverQueriesType,
+        top_k: Optional[int] = None,
+        id: str = None,  # for tracing, diagnosing, and training
+        **kwargs,
+    ) -> RetrieverOutputType:
+        raise NotImplementedError("Async retrieve is not implemented")
+
+    # TODO: adapt the generator to auto-track the prompt_kwargs as parameters
+    def forward(
+        self,
+        input: Union[RetrieverQueriesType, Parameter],
+        top_k: Optional[
+            int
+        ] = None,  # TODO: top_k can be trained in the future if its formulated as a parameter
+        id: str = None,  # for tracing, diagnosing, and training
+        **kwargs,
+    ) -> Parameter:
+        r"""Customized forward on top of the GradComponent forward method.
+
+        To track the input as Parameters and set the parameter type as RETRIEVER_OUTPUT in the response.
+        """
+        # convert input to parameter if it is not
+        if not isinstance(input, Parameter):
+            input = Parameter(
+                data=input,
+                name="input",
+                requires_opt=True,
+                param_type=ParameterType.INPUT,
+            )
+        # trace the top_k in the DAG graph
+        top_k = Parameter(
+            data=top_k or self.top_k,
+            name="top_k",
+            requires_opt=True,
+            param_type=ParameterType.HYPERPARAM,
+        )
+        if input is None:
+            raise ValueError("Input cannot be empty")
+        response = super().forward(input, top_k=top_k, **kwargs)
+        response.param_type = (
+            ParameterType.RETRIEVER_OUTPUT
+        )  # be more specific about the type
+        return response
+
+    def backward(
+        self,
+        response: Parameter,
+        id: Optional[str] = None,
+        backward_engine: Optional["Generator"] = None,
+    ):
+        r"""Backward the response to pass the score to predecessors.
+        Function as a relay component"""
+        log.info(f"Retriever backward: {response.name}")
+        children_params = response.predecessors
+
+        # is_chain = True
+        if response.get_gradient_and_context_text().strip() == "":
+            log.info(f"Generator: Backward: No gradient found for {response}.")
+
+        for pred in children_params:
+            pred.set_score(response._score)
+            from adalflow.utils.logger import printc
+
+            printc(
+                f"Retriever: Backward: {pred.name} set_score: {response._score}, {response.name}",
+                "blue",
+            )
+            if pred.param_type == ParameterType.DEMOS:
+                pred.add_score_to_trace(
+                    trace_id=id, score=response._score, is_teacher=self.teacher_mode
+                )
---
+++ packages/adalflow/adalflow-0.2.7-py3-none-any.whl/adalflow/optim/grad_component.py
@@ -0,0 +1,161 @@
+"""Base class for Autograd Components that can be called and backpropagated through."""
+
+from typing import TYPE_CHECKING
+from collections import OrderedDict
+import logging
+
+if TYPE_CHECKING:
+    from adalflow.core.generator import BackwardEngine
+    from adalflow.optim.parameter import Parameter
+
+from adalflow.optim.types import ParameterType
+
+from adalflow.core.component import Component
+from adalflow.optim.function import BackwardContext
+
+__all__ = ["GradComponent"]
+log = logging.getLogger(__name__)
+
+
+class GradComponent(Component):
+    __doc__ = """A base class to define interfaces for an auto-grad component/operator.
+
+    Compared with `Component`, `GradComponent` defines three important interfaces:
+    - `forward`: the forward pass of the function, returns a `Parameter` object that can be traced and backpropagated.
+    - `backward`: the backward pass of the function, updates the gradients/prediction score backpropagated from a "loss" parameter.
+    - `set_backward_engine`: set the backward engine(a form of generator) to the component, which is used to backpropagate the gradients using LLM.
+
+    The __call__ method will check if the component is in training mode,
+    and call the `forward` method to return a `Parameter` object if it is in training mode,
+    otherwise, it will call the `call` method to return the output such as "GeneratorOutput", "RetrieverOutput", etc.
+    """
+    backward_engine: "BackwardEngine"
+    _component_type = "grad"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        super().__setattr__("backward_engine", None)
+
+    def __call__(self, *args, **kwargs):
+        if self.training:
+            return self.forward(*args, **kwargs)
+        else:
+            return self.call(*args, **kwargs)
+
+    def set_backward_engine(self, backward_engine: "BackwardEngine", *args, **kwargs):
+        raise NotImplementedError("set_backward_engine method is not implemented")
+
+    def call(self, *args, **kwargs):
+        raise NotImplementedError("call method is not implemented")
+
+    async def acall(self, *args, **kwargs):
+        r"""Implement this for your async call."""
+        raise NotImplementedError("acall method is not implemented")
+
+    def forward(self, *args, **kwargs) -> "Parameter":
+        r"""Default forward method for training:
+        1. for all args and kwargs, if it is a `Parameter` object, it will be tracked as `Predecessor`.
+        2. Trace input_args and full_response in the parameter object.
+        3. Return the parameter object.
+        """
+
+        from adalflow.optim.parameter import Parameter
+
+        log.debug(
+            f"Forwarding through {self.name} with args: {args} and kwargs: {kwargs}"
+        )
+
+        # if "id" not in kwargs:
+        #     raise ValueError(
+        #         "id must be provided in the kwargs of a GradComponent for tracing."
+        #     )
+
+        # 1. get all predecessors from all args and kwargs
+        input_args = OrderedDict()
+
+        # Add positional args to the ordered dict
+        for idx, arg in enumerate(args):
+            input_args[f"arg_{idx}"] = arg
+
+        # Add keyword args to the ordered dict, preserving order
+        predecessors = []
+        for v in input_args.values():
+            if isinstance(v, Parameter):
+                predecessors.append(v)
+                if v.param_type == ParameterType.INPUT:
+                    v.data_id = kwargs.get("id", None)
+        for v in kwargs.values():
+            if isinstance(v, Parameter):
+                predecessors.append(v)
+                if v.param_type == ParameterType.INPUT:
+                    v.data_id = kwargs.get("id", None)
+
+        # 2. unwrap the parameter object to take only the data, successor_map_fn: lambda x: x.data in default
+        # unwrap args
+        unwrapped_args = []
+        for k, v in input_args.items():
+            if isinstance(v, Parameter):
+                unwrapped_args.append(v.map_to_successor(self))
+            else:
+                unwrapped_args.append(v)
+
+        unwrapped_kwargs = {}
+        # unwrap kwargs
+        for k, v in kwargs.items():
+            if isinstance(v, Parameter):
+                unwrapped_kwargs[k] = v.map_to_successor(self)
+            else:
+                unwrapped_kwargs[k] = v
+
+        # 3. call the function with unwrapped args and kwargs
+        unwrapped_args = tuple(unwrapped_args)
+
+        log.debug(f"Unwrapped args: {unwrapped_args}")
+        log.debug(f"Unwrapped kwargs: {unwrapped_kwargs}")
+
+        call_response = self.call(*unwrapped_args, **unwrapped_kwargs)
+
+        # 4. Create a Parameter object to trace the forward pass
+        input_args.update(kwargs)
+        response = Parameter(
+            data=call_response,
+            name=self.name + "_output",
+            role_desc=self.name + " response",
+            param_type=ParameterType.OUTPUT,
+        )
+        response.set_predecessors(predecessors)
+        response.trace_forward_pass(input_args=input_args, full_response=call_response)
+        response.set_grad_fn(
+            BackwardContext(
+                backward_fn=self.backward,
+                response=response,
+                id=kwargs.get("id", None),
+            )
+        )
+        return response
+
+    def backward(self, *, response: "Parameter", id: str = None, **kwargs):
+        """Backward pass of the function. In default, it will pass all the scores to the predecessors.
+
+        Note: backward is mainly used internally and better to only allow kwargs as the input.
+
+        Subclass should implement this method if you need additional backward logic.
+        """
+        log.info(f"GradComponent backward: {response.name}")
+        children_params = response.predecessors
+
+        if response.get_gradient_and_context_text().strip() == "":
+            log.info(f"Generator: Backward: No gradient found for {response}.")
+
+        for pred in children_params:
+            pred.set_score(response._score)
+            from adalflow.utils.logger import printc
+
+            printc(
+                f"Retriever: Backward: {pred.name} set_score: {response._score}, {response.name}",
+                "blue",
+            )
+            if pred.param_type == ParameterType.DEMOS:
+                pred.add_score_to_trace(
+                    trace_id=id, score=response._score, is_teacher=self.teacher_mode
+                )
