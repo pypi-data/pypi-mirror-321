f9f70d80f232d9e8cde3f480784d13fe5c33050d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 19:04:59 +0000

    Add package divi/0.0.1.dev8/divi-0.0.1.dev8.tar.gz

---
+++ packages/divi/divi-0.0.1.dev8.tar.gz/divi-0.0.1.dev8/divi/proto/core/v1/core_pb2.pyi
@@ -0,0 +1,6 @@
+from divi.proto.core.v1 import health_check_request_pb2 as _health_check_request_pb2
+from divi.proto.core.v1 import health_check_response_pb2 as _health_check_response_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
