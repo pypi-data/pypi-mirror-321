8a2ab083e5b4db600e1a4242ddf9912f7ed5aba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:00:40 +0000

    Add package ossindex_api_client/2025.15.0/ossindex_api_client-2025.15.0-py3-none-any.whl

---
+++ packages/ossindex_api_client/ossindex_api_client-2025.15.0-py3-none-any.whl/ossindex_api_client/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from ossindex_api_client.api.component_vulnerability_reports_api import ComponentVulnerabilityReportsApi
+from ossindex_api_client.api.version_api import VersionApi
+
