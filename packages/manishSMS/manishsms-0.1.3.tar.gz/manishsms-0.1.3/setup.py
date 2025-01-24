5abd84811c3b28b44ccbc67371e82eff38364660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:28 +0000

    Add package manishSMS/0.1.3/manishsms-0.1.3.tar.gz

---
+++ packages/manishSMS/manishsms-0.1.3.tar.gz/manishsms-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='manishSMS',
+    version='0.1.3',
+    author='Manish Prasad',
+    author_email='m4manishp4prasad@gmail.com.com',
+    description='A School Management System',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Manish-Let-It-Be/manishSMS',  
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'startsms=manishSMS.sms:main',  
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'tabulate',
+        'openpyxl',
+    ],
+)
