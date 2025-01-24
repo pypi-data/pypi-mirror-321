2f4e2abf59c710a78308f100b784be8d800fd292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 23:04:46 +0000

    Add package blnk-chat/0.2.0/blnk_chat-0.2.0.tar.gz

---
+++ packages/blnk-chat/blnk_chat-0.2.0.tar.gz/blnk_chat-0.2.0/src/blnk_chat/apis/echo_api.py
@@ -0,0 +1,11 @@
+from .base_api import BaseAPI
+
+class EchoAPI(BaseAPI):
+    def __init__(self):
+        super().__init__()  # Initialize BaseAPI
+        
+    async def send_message(self, message):
+        return f"Echo: {message}"
+    
+    def get_name(self):
+        return "echo"
