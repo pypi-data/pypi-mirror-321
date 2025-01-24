e3c627bd6c93b5d92e5d79856c63c0eaa96951e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 23:04:54 +0000

    Add package reflex-agent/0.1.0a30/reflex_agent-0.1.0a30.tar.gz

---
+++ packages/reflex-agent/reflex_agent-0.1.0a30.tar.gz/reflex_agent-0.1.0a30/flexai/capabilities/truncate_messages.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from flexai.capability import Capability
+from flexai.message import Message
+
+
+@dataclass
+class TruncateMessages(Capability):
+    """Truncate the input messages to the LLM to a maximum number."""
+
+    # The maximum number of messages to keep.
+    max_messages: int
+
+    async def modify_messages(self, messages: list[Message]) -> list[Message]:
+        return messages[-self.max_messages :]
