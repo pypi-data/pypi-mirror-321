686f925f0b9b7a4cb50a4b124bdc9326fc2790da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:04:43 +0000

    Add package flask-wiz/1.9.6/flask_wiz-1.9.6.tar.gz

---
+++ packages/flask-wiz/flask_wiz-1.9.6.tar.gz/flask_wiz-1.9.6/setup.py
@@ -0,0 +1,31 @@
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+dependencies = [
+    'flask',
+    'click',
+    'Flask',  
+    # 'sqlite3',
+    # 'mysql-connector-python',
+    # 'psycopg2',
+    # 'pymongo',
+]
+
+setup(
+    name='flask-wiz',
+    version='1.9.6',
+    author='Krish Soni',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=dependencies,  
+    entry_points={
+        'console_scripts': [
+            'flask-wiz=flask_wiz.cli:cli',
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
