00c97307d4d577300a959f9652ce26b0520dff58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:01:37 +0000

    Add package fpsdokgjihdsnvmophirdheegnm09445384534ujklfng89403jit3n/0.0.4/fpsdokgjihdsnvmophirdheegnm09445384534ujklfng89403jit3n-0.0.4.tar.gz

---
+++ packages/fpsdokgjihdsnvmophirdheegnm09445384534ujklfng89403jit3n/fpsdokgjihdsnvmophirdheegnm09445384534ujklfng89403jit3n-0.0.4.tar.gz/fpsdokgjihdsnvmophirdheegnm09445384534ujklfng89403jit3n-0.0.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+
+setup(name='custom-tests',
+      version='0.0.1',
+      description='OpenMetadata Custom Tests',
+      packages=find_packages(),
+     )
