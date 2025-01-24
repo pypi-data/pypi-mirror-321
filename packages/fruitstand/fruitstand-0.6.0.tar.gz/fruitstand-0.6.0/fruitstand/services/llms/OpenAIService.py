a80d08e4041f8d4e49c43fc2df26adfc57f3c64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:05:07 +0000

    Add package fruitstand/0.6.0/fruitstand-0.6.0.tar.gz

---
+++ packages/fruitstand/fruitstand-0.6.0.tar.gz/fruitstand-0.6.0/fruitstand/services/llms/OpenAIService.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+from fruitstand.services.llms.LLMService import LLMService
+
+class OpenAIService(LLMService):
+    def __init__(self, api_key: str) -> None:
+        self.service = OpenAI(api_key=api_key)
+    
+    def query(self, model: str, text: str) -> str:
+        chat_completion = self.service.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": text
+                }
+            ],
+            model=model,
+        )
+        
+        return chat_completion.choices[0].message.content
