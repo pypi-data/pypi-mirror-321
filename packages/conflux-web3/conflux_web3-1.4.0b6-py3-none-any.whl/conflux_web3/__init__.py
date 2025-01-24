1510a90adf6844182a2ab0a6bc213b62b65ab9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 23:05:26 +0000

    Add package conflux-web3/1.4.0b6/conflux_web3-1.4.0b6-py3-none-any.whl

---
+++ packages/conflux-web3/conflux_web3-1.4.0b6-py3-none-any.whl/conflux_web3/__init__.py
@@ -0,0 +1,22 @@
+# should be placed at the most front
+import _web3_hook
+
+from importlib.metadata import version
+
+from conflux_web3.main import Web3
+from conflux_web3.dev import (
+    get_local_web3,
+    get_mainnet_web3,
+    get_testnet_web3
+)
+HTTPProvider = Web3.HTTPProvider
+
+__version__ = version("conflux_web3")
+
+__all__ = [
+    "Web3",
+    "HTTPProvider",
+    "get_local_web3",
+    "get_mainnet_web3",
+    "get_testnet_web3",
+]
