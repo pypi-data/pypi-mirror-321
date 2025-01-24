b8f82a7aae6042bcf4604c07aa84b85a2e1ca517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:50 +0000

    Add package dagster-chroma/0.0.1/dagster_chroma-0.0.1.tar.gz

---
+++ packages/dagster-chroma/dagster_chroma-0.0.1.tar.gz/dagster_chroma-0.0.1/dagster_chroma/__init__.py
@@ -0,0 +1,8 @@
+from dagster_chroma.resource import (
+    ChromaResource as ChromaResource,
+)
+
+from dagster_chroma.config import (
+    LocalConfig as LocalConfig,
+    HttpConfig as HttpConfig,
+)
