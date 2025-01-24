56c7829baf38ab35878cb8c174a5f4204bc28fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:59:55 +0000

    Add package my-src-project/0.1.3/my_src_project-0.1.3-py3-none-any.whl

---
+++ packages/my-src-project/my_src_project-0.1.3-py3-none-any.whl/my_project/module2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+#
+# Copyright (C) 2025 - 2025 Guanhao Sun, Inc. All Rights Reserved 
+#
+# @Time    : 2025/1/15 9:52
+# @Author  : Guanhao Sun
+# @File    : module2.py
+# @IDE     : PyCharm
+
+def greet(name):
+    return f"Hello, {name}!"
