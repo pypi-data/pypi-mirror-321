66892ec7995daf14ae535962cf8a613e4348a912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:17 +0000

    Add package MeUtils/2025.1.16.17.15.52/MeUtils-2025.1.16.17.15.52-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2025.1.16.17.15.52-py3-none-any.whl/meutils/apis/vidu/x.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : x
+# @Time         : 2024/9/25 10:42
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+from meutils.hash_utils import md5
+
+# md5("8daf5c4765c493e2")
