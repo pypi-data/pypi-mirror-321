93117c8fbde3cf924baf11d21dd2fb97be9775d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 22:05:02 +0000

    Add package bsmplot/0.2.8/bsmplot-0.2.8-py3-none-any.whl

---
+++ packages/bsmplot/bsmplot-0.2.8-py3-none-any.whl/bsmplot/version.py
@@ -0,0 +1,9 @@
+PROJECT_NAME     = 'bsmplot'
+
+VER_MAJOR        = 0
+VER_MINOR        = 2
+VER_RELEASE      = 8
+
+VER_FLAGS        = ""
+
+__version__ = f'{VER_MAJOR}.{VER_MINOR}.{VER_RELEASE}{VER_FLAGS}'
