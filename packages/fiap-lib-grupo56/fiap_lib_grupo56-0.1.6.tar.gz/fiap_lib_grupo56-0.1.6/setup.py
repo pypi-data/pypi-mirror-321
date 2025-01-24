e672ade04a1ff0d6dd37865bdf5114ae570fc8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:08:09 +0000

    Add package fiap-lib-grupo56/0.1.6/fiap_lib_grupo56-0.1.6.tar.gz

---
+++ packages/fiap-lib-grupo56/fiap_lib_grupo56-0.1.6.tar.gz/fiap_lib_grupo56-0.1.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='fiap_lib_grupo56',
+   version='0.1.6',
+   packages=find_packages(),
+   description='Biblioteca para o trabalho Tech Challenge do grupo 56 da 4MLET',
+   author='Fabio Vargas Matos',
+   author_email='fabiomatos@baneses.com.br',
+   url='https://github.com/fabiomatos71/Pos',
+   install_requires=[],
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.12.7',
+)
