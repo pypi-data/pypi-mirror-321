0b92806e4de940731114b315bb3423ffdfb20079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:53:00 +0000

    Add package LAGEF-UFF-Shoreline/0.0.1/LAGEF-UFF-Shoreline-0.0.1.tar.gz

---
+++ packages/LAGEF-UFF-Shoreline/LAGEF-UFF-Shoreline-0.0.1.tar.gz/LAGEF-UFF-Shoreline-0.0.1/README.md
@@ -0,0 +1,3 @@
+## Repositório não oficial para shoreline Lagef - uff - Pablo Simões
+
+# Colocar aqui as bilbiotecas necessárias ou outras coisas, testes por enquanto. 
---
+++ packages/LAGEF-UFF-Shoreline/LAGEF-UFF-Shoreline-0.0.1.tar.gz/LAGEF-UFF-Shoreline-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.MD','r') as arq:
+    readme = arq.read()
+
+setup(name='LAGEF-UFF-Shoreline',
+    version='0.0.1',
+    license='MIT License',
+    author='Pablo Simoes',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='pablosergio.simoes@gmail.com',
+    keywords='lagef uff',
+    description=u'Wrapper não oficial do Panda Video',
+    packages=['LAGEF_shoreline'],
+    install_requires=['requests','pandas','geemap','numpy','random','earthengine-api'],)
+
+
+
+
+
+
+
