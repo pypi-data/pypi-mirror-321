8a828e30effcd64674fd4d6d24d7d31487abf8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 20:05:10 +0000

    Add package copycode/0.2.2/copycode-0.2.2.tar.gz

---
+++ packages/copycode/copycode-0.2.2.tar.gz/copycode-0.2.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='copycode',
+    version='0.2.2',
+    description='Copy all your code to your clipboard or a file with one command.',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author='MartinVeltman',
+    author_email='martin.veltman@winvent.nl',
+    license='MIT',
+    url='https://github.com/MartinVeltman/CopyCode',
+    project_urls={
+        "Source Code": "https://github.com/MartinVeltman/CopyCode",
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pyperclip',
+        'pyyaml'
+    ],
+    entry_points={
+        'console_scripts': [
+            'copycode=copycode.main:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    python_requires='>=3.6',
+)
