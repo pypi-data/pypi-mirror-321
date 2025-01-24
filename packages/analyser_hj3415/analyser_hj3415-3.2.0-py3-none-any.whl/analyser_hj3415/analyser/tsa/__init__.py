1b265788ef0822f5e5d1aa58a093ec73a0d49969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:05:16 +0000

    Add package analyser_hj3415/3.2.0/analyser_hj3415-3.2.0-py3-none-any.whl

---
+++ packages/analyser_hj3415/analyser_hj3415-3.2.0-py3-none-any.whl/analyser_hj3415/analyser/tsa/__init__.py
@@ -0,0 +1,15 @@
+from analyser_hj3415.analyser.tsa.lstm import *
+from analyser_hj3415.analyser.tsa.prophet import *
+
+MIs = {
+        "wti": "CL=F",
+        "gold": "GC=F",
+        "silver": "SI=F",
+        "usdidx": "DX-Y.NYB",
+        "usdkrw": "KRW=X",
+        "sp500": "^GSPC",
+        "kospi": "^KS11",
+        "nikkei": "^N225",
+        "china": "^HSI",
+        "irx": "^IRX",
+    }
