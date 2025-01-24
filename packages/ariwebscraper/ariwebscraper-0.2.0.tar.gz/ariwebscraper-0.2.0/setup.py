8f0482745a937164e3badd0236151a70631259d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 20:05:23 +0000

    Add package ariwebscraper/0.2.0/ariwebscraper-0.2.0.tar.gz

---
+++ packages/ariwebscraper/ariwebscraper-0.2.0.tar.gz/ariwebscraper-0.2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ariwebscraper',
+    version='0.2.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'beautifulsoup4==4.9.3',
+        'requests==2.25.1',
+    ],
+    description='A simple web scraper',
+    author='John/Jane Doe',
+    author_email='j.doe@example.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
