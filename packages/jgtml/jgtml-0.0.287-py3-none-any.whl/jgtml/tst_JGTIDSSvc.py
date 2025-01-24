0d8d022156b9d23664d8d696e6136bc5fa28618e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:04:58 +0000

    Add package jgtml/0.0.287/jgtml-0.0.287-py3-none-any.whl

---
+++ packages/jgtml/jgtml-0.0.287-py3-none-any.whl/jgtml/tst_JGTIDSSvc.py
@@ -0,0 +1,20 @@
+
+#%%
+
+
+#from jgtpy.JGTIDS import _ids_add_fdb_column_logics_v2
+#from jgtpy import jgtapyhelper as th
+
+
+#from jgtpy.JGTIDSSvc import get_ids
+#from jgtpy.JGTIDSRequest import JGTIDSRequest
+i="GBP/USD"
+t="H1"
+
+from jgtapp import ids
+
+ids(i,t)
+
+from jgtpy.JGTIDS
+#df = get_ids(rq=JGTIDSRequest(i,t))
+# %%
