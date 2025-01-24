14eb3083962c3801eaa627c54ab732a43f09a50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 20:05:29 +0000

    Add package mypythonlibaco1/0.1.4/mypythonlibaco1-0.1.4.tar.gz

---
+++ packages/mypythonlibaco1/mypythonlibaco1-0.1.4.tar.gz/mypythonlibaco1-0.1.4/mypythonlibaco1/myfunctions.py
@@ -0,0 +1,6 @@
+import math
+import mypythonlibaco1
+
+def apply_sqrt(inp):
+    out = math.sqrt(inp) +2
+    return out
---
+++ packages/mypythonlibaco1/mypythonlibaco1-0.1.4.tar.gz/mypythonlibaco1-0.1.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mypythonlibaco1',
+    packages=find_packages(),
+    version='0.1.4',
+    description='My first Custom Python library.',
+    author='aco',
+    url='https://github.com/albertcdc/my_custom_codes'
+)
