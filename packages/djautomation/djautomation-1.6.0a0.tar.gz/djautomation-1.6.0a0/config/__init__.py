9c15faa1bf1ba1a04a01a70e0e1f2faa692602b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:45 +0000

    Add package djautomation/1.6.0a0/djautomation-1.6.0a0.tar.gz

---
+++ packages/djautomation/djautomation-1.6.0a0.tar.gz/djautomation-1.6.0a0/config/__init__.py
@@ -0,0 +1,10 @@
+"""
+modules/download/__init__.py
+
+You can leave this empty or use it to define imports accessible at the
+'modules.download' package level.
+"""
+
+# Example of exposing some imports:
+# from .downloader import download_track
+# from .post_process import organize_downloads
