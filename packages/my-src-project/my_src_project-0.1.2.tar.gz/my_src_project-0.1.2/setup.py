089133e3e39a63a031a7bcfe14f87e3c41896c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:53:10 +0000

    Add package my-src-project/0.1.2/my_src_project-0.1.2.tar.gz

---
+++ packages/my-src-project/my_src_project-0.1.2.tar.gz/my_src_project-0.1.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+#
+# Copyright (C) 2025 - 2025 Guanhao Sun, Inc. All Rights Reserved 
+#
+# @Time    : 2025/1/15 10:03
+# @Author  : Guanhao Sun
+# @File    : setup.py
+# @IDE     : PyCharm
+
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="my_src_project",
+    version="0.1.2",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[],
+    extras_require={
+        "dev": ["pytest"],
+    },
+)
---
+++ packages/my-src-project/my_src_project-0.1.2.tar.gz/my_src_project-0.1.2/src/my_project/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+#
+# Copyright (C) 2025 - 2025 Guanhao Sun, Inc. All Rights Reserved 
+#
+# @Time    : 2025/1/15 9:51
+# @Author  : Guanhao Sun
+# @File    : __init__.py
+# @IDE     : PyCharm
+
+__version__ = "0.1.2"
