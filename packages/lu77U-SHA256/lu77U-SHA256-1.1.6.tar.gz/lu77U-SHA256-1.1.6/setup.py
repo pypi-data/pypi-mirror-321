70f1d59afdd26b21dc5b330f726f6a5e8b3dfec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:30 +0000

    Add package lu77U-SHA256/1.1.6/lu77U-SHA256-1.1.6.tar.gz

---
+++ packages/lu77U-SHA256/lu77U-SHA256-1.1.6.tar.gz/lu77U-SHA256-1.1.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lu77U-SHA256",
+    version="1.1.6",
+    author="Sam MG Harish",
+    home_page="https://github.com/sam-mg/Python-SHA-256-Implementation",
+    license="Apache Software License 2.0",
+    description="An Fully Manual SHA-256 Hasher",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["click"],
+    entry_points={
+        "console_scripts": [
+            "lu77U-SHA256 = lu77U_SHA256.Main:main"
+        ]
+    }
+)
