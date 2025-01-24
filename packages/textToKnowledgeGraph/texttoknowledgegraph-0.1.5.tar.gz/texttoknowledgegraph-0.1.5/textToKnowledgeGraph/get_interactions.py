c7fcc2dd392c7aa822103e777be43561ff36942b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 19:05:18 +0000

    Add package textToKnowledgeGraph/0.1.5/texttoknowledgegraph-0.1.5.tar.gz

---
+++ packages/textToKnowledgeGraph/texttoknowledgegraph-0.1.5.tar.gz/texttoknowledgegraph-0.1.5/textToKnowledgeGraph/get_interactions.py
@@ -0,0 +1,52 @@
+import warnings
+import os
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.output_parsers.openai_functions import JsonKeyOutputFunctionsParser 
+from .bel_model import get_bel_extraction_model
+warnings.filterwarnings("ignore")
+
+
+def get_prompt(identifier, filepath):
+    # Determine the directory of the package
+    package_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Determine the absolute path to the file
+    absolute_filepath = os.path.join(package_dir, filepath)
+
+    with open(absolute_filepath, 'rb') as file:
+        content = file.read()
+
+    # Check for BOM and remove it
+    if content.startswith(b'\xef\xbb\xbf'):
+        content = content[3:]
+
+    lines = content.decode('utf-8').splitlines()
+    prompt = []
+    capture = False
+    for line in lines:
+        if line.strip().startswith('#') and identifier in line:
+            capture = True
+            continue
+        if capture:
+            if line.strip().startswith('#') and len(prompt) > 0:
+                break
+            prompt.append(line)
+    return ''.join(prompt)
+
+
+filepath = 'textToKnowledgeGraph/prompt_file_v5.txt'
+prompt_identifier = 'general prompt'
+prompt = get_prompt(prompt_identifier, filepath)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", prompt),
+    ("human", "{text}")
+])
+
+
+def initialize_chains(api_key):
+    """Initialize extraction chains with the provided API key."""
+    bel_extraction_model = get_bel_extraction_model(api_key)
+
+    bel_extraction_chain = prompt | bel_extraction_model | JsonKeyOutputFunctionsParser(key_name="interactions")
+    return bel_extraction_chain
