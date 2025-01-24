200b4899e790e8c4fa01ac2fcfd33b6f8521d96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:00:44 +0000

    Add package flet-geolocator/0.1.0.dev1/flet_geolocator-0.1.0.dev1.tar.gz

---
+++ packages/flet-geolocator/flet_geolocator-0.1.0.dev1.tar.gz/flet_geolocator-0.1.0.dev1/src/flet_geolocator/__init__.py
@@ -0,0 +1,12 @@
+from flet_geolocator.geolocator import (
+    Geolocator,
+    GeolocatorActivityType,
+    GeolocatorAndroidSettings,
+    GeolocatorAppleSettings,
+    GeolocatorPermissionStatus,
+    GeolocatorPosition,
+    GeolocatorPositionAccuracy,
+    GeolocatorPositionChangeEvent,
+    GeolocatorSettings,
+    GeolocatorWebSettings,
+)
