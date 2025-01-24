c0a7496aa0dff9a613e4571a41623388747fdfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:39 +0000

    Add package chargebee/3.0.0/chargebee-3.0.0-py3-none-any.whl

---
+++ packages/chargebee/chargebee-3.0.0-py3-none-any.whl/chargebee/util.py
@@ -0,0 +1,70 @@
+from chargebee import compat
+from collections import OrderedDict
+from enum import Enum
+
+
+def serialize(value, prefix=None, idx=None):
+
+    serialized = OrderedDict()
+
+    if isinstance(value, dict):
+        for k, v in list(value.items()):
+            if k in (
+                "meta_data",
+                "metaData",
+                "metadata",
+                "checkout_info",
+            ) and isinstance(v, dict):
+                serialized.update({k: v})
+            elif k in (
+                "exemption_details",
+                "item_family_ids",
+                "item_price_periods",
+                "currencies",
+            ) and isinstance(v, list):
+                serialized.update({k: v})
+            elif k in ("in", "not_in", "between") and isinstance(v, list):
+                v = [str(i) for i in v]
+                key = "".join(
+                    [
+                        prefix or "",
+                        "[%s]" % k if prefix is not None else k,
+                        "[%s]" % idx if idx is not None else "",
+                    ]
+                )
+                serialized.update({key: str(v)})
+            elif isinstance(v, (dict, list, tuple)):
+                serialized.update(serialize(v, k))
+            else:
+                if isinstance(v, Enum):
+                    v = str(v)
+                key = "".join(
+                    [
+                        prefix or "",
+                        "[%s]" % k if prefix is not None else k,
+                        "[%s]" % idx if idx is not None else "",
+                    ]
+                )
+                serialized.update({key: get_val(v)})
+
+    elif isinstance(value, (list, tuple)):
+        for i, v in enumerate(value):
+            serialized.update(serialize(v, prefix, i))
+
+    else:
+        if prefix is not None and idx is not None:
+            key = prefix + "[" + str(idx) + "]"
+            serialized.update({key: get_val(value)})
+        else:
+            raise TypeError("only hash or arrays are allowed as value")
+
+    return serialized
+
+
+def get_val(val):
+    if val is None:
+        return ""
+    elif isinstance(val, bool):
+        return str(val).lower()
+    else:
+        return val
