b1687159a9f01768c028c685c43202841e4b3a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 19:05:39 +0000

    Add package agentifyme/0.1.29/agentifyme-0.1.29.tar.gz

---
+++ packages/agentifyme/agentifyme-0.1.29.tar.gz/agentifyme-0.1.29/agentifyme/__init__.py
@@ -0,0 +1,8 @@
+from agentifyme.client import AsyncClient, Client, WorkflowExecutionError
+from agentifyme.config import AgentifyMeConfig
+from agentifyme.logger import get_logger
+from agentifyme.tasks import task
+from agentifyme.workflows import workflow
+
+__version__ = "0.1.29"
+__all__ = ["get_logger", "AgentifyMeConfig", "task", "workflow", "Client", "AsyncClient", "WorkflowExecutionError"]
