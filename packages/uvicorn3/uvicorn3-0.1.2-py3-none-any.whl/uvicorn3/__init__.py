018a8105e6ebc29a15d5ef922dc57e881f7d2ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:04:55 +0000

    Add package uvicorn3/0.1.2/uvicorn3-0.1.2-py3-none-any.whl

---
+++ packages/uvicorn3/uvicorn3-0.1.2-py3-none-any.whl/uvicorn3/__init__.py
@@ -0,0 +1,5 @@
+from uvicorn3.config import Config
+from uvicorn3.main import Server, main, run
+
+__version__ = "0.34.0"
+__all__ = ["main", "run", "Config", "Server"]
---
+++ packages/uvicorn3/uvicorn3-0.1.2-py3-none-any.whl/uvicorn3/__main__.py
@@ -0,0 +1,4 @@
+import uvicorn3
+
+if __name__ == "__main__":
+    uvicorn3.main()
