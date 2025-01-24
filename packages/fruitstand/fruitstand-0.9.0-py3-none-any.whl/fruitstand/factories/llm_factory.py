bafb84342184b204d45e19f818757fbf07d012ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 18:04:55 +0000

    Add package fruitstand/0.9.0/fruitstand-0.9.0-py3-none-any.whl

---
+++ packages/fruitstand/fruitstand-0.9.0-py3-none-any.whl/fruitstand/factories/llm_factory.py
@@ -0,0 +1,10 @@
+from fruitstand.services.llms.AnthropicService import AnthropicService
+from fruitstand.services.llms.OpenAIService import OpenAIService
+
+def getLLM(llm: str, api_key: str) -> OpenAIService:
+    if llm == "openai":
+        return OpenAIService(api_key)
+    elif llm == "anthropic":
+        return AnthropicService(api_key)
+    else:
+        raise TypeError(f"{llm} is not a valid embeddings llm")
