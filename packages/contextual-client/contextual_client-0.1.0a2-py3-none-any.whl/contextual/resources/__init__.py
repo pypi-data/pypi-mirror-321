58c2021c4c3a25b7637171608e0ea5a58cae2d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:02:07 +0000

    Add package contextual-client/0.1.0a2/contextual_client-0.1.0a2-py3-none-any.whl

---
+++ packages/contextual-client/contextual_client-0.1.0a2-py3-none-any.whl/contextual/resources/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .agents import (
+    AgentsResource,
+    AsyncAgentsResource,
+    AgentsResourceWithRawResponse,
+    AsyncAgentsResourceWithRawResponse,
+    AgentsResourceWithStreamingResponse,
+    AsyncAgentsResourceWithStreamingResponse,
+)
+from .lmunit import (
+    LMUnitResource,
+    AsyncLMUnitResource,
+    LMUnitResourceWithRawResponse,
+    AsyncLMUnitResourceWithRawResponse,
+    LMUnitResourceWithStreamingResponse,
+    AsyncLMUnitResourceWithStreamingResponse,
+)
+from .datastores import (
+    DatastoresResource,
+    AsyncDatastoresResource,
+    DatastoresResourceWithRawResponse,
+    AsyncDatastoresResourceWithRawResponse,
+    DatastoresResourceWithStreamingResponse,
+    AsyncDatastoresResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "DatastoresResource",
+    "AsyncDatastoresResource",
+    "DatastoresResourceWithRawResponse",
+    "AsyncDatastoresResourceWithRawResponse",
+    "DatastoresResourceWithStreamingResponse",
+    "AsyncDatastoresResourceWithStreamingResponse",
+    "AgentsResource",
+    "AsyncAgentsResource",
+    "AgentsResourceWithRawResponse",
+    "AsyncAgentsResourceWithRawResponse",
+    "AgentsResourceWithStreamingResponse",
+    "AsyncAgentsResourceWithStreamingResponse",
+    "LMUnitResource",
+    "AsyncLMUnitResource",
+    "LMUnitResourceWithRawResponse",
+    "AsyncLMUnitResourceWithRawResponse",
+    "LMUnitResourceWithStreamingResponse",
+    "AsyncLMUnitResourceWithStreamingResponse",
+]
---
+++ packages/contextual-client/contextual_client-0.1.0a2-py3-none-any.whl/contextual/types/__init__.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .agent import Agent as Agent
+from .datastore import Datastore as Datastore
+from .agent_metadata import AgentMetadata as AgentMetadata
+from .agent_list_params import AgentListParams as AgentListParams
+from .datastore_metadata import DatastoreMetadata as DatastoreMetadata
+from .agent_create_params import AgentCreateParams as AgentCreateParams
+from .agent_update_params import AgentUpdateParams as AgentUpdateParams
+from .create_agent_output import CreateAgentOutput as CreateAgentOutput
+from .list_agents_response import ListAgentsResponse as ListAgentsResponse
+from .lmunit_create_params import LMUnitCreateParams as LMUnitCreateParams
+from .datastore_list_params import DatastoreListParams as DatastoreListParams
+from .lmunit_create_response import LMUnitCreateResponse as LMUnitCreateResponse
+from .datastore_create_params import DatastoreCreateParams as DatastoreCreateParams
+from .list_datastores_response import ListDatastoresResponse as ListDatastoresResponse
+from .create_datastore_response import CreateDatastoreResponse as CreateDatastoreResponse
