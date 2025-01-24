305631458dfec2cbf69bc970b58107f1ecdd3bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:56:19 +0000

    Add package chris-testing-versioning-stuff/1.1.0/chris_testing_versioning_stuff-1.1.0-py3-none-any.whl

---
+++ packages/chris-testing-versioning-stuff/chris_testing_versioning_stuff-1.1.0-py3-none-any.whl/hello/hello.py
@@ -0,0 +1,8 @@
+def main(name: str = "python-template-test") -> str:
+    if not isinstance(name, str):
+        raise NotImplementedError("Only strings are supported!??!!")
+    return f"Hello {name}!"
+
+
+if __name__ == "__main__":
+    main()
