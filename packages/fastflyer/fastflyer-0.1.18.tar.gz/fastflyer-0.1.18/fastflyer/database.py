c9f69ab6e049c8280a0d461969dd08be1a1ad4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 23:05:18 +0000

    Add package fastflyer/0.1.18/fastflyer-0.1.18.tar.gz

---
+++ packages/fastflyer/fastflyer-0.1.18.tar.gz/fastflyer-0.1.18/fastflyer/database.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from fastkit.database import get_mysql_pool  # pylint: disable=unused-import # noqa
+from fastkit.database import get_dataset_pool  # pylint: disable=unused-import # noqa
