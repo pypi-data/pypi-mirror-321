2d2372a2a0e319d977f540b223d26e6e42c24b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:00:44 +0000

    Add package weauth/1.5.6/weauth-1.5.6-py3-none-any.whl

---
+++ packages/weauth/weauth-1.5.6-py3-none-any.whl/weauth/listener/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.10
+# -*- coding: utf-8 -*-
+# author： NearlyHeadlessJack
+# email: wang@rjack.cn
+# datetime： 2024/7/2 下午5:26
+# ide： PyCharm
+# file: __init__.py
+from .listener import Listener
+from .wechat_confirm_listener import WeChatConfirmListener
