f4bcbb39102bea9d2cd218da31bc682c6e7528db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:01 +0000

    Add package embykeeper/6.4.2/embykeeper-6.4.2-py3-none-any.whl

---
+++ packages/embykeeper/embykeeper-6.4.2-py3-none-any.whl/embykeeper/telechecker/messager/test_smart_pornemby.py
@@ -0,0 +1,12 @@
+from datetime import time
+
+from .smart_pornemby import SmartPornembyMessager
+
+__ignore__ = True
+
+
+class TestSmartPornembyMessager(SmartPornembyMessager):
+    name = "Pornemby 智能水群测试"
+    chat_name = "api_group"
+    msg_per_day = 1000
+    at = (time(0, 0), time(23, 59))
