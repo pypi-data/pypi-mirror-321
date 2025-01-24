b869699b3488f12eaa90953a1f947cccc6677407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 19:05:21 +0000

    Add package aabpl/0.1.3/aabpl-0.1.3-py3-none-any.whl

---
+++ packages/aabpl/aabpl-0.1.3-py3-none-any.whl/primelocations/__init__.py
@@ -0,0 +1,21 @@
+from importlib.metadata import version
+# TODO replace * with more expliced exports for the final version of the package 
+# from .radius_search.grid_class import *
+# from .radius_search.optimal_grid_spacing import *
+# from .radius_search.offset_regions import *
+# from ..utils.general import *
+# from ..illustrations.illustrate_nested_grid import *
+# from ..illustrations.illustrate_optimal_grid_spacing import *
+# from ..illustrations.illustrate_point_to_cell_region_assignment import *
+# from .nested_search import *
+# from .radius_search.radius_search_class import *
+# from ..illustrations.plot_utils import *
+# from .radius_search.two_dimensional_weak_ordering_class import *
+from .main import *
+# from ..illustrations.plot_utils import *
+# from ..tests_and_old_stuff.two_dimensional_weak_ordering import *
+# from ..tests_and_old_stuff.unassigned_functions import *
+
+# TODO ensure to no exports imports (like np.array)?
+
+# __version__ = version('primelocations')
