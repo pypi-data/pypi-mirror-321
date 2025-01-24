b0af7fb41843f784c8cbeddbddf04fb995acccf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 20:04:31 +0000

    Add package cba-events/0.1.8/cba_events-0.1.8.tar.gz

---
+++ packages/cba-events/cba_events-0.1.8.tar.gz/cba_events-0.1.8/cba_events/events/types.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field
+from typing import Optional, Dict, Any
+from .base_event import BaseEvent
+
+
+# @dataclass
+class StockpileEvent(BaseEvent):
+    """Stockpile-related event class"""
+
+    action: str
+    details: Dict[str, Any] = field(default_factory=dict)
+
+    def __post_init__(self):
+        super().__init__(event_type="stockpile")
+
+
+class PitEvent(BaseEvent):
+    """Stockpile-related event class"""
+
+    action: str
+    details: Dict[str, Any] = field(default_factory=dict)
+
+    def __post_init__(self):
+        super().__init__(event_type="pit")
