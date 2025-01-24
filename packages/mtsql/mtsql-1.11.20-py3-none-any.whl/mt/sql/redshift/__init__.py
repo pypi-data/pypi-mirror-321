eb41f8fc145c635b79da94c0de7c5b4ad48d0a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:17 +0000

    Add package mtsql/1.11.20/mtsql-1.11.20-py3-none-any.whl

---
+++ packages/mtsql/mtsql-1.11.20-py3-none-any.whl/mt/sql/redshift/__init__.py
@@ -0,0 +1,27 @@
+from pkg_resources import DistributionNotFound, get_distribution, parse_version
+from sqlalchemy.dialects import registry  # noqa
+
+from .main import *
+
+__api__ = [
+    "rename_schema",
+    "get_frame_length",
+    "rename_table",
+    "vacuum_table",
+    "drop_table",
+    "rename_view",
+    "drop_view",
+    "rename_matview",
+    "refresh_matview",
+    "drop_matview",
+    "rename_column",
+    "drop_column",
+    "conform",
+]
+
+
+registry.register(
+    "mtsql_redshift",
+    "mt.sql.redshift.dialect",
+    "RedshiftDialect",
+)
