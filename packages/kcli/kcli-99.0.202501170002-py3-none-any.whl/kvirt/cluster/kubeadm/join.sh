19666e0917a9ae15d26ede0bb50d5647dd5b3ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 23:05:05 +0000

    Add package kcli/99.0.202501170002/kcli-99.0.202501170002-py3-none-any.whl

---
+++ packages/kcli/kcli-99.0.202501170002-py3-none-any.whl/kvirt/cluster/kubeadm/join.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+{% if api_ip == None %}
+{% set api_ip = '{0}-ctlplane-1'.format(cluster)|kcli_info('ip') if scale|default(False) and 'ctlplane-0' in name else first_ip %}
+{% endif %}
+
+TOKEN={{ token }}
+CTLPLANES="{{ '--control-plane --certificate-key %s' % cert_key if 'ctlplane' in name else '' }}"
+
+echo {{ api_ip }} api.{{ cluster }}.{{ domain }} >> /etc/hosts
+kubeadm join {{ api_ip }}:6443 --token $TOKEN --discovery-token-unsafe-skip-ca-verification $CTLPLANES
+
+{% if registry %}
+echo """[[registry]]
+location=\"{{ api_ip }}:5000\"
+insecure=true""" > /etc/containers/registries.conf.d/003-{{ cluster }}.conf
+{% endif %}
