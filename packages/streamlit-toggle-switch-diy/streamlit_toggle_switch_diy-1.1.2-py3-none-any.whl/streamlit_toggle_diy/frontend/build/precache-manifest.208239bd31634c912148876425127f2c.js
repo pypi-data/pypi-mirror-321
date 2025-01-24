138bc8639878241d7d24e66b0d117fe95e4772f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:05:47 +0000

    Add package streamlit-toggle-switch-diy/1.1.2/streamlit_toggle_switch_diy-1.1.2-py3-none-any.whl

---
+++ packages/streamlit-toggle-switch-diy/streamlit_toggle_switch_diy-1.1.2-py3-none-any.whl/streamlit_toggle_diy/frontend/build/precache-manifest.208239bd31634c912148876425127f2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389b0f69b89fe5622373ab4b8c76eff6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59319df519da97da0112",
+    "url": "./static/js/2.82cd69ef.chunk.js"
+  },
+  {
+    "revision": "fbabe484400fd249a0740ba2c0d294dd",
+    "url": "./static/js/2.82cd69ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c34e519e8a42b3bbdc7",
+    "url": "./static/js/main.72a874a0.chunk.js"
+  },
+  {
+    "revision": "ba8e30c930136a0acec7",
+    "url": "./static/js/runtime-main.d76c522e.js"
+  }
+]);
---
+++ packages/streamlit-toggle-switch-diy/streamlit_toggle_switch_diy-1.1.2-py3-none-any.whl/streamlit_toggle_diy/frontend/build/precache-manifest.669f85f994ea2c0d193915faa9d91f97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7901191f006629a2f7fa688007b1357",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59319df519da97da0112",
+    "url": "./static/js/2.82cd69ef.chunk.js"
+  },
+  {
+    "revision": "fbabe484400fd249a0740ba2c0d294dd",
+    "url": "./static/js/2.82cd69ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33c5287765518508ca9",
+    "url": "./static/js/main.93eb9d37.chunk.js"
+  },
+  {
+    "revision": "ba8e30c930136a0acec7",
+    "url": "./static/js/runtime-main.d76c522e.js"
+  }
+]);
