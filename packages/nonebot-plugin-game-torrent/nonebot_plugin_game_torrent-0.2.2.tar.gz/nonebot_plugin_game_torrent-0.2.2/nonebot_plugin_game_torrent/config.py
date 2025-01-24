a17339d0ce5c78596968097e2247776e5e39bff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:47 +0000

    Add package nonebot-plugin-game-torrent/0.2.2/nonebot_plugin_game_torrent-0.2.2.tar.gz

---
+++ packages/nonebot-plugin-game-torrent/nonebot_plugin_game_torrent-0.2.2.tar.gz/nonebot_plugin_game_torrent-0.2.2/nonebot_plugin_game_torrent/config.py
@@ -0,0 +1,20 @@
+"""
+File: config.py
+    Description: 插件配置文件
+"""
+
+from typing import Optional
+
+from nonebot import get_plugin_config
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    torrent_send_format: Optional[str] = (
+        "game: {game_name}\nsize: {size}\nlast_update: {last_update}\nmagnet: {magnet}\n"
+    )
+
+    # TODO 是否上传种子文件至群文件
+
+
+plugin_config = get_plugin_config(Config)
