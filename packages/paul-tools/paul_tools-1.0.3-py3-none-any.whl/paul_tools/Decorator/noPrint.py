35596af955472ea93316ae8d96442f943482b722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:58:39 +0000

    Add package paul-tools/1.0.3/paul_tools-1.0.3-py3-none-any.whl

---
+++ packages/paul-tools/paul_tools-1.0.3-py3-none-any.whl/paul_tools/Decorator/noPrint.py
@@ -0,0 +1,27 @@
+from .__init__ import *
+
+import sys
+import os
+from contextlib import contextmanager
+
+
+__all__ = ["noPrint"]
+
+
+@contextmanager
+def ContextManagersNoPrint():
+    _original_stdout = sys.stdout
+    sys.stdout = open(os.devnull, 'w')
+
+    yield _original_stdout
+
+    sys.stdout.close()
+    sys.stdout = _original_stdout
+
+
+def noPrint(func: Callable):
+    @functools.wraps(func)  # 保留原函數的元數據
+    def wrapper(*args, **kwargs):
+        with ContextManagersNoPrint():  # 使用上下文管理器禁用打印
+            return func(*args, **kwargs)
+    return wrapper
