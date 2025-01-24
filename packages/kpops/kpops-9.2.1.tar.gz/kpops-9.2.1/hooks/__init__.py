3e4e826b6f1385864fed781c6d205b64beee3340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:03:32 +0000

    Add package kpops/9.2.1/kpops-9.2.1.tar.gz

---
+++ packages/kpops/kpops-9.2.1.tar.gz/kpops-9.2.1/hooks/__init__.py
@@ -0,0 +1,5 @@
+"""KPOps pre-commit hooks."""
+
+from pathlib import Path
+
+ROOT = Path(__file__).parents[1].resolve()
---
+++ packages/kpops/kpops-9.2.1.tar.gz/kpops-9.2.1/kpops/const/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "9.2.1"
+KPOPS = "KPOps"
+KPOPS_MODULE = "kpops."
