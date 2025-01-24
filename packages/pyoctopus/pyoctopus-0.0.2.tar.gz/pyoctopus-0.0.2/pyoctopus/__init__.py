15a95cf71347f4bc1278cc862c0f9f873cc4296d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:04:15 +0000

    Add package pyoctopus/0.0.2/pyoctopus-0.0.2.tar.gz

---
+++ packages/pyoctopus/pyoctopus-0.0.2.tar.gz/pyoctopus-0.0.2/pyoctopus/__init__.py
@@ -0,0 +1,16 @@
+from .site import new as site
+
+from .reqeust import Request
+from .reqeust import new as request
+from .response import Response
+from .response import new as response
+from .limiter import new as limiter
+from .octopus import new
+from .types import R, Converter, Collector, Processor, Matcher, Terminable
+
+from .converter import *
+from .selector import *
+from .matcher import *
+from .processor import *
+from .store import *
+from .collector import *
