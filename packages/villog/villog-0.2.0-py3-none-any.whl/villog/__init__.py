e92afa751cd8f3f23db77d3beb976001b63ce650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:00:01 +0000

    Add package villog/0.2.0/villog-0.2.0-py3-none-any.whl

---
+++ packages/villog/villog-0.2.0-py3-none-any.whl/villog/__init__.py
@@ -0,0 +1,7 @@
+'''A simple logger'''
+
+from villog.log import Logger
+from villog import writexcel
+from villog import mssql
+from villog import pdf
+from villog.readexcel import ReadExcel
---
+++ packages/villog/villog-0.2.0-py3-none-any.whl/villog/pdf.py
@@ -0,0 +1,18 @@
+'''
+    This module is responsible for generating PDF files from HTML and CSS files.
+'''
+
+from weasyprint import HTML, CSS
+
+def generate(html_string: str,
+             output_path: str,
+             css_string: str | None = None) -> None:
+    '''
+        Generate a PDF file from a HTML and CSS file.
+
+        Parameters:
+            html_path (str): HTML string.
+            css_path (str): CSS string.
+    '''
+    HTML(string = html_string).write_pdf(output_path,
+                                         stylesheets = [CSS(string = css_string or "")]) # pylint: disable = line-too-long
