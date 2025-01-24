43c90715a245237a536847a121185eca48b2ce3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:04:01 +0000

    Add package cmlidar/0.0.6/cmlidar-0.0.6.tar.gz

---
+++ packages/cmlidar/cmlidar-0.0.6.tar.gz/cmlidar-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cmlidar',
+    version='v0.0.6',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+)
