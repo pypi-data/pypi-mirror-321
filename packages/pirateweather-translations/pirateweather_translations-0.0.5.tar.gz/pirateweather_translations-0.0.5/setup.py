c94730118b321a50766cbf71edbffaa7160d3ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 21:05:01 +0000

    Add package pirateweather-translations/0.0.5/pirateweather_translations-0.0.5.tar.gz

---
+++ packages/pirateweather-translations/pirateweather_translations-0.0.5.tar.gz/pirateweather_translations-0.0.5/setup.py
@@ -0,0 +1,33 @@
+"""Set up the Pirate Weather library."""
+
+import os
+
+from setuptools import find_packages, setup
+
+__version__ = "0.0.5"
+
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")) as f:
+    README = f.read()
+
+repo_url = "https://github.com/Pirate-Weather/translations"
+
+
+setup(
+    name="pirateweather-translations",
+    version=__version__,
+    author="Pirate-Weather",
+    description=("Translate Pirate Weather API summaries into any language"),
+    license="BSD 2-clause",
+    keywords="weather API pirateweather translations",
+    url=repo_url,
+    download_url=f"{repo_url}/archive/{__version__}.tar.gz",
+    packages=find_packages(where="pirateweather_translations"),
+    package_dir={"": "pirateweather_translations"},
+    include_package_data=True,  # Include package data files
+    package_data={
+        "pirateweather_translations.lang": ["*.py"],  # Include all .py files in lang/
+    },    
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
