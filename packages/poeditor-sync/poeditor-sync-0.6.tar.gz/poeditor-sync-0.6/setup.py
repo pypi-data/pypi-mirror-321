27cb5fe1e388578af791e2c57995cee6423074e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 19:04:52 +0000

    Add package poeditor-sync/0.6/poeditor-sync-0.6.tar.gz

---
+++ packages/poeditor-sync/poeditor-sync-0.6.tar.gz/poeditor-sync-0.6/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+try:
+    long_description = open('README.md', 'r').read()
+except:
+    long_description = ''
+
+setup(
+    name='poeditor-sync',
+    version='0.6',
+    packages=['poeditor_sync'],
+    py_modules=['cmd'],
+    entry_points={
+        'console_scripts': ['poeditor = poeditor_sync.cmd:poeditor'],
+    },
+    url='https://github.com/mick88/poeditor-sync',
+    license='MIT',
+    author='Michal Dabski',
+    author_email='contact@michaldabski.com',
+    install_requires=[
+        'click>=8',
+        'poeditor',
+        'pyyaml',
+    ],
+    description='Command line client for POEditor service',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Utilities',
+    ],
+    project_urls={
+        'Source': 'https://github.com/mick88/poeditor-sync',
+        'Tracker': 'https://github.com/mick88/poeditor-sync/issues',
+        'Changelog': 'https://github.com/mick88/poeditor-sync/blob/master/HISTORY.txt',
+        'Example configuration': 'https://github.com/mick88/poeditor-sync/blob/master/example/poeditor.yml',
+        'POEditor': 'https://poeditor.com/',
+    },
+)
