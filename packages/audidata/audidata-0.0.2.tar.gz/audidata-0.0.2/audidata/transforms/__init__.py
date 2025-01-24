b5cc41454c169246fa7549fdb030ecedb6cc4f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:01:19 +0000

    Add package audidata/0.0.2/audidata-0.0.2.tar.gz

---
+++ packages/audidata/audidata-0.0.2.tar.gz/audidata-0.0.2/audidata/transforms/__init__.py
@@ -0,0 +1,4 @@
+from audidata.transforms.audio import Mono, Normalize, TimeShift
+from audidata.transforms.onehot import OneHot
+from audidata.transforms.midi import Note2Token
+from audidata.transforms.text import TextNormalization
