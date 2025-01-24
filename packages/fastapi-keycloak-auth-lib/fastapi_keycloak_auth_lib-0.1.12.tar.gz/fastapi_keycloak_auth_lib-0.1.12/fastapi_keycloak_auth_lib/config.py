af9d5911cf129db8b63946bd06ecbafdf1f111ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:59:33 +0000

    Add package fastapi-keycloak-auth-lib/0.1.12/fastapi_keycloak_auth_lib-0.1.12.tar.gz

---
+++ packages/fastapi-keycloak-auth-lib/fastapi_keycloak_auth_lib-0.1.12.tar.gz/fastapi_keycloak_auth_lib-0.1.12/fastapi_keycloak_auth_lib/config.py
@@ -0,0 +1,29 @@
+import os
+
+class KeycloakConfig:
+    def __init__(self):
+        self.keycloak_url = os.getenv("KEYCLOAK_URL", "https://default-keycloak-url.com")
+        self.realm = os.getenv("KEYCLOAK_REALM", "default-realm")
+        self.client_id = os.getenv("KEYCLOAK_CLIENT_ID", "default-client-id")
+        self.username = os.getenv("KEYCLOAK_USERNAME", None)
+        self.password = os.getenv("KEYCLOAK_PASSWORD", None)
+        self.cert_url = os.getenv("cert_url", None)
+
+    def update(self, keycloak_url=None, realm=None, client_id=None, username=None, password=None):
+        if keycloak_url:
+            self.keycloak_url = keycloak_url
+        if realm:
+            self.realm = realm
+        if client_id:
+            self.client_id = client_id
+        if username:
+            self.username = username
+        if password:
+            self.password = password
+
+
+# Global configuration object
+keycloak_config = KeycloakConfig()
+
+def set_keycloak_config(keycloak_url=None, realm=None, client_id=None, username=None, password=None):
+    keycloak_config.update(keycloak_url, realm, client_id, username, password)
