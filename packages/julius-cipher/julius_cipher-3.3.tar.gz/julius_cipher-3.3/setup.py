fe65c1e82a18fa902f903f5aec9b3b9d5e9d9dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 19:05:03 +0000

    Add package julius-cipher/3.3/julius_cipher-3.3.tar.gz

---
+++ packages/julius-cipher/julius_cipher-3.3.tar.gz/julius_cipher-3.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = '3.3'
+DESCRIPTION = 'A simple command line program use to encipher/decipher text to/from Caesar Cipher.'
+with open("readme.md") as file:
+    LONG_DESCRIPTION = file.read()
+
+setup(
+    name = "julius-cipher",
+    version = VERSION,
+    author = "Zizel",
+    author_email = "danbua999@gmail.com",
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/uwungu01-rep/caesar-cipher-tool-python.edition",
+    packages = find_packages(),
+    install_requires = [], # add any additional packages that 
+    # needs to be installed along with your package. Eg: 'caer'
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'caesar  = caesar_cipher_tool_pye_edition.main:main',
+        ],
+    },
+)
