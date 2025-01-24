6e3fefad6295e6e01b316bdd821b440703da419f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:17 +0000

    Add package bittensor-wallet/3.0.0/bittensor_wallet-3.0.0.tar.gz

---
+++ packages/bittensor-wallet/bittensor_wallet-3.0.0.tar.gz/bittensor_wallet-3.0.0/bittensor_wallet/__init__.py
@@ -0,0 +1,16 @@
+from bittensor_wallet.bittensor_wallet import (
+    # classes
+    Config as Config,
+    Keyfile as Keyfile,
+    Keypair as Keypair,
+    Wallet as Wallet,
+    # modules
+    config as config,
+    errors as errors,
+    keyfile as keyfile,
+    keypair as keypair,
+    utils as utils,
+    wallet as wallet,
+)
+
+__version__ = "3.0.0"
