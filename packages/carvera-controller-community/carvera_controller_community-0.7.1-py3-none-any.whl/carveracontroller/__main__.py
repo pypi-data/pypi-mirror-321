b09d5494050422d870d0c4ad58ba381ef33517fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 21:05:05 +0000

    Add package carvera-controller-community/0.7.1/carvera_controller_community-0.7.1-py3-none-any.whl

---
+++ packages/carvera-controller-community/carvera_controller_community-0.7.1-py3-none-any.whl/carveracontroller/__main__.py
@@ -0,0 +1,8 @@
+from carveracontroller.main import main, init_lang, Lang
+
+# tr is used throughout the kivvy .kv definition files
+# thus kivy expects it be availiable from the caller
+default_lang = init_lang()
+tr = Lang(default_lang)
+
+main()
