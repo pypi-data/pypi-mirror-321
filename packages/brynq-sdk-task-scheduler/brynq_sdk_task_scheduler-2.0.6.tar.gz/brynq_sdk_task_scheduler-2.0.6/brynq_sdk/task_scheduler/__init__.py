f78da2a0f016caca0addf70ff45627aef33e37ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:02:18 +0000

    Add package brynq-sdk-task-scheduler/2.0.6/brynq_sdk_task_scheduler-2.0.6.tar.gz

---
+++ packages/brynq-sdk-task-scheduler/brynq_sdk_task_scheduler-2.0.6.tar.gz/brynq_sdk_task_scheduler-2.0.6/brynq_sdk/task_scheduler/__init__.py
@@ -0,0 +1,1 @@
+from brynq_sdk.task_scheduler.task_scheduler import TaskScheduler
---
+++ packages/brynq-sdk-task-scheduler/brynq_sdk_task_scheduler-2.0.6.tar.gz/brynq_sdk_task_scheduler-2.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='brynq_sdk_task_scheduler',
+    version='2.0.6',
+    description='Code to execute tasks in BrynQ.com with the task scheduler',
+    long_description='Code to execute tasks in the BrynQ.com platform with the task scheduler',
+    author='BrynQ',
+    author_email='support@brynq.com',
+    packages=find_namespace_packages(include=['brynq_sdk*']),
+    license='BrynQ License',
+    install_requires=[
+        'brynq-sdk-brynq>=1,<2',
+        'brynq-sdk-functions>=1,<2',
+        'brynq-sdk-mysql>=1,<1.0.1',
+        'brynq-sdk-mandrill>=1,<1.1.2',
+        'brynq-sdk-elastic>=2,<3'
+    ],
+    zip_safe=False,
+)
