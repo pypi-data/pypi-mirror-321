f2a0982f04c8f0e54071f66ec001b039894216fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:01:54 +0000

    Add package jrnmm/0.1.1.post1/jrnmm-0.1.1.post1.tar.gz

---
+++ packages/jrnmm/jrnmm-0.1.1.post1.tar.gz/jrnmm-0.1.1.post1/jrnmm/__init__.py
@@ -0,0 +1,7 @@
+"""jrnmm: Simulating trajectories from the stochastic Jansen-Rit neural mass model"""
+
+__version__ = "0.1.1.post1"
+
+from jrnmm.simulator import simulate
+
+__all__ = ["simulate"]
