cb7d22ad99ec554b651ed352565db7e83d67a38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 20:04:31 +0000

    Add package team3-pobl-chassis/1.0.17/team3_pobl_chassis-1.0.17.tar.gz

---
+++ packages/team3-pobl-chassis/team3_pobl_chassis-1.0.17.tar.gz/team3_pobl_chassis-1.0.17/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="team3_pobl_chassis",
+    version="1.0.17",
+    packages=find_packages(),
+    install_requires=[],
+    author="macc-team3",
+    author_email="gorka.etxebarria@alumni.mondragon.edu",
+    description="A reusable library for microservices",
+)
