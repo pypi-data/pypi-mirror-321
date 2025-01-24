f5eca80819e467b6221fea3aa88d0add6da6cf94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:07 +0000

    Add package mtsql/1.11.14/mtsql-1.11.14-py3-none-any.whl

---
+++ packages/mtsql/mtsql-1.11.14-py3-none-any.whl/mt/sql/redshift/__init__.py
@@ -0,0 +1,17 @@
+from .main import *
+
+__api__ = [
+    "rename_schema",
+    "get_frame_length",
+    "rename_table",
+    "vacuum_table",
+    "drop_table",
+    "rename_view",
+    "drop_view",
+    "rename_matview",
+    "refresh_matview",
+    "drop_matview",
+    "rename_column",
+    "drop_column",
+    "conform",
+]
