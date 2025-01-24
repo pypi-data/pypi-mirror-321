ee7dd2aad0a697e885a0b5d2452a6abd78a63692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 20:05:27 +0000

    Add package pykeyboard-kurigram/0.1.0/pykeyboard_kurigram-0.1.0-py3-none-any.whl

---
+++ packages/pykeyboard-kurigram/pykeyboard_kurigram-0.1.0-py3-none-any.whl/pykeyboard/__init__.py
@@ -0,0 +1,6 @@
+
+
+from .inline_keyboard import InlineKeyboard, InlineButton
+from .inline_pagination_keyboard import InlinePaginationKeyboard, InlineButton
+from .reply_keyboard import (
+    ReplyKeyboard, ReplyButton, ReplyKeyboardRemove, ForceReply)
