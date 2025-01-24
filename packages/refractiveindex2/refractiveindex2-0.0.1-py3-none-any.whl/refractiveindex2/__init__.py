0d3cb62e6dfacecaa8ee74124a099d36f25c4147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:53:40 +0000

    Add package refractiveindex2/0.0.1/refractiveindex2-0.0.1-py3-none-any.whl

---
+++ packages/refractiveindex2/refractiveindex2-0.0.1-py3-none-any.whl/refractiveindex2/__init__.py
@@ -0,0 +1,10 @@
+"""refractiveindex2: A python interface to the refractiveindex.info database.
+
+Copyright (c) 2025 Martin F. Scubert
+"""
+
+__version__ = "v0.0.1"
+__author__ = "Martin F. Schubert <mfschubert@gmail.com>"
+
+from .refractiveindex import RefractiveIndexMaterial as RefractiveIndexMaterial
+from .refractiveindex import _CATALOG as _CATALOG
