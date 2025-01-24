dda7298521ddb06a4e59dbbb1a3e93df3cd7784d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 18:05:31 +0000

    Add package haiway/0.9.0/haiway-0.9.0-py3-none-any.whl

---
+++ packages/haiway/haiway-0.9.0-py3-none-any.whl/haiway/types/__init__.py
@@ -0,0 +1,11 @@
+from haiway.types.frozen import frozenlist
+from haiway.types.missing import MISSING, Missing, is_missing, not_missing, when_missing
+
+__all__ = [
+    "MISSING",
+    "Missing",
+    "frozenlist",
+    "is_missing",
+    "not_missing",
+    "when_missing",
+]
