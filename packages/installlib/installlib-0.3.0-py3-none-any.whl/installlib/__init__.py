f3a7d4a59eb12eeba224f6a3b18eed6e17c22703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:03:59 +0000

    Add package installlib/0.3.0/installlib-0.3.0-py3-none-any.whl

---
+++ packages/installlib/installlib-0.3.0-py3-none-any.whl/installlib/__init__.py
@@ -0,0 +1,33 @@
+"""
+********************************************************************************
+installlib
+********************************************************************************
+
+.. currentmodule:: installlib
+
+
+.. toctree::
+    :maxdepth: 1
+
+
+"""
+
+from __future__ import print_function
+
+import os
+
+
+__author__ = ["Chen Kasirer"]
+__copyright__ = "Gramazio Kohler Research, ETH Zurich"
+__license__ = "MIT License"
+__email__ = "kasirer@arch.ethz.ch"
+__version__ = "0.3.0"
+
+
+HERE = os.path.dirname(__file__)
+
+HOME = os.path.abspath(os.path.join(HERE, "../../"))
+DOCS = os.path.abspath(os.path.join(HOME, "docs"))
+
+
+__all__ = ["HOME", "DOCS"]
