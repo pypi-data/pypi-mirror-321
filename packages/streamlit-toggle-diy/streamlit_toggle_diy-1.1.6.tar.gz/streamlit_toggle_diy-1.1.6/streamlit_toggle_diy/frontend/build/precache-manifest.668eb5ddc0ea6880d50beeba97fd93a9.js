43c57ff391c40bf1f87d1896a445073b9942ee77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:04:54 +0000

    Add package streamlit-toggle-diy/1.1.6/streamlit_toggle_diy-1.1.6.tar.gz

---
+++ packages/streamlit-toggle-diy/streamlit_toggle_diy-1.1.6.tar.gz/streamlit_toggle_diy-1.1.6/streamlit_toggle_diy/frontend/build/precache-manifest.668eb5ddc0ea6880d50beeba97fd93a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c96486a611e6f953d285b857e6dd68",
+    "url": "./index.html"
+  },
+  {
+    "revision": "86805c181e0dc035e517",
+    "url": "./static/js/2.8590d27d.chunk.js"
+  },
+  {
+    "revision": "fbabe484400fd249a0740ba2c0d294dd",
+    "url": "./static/js/2.8590d27d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138d1b64a39c299ad20b",
+    "url": "./static/js/main.d5e2b6f4.chunk.js"
+  },
+  {
+    "revision": "459827b3e50cfc060dc2",
+    "url": "./static/js/runtime-main.cc20bba3.js"
+  }
+]);
