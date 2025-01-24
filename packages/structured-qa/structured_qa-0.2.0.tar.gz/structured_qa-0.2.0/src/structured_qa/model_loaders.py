707c9399d58f728d8e20b356dfe40765b9d1ee74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 19:04:50 +0000

    Add package structured-qa/0.2.0/structured_qa-0.2.0.tar.gz

---
+++ packages/structured-qa/structured_qa-0.2.0.tar.gz/structured_qa-0.2.0/src/structured_qa/model_loaders.py
@@ -0,0 +1,27 @@
+import torch
+from llama_cpp import Llama
+
+
+def load_llama_cpp_model(model_id: str) -> Llama:
+    """
+    Loads the given model_id using Llama.from_pretrained.
+
+    Examples:
+        >>> model = load_llama_cpp_model("allenai/OLMoE-1B-7B-0924-Instruct-GGUF/olmoe-1b-7b-0924-instruct-q8_0.gguf")
+
+    Args:
+        model_id (str): The model id to load.
+            Format is expected to be `{org}/{repo}/{filename}`.
+
+    Returns:
+        Llama: The loaded model.
+    """
+    org, repo, filename = model_id.split("/")
+    model = Llama.from_pretrained(
+        repo_id=f"{org}/{repo}",
+        filename=filename,
+        n_ctx=0,  # 0 means that the model limit will be used, instead of the default (512) or other hardcoded value
+        verbose=False,
+        n_gpu_layers=-1 if torch.cuda.is_available() else 0,
+    )
+    return model
