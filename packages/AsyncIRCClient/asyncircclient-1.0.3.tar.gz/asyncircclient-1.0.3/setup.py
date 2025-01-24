18ffb87508577847bfa0fa2b07f0ba4a86577ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:53:25 +0000

    Add package AsyncIRCClient/1.0.3/asyncircclient-1.0.3.tar.gz

---
+++ packages/AsyncIRCClient/asyncircclient-1.0.3.tar.gz/asyncircclient-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='AsyncIRCClient',
+    version='1.0.3',
+    description='Async IRC Client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='CoreTaxxe',
+    author_email='coretaxxe@gmail.com',
+    packages=['async_irc_client'],
+    install_requires=[
+        "loguru",
+        "python_socks"
+    ],
+)
