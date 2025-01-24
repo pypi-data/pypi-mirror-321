c90da3caaa6b66cd30bd31c5fd573afe7f10a6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 20:04:29 +0000

    Add package CarbonIQ/0.1.0/carboniq-0.1.0.tar.gz

---
+++ packages/CarbonIQ/carboniq-0.1.0.tar.gz/carboniq-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CarbonIQ",
+    version="0.1.0",
+    author="Md Ashikur Rahman",
+    author_email="mdashikur.rafi@gmail.com",
+    description="A Python package to track carbon emissions for scripts and applications.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ashikrafi/CarbonIQ",
+    packages=find_packages(),
+    install_requires=[
+        "pynvml",
+        "psutil"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "carboniq=carboniq.__main__:main",  # CLI entry point (if applicable)
+        ],
+    },
+    include_package_data=True,
+)
