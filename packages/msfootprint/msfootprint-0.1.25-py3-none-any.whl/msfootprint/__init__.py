59082a87bb32ab9a6998dfd1f7f470c6461b602f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:00:02 +0000

    Add package msfootprint/0.1.25/msfootprint-0.1.25-py3-none-any.whl

---
+++ packages/msfootprint/msfootprint-0.1.25-py3-none-any.whl/msfootprint/__init__.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.simplefilter("ignore")
+
+from .buildingfootprint import getBuildingFootprint
+from .buildingfootprint import FindTableorFolder
+from .find_footprinttable import load_USStates
