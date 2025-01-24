e9c63193ce1c89eeb157a097420690347ff1a105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 23:04:50 +0000

    Add package certbot-dns-spaceship/1.0.3/certbot_dns_spaceship-1.0.3.tar.gz

---
+++ packages/certbot-dns-spaceship/certbot_dns_spaceship-1.0.3.tar.gz/certbot_dns_spaceship-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="certbot-dns-spaceship",
+    version="1.0.3",
+    description="Spaceship DNS Authenticator plugin for Certbot",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/version-two/certbot_dns_spaceship",
+    author="Mario Chamuty",
+    author_email="info@versiontwo.sk",
+    license="Apache License 2.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "certbot>=1.0.0",
+        "requests",
+        "tldextract",
+    ],
+    entry_points={
+        "certbot.plugins": [
+            "dns-spaceship = certbot_dns_spaceship.authenticator:SpaceshipDNSAuthenticator",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
+    ],
+)
+    
