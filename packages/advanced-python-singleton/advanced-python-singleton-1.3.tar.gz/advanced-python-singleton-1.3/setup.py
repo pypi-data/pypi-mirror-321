f6816e23030c4bfde2f2c451ac645381fbe806b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:15 +0000

    Add package advanced-python-singleton/1.3/advanced-python-singleton-1.3.tar.gz

---
+++ packages/advanced-python-singleton/advanced-python-singleton-1.3.tar.gz/advanced-python-singleton-1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='advanced-python-singleton',
+    version='1.3',
+    author='jogakdal',
+    author_email='jogakdal@gmail.com',
+    description='Singleton & TtlSingleton meta-class',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/jogakdal/advanced-singleton',
+    install_requires=['expiringdict'],
+    packages=find_packages(exclude=[]),
+    keywords=['jogakdal', 'Singleton', 'TTL', 'metaclass'],
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
