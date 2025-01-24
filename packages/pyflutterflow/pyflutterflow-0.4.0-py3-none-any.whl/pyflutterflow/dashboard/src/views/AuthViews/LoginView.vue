1c28168cc65a0ef6b657f8b7fbb6f65c77f78680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:13 +0000

    Add package pyflutterflow/0.4.0/pyflutterflow-0.4.0-py3-none-any.whl

---
+++ packages/pyflutterflow/pyflutterflow-0.4.0-py3-none-any.whl/pyflutterflow/dashboard/src/views/AuthViews/LoginView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="shadow rounded-lg md:shadow-xl md:rounded-xl outline outline-1 outline-surface-300 p-5 md:p-10 max-w-lg mx-auto md:m-10">
+    <LoginComponent />
+  </div>
+</template>
+
+<script setup>
+import LoginComponent from '@/components/auth/LoginComponent.vue';
+</script>
