f32ddbf9d6ff08448492581e0063dd15f17b417f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:08 +0000

    Add package mtsql/1.11.19/mtsql-1.11.19-py3-none-any.whl

---
+++ packages/mtsql/mtsql-1.11.19-py3-none-any.whl/mt/sql/redshift/__init__.py
@@ -0,0 +1,43 @@
+from pkg_resources import DistributionNotFound, get_distribution, parse_version
+from sqlalchemy.dialects import registry  # noqa
+
+from .main import *
+
+__api__ = [
+    "rename_schema",
+    "get_frame_length",
+    "rename_table",
+    "vacuum_table",
+    "drop_table",
+    "rename_view",
+    "drop_view",
+    "rename_matview",
+    "refresh_matview",
+    "drop_matview",
+    "rename_column",
+    "drop_column",
+    "conform",
+]
+
+
+for package in ["psycopg2", "psycopg2-binary", "psycopg2cffi"]:
+    try:
+        if get_distribution(package).parsed_version < parse_version("2.5"):
+            raise ImportError("Minimum required version for psycopg2 is 2.5")
+        break
+    except DistributionNotFound:
+        pass
+
+registry.register("rs", "mt.sql.redshift.dialect", "RedshiftDialect_psycopg2")
+registry.register("rs.psycopg2", "mt.sql.redshift.dialect", "RedshiftDialect_psycopg2")
+registry.register(
+    "rs+psycopg2cffi",
+    "mt.sql.redshift.dialect",
+    "RedshiftDialect_psycopg2cffi",
+)
+
+registry.register(
+    "mtsql_redshift",
+    "mt.sql.redshift.dialect",
+    "RedshiftDialect_redshift_connector",
+)
