a161a2bbd03af23e29ca22831fe2efeea8f05b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:59:42 +0000

    Add package coordinates2country/1.0.7/coordinates2country-1.0.7.tar.gz

---
+++ packages/coordinates2country/coordinates2country-1.0.7.tar.gz/coordinates2country-1.0.7/README.md
@@ -0,0 +1,35 @@
+# coordinates2country-py
+
+A Python port of the excellent [coordinates2country](https://github.com/coordinates2country/coordinates2country) Java library.
+
+What country is at a particular latitude/longitude? This Python library tells you quickly, without using the Internet and without requiring any permission.
+
+- Fast reverse geocoding
+- Never needs an Internet connection, not even the first time
+- Less than 100kB
+
+## Installation
+
+```bash
+pip install coordinates2country-py
+```
+
+## Usage
+
+```python
+from coordinates2country import Coordinates2Country
+
+c2c = Coordinates2Country()
+
+# Get country name in English
+print(c2c.country(50.1, 10.2))  # Output: Germany
+
+# Get ISO country code
+print(c2c.country_code(50.1, 10.2))  # Output: DE
+
+# Get Wikidata QID
+print(c2c.country_qid(50.1, 10.2))  # Output: 183
+```
+## Credits
+
+This is a Python port of the coordinates2country Java library. All credit for the original implementation and data goes to the original authors.
---
+++ packages/coordinates2country/coordinates2country-1.0.7.tar.gz/coordinates2country-1.0.7/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="coordinates2country",
+    version="1.0.7",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'coordinates2country': ['resources/*'],
+    },
+    install_requires=[
+        'Pillow',  # For image processing
+    ],
+    author="Abram Astle",
+    author_email="castle676767@gmail.com",
+    description="Fast reverse geocoding without internet connection",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/coordinates2country-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    options={
+        'easy_install': {
+            'force_installation': True,  # Force overwrite of existing installation
+        }
+    },
+    zip_safe=False,  # This ensures the package is always extracted
+)
