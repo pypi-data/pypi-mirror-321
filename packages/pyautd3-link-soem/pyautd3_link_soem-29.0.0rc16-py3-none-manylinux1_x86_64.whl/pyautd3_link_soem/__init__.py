79249f70d5204de8689c1b1674d521a86ed69550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:06:45 +0000

    Add package pyautd3-link-soem/29.0.0rc16/pyautd3_link_soem-29.0.0rc16-py3-none-manylinux1_x86_64.whl

---
+++ packages/pyautd3-link-soem/pyautd3_link_soem-29.0.0rc16-py3-none-manylinux1_x86_64.whl/pyautd3_link_soem/__init__.py
@@ -0,0 +1,27 @@
+import pyautd3
+
+from pyautd3_link_soem.native_methods.autd3capi_link_soem import NativeMethods as Soem
+from pyautd3_link_soem.native_methods.autd3capi_link_soem import ProcessPriority, SyncMode, TimerStrategy
+
+from .adapter import EtherCATAdapter
+from .local import SOEM
+from .remote import RemoteSOEM
+from .status import Status
+from .thread_priority import ThreadPriority
+
+pyautd3._ext_tracing_init.append(lambda: Soem().link_soem_tracing_init())
+
+
+__all__ = [
+    "SOEM",
+    "EtherCATAdapter",
+    "ProcessPriority",
+    "RemoteSOEM",
+    "Status",
+    "SyncMode",
+    "ThreadPriority",
+    "TimerStrategy",
+]
+
+
+__version__ = "29.0.0rc16"
