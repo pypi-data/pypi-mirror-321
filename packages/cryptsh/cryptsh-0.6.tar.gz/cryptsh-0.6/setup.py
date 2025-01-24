68ce5b3e88434099d9db4dacbad92c16b39d4c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 20:05:06 +0000

    Add package cryptsh/0.6/cryptsh-0.6.tar.gz

---
+++ packages/cryptsh/cryptsh-0.6.tar.gz/cryptsh-0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cryptsh",  # Choose a unique package name
+    version="0.6",
+    packages=find_packages(),
+    install_requires=[  # Dependencies that your app needs
+        "python-binance",
+        "tabulate",
+        "keyboard",
+        "psycopg2",
+        "setuptools",
+        "supabase",
+        
+    ],
+    entry_points={  # If your script is executable
+        "console_scripts": [
+            "cryptsh = cryptsh.main:main",  # Adjust the module name and function
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        'cryptsh': ['intents.json'],  # Include intents.json file in the package
+    },
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[  # Categorize your package
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
