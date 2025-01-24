3a4027a8e86513d2c2e342833368c84bc4fd24f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:36 +0000

    Add package nonebot-plugin-bilichat/6.0.8/nonebot_plugin_bilichat-6.0.8-py3-none-any.whl

---
+++ packages/nonebot-plugin-bilichat/nonebot_plugin_bilichat-6.0.8-py3-none-any.whl/nonebot_plugin_bilichat/request_api/__init__.py
@@ -0,0 +1,37 @@
+import random
+import sys
+
+from nonebot.log import logger
+from yarl import URL
+
+from nonebot_plugin_bilichat.config import config
+
+from .base import RequestAPI
+
+request_apis: list[RequestAPI] = []
+for api in config.api.request_api:
+    if api.enabled:
+        try:
+            request_api = RequestAPI(URL(api.api), api.token, api.weight, api.note)
+            request_apis.append(request_api)
+            logger.info(f"API {api.api} 初始化成功, 权重: {api.weight}, 备注: {api.note}")
+        except Exception as e:
+            logger.exception(f"API {api.api} 初始化失败, 跳过: {e}")
+    else:
+        logger.warning(f"API {api.api} 未启用")
+
+if config.api.local_api_config is not None and config.api.local_api_config.enable:
+    from .local import LOCAL_REQUEST_API_PATH, LOCAL_REQUEST_API_TOKEN
+
+    request_apis.append(
+        RequestAPI(URL(LOCAL_REQUEST_API_PATH), LOCAL_REQUEST_API_TOKEN, 0, "本地 API", skip_version_checking=True)
+    )
+
+if not request_apis:
+    [logger.error("未找到可用 API!!!!!!!!!!!!!!!!!") for _ in range(10)]
+    sys.exit()
+
+def get_request_api() -> RequestAPI:
+    if not request_apis:
+        raise RuntimeError("未找到可用的 Bilichat API 服务, 请添加至少一个 API 服务")
+    return random.choice(request_apis)
