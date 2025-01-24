eba920da425b81821bded1ba0d3c42ab625b344d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:16 +0000

    Add package lu77U-SHA256/1.1.5/lu77U_SHA256-1.1.5-py3-none-any.whl

---
+++ packages/lu77U-SHA256/lu77U_SHA256-1.1.5-py3-none-any.whl/lu77U_SHA256/Custom_Print.py
@@ -0,0 +1,13 @@
+import sys
+import time
+
+class CustomPrint:
+    def __init__(self, text):
+        self.text = text
+
+    def custom_print(self):
+        for char in self.text:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(0.075)
+        print()
