437bf98c84c027568327ada941f9efa8418e1d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 21:04:31 +0000

    Add package vizmath/0.0.39/vizmath-0.0.39-py3-none-any.whl

---
+++ packages/vizmath/vizmath-0.0.39-py3-none-any.whl/vizmath/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023-2024, Nick Gerend
+# This file is part of the vizmath library, distributed under a Dual License: Non-Commercial Use and Commercial Use. See LICENSE-NC and LICENSE-COM for details.
+__version__ = "0.0.39"
+from vizmath.functions import *
+from vizmath.draw import *
+from vizmath.radial_treemap import *
+from vizmath.crystal_bar_chart import *
+from vizmath.beeswarm import *
+from vizmath.quadtile_chart import *
