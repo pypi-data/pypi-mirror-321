e5a81f8c89034b54e65de1b90ed0cabab58d9e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 18:06:05 +0000

    Add package namespaxe/0.1.6/namespaxe-0.1.6.tar.gz

---
+++ packages/namespaxe/namespaxe-0.1.6.tar.gz/namespaxe-0.1.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='namespaxe',
+    version='0.1.6',
+    author='Gabriel Nzilantuzu',
+    author_email='gabrielnzilantuzu@pyincorporation.com',
+    description='A command-line tool for interacting with cloud services.',
+    long_description=open('../README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/pyincorporation-com/namespaxe-cli',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'click',
+        'pyyaml',
+        'tabulate',
+    ],
+    entry_points={
+        'console_scripts': [
+            'namespaxe = namespaxe.cli:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
