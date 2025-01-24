9547d1dfb44d0294023f33bf9aa517ebc51f20a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 23:05:05 +0000

    Add package tach/0.21.0/tach-0.21.0-cp37-abi3-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/tach/tach-0.21.0-cp37-abi3-manylinux_2_17_i686.manylinux2014_i686.whl/tach/mod.py
@@ -0,0 +1,118 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from tach import errors
+from tach import filesystem as fs
+from tach.colors import BCOLORS
+from tach.filesystem import build_project_config_path
+from tach.interactive import (
+    InteractiveModuleConfiguration,
+    get_selected_modules_interactive,
+)
+from tach.parsing import dump_project_config_to_toml
+
+if TYPE_CHECKING:
+    from tach.extension import ProjectConfig
+
+
+def update_modules(
+    project_config: ProjectConfig,
+    project_root: Path,
+    selected_source_roots: list[Path],
+    selected_modules: list[Path],
+    selected_utilities: list[Path],
+):
+    if set(project_config.source_roots) != set(selected_source_roots):
+        # Only assign to this field if it has changed,
+        # since the project config writes any field that
+        # has been touched out to TOML.
+        project_config.source_roots = [
+            source_root.relative_to(project_root)
+            for source_root in selected_source_roots
+        ]
+
+    module_paths = [
+        fs.file_to_module_path(
+            source_roots=tuple(selected_source_roots),
+            file_path=selected_module_file_path,
+        )
+        for selected_module_file_path in selected_modules
+    ]
+    utility_paths = [
+        fs.file_to_module_path(
+            source_roots=tuple(selected_source_roots),
+            file_path=selected_utility_file_path,
+        )
+        for selected_utility_file_path in selected_utilities
+    ]
+    project_config.set_modules(module_paths=[*module_paths, *utility_paths])
+    project_config.mark_utilities(utility_paths=utility_paths)
+
+    project_config_path = build_project_config_path(project_root)
+    config_toml_content = dump_project_config_to_toml(project_config)
+    fs.write_file(project_config_path, config_toml_content, root=project_root)
+
+
+@dataclass
+class ValidationResult:
+    ok: bool
+    errors: list[str] = field(default_factory=list)
+
+
+def validate_configuration(
+    configuration: InteractiveModuleConfiguration,
+) -> ValidationResult:
+    errors: list[str] = []
+    for module_path in configuration.module_paths:
+        module_path = Path(module_path).resolve()
+
+        if not any(
+            source_root in module_path.parents
+            for source_root in configuration.source_roots
+        ):
+            # This module exists outside of the source root
+            # This is not allowed and should be reported as a configuration error
+            errors.append(
+                f"Module '{module_path}' is not contained within any source root: {[str(root) for root in configuration.source_roots]}"
+            )
+    return ValidationResult(ok=not errors, errors=errors)
+
+
+def mod_edit_interactive(
+    project_root: Path,
+    project_config: ProjectConfig,
+    exclude_paths: list[str],
+    depth: int | None = 1,
+) -> tuple[bool, list[str]]:
+    if not Path(project_root).is_dir():
+        raise errors.TachSetupError(f"The path {project_root} is not a directory.")
+
+    interactive_module_configuration = get_selected_modules_interactive(
+        path=project_root,
+        project_config=project_config,
+        depth=depth,
+        exclude_paths=exclude_paths,
+    )
+    if interactive_module_configuration is not None:
+        validation_result = validate_configuration(interactive_module_configuration)
+        if validation_result.errors:
+            return False, [
+                f"{BCOLORS.FAIL}Validation error: {BCOLORS.WARNING}{error}{BCOLORS.ENDC}"
+                for error in validation_result.errors
+            ]
+        update_modules(
+            project_config=project_config,
+            project_root=project_root,
+            selected_source_roots=interactive_module_configuration.source_roots,
+            selected_modules=interactive_module_configuration.module_paths,
+            selected_utilities=interactive_module_configuration.utility_paths,
+        )
+        return True, []
+    else:
+        return False, [f"{BCOLORS.OKCYAN}No changes saved.{BCOLORS.ENDC}"]
+
+
+__all__ = ["mod_edit_interactive"]
---
+++ packages/tach/tach-0.21.0-cp37-abi3-manylinux_2_17_i686.manylinux2014_i686.whl/tach/show.py
@@ -0,0 +1,165 @@
+from __future__ import annotations
+
+import json
+from copy import copy
+from json.decoder import JSONDecodeError
+from typing import TYPE_CHECKING
+from urllib import error, request
+
+from tach import filesystem as fs
+from tach.constants import GAUGE_API_BASE_URL
+from tach.extension import DependencyConfig, ModuleConfig, ProjectConfig
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    import pydot  # type: ignore
+
+
+def generate_show_url(
+    project_root: Path,
+    project_config: ProjectConfig,
+    included_paths: list[Path] | None = None,
+) -> str | None:
+    if included_paths:
+        project_config = filter_project_config(
+            project_config, project_root=project_root, included_paths=included_paths
+        )
+    json_data = project_config.model_dump_json()
+    json_bytes = json_data.encode("utf-8")
+    req = request.Request(
+        f"{GAUGE_API_BASE_URL}/api/show/graph/1.3",
+        data=json_bytes,
+        headers={"Content-Type": "application/json"},
+    )
+
+    try:
+        # Send the request and read the response
+        with request.urlopen(req) as response:
+            response_data = response.read().decode("utf-8")
+            response_json = json.loads(response_data)
+            uid = response_json.get("uid")
+            return f"{GAUGE_API_BASE_URL}/show?uid={uid}"
+    except (UnicodeDecodeError, JSONDecodeError, error.URLError) as e:
+        print(f"Error: {e}")
+        return None
+
+
+def module_path_is_included_in_paths(
+    source_roots: tuple[Path, ...], module_path: str, included_paths: list[Path] | None
+) -> bool:
+    if included_paths is None:
+        return True
+    module_fs_path = fs.module_to_pyfile_or_dir_path(source_roots, module_path)
+    if not module_fs_path:
+        return False
+    for included_path in included_paths:
+        if included_path in module_fs_path.parents or included_path == module_fs_path:
+            return True
+    return False
+
+
+def filter_project_config(
+    project_config: ProjectConfig,
+    project_root: Path,
+    included_paths: list[Path],
+) -> ProjectConfig:
+    source_roots = tuple(
+        map(lambda source_root: project_root / source_root, project_config.source_roots)
+    )
+    included_paths = list(map(lambda path: project_root / path, included_paths))
+    all_modules = copy(project_config.modules)
+    project_config.set_modules([])
+    filtered_modules: list[ModuleConfig] = []
+
+    for module in all_modules:
+        module_is_included = module_path_is_included_in_paths(
+            source_roots, module.path, included_paths
+        )
+        if module_is_included:
+            filtered_modules.append(module)
+        else:
+            filtered_dependencies: list[DependencyConfig] = []
+            for dependency in module.depends_on:
+                dependency_is_included = module_path_is_included_in_paths(
+                    source_roots, dependency.path, included_paths
+                )
+                if dependency_is_included:
+                    filtered_dependencies.append(dependency)
+            if filtered_dependencies:
+                module.depends_on = filtered_dependencies
+                filtered_modules.append(module)
+
+    return ProjectConfig.with_modules(project_config, filtered_modules)
+
+
+def generate_module_graph_dot_file(
+    project_root: Path,
+    project_config: ProjectConfig,
+    output_filepath: Path,
+    included_paths: list[Path] | None = None,
+) -> None:
+    # Local import because networkx takes about ~100ms to load
+    import networkx as nx
+
+    graph = nx.DiGraph()  # type: ignore
+
+    def upsert_edge(graph: nx.DiGraph, module: str, dependency: str) -> None:  # type: ignore
+        if module not in graph:
+            graph.add_node(module)  # type: ignore
+        if dependency not in graph:
+            graph.add_node(dependency)  # type: ignore
+        graph.add_edge(module, dependency)  # type: ignore
+
+    included_paths = (
+        list(map(lambda path: project_root / path, included_paths))
+        if included_paths
+        else None
+    )
+    if included_paths:
+        modules = filter_project_config(
+            project_config, project_root, included_paths
+        ).modules
+    else:
+        modules = project_config.modules
+
+    for module in modules:
+        for dependency in module.depends_on:
+            upsert_edge(graph, module.path, dependency.path)  # type: ignore
+
+    pydot_graph: pydot.Dot = nx.nx_pydot.to_pydot(graph)  # type: ignore
+    dot_data: str = pydot_graph.to_string()  # type: ignore
+
+    output_filepath.write_text(dot_data)  # type: ignore
+
+
+def generate_module_graph_mermaid(
+    project_root: Path,
+    project_config: ProjectConfig,
+    output_filepath: Path,
+    included_paths: list[Path] | None = None,
+):
+    if included_paths:
+        project_config = filter_project_config(
+            project_config, project_root, included_paths
+        )
+    edges: list[str] = []
+    isolated: list[str] = []
+    for module in project_config.modules:
+        for dependency in module.depends_on:
+            edges.append(
+                f"    {module.path.strip('<>')} --> {dependency.path.strip('<>')}"
+            )
+        if not module.depends_on:
+            isolated.append(f"    {module.path.strip('<>')}")
+
+    mermaid_graph = "graph TD\n" + "\n".join(edges) + "\n" + "\n".join(isolated)
+
+    output_filepath.write_text(mermaid_graph)
+
+
+__all__ = [
+    "generate_show_url",
+    "generate_module_graph_dot_file",
+    "generate_module_graph_mermaid",
+]
