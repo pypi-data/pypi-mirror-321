3a93ed72aed845ee4cb94b59bbe25486507139eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:59:11 +0000

    Add package rdee-easyarg/25.1.16/rdee_easyarg-25.1.16.tar.gz

---
+++ packages/rdee-easyarg/rdee_easyarg-25.1.16.tar.gz/rdee_easyarg-25.1.16/utest/utest.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+try:
+    import easyarg
+except:
+    print("Install this package first")
+    raise
+
+ea = easyarg.EasyArg()
+
+
+@ea.command()
+def add(x: int, y: int = 0) -> int:
+    """Add two numbers"""
+    print(f"{x + y=}")
+
+
+@ea.command()
+def mul(a: float | str, B: float, c: float = 1.0) -> float:
+    """
+    Multiply numbers
+
+    Last Update: @2025-01-15 22:09:36
+    """
+    print(f"{(a * B * c)=}")
+
+
+@ea.command(desc="divide calculation")
+def div(q1: float | str, Q2: float, q3: float = 1.0) -> float:
+    print(f"{(q1 / Q2 / q3)=}")
+
+
+@ea.command()
+def div2(q1: float | str, Q2: float, q3: float = 1.0) -> float:
+    print(f"{(q1 / Q2 / q3)=}")
+
+
+if __name__ == "__main__":
+    ea.parse()
