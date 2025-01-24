ada011f6691ef16413c862795c3ce94f3b334088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:41 +0000

    Add package pyhelmgen/1.4/pyhelmgen-1.4-py3-none-any.whl

---
+++ packages/pyhelmgen/pyhelmgen-1.4-py3-none-any.whl/HelmFromComposer/__init__.py
@@ -0,0 +1,10 @@
+# package meta data
+__version__ = '1.4' 
+__author__ = 'Nick Caravias'
+__email__ = 'nick.caravias@gmail.com'
+
+# imports
+from .HelmFromComposer import HelmFromComposer
+
+# public interface
+__all__ = ['HelmFromComposer']
