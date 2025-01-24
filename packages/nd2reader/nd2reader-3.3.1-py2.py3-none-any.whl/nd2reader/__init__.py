c4844066a70d2e26f71c9a389148f66edca0940e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:02:23 +0000

    Add package nd2reader/3.3.1/nd2reader-3.3.1-py2.py3-none-any.whl

---
+++ packages/nd2reader/nd2reader-3.3.1-py2.py3-none-any.whl/nd2reader/__init__.py
@@ -0,0 +1,13 @@
+from os import path
+from nd2reader.reader import ND2Reader
+from nd2reader.legacy import Nd2
+
+try:
+    import importlib.metadata as importlib_metadata
+except:
+    import importlib_metadata
+
+try:
+    __version__ = importlib_metadata.version(__name__)
+except:
+    print('Unable to read version number')
