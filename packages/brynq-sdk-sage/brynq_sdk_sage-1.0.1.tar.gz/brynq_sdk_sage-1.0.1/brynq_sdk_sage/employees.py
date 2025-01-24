d93b7997b3c36a4cc91072e9a79df68a71bb8711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 19:04:57 +0000

    Add package brynq-sdk-sage/1.0.1/brynq_sdk_sage-1.0.1.tar.gz

---
+++ packages/brynq-sdk-sage/brynq_sdk_sage-1.0.1.tar.gz/brynq_sdk_sage-1.0.1/brynq_sdk_sage/employees.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import requests
+
+
+class Employees:
+    def __init__(self, headers, base_url):
+        self.headers = headers
+        self.base_url = base_url
+
+    def get(self) -> pd.DataFrame:
+        resp = requests.get(f"{self.base_url}employee/employees", headers=self.headers)
+        resp.raise_for_status()
+        data = resp.json()
+        df = pd.DataFrame(data)
+
+        return df
+
+    def new(self, data) -> requests.Response:
+        resp = requests.post(url=f"{self.base_url}employee/employees",
+                             headers=self.headers,
+                             json=data)
+        resp.raise_for_status()
+
+        return resp
+
+    def edit(self, data) -> requests.Response:
+        resp = requests.put(url=f"{self.base_url}employee/employees",
+                            headers=self.headers,
+                            json=data)
+        resp.raise_for_status()
+
+        return resp
