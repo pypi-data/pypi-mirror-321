afa083d62a5150166725c041d7d786ee4cc42796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:05:04 +0000

    Add package boilrpy/0.3.0/boilrpy-0.3.0-py3-none-any.whl

---
+++ packages/boilrpy/boilrpy-0.3.0-py3-none-any.whl/boilrpy/file_generators/__init__.py
@@ -0,0 +1,13 @@
+from .readme_generator import ReadmeGenerator
+from .license_generator import LicenseGenerator
+from .gitignore_generator import GitignoreGenerator
+from .changelog_generator import ChangelogGenerator
+from .main_file_generator import MainFileGenerator
+
+__all__ = [
+    'ReadmeGenerator',
+    'LicenseGenerator',
+    'GitignoreGenerator',
+    'ChangelogGenerator',
+    'MainFileGenerator'
+]
