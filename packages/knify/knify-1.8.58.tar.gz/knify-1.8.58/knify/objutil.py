3ba7b6be05ffe2baf0f3937cda424cd30b434eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:10 +0000

    Add package knify/1.8.58/knify-1.8.58.tar.gz

---
+++ packages/knify/knify-1.8.58.tar.gz/knify-1.8.58/knify/objutil.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+
+
+def default_if_none(obj: object, default_value: object) -> object:
+    return default_value if obj is None or obj == '' else obj
+
+
+def has_keys(obj: object) -> bool:
+    return obj is not None and len(obj.keys()) > 0
