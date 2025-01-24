382f117ede47aff98dbba160ddcee5b21bf44fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:28 +0000

    Add package ucxdsa/2025.1.15/ucxdsa-2025.1.15.tar.gz

---
+++ packages/ucxdsa/ucxdsa-2025.1.15.tar.gz/ucxdsa-2025.1.15/src/dsa/__init__.py
@@ -0,0 +1,7 @@
+'''
+dsa package for UC Berkeley Extension's Computer Science X404.1 Data Structures and Algorithms class
+
+Last Revision: 12/24/2024
+'''
+version = '2024.12.24'
+__version__ = '2024.12.24'
---
+++ packages/ucxdsa/ucxdsa-2025.1.15.tar.gz/ucxdsa-2025.1.15/src/dsa/sorttools.py
@@ -0,0 +1,48 @@
+""" Module to access functions for sort benchmarking. """
+import random
+
+def rand_int_array(n: int, maxnum: int) -> list:
+    """ 
+    Return an array of n integers of random numbers from 0 to maxnum.
+
+    Args:
+        n (int): The number of integers to generate.
+        maxnum (int): The maximum number in a range (0-maxnum inclusive).
+    Returns:
+        Array of n integers of random numbers from 0 to maxnum.
+    """
+    array = [None] * n
+    for i in range(n):
+        array[i] = random.randint(0, maxnum)
+    return array
+
+def filled_array(n: int) -> list:
+    """ 
+    Return an array filled with integers from 0 to n-1.
+
+    Args:
+        n (int): the number of integers to generate.
+
+    Returns:
+        Array filled with integers from 0 to n-1.
+    """
+    array = [None] * n
+    for i in range(n):
+        array[i] = i
+    return array    
+
+def shuffle_array(n: int) -> list:
+    """ 
+    Return a shuffled array filled with integers from 0 to n-1.
+
+    Args:
+        n (int): The number of integers to generate.
+    Returns:
+        Array shuffled with integers from 0 to n-1.
+    """
+    array = filled_array(n)
+    for i in range(n):
+        r = random.randint(i, n-1)
+        array[i], array[r] = array[r], array[i]
+    return array
+
