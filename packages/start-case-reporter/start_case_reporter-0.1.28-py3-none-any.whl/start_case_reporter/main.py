9fc27b64d497762a9ab8a5a0510b49df25b1ff2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:05:41 +0000

    Add package start-case-reporter/0.1.28/start_case_reporter-0.1.28-py3-none-any.whl

---
+++ packages/start-case-reporter/start_case_reporter-0.1.28-py3-none-any.whl/start_case_reporter/main.py
@@ -0,0 +1,66 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QHBoxLayout
+
+
+def prepare_form_data(prefix, fullname, position):
+    print(f"Prefix: {prefix}, Full Name: {fullname}, Position: {position}")
+
+
+class MainForm(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Case Reporter")
+        self.setGeometry(100, 100, 500, 100)
+
+        layout = QVBoxLayout()
+
+        auth_zone = QHBoxLayout()
+        self.txt_username = QLineEdit(self)
+        self.txt_username.setPlaceholderText("Username")
+        self.txt_password = QLineEdit(self)
+        self.txt_password.setEchoMode(QLineEdit.EchoMode.Password)
+        self.txt_password.setPlaceholderText("Password")
+
+        auth_zone.addWidget(self.txt_username)
+        auth_zone.addWidget(self.txt_password)
+
+        person_1 = QHBoxLayout()
+        self.txt_prefix_1 = QLineEdit(self)
+        self.txt_prefix_1.setPlaceholderText("คำนำหน้า")
+        self.txt_fullname_1 = QLineEdit(self)
+        self.txt_fullname_1.setPlaceholderText("ชื่อ - นามสกุล")
+        self.txt_position_1 = QLineEdit(self)
+        self.txt_position_1.setPlaceholderText("ตำแหน่ง")
+
+        person_1.addWidget(self.txt_prefix_1)
+        person_1.addWidget(self.txt_fullname_1)
+        person_1.addWidget(self.txt_position_1)
+
+        person_1.setSpacing(10)
+
+        submit_button = QPushButton("Start process", self)
+        submit_button.clicked.connect(self.on_submit_click)
+
+        layout.addLayout(auth_zone)
+        layout.addLayout(person_1)
+        layout.addWidget(submit_button)
+
+        self.setLayout(layout)
+
+    def on_submit_click(self):
+        txt_prefix_1 = self.txt_prefix_1.text()
+        txt_fullname_1 = self.txt_fullname_1.text()
+        txt_position_1 = self.txt_position_1.text()
+        prepare_form_data(txt_prefix_1, txt_fullname_1, txt_position_1)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainForm()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
