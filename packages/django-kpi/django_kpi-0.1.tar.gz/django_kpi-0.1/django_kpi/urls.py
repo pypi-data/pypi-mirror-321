3890dc7436fce0e7a743a0815e9daeacfd49ff77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:59:57 +0000

    Add package django-kpi/0.1/django_kpi-0.1.tar.gz

---
+++ packages/django-kpi/django_kpi-0.1.tar.gz/django_kpi-0.1/django_kpi/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'kpi'
+
+urlpatterns = [
+    path('get-model-fields/', views.get_model_fields, name='get_model_fields'),
+    path('get-field-values/', views.get_field_values, name='get_field_values'),
+]
