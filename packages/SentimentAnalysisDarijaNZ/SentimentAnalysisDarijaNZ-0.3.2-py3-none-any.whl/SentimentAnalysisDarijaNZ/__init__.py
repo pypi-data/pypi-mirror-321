c202a6f8a174b2af9ebe6dd6f327daac32fb912d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 18:05:47 +0000

    Add package SentimentAnalysisDarijaNZ/0.3.2/SentimentAnalysisDarijaNZ-0.3.2-py3-none-any.whl

---
+++ packages/SentimentAnalysisDarijaNZ/SentimentAnalysisDarijaNZ-0.3.2-py3-none-any.whl/SentimentAnalysisDarijaNZ/__init__.py
@@ -0,0 +1,4 @@
+from .similarity import levenshtein_similarity, sequence_similarity, phonetic_similarity ,find_similar_brands
+from .filtering import find_similar_brands, filterbrand, filterproduit
+from .sentiment import sentiment_analysis8, calul_moyen_scores,sentiment_analysis_darija,bigdatapositif,bigdatanegatif
+from .utils import csv_to_list, get_first_column ,ratio
