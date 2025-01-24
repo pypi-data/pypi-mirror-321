5c7767131eb6ef368d04e6f26763e11fd5364980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 22:04:46 +0000

    Add package abacusai/1.4.27/abacusai-1.4.27.tar.gz

---
+++ packages/abacusai/abacusai-1.4.27.tar.gz/abacusai-1.4.27/abacusai/code_edits.py
@@ -0,0 +1,33 @@
+from .return_class import AbstractApiClass
+
+
+class CodeEdits(AbstractApiClass):
+    """
+        A code edit response from an LLM
+
+        Args:
+            client (ApiClient): An authenticated API Client instance
+            codeEdits (list[codeedit]): The code changes to be applied.
+    """
+
+    def __init__(self, client, codeEdits=None):
+        super().__init__(client, None)
+        self.code_edits = codeEdits
+        self.deprecated_keys = {}
+
+    def __repr__(self):
+        repr_dict = {f'code_edits': repr(self.code_edits)}
+        class_name = "CodeEdits"
+        repr_str = ',\n  '.join([f'{key}={value}' for key, value in repr_dict.items(
+        ) if getattr(self, key, None) is not None and key not in self.deprecated_keys])
+        return f"{class_name}({repr_str})"
+
+    def to_dict(self):
+        """
+        Get a dict representation of the parameters in this class
+
+        Returns:
+            dict: The dict value representation of the class parameters
+        """
+        resp = {'code_edits': self.code_edits}
+        return {key: value for key, value in resp.items() if value is not None and key not in self.deprecated_keys}
