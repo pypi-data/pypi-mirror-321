b1ccf8e33307bbdd935e7a19f216ea58cc88affd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:01:03 +0000

    Add package multiversx-sdk-cli/9.10.1/multiversx_sdk_cli-9.10.1-py3-none-any.whl

---
+++ packages/multiversx-sdk-cli/multiversx_sdk_cli-9.10.1-py3-none-any.whl/multiversx_sdk_cli/cosign_transaction.py
@@ -0,0 +1,31 @@
+from typing import Any, Dict
+
+import requests
+from multiversx_sdk import TransactionsConverter
+
+from multiversx_sdk_cli.errors import GuardianServiceError
+from multiversx_sdk_cli.interfaces import ITransaction
+
+
+def cosign_transaction(transaction: ITransaction, service_url: str, guardian_code: str) -> ITransaction:
+    tx_converter = TransactionsConverter()
+    payload = {
+        "code": f"{guardian_code}",
+        "transaction": tx_converter.transaction_to_dictionary(transaction)
+    }
+
+    url = f"{service_url}/sign-transaction"
+    response = requests.post(url, json=payload)
+    check_for_guardian_error(response.json())
+
+    tx_as_dict = response.json()["data"]["transaction"]
+    transaction.guardian_signature = bytes.fromhex(tx_as_dict["guardianSignature"])
+
+    return transaction
+
+
+def check_for_guardian_error(response: Dict[str, Any]):
+    error = response["error"]
+
+    if error:
+        raise GuardianServiceError(error)
