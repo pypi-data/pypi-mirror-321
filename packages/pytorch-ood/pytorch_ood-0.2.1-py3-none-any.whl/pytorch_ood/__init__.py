944b263f0a4e2bb3d074130104bff3427d3b31be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 18:05:43 +0000

    Add package pytorch-ood/0.2.1/pytorch_ood-0.2.1-py3-none-any.whl

---
+++ packages/pytorch-ood/pytorch_ood-0.2.1-py3-none-any.whl/pytorch_ood/__init__.py
@@ -0,0 +1,8 @@
+"""
+PyTorch Out-of-Distribution Detection
+"""
+__version__ = "0.2.1"
+
+from . import api, dataset, detector, loss, model, utils
+
+__all__ = ["dataset", "detector", "loss", "model", "utils", "api", "__version__"]
