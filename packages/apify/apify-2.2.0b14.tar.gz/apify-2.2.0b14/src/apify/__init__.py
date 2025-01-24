454a720321d49a40cf225dccab34507af46fdf4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:06:38 +0000

    Add package apify/2.2.0b14/apify-2.2.0b14.tar.gz

---
+++ packages/apify/apify-2.2.0b14.tar.gz/apify-2.2.0b14/src/apify/__init__.py
@@ -0,0 +1,24 @@
+from importlib import metadata
+
+from apify_shared.consts import WebhookEventType
+from crawlee import Request
+from crawlee.events._types import Event
+
+from apify._actor import Actor
+from apify._configuration import Configuration
+from apify._models import Webhook
+from apify._proxy_configuration import ProxyConfiguration, ProxyInfo
+
+__version__ = metadata.version('apify')
+
+__all__ = [
+    'Actor',
+    'Configuration',
+    'Event',
+    'ProxyConfiguration',
+    'ProxyInfo',
+    'Request',
+    'Webhook',
+    'WebhookEventType',
+    '__version__',
+]
