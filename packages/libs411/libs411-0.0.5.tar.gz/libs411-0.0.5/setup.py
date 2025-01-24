6224233d1bdfe619be53a7fb48d89b0e5800f421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:04:40 +0000

    Add package libs411/0.0.5/libs411-0.0.5.tar.gz

---
+++ packages/libs411/libs411-0.0.5.tar.gz/libs411-0.0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libs411',
+    version='0.0.5',
+    author='fulei',
+    description='a project for development',
+    packages=find_packages()
+)
