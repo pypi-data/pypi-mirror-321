cda6e721323bd38d1e473f8b1b580f70a87e47db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 18:06:23 +0000

    Add package ffun/1.16.5/ffun-1.16.5-py3-none-any.whl

---
+++ packages/ffun/ffun-1.16.5-py3-none-any.whl/ffun/user_settings/values.py
@@ -0,0 +1,26 @@
+import decimal
+
+from ffun.core.register import Entity, Register
+from ffun.user_settings.types import Type
+
+
+class Value(Entity):
+    def __init__(
+        self,
+        key: int,
+        name: str,
+        type: Type,
+        default: str | int | bool | decimal.Decimal | None,
+        description: str | None,
+    ) -> None:
+        super().__init__(key=key)
+        self.name = name
+        self.type = type
+        self.default = default
+        self.description = description
+
+
+SettingsRegister = Register[Value]
+
+
+user_settings: SettingsRegister = Register()
