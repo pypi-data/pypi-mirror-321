60cc9bfb5daafae5ecf1a48b6f4277cd2f12bb2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 23:04:47 +0000

    Add package large-image/1.30.7.dev14/large_image-1.30.7.dev14.tar.gz

---
+++ packages/large-image/large_image-1.30.7.dev14.tar.gz/large_image-1.30.7.dev14/large_image/exceptions.py
@@ -0,0 +1,43 @@
+import errno
+
+
+class TileGeneralError(Exception):
+    pass
+
+
+class TileSourceError(TileGeneralError):
+    pass
+
+
+class TileSourceAssetstoreError(TileSourceError):
+    pass
+
+
+class TileSourceXYZRangeError(TileSourceError):
+    pass
+
+
+class TileSourceInefficientError(TileSourceError):
+    pass
+
+
+class TileSourceMalformedError(TileSourceError):
+    pass
+
+
+class TileSourceFileNotFoundError(TileSourceError, FileNotFoundError):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(errno.ENOENT, *args, **kwargs)
+
+
+class TileCacheError(TileGeneralError):
+    pass
+
+
+class TileCacheConfigurationError(TileCacheError):
+    pass
+
+
+TileGeneralException = TileGeneralError
+TileSourceException = TileSourceError
+TileSourceAssetstoreException = TileSourceAssetstoreError
