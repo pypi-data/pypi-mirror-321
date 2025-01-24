0d87703dbf0effceb5b381fa5d131bb7f08f1e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:00:25 +0000

    Add package hacs-frontend/20250115072642/hacs_frontend-20250115072642.tar.gz

---
+++ packages/hacs-frontend/hacs_frontend-20250115072642.tar.gz/hacs_frontend-20250115072642/setup.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+setup(
+    name="hacs-frontend",
+    version="20250115072642",
+    description="The HACS frontend",
+    url="https://github.com/hacs/frontend",
+    author="Joakim Sorensen",
+    author_email="ludeeus@gmail.com",
+    packages=find_packages(include=["hacs_frontend", "hacs_frontend.*"]),
+    include_package_data=True,
+    zip_safe=False,
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+)
