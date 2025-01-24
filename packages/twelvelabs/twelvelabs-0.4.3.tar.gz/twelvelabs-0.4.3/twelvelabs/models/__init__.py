a4a940d8811ad4a5cbb492f63211907b454e493d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:16 +0000

    Add package twelvelabs/0.4.3/twelvelabs-0.4.3.tar.gz

---
+++ packages/twelvelabs/twelvelabs-0.4.3.tar.gz/twelvelabs-0.4.3/twelvelabs/models/__init__.py
@@ -0,0 +1,66 @@
+from ._base import Object, ObjectWithTimestamp, PageInfo, TokenPageInfo, RootModelList
+from .index import Index, IndexListWithPagination
+from .task import (
+    Task,
+    TaskStatus,
+    TaskListWithPagination,
+    TransferImportResponse,
+    TransferImportStatusResponse,
+    TransferImportLog,
+)
+from .video import Video, VideoValue, VideoListWithPagination
+from .search import (
+    SearchData,
+    SearchResult,
+    CombinedSearchResult,
+)
+from .generate import (
+    GenerateOpenEndedTextResult,
+    GenerateSummarizeChapterResult,
+    GenerateSummarizeHighlightResult,
+    GenerateSummarizeResult,
+    GenerateOpenEndedTextStreamResult,
+)
+from .embed import (
+    CreateEmbeddingsResult,
+    EmbeddingsTask,
+    EmbeddingsTaskStatus,
+    CreateEmbeddingsTaskVideoParams,
+    EmbeddingsTaskListWithPagination,
+    Embedding,
+    SegmentEmbedding,
+)
+
+__all__ = [
+    "Object",
+    "ObjectWithTimestamp",
+    "PageInfo",
+    "TokenPageInfo",
+    "Index",
+    "IndexListWithPagination",
+    "Task",
+    "TaskListWithPagination",
+    "TaskStatus",
+    "TransferImportResponse",
+    "TransferImportStatusResponse",
+    "TransferImportLog",
+    "Video",
+    "VideoValue",
+    "VideoListWithPagination",
+    "SearchData",
+    "SearchResult",
+    "CombinedSearchResult",
+    "GenerateOpenEndedTextResult",
+    "GenerateSummarizeChapterResult",
+    "GenerateSummarizeHighlightResult",
+    "GenerateSummarizeResult",
+    "GenerateOpenEndedTextStreamResult",
+    "CreateEmbeddingsResult",
+    "EmbeddingsTask",
+    "EmbeddingsTaskStatus",
+    "CreateEmbeddingsTaskVideoParams",
+    "EmbeddingsTaskListWithPagination",
+    "Embedding",
+    "SegmentEmbedding",
+    "RootModelList",
+]
---
+++ packages/twelvelabs/twelvelabs-0.4.3.tar.gz/twelvelabs-0.4.3/twelvelabs/resources/generate.py
@@ -0,0 +1,59 @@
+from typing import Union, List, Literal, Optional
+
+from ..resource import APIResource
+from .. import models
+from ..util import remove_none_values
+
+
+class Generate(APIResource):
+    def summarize(
+        self,
+        video_id: str,
+        type: Literal["summary", "chapter", "highlight"],
+        *,
+        prompt: Optional[str] = None,
+        temperature: Optional[float] = None,
+        **kwargs,
+    ) -> models.GenerateSummarizeResult:
+        json = {
+            "video_id": video_id,
+            "type": type,
+            "prompt": prompt,
+            "temperature": temperature,
+        }
+        res = self._post("summarize", json=remove_none_values(json), **kwargs)
+        return models.GenerateSummarizeResult(**res)
+
+    def text(
+        self,
+        video_id: str,
+        prompt: str,
+        *,
+        temperature: Optional[float] = None,
+        **kwargs,
+    ) -> models.GenerateOpenEndedTextResult:
+        json = {
+            "video_id": video_id,
+            "prompt": prompt,
+            "temperature": temperature,
+            "stream": False,
+        }
+        res = self._post("generate", json=json, **kwargs)
+        return models.GenerateOpenEndedTextResult(**res)
+
+    def text_stream(
+        self,
+        video_id: str,
+        prompt: str,
+        *,
+        temperature: Optional[float] = None,
+        **kwargs,
+    ) -> models.GenerateOpenEndedTextStreamResult:
+        json = {
+            "video_id": video_id,
+            "prompt": prompt,
+            "temperature": temperature,
+            "stream": True,
+        }
+        res = self._post("generate", json=json, stream=True, **kwargs)
+        return models.GenerateOpenEndedTextStreamResult(stream=res)
