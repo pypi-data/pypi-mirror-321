429d3597e827ea5764086655cb09aed5232ea6ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:58:14 +0000

    Add package LCPDelta/2.1.0/lcpdelta-2.1.0.tar.gz

---
+++ packages/LCPDelta/lcpdelta-2.1.0.tar.gz/lcpdelta-2.1.0/lcpdelta_python_package/src/lcp_delta/enact/services/plant_service.py
@@ -0,0 +1,10 @@
+def generate_plant_request(plant_id: str) -> dict:
+    return {"PlantId": plant_id}
+
+
+def generate_country_fuel_request(country_id: str, fuel_id: str) -> dict:
+    return {"Country": country_id, "Fuel": fuel_id}
+
+
+def process_country_fuel_response(response: dict) -> dict:
+    return response["data"]
