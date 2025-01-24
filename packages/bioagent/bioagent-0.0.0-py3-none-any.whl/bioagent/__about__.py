4b0356e5b65e9608385e1de7f428bc6c2453756a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:58:36 +0000

    Add package bioagent/0.0.0/bioagent-0.0.0-py3-none-any.whl

---
+++ packages/bioagent/bioagent-0.0.0-py3-none-any.whl/bioagent/__about__.py
@@ -0,0 +1,14 @@
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__version__",
+    "__author__",
+    "__license__",
+    "__url__",
+]
+
+__title__ = "bioagent"
+__summary__ = "BioAgent Library"
+__version__ = "0.0.0"
+__author__ = "Hugues Van Assel"
+__license__ = "MIT"
