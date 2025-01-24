de98847fa9f86add4fca910be612c31ab93758bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:58:27 +0000

    Add package bioviewer/0.2.1/bioviewer-0.2.1.tar.gz

---
+++ packages/bioviewer/bioviewer-0.2.1.tar.gz/bioviewer-0.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="bioviewer",  # Package name
+    version="0.2.1",    # Initial release version
+    author="Moritz Alkofer",
+    author_email="moritz.alkofer@protonmail.com",
+    description="This library is designed to build visualization tools for biosignals such as EEG or ECG.",
+    url="https://github.com/MoritzAlkofer/BIOViewer",
+    packages=find_packages(),
+)
