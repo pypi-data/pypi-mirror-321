1717d404e6caf8ffd6a3d6c1710e86ce3c1245a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:03:58 +0000

    Add package fx-shared-models/0.1.1/fx_shared_models-0.1.1.tar.gz

---
+++ packages/fx-shared-models/fx_shared_models-0.1.1.tar.gz/fx_shared_models-0.1.1/shared_models/system_settings/models/__init__.py
@@ -0,0 +1,10 @@
+from .base import SystemSetting
+from .email import EmailConfiguration, EmailTemplate, SystemEmail, EmailContent
+
+__all__ = [
+    'SystemSetting',
+    'EmailConfiguration',
+    'EmailTemplate',
+    'SystemEmail',
+    'EmailContent'
+] 
