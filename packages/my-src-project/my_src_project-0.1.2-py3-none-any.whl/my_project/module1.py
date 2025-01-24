d3eb4d57efefeb9d452b742d84f7c149c3b775dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:53:10 +0000

    Add package my-src-project/0.1.2/my_src_project-0.1.2-py3-none-any.whl

---
+++ packages/my-src-project/my_src_project-0.1.2-py3-none-any.whl/my_project/module1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+#
+# Copyright (C) 2025 - 2025 Guanhao Sun, Inc. All Rights Reserved 
+#
+# @Time    : 2025/1/15 9:52
+# @Author  : Guanhao Sun
+# @File    : module1.py
+# @IDE     : PyCharm
+
+
+def sieg_heil():
+    return "Heil wkk!\nsb wkk!"
