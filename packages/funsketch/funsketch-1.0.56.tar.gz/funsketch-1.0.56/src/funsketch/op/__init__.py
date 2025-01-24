6dd6c81fc0bb6133361a178b36bd8ce7a145338e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:56:57 +0000

    Add package funsketch/1.0.56/funsketch-1.0.56.tar.gz

---
+++ packages/funsketch/funsketch-1.0.56.tar.gz/funsketch-1.0.56/src/funsketch/op/__init__.py
@@ -0,0 +1,5 @@
+from .analyse import update_text_episode
+from .episode import sync_episode_data
+from .sketch import sync_sketch_data
+
+__all__ = ["sync_sketch_data", "sync_episode_data", "update_text_episode"]
