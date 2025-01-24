23d75b8ea26c8a749979ab42a9315a5d673c754d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 22:05:16 +0000

    Add package itsai-aspire/0.1.3/itsai_aspire-0.1.3.tar.gz

---
+++ packages/itsai-aspire/itsai_aspire-0.1.3.tar.gz/itsai_aspire-0.1.3/itsai/aspire/vanna/flask/_cache.py
@@ -0,0 +1,85 @@
+import uuid
+from abc import ABC, abstractmethod
+
+
+class Cache(ABC):
+    """
+    Define the interface for a cache that can be used to store data in a Flask app.
+    """
+
+    @abstractmethod
+    def generate_id(self, *args, **kwargs):
+        """
+        Generate a unique ID for the cache.
+        """
+        pass
+
+    @abstractmethod
+    def get(self, id, field):
+        """
+        Get a value from the cache.
+        """
+        pass
+
+    @abstractmethod
+    def get_all(self, field_list) -> list:
+        """
+        Get all values from the cache.
+        """
+        pass
+
+    @abstractmethod
+    def set(self, id, field, value):
+        """
+        Set a value in the cache.
+        """
+        pass
+
+    @abstractmethod
+    def delete(self, id):
+        """
+        Delete a value from the cache.
+        """
+        pass
+
+
+import json
+import pathlib
+
+
+class MemoryCache(Cache):
+    def __init__(self):
+        self.cache = {}
+
+    def _dump(self) -> None:
+        pathlib.Path('tmp.json').write_text(json.dumps(self.cache, indent=4))
+
+    def generate_id(self, *args, **kwargs):
+        return str(uuid.uuid4())
+
+    def set(self, id, field, value):
+        if id not in self.cache:
+            self.cache[id] = {}
+
+        self.cache[id][field] = value
+        # self._dump()
+
+    def get(self, id, field):
+        if id not in self.cache:
+            return None
+
+        if field not in self.cache[id]:
+            return None
+
+        return self.cache[id][field]
+
+    def get_all(self, field_list) -> list:
+        return [
+            {'id': id, **{field: self.get(id=id, field=field) for field in field_list}}
+            for id in self.cache
+        ]
+
+    def delete(self, id):
+        if id in self.cache:
+            del self.cache[id]
+        # self._dump()
