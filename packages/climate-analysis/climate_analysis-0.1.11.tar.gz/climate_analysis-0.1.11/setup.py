c9d8af268afce0d06bcbcee875a15570dca8ccfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:56:20 +0000

    Add package climate-analysis/0.1.11/climate_analysis-0.1.11.tar.gz

---
+++ packages/climate-analysis/climate_analysis-0.1.11.tar.gz/climate_analysis-0.1.11/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="climate_analysis",
+    version="0.1.11",
+    description="Climate analysis tools for generating maps and time-series",
+    author="Bijan Fallah",
+    author_email="bijan.fallah@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "xarray",
+        "dask",
+        "intake",
+        "intake-esem"
+        "matplotlib",
+        "cartopy",
+        "xesmf",
+        "requests_cache",
+        "seaborn",
+        "tqdm",
+        "gcsfs",
+    ],
+    entry_points={
+        "console_scripts": [
+            "generate-maps=climate_analysis.maps:main",
+            "generate-time-series=climate_analysis.time_series:main",
+        ],
+    },
+)
