b81edafe159d14b78c8fd79305e7c7126036fab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:03:36 +0000

    Add package cubed/0.20.0/cubed-0.20.0-py3-none-any.whl

---
+++ packages/cubed/cubed-0.20.0-py3-none-any.whl/cubed/random.py
@@ -0,0 +1,35 @@
+import random as pyrandom
+
+from numpy.random import Generator, Philox
+
+from cubed.backend_array_api import namespace as nxp
+from cubed.backend_array_api import numpy_array_to_backend_array
+from cubed.core.ops import map_blocks
+from cubed.utils import block_id_to_offset, normalize_shape
+from cubed.vendor.dask.array.core import normalize_chunks
+
+
+def random(size, *, chunks=None, spec=None):
+    """Return random floats in the half-open interval [0.0, 1.0)."""
+    shape = normalize_shape(size)
+    dtype = nxp.float64
+    chunks = normalize_chunks(chunks, shape=shape, dtype=dtype)
+    numblocks = tuple(map(len, chunks))
+    root_seed = pyrandom.getrandbits(128)
+
+    return map_blocks(
+        _random,
+        dtype=dtype,
+        chunks=chunks,
+        spec=spec,
+        numblocks=numblocks,
+        root_seed=root_seed,
+    )
+
+
+def _random(x, numblocks=None, root_seed=None, block_id=None):
+    stream_id = block_id_to_offset(block_id, numblocks)
+    rg = Generator(Philox(key=root_seed + stream_id))
+    out = rg.random(x.shape)
+    out = numpy_array_to_backend_array(out)
+    return out
---
+++ packages/cubed/cubed-0.20.0-py3-none-any.whl/cubed/storage/zarr.py
@@ -0,0 +1,113 @@
+from operator import mul
+from typing import Optional, Union
+
+import numpy as np
+import zarr
+from toolz import reduce
+
+from cubed.storage.backend import open_backend_array
+from cubed.types import T_DType, T_RegularChunks, T_Shape, T_Store
+
+
+class LazyZarrArray:
+    """A Zarr array that may not have been written to storage yet.
+
+    On creation, a normal Zarr array's metadata is immediately written to storage,
+    and there is no way to avoid this, hence this class, which separates the creation
+    of the object in memory and the creation of array metadata in storage.
+    """
+
+    def __init__(
+        self,
+        store: T_Store,
+        shape: T_Shape,
+        dtype: T_DType,
+        chunks: T_RegularChunks,
+        path: Optional[str] = None,
+        **kwargs,
+    ):
+        """Create a Zarr array lazily in memory."""
+        self.shape = shape
+        self.dtype = np.dtype(dtype)
+        self.chunks = chunks
+        self.store = store
+        self.path = path
+        self.kwargs = kwargs
+
+    @property
+    def size(self):
+        """Number of elements in the array."""
+        return reduce(mul, self.shape, 1)
+
+    @property
+    def nbytes(self) -> int:
+        """Number of bytes in array"""
+        return self.size * self.dtype.itemsize
+
+    def create(self, mode: str = "w-") -> zarr.Array:
+        """Create the Zarr array in storage.
+
+        The Zarr array's metadata is initialized in the backing store, and any
+        initial values are set on the array.
+
+        Parameters
+        ----------
+        mode : str
+            The mode to open the Zarr array with using ``zarr.open``.
+            Default is 'w-', which means create, fail it already exists.
+        """
+        target = open_backend_array(
+            self.store,
+            mode=mode,
+            shape=self.shape,
+            dtype=self.dtype,
+            chunks=self.chunks,
+            path=self.path,
+            **self.kwargs,
+        )
+        return target
+
+    def open(self) -> zarr.Array:
+        """Open the Zarr array for reading or writing and return it.
+
+        Note that the Zarr array must have been created or this method will raise an exception.
+        """
+        # r+ means read/write, fail if it doesn't exist
+        return open_backend_array(
+            self.store,
+            mode="r+",
+            shape=self.shape,
+            dtype=self.dtype,
+            chunks=self.chunks,
+            path=self.path,
+            **self.kwargs,
+        )
+
+    def __repr__(self) -> str:
+        return f"cubed.storage.zarr.LazyZarrArray<shape={self.shape}, dtype={self.dtype}, chunks={self.chunks}>"
+
+
+T_ZarrArray = Union[zarr.Array, LazyZarrArray]
+
+
+def lazy_zarr_array(
+    store: T_Store,
+    shape: T_Shape,
+    dtype: T_DType,
+    chunks: T_RegularChunks,
+    path: Optional[str] = None,
+    **kwargs,
+) -> LazyZarrArray:
+    return LazyZarrArray(
+        store,
+        shape,
+        dtype,
+        chunks,
+        path=path,
+        **kwargs,
+    )
+
+
+def open_if_lazy_zarr_array(array: T_ZarrArray) -> zarr.Array:
+    """If array is a LazyZarrArray then open it, leaving other arrays unchanged."""
+    return array.open() if isinstance(array, LazyZarrArray) else array
