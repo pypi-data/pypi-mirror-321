1bdf81820aa70fc194c2823c97491c399ff9808d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:50 +0000

    Add package natten-version-manager/0.0.1b1/natten_version_manager-0.0.1b1-py3-none-any.whl

---
+++ packages/natten-version-manager/natten_version_manager-0.0.1b1-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+from .natten_installer import (
+    install_natten,
+    parent_python,
+    make_natten_package_command,
+    find_closest_version_index,
+    generate_possible_natten_versions,
+    format_cuda_version,
+    torch_cuda_version
+)
+from . import pypi
