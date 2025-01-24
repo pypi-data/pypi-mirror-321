515d83af51fbf00eb83df6290449a932146e1935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 18:05:58 +0000

    Add package tablelocking-nonprod/0.2.1/tablelocking_nonprod-0.2.1-py3-none-any.whl

---
+++ packages/tablelocking-nonprod/tablelocking_nonprod-0.2.1-py3-none-any.whl/tablelocking_nonprod/functions.py
@@ -0,0 +1,8 @@
+def check_table(table_name: str, token: str) -> str:
+    print("In test function")
+    return f"return test value: {table_name} / {token}"
+
+
+def lock_table(table_name: str, token: str) -> str:
+    print("In test function")
+    return f"return test value: {table_name} / {token}"
