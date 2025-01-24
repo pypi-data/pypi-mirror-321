7641221644992af07d2a9b9a4cdc368576d63966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:17 +0000

    Add package upbge-stubs/0.3.1.27.dev12780130978/upbge-stubs-0.3.1.27.dev12780130978.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev12780130978.tar.gz/upbge-stubs-0.3.1.27.dev12780130978/sequence_video_modifier_type_items/__init__.pyi
@@ -0,0 +1,32 @@
+"""
+
+
+Sequence Video Modifier Type Items
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:BRIGHT_CONTRAST: 
+  Brightness/Contrast.
+
+:COLOR_BALANCE:   
+  Color Balance.
+
+:CURVES:          
+  Curves.
+
+:HUE_CORRECT:     
+  Hue Correct.
+
+:MASK:            
+  Mask.
+
+:TONEMAP:         
+  Tone Map.
+
+:WHITE_BALANCE:   
+  White Balance.
+
+.. _rna-enum-sequence-video-modifier-type-items:
+
+"""
+
+import typing
