c623a330ae0eaf1d81841b9249abae6c73009835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:00:51 +0000

    Add package scitacean/25.1.0/scitacean-25.1.0-py3-none-any.whl

---
+++ packages/scitacean/scitacean-25.1.0-py3-none-any.whl/scitacean/error.py
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 SciCat Project (https://github.com/SciCatProject/scitacean)
+"""Exception classes."""
+
+
+class FileUploadError(RuntimeError):
+    """Raised when file upload fails."""
+
+
+class IntegrityError(RuntimeError):
+    """Raised when a dataset or file is broken."""
+
+
+class ScicatCommError(RuntimeError):
+    """Raised when communication with SciCat fails."""
+
+
+class ScicatLoginError(RuntimeError):
+    """Raised when login to SciCat server fails."""
