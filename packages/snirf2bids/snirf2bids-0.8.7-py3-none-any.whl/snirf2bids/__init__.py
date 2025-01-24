4f3d92c44798452dc0432c43468e3c857969d34a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:59:35 +0000

    Add package snirf2bids/0.8.7/snirf2bids-0.8.7-py3-none-any.whl

---
+++ packages/snirf2bids/snirf2bids-0.8.7-py3-none-any.whl/snirf2bids/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py
+from .snirf2bids import *
+from .__version__ import __version__ as __version__
---
+++ packages/snirf2bids/snirf2bids-0.8.7-py3-none-any.whl/snirf2bids/__version__.py
@@ -0,0 +1,3 @@
+# __version__.py
+__VERSION__ = (0, 8, 7)
+__version__ = '.'.join(map(str, __VERSION__))
