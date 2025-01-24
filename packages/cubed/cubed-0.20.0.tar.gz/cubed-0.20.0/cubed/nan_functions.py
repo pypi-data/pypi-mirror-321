5a195a8368b14482e4df52c413e573f4bcbd1a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:03:36 +0000

    Add package cubed/0.20.0/cubed-0.20.0.tar.gz

---
+++ packages/cubed/cubed-0.20.0.tar.gz/cubed-0.20.0/cubed/nan_functions.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+from cubed.array_api.dtypes import (
+    _numeric_dtypes,
+    _signed_integer_dtypes,
+    _unsigned_integer_dtypes,
+    complex64,
+    complex128,
+    float32,
+    float64,
+    int64,
+    uint64,
+)
+from cubed.backend_array_api import namespace as nxp
+from cubed.core import reduction
+
+# TODO: refactor once nan functions are standardized:
+# https://github.com/data-apis/array-api/issues/621
+
+
+def nanmean(x, /, *, axis=None, keepdims=False, split_every=None):
+    """Compute the arithmetic mean along the specified axis, ignoring NaNs."""
+    dtype = x.dtype
+    intermediate_dtype = [("n", nxp.int64), ("total", nxp.float64)]
+    return reduction(
+        x,
+        _nanmean_func,
+        combine_func=_nanmean_combine,
+        aggregate_func=_nanmean_aggregate,
+        axis=axis,
+        intermediate_dtype=intermediate_dtype,
+        dtype=dtype,
+        keepdims=keepdims,
+        split_every=split_every,
+    )
+
+
+# note that the array API doesn't have nansum or nanmean, so these functions may fail
+
+
+def _nanmean_func(a, **kwargs):
+    n = _nannumel(a, **kwargs)
+    total = nxp.nansum(a, **kwargs)
+    return {"n": n, "total": total}
+
+
+def _nanmean_combine(a, **kwargs):
+    n = nxp.nansum(a["n"], **kwargs)
+    total = nxp.nansum(a["total"], **kwargs)
+    return {"n": n, "total": total}
+
+
+def _nanmean_aggregate(a):
+    with np.errstate(divide="ignore", invalid="ignore"):
+        return nxp.divide(a["total"], a["n"])
+
+
+def _nannumel(x, **kwargs):
+    """A reduction to count the number of elements, excluding nans"""
+    return nxp.sum(~(nxp.isnan(x)), **kwargs)
+
+
+def nansum(x, /, *, axis=None, dtype=None, keepdims=False, split_every=None):
+    """Return the sum of array elements over a given axis treating NaNs as zero."""
+    if x.dtype not in _numeric_dtypes:
+        raise TypeError("Only numeric dtypes are allowed in nansum")
+    if dtype is None:
+        if x.dtype in _signed_integer_dtypes:
+            dtype = int64
+        elif x.dtype in _unsigned_integer_dtypes:
+            dtype = uint64
+        elif x.dtype == float32:
+            dtype = float64
+        elif x.dtype == complex64:
+            dtype = complex128
+        else:
+            dtype = x.dtype
+    return reduction(
+        x,
+        nxp.nansum,
+        axis=axis,
+        dtype=dtype,
+        keepdims=keepdims,
+        split_every=split_every,
+    )
---
+++ packages/cubed/cubed-0.20.0.tar.gz/cubed-0.20.0/cubed/runtime/executors/dask.py
@@ -0,0 +1,184 @@
+import asyncio
+from typing import (
+    Any,
+    AsyncIterator,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    Union,
+)
+
+from aiostream import stream
+from aiostream.core import Stream
+from dask.distributed import Client
+from networkx import MultiDiGraph
+
+from cubed.runtime.backup import use_backups_default
+from cubed.runtime.executors.asyncio import async_map_unordered
+from cubed.runtime.pipeline import visit_node_generations, visit_nodes
+from cubed.runtime.types import Callback, CubedPipeline, DagExecutor
+from cubed.runtime.utils import (
+    asyncio_run,
+    execution_stats,
+    gensym,
+    handle_callbacks,
+    handle_operation_start_callbacks,
+)
+from cubed.spec import Spec
+
+
+# note we can't call `pipeline_func` just `func` here as it clashes with `dask.distributed.Client.map``
+@execution_stats
+def run_func(input, pipeline_func=None, config=None, name=None, compute_id=None):
+    result = pipeline_func(input, config=config)
+    return result
+
+
+async def map_unordered(
+    client: Client,
+    map_function: Callable[..., Any],
+    map_iterdata: Iterable[Union[List[Any], Tuple[Any, ...], Dict[str, Any]]],
+    retries: int = 2,
+    use_backups: bool = False,
+    batch_size: Optional[int] = None,
+    return_stats: bool = False,
+    name: Optional[str] = None,
+    **kwargs,
+) -> AsyncIterator[Any]:
+    def create_futures_func(input, **kwargs):
+        input = list(input)  # dask expects a sequence (it calls `len` on it)
+        key = name or gensym("map")
+        key = key.replace("-", "_")  # otherwise array number is not shown on dashboard
+        return [
+            (i, asyncio.ensure_future(f))
+            for i, f in zip(
+                input,
+                client.map(map_function, input, key=key, retries=retries, **kwargs),
+            )
+        ]
+
+    def create_backup_futures_func(input, **kwargs):
+        input = list(input)  # dask expects a sequence (it calls `len` on it)
+        key = name or gensym("backup")
+        key = key.replace("-", "_")  # otherwise array number is not shown on dashboard
+        return [
+            (i, asyncio.ensure_future(f))
+            for i, f in zip(input, client.map(map_function, input, key=key, **kwargs))
+        ]
+
+    async for result in async_map_unordered(
+        create_futures_func,
+        map_iterdata,
+        use_backups=use_backups,
+        create_backup_futures_func=create_backup_futures_func,
+        batch_size=batch_size,
+        return_stats=return_stats,
+        name=name,
+        **kwargs,
+    ):
+        yield result
+
+
+def pipeline_to_stream(
+    client: Client, name: str, pipeline: CubedPipeline, **kwargs
+) -> Stream:
+    return stream.iterate(
+        map_unordered(
+            client,
+            run_func,
+            pipeline.mappable,
+            return_stats=True,
+            name=name,
+            pipeline_func=pipeline.function,
+            config=pipeline.config,
+            **kwargs,
+        )
+    )
+
+
+def check_runtime_memory(spec, client):
+    allowed_mem = spec.allowed_mem if spec is not None else None
+    scheduler_info = client.scheduler_info()
+    workers = scheduler_info.get("workers")
+    if workers is None or len(workers) == 0:
+        raise ValueError("Cluster has no workers running")
+    runtime_memory = min(w["memory_limit"] // w["nthreads"] for w in workers.values())
+    if allowed_mem is not None:
+        if runtime_memory < allowed_mem:
+            raise ValueError(
+                f"Runtime memory ({runtime_memory}) is less than allowed_mem ({allowed_mem})"
+            )
+
+
+async def async_execute_dag(
+    dag: MultiDiGraph,
+    callbacks: Optional[Sequence[Callback]] = None,
+    resume: Optional[bool] = None,
+    spec: Optional[Spec] = None,
+    compute_arrays_in_parallel: Optional[bool] = None,
+    compute_kwargs: Optional[Dict[str, Any]] = None,
+    **kwargs,
+) -> None:
+    compute_kwargs = compute_kwargs or {}
+    async with Client(asynchronous=True, **compute_kwargs) as client:
+        if spec is not None:
+            check_runtime_memory(spec, client)
+            if "use_backups" not in kwargs and use_backups_default(spec):
+                kwargs["use_backups"] = True
+        if not compute_arrays_in_parallel:
+            # run one pipeline at a time
+            for name, node in visit_nodes(dag, resume=resume):
+                handle_operation_start_callbacks(callbacks, name)
+                st = pipeline_to_stream(client, name, node["pipeline"], **kwargs)
+                async with st.stream() as streamer:
+                    async for result, stats in streamer:
+                        handle_callbacks(callbacks, result, stats)
+        else:
+            for gen in visit_node_generations(dag, resume=resume):
+                # run pipelines in the same topological generation in parallel by merging their streams
+                streams = [
+                    pipeline_to_stream(client, name, node["pipeline"], **kwargs)
+                    for name, node in gen
+                ]
+                merged_stream = stream.merge(*streams)
+                async with merged_stream.stream() as streamer:
+                    async for result, stats in streamer:
+                        handle_callbacks(callbacks, result, stats)
+
+
+class DaskExecutor(DagExecutor):
+    """An execution engine that uses Dask Distributed's async API."""
+
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    @property
+    def name(self) -> str:
+        return "dask"
+
+    def execute_dag(
+        self,
+        dag: MultiDiGraph,
+        callbacks: Optional[Sequence[Callback]] = None,
+        resume: Optional[bool] = None,
+        spec: Optional[Spec] = None,
+        compute_id: Optional[str] = None,
+        compute_kwargs: Optional[Dict[str, Any]] = None,
+        **kwargs,
+    ) -> None:
+        merged_kwargs = {**self.kwargs, **kwargs}
+        asyncio_run(
+            async_execute_dag(
+                dag,
+                callbacks=callbacks,
+                resume=resume,
+                spec=spec,
+                compute_kwargs=compute_kwargs,
+                compute_id=compute_id,
+                **merged_kwargs,
+            )
+        )
