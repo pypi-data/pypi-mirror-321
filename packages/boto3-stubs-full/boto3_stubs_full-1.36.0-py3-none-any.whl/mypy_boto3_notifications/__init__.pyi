a2ca045822f7682097ba30024ee112d6d2ab1bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 14:05:00 +0000

    Add package boto3-stubs-full/1.36.0/boto3_stubs_full-1.36.0-py3-none-any.whl

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.36.0-py3-none-any.whl/mypy_boto3_notifications/__init__.pyi
@@ -0,0 +1,50 @@
+"""
+Main interface for notifications service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_notifications import (
+        Client,
+        ListChannelsPaginator,
+        ListEventRulesPaginator,
+        ListNotificationConfigurationsPaginator,
+        ListNotificationEventsPaginator,
+        ListNotificationHubsPaginator,
+        UserNotificationsClient,
+    )
+
+    session = Session()
+    client: UserNotificationsClient = session.client("notifications")
+
+    list_channels_paginator: ListChannelsPaginator = client.get_paginator("list_channels")
+    list_event_rules_paginator: ListEventRulesPaginator = client.get_paginator("list_event_rules")
+    list_notification_configurations_paginator: ListNotificationConfigurationsPaginator = client.get_paginator("list_notification_configurations")
+    list_notification_events_paginator: ListNotificationEventsPaginator = client.get_paginator("list_notification_events")
+    list_notification_hubs_paginator: ListNotificationHubsPaginator = client.get_paginator("list_notification_hubs")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import UserNotificationsClient
+from .paginator import (
+    ListChannelsPaginator,
+    ListEventRulesPaginator,
+    ListNotificationConfigurationsPaginator,
+    ListNotificationEventsPaginator,
+    ListNotificationHubsPaginator,
+)
+
+Client = UserNotificationsClient
+
+__all__ = (
+    "Client",
+    "ListChannelsPaginator",
+    "ListEventRulesPaginator",
+    "ListNotificationConfigurationsPaginator",
+    "ListNotificationEventsPaginator",
+    "ListNotificationHubsPaginator",
+    "UserNotificationsClient",
+)
