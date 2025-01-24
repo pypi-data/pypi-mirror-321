dcc9b128ae268c5912e97e59eeb34ed1da8dd0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 22:05:05 +0000

    Add package tspmetrics/0.0.3/tspmetrics-0.0.3.tar.gz

---
+++ packages/tspmetrics/tspmetrics-0.0.3.tar.gz/tspmetrics-0.0.3/tspmetrics/__init__.py
@@ -0,0 +1,6 @@
+from alt import top_of_permafrost, active_layer_thickness, tsp_active_layer_thickness, tsp_top_of_permafrost
+from amplitude import annual_amplitude, tsp_amplitude
+from classification import in_active_layer, in_permafrost, in_talik, in_warm_permafrost, tsp_classification
+from thaw_depth_duration import tsp_tdd, thaw_depth_duration
+from thermal_integral import thermal_integral, tsp_thermal_integral
+from zero_amplitude import dzaa, tza, tsp_tza, tsp_dzaa
