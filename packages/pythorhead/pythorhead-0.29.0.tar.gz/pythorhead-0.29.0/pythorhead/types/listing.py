42289ef4e348b9ac15448025b4fa9fe5706a0225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 22:05:10 +0000

    Add package pythorhead/0.29.0/pythorhead-0.29.0.tar.gz

---
+++ packages/pythorhead/pythorhead-0.29.0.tar.gz/pythorhead-0.29.0/pythorhead/types/listing.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class ListingType(str, Enum):
+    All = "All"
+    Community = "Community"
+    Local = "Local"
+    Subscribed = "Subscribed"
+    ModeratorView = "ModeratorView"
