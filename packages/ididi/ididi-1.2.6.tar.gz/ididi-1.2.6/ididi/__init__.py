cf010f613d1b1adbc900a7a800846cf01002192a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:56:49 +0000

    Add package ididi/1.2.6/ididi-1.2.6.tar.gz

---
+++ packages/ididi/ididi-1.2.6.tar.gz/ididi-1.2.6/ididi/__init__.py
@@ -0,0 +1,40 @@
+"""
+IDIDI
+~~~~~~~~~~~~~~~~~~~~~
+
+Ididi is a pythonic dependency injection lib, with ergonomic apis, without boilplate code, works out of the box.
+
+>>> import ididi
+>>> service = ididi.resolve(MyService)
+>>> assert isinstance(service, MyService)
+
+source code is at <https://github.com/raceychan/ididi>.
+
+copyright: (c) 2024 by race chan.
+license: MIT, see LICENSE for more details.
+"""
+
+VERSION = "1.2.6"
+
+__version__ = VERSION
+
+from .api import entry as entry
+from .api import resolve as resolve
+from .graph import AsyncScope as AsyncScope
+from .graph import DependencyGraph as DependencyGraph
+from .graph import SyncScope as SyncScope
+from .interfaces import INode as INode
+from .interfaces import INodeConfig as INodeConfig
+from ._node import DependentNode as DependentNode
+from ._node import Ignore as Ignore
+from ._node import NodeConfig as NodeConfig
+from ._node import use as use
+from .utils.typing_utils import AsyncResource as AsyncResource
+from .utils.typing_utils import Resource as Resource
+
+try:
+    import graphviz as graphviz  # type: ignore
+except ImportError:
+    pass
+else:
+    from .visual import Visualizer as Visualizer
