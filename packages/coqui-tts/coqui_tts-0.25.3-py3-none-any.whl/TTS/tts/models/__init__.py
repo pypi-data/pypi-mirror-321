2aa129e308a85807690711f94cd89e2d04e6bcd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:30 +0000

    Add package coqui-tts/0.25.3/coqui_tts-0.25.3-py3-none-any.whl

---
+++ packages/coqui-tts/coqui_tts-0.25.3-py3-none-any.whl/TTS/tts/models/__init__.py
@@ -0,0 +1,17 @@
+import logging
+from typing import Dict, List, Union
+
+from TTS.utils.generic_utils import find_module
+
+logger = logging.getLogger(__name__)
+
+
+def setup_model(config: "Coqpit", samples: Union[List[List], List[Dict]] = None) -> "BaseTTS":
+    logger.info("Using model: %s", config.model)
+    # fetch the right model implementation.
+    if "base_model" in config and config["base_model"] is not None:
+        MyModel = find_module("TTS.tts.models", config.base_model.lower())
+    else:
+        MyModel = find_module("TTS.tts.models", config.model.lower())
+    model = MyModel.init_from_config(config=config, samples=samples)
+    return model
