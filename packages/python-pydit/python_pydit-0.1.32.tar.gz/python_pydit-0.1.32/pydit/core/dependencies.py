5ba957e2c3d852d5f83b486d3947c2221e2bc264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:06:55 +0000

    Add package python-pydit/0.1.32/python_pydit-0.1.32.tar.gz

---
+++ packages/python-pydit/python_pydit-0.1.32.tar.gz/python_pydit-0.1.32/pydit/core/dependencies.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import Any
+from pydit.types.dependency import Dependency as DependencyType
+
+
+@dataclass
+class Dependency:
+    value: Any
+    token: str
+
+
+dependencies: dict[str, DependencyType] = {}
