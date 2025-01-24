ff94ee74dab825f5b6c2d9646b1f0b5e9ac5ce4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 19:05:54 +0000

    Add package docp/0.1.0b1/docp-0.1.0b1-py3-none-any.whl

---
+++ packages/docp/docp-0.1.0b1-py3-none-any.whl/docp/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+:Purpose:   This module provides the project initilisation logic.
+
+:Platform:  Linux/Windows | Python 3.10+
+:Developer: J Berendt
+:Email:     development@s3dev.uk
+
+:Comments:  n/a
+
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
+# Bring entry-points to the surface.
+try:
+    from loaders.chroma import ChromaLoader
+except ImportError as err:
+    # The chroma loader requires a lot of backend which is not required for the parser.
+    msg = f'An error occurred while importing the Chroma loader:\n- {err}'
+    raise ImportError(msg) from err
+
+try:
+    from .parsers.pdfparser import PDFParser
+    from ._version import __version__
+except ImportError:
+    from parsers.pdfparser import PDFParser
+    from _version import __version__
