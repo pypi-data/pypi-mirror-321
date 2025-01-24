e21a598d909155f17fa5dbe9ffe696dbfe69563f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 19:05:26 +0000

    Add package delos-cosmos/0.1.17/delos_cosmos-0.1.17.tar.gz

---
+++ packages/delos-cosmos/delos_cosmos-0.1.17.tar.gz/delos_cosmos-0.1.17/src/cosmos/__init__.pyi
@@ -0,0 +1,6 @@
+from .client import CosmosClient as CosmosClient
+from .endpoints import CosmosEndpoints as CosmosEndpoints
+from .endpoints import Endpoints as Endpoints
+from .endpoints import FileEndpoints as FileEndpoints
+
+__all__ = ["CosmosClient", "CosmosEndpoints", "Endpoints", "FileEndpoints"]
