975bc9f1a762dcbc54d20b2756da12409f4c4b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:04:06 +0000

    Add package multiversx-sdk/1.0.0b0/multiversx_sdk-1.0.0b0-py3-none-any.whl

---
+++ packages/multiversx-sdk/multiversx_sdk-1.0.0b0-py3-none-any.whl/multiversx_sdk/entrypoints/__init__.py
@@ -0,0 +1,9 @@
+from multiversx_sdk.entrypoints.entrypoints import (
+    DevnetEntrypoint,
+    MainnetEntrypoint,
+    NetworkEntrypoint,
+    TestnetEntrypoint,
+    LocalnetEntrypoint,
+)
+
+__all__ = ["DevnetEntrypoint", "MainnetEntrypoint", "NetworkEntrypoint", "TestnetEntrypoint", "LocalnetEntrypoint"]
