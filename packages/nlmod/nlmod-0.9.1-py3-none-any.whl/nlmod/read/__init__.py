04998580d011d04b8076a2ee0ce1197e7f9e7d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 18:05:28 +0000

    Add package nlmod/0.9.1/nlmod-0.9.1-py3-none-any.whl

---
+++ packages/nlmod/nlmod-0.9.1-py3-none-any.whl/nlmod/read/__init__.py
@@ -0,0 +1,21 @@
+# ruff: noqa: F401
+from . import (
+    administrative,
+    ahn,
+    bgt,
+    bofek,
+    bro,
+    brp,
+    geotop,
+    jarkus,
+    knmi,
+    knmi_data_platform,
+    meteobase,
+    nhi,
+    regis,
+    rws,
+    waterboard,
+    webservices,
+)
+from .geotop import get_geotop
+from .regis import get_regis
