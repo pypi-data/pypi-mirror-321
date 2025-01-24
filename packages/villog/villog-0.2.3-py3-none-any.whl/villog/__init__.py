f7160981edd0bb1596d4cca7d245525c8831597e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:01:40 +0000

    Add package villog/0.2.3/villog-0.2.3-py3-none-any.whl

---
+++ packages/villog/villog-0.2.3-py3-none-any.whl/villog/__init__.py
@@ -0,0 +1,7 @@
+'''A simple logger'''
+
+from villog.log import Logger
+from villog import writexcel
+from villog import mssql
+from villog.pdf import generate as generate_pdf
+from villog.readexcel import ReadExcel
