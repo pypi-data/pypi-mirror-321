c8fb8064ada96ff70e12dac09e0777ad99786147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:01:28 +0000

    Add package dagster-weaviate/0.0.1/dagster_weaviate-0.0.1-py3-none-any.whl

---
+++ packages/dagster-weaviate/dagster_weaviate-0.0.1-py3-none-any.whl/dagster_weaviate/__init__.py
@@ -0,0 +1,8 @@
+from dagster_weaviate.resource import (
+    WeaviateResource as WeaviateResource,
+)
+
+from dagster_weaviate.config import (
+    LocalConfig as LocalConfig,
+    CloudConfig as CloudConfig,
+)
