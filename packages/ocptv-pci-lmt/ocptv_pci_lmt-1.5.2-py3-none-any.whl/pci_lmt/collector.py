83d987a4873c9a2e2789d27bfa26fa76a0ce7023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 20:05:11 +0000

    Add package ocptv-pci-lmt/1.5.2/ocptv_pci_lmt-1.5.2-py3-none-any.whl

---
+++ packages/ocptv-pci-lmt/ocptv_pci_lmt-1.5.2-py3-none-any.whl/pci_lmt/collector.py
@@ -0,0 +1,219 @@
+# (c) Meta Platforms, Inc. and affiliates.
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+import argparse
+import logging
+import os
+import time
+import typing as ty
+
+from pci_lmt import __version__ as PCI_LMT_VERSION
+from pci_lmt.config import MarginType, PlatformConfig
+from pci_lmt.device import PciDevice
+from pci_lmt.host import HostInfo
+from pci_lmt.pcie_lane_margining import PcieDeviceLaneMargining
+from pci_lmt.results import LmtLaneResult, LmtTestInfo, Reporter
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+class PcieLmCollector:
+    # pylint: disable=too-many-instance-attributes
+    def __init__(self, test_info: LmtTestInfo, bdf_list: ty.List[str]):
+        # TODO: Can use the test_info directly instead of instantiating
+        # individual members. But, this change may litter the code. So,
+        # will handle this in a separate PR.
+        self.receiver_number = test_info.receiver_number
+        self.error_count_limit = test_info.error_count_limit
+        self.step = test_info.step
+        self.margin_type = test_info.margin_type
+        self.force_margin = test_info.force_margin
+        self.devices = [PcieDeviceLaneMargining(PciDevice(bdf)) for bdf in bdf_list]
+
+    @property
+    def _primed_devices(self) -> ty.Generator[PcieDeviceLaneMargining, None, None]:
+        for dev in self.devices:
+            if not dev.primed:
+                continue
+            yield dev
+
+    def normal_settings_on_device_list(self):
+        for dev in self._primed_devices:
+            for lane in range(dev.device_info.width):
+                dev.goto_normal_settings(lane=lane, receiver_number=self.receiver_number)
+
+    def clear_error_log_on_device_list(self):
+        for dev in self._primed_devices:
+            for lane in range(dev.device_info.width):
+                dev.clear_error_log(lane=lane, receiver_number=self.receiver_number)
+
+    def no_command_on_device_list(self):
+        for dev in self._primed_devices:
+            for lane in range(dev.device_info.width):
+                dev.no_command(lane=lane)
+
+    def info_lane_margin_on_device_list(self):
+        for dev in self.devices:
+            # Collect lane margin capabilties only from lane-0 since it's going to be same
+            # for all lanes on that device.
+            ret = dev.goto_normal_settings(lane=0, receiver_number=self.receiver_number)
+            ret = dev.fetch_margin_control_capabilities(lane=0, receiver_number=self.receiver_number)
+
+            status_str = f"Device {dev.device_info.bdf} ReceiverNum {self.receiver_number} "
+            if ret["error"]:
+                dev.primed = False
+                status_str += f"NOT PRIMED {ret['error']}"
+            else:
+                # By default, allow devices only with independent error sampler to be primed.
+                # Allow devices with no independent error sampler to be primed only if it's forced by user.
+                if dev.device_info.ind_error_sampler:
+                    dev.primed = True
+                    status_str += "PRIMED"
+                elif self.force_margin:
+                    dev.primed = True
+                    status_str += "PRIMED (forcing margin on non-independent sampler)"
+                else:
+                    dev.primed = False
+                    status_str += "NOT PRIMED (doesn't support independent error sampler)"
+
+            if dev.primed:
+                logger.info(status_str)
+            else:
+                logger.warning(status_str)
+                # Mark all lanes faulty.
+                for lane in range(dev.device_info.width):
+                    dev.lane_errors[lane] = status_str
+
+    def setup_lane_margin_on_device_list(self):
+        for dev in self._primed_devices:
+            for lane in range(dev.device_info.width):
+                dev.set_error_count_limit(
+                    lane=lane,
+                    receiver_number=self.receiver_number,
+                    error_count_limit=self.error_count_limit,
+                )
+
+                if self.margin_type in (MarginType.TIMING_LEFT, MarginType.TIMING_RIGHT, MarginType.TIMING_NONE):
+                    dev.step_margin_timing_offset_right_left_of_default(
+                        lane=lane,
+                        receiver_number=self.receiver_number,
+                        margin_type=self.margin_type,
+                        steps=self.step,
+                    )
+                else:
+                    dev.step_margin_voltage_offset_up_down_of_default(
+                        lane=lane,
+                        receiver_number=self.receiver_number,
+                        margin_type=self.margin_type,
+                        steps=self.step,
+                    )
+
+    def collect_lane_margin_on_device_list(self) -> ty.List[LmtLaneResult]:
+        """Returns the Lane Margining Test result from all lanes as a list."""
+
+        results = []
+        for dev in self.devices:
+            # Collect results from all devices and lanes
+            # irrespective of device prime or lane error status.
+            for lane in range(dev.device_info.width):
+                lane_result = LmtLaneResult(
+                    device_info=dev.device_info,
+                    lane=lane,
+                    receiver_number=ty.cast(int, self.receiver_number),
+                    step=self.step,
+                    margin_type=self.margin_type.value,
+                )
+
+                if self.margin_type in (MarginType.TIMING_LEFT, MarginType.TIMING_RIGHT, MarginType.TIMING_NONE):
+                    margin_status = dev.decode_step_margin_timing_offset_right_left_of_default(lane=lane)
+                else:
+                    margin_status = dev.decode_step_margin_voltage_offset_up_down_of_default(lane=lane)
+
+                sampler = dev.fetch_sample_count(lane=lane, receiver_number=self.receiver_number)
+                if margin_status["error"] or sampler["error"]:
+                    lane_result.error = True
+                    lane_result.error_msg = margin_status["error"] if margin_status["error"] else sampler["error"]
+                else:
+                    # TODO Check if this needs to be divided by Sampling (aka, 64)
+                    lane_result.error = False
+                    lane_result.sample_count = sampler["sample_count"]
+                    lane_result.sample_count_bits = sampler["sample_count_bits"]
+                    lane_result.error_count = margin_status["error_count"]
+                    lane_result.ber = margin_status["error_count"] / sampler["sample_count_bits"]
+
+                results.append(lane_result)
+
+        return results
+
+
+def get_run_id() -> str:
+    """Returns an unique ID using RNG."""
+    return os.popen("od -N 16 -t uL -An /dev/urandom | sed 's/ //g'").read().split("\n")[0]
+
+
+def get_curr_timestamp() -> int:
+    """Returns the current unix timestamp."""
+    return int(os.popen("date +%s").read().split("\n")[0])
+
+
+def collect_lmt_on_bdfs(test_info: LmtTestInfo, bdf_list: ty.List[str]) -> ty.List[LmtLaneResult]:
+    logger.info("%s", test_info)
+    devices = PcieLmCollector(test_info, bdf_list)
+
+    devices.no_command_on_device_list()
+    devices.info_lane_margin_on_device_list()
+    devices.no_command_on_device_list()
+    devices.clear_error_log_on_device_list()
+    devices.normal_settings_on_device_list()
+    devices.setup_lane_margin_on_device_list()
+
+    start_time = time.time()
+    time.sleep(test_info.dwell_time_secs)
+    results = devices.collect_lane_margin_on_device_list()
+    stop_time = time.time()
+    test_info.elapsed_time_secs = stop_time - start_time
+
+    # Append test_info to individual results before returning.
+    for result in results:
+        result.test_info = test_info
+
+    return results
+
+
+# FIXME: the args param should not be here, arg parsing and usage should be limited to main.py
+# instead, replace this with the actual inputs needed; if it's too many, a RuntimeConfig can be made
+# pylint: disable=too-many-locals
+def run_lmt(args: argparse.Namespace, config: PlatformConfig, host: HostInfo, reporter: Reporter) -> None:
+    """Runs LMT tests on all the interfaces listed in the platform_config."""
+
+    logger.info("Loading config: %s", config)
+    with reporter.start_run(host):
+        test_info = LmtTestInfo()
+        test_info.run_id = get_run_id()
+        test_info.timestamp = get_curr_timestamp()
+        test_info.host_id = host.host_id
+        test_info.hostname = host.hostname
+        test_info.model_name = host.model_name
+        test_info.dwell_time_secs = args.dwell_time
+        test_info.error_count_limit = args.error_count_limit
+        test_info.force_margin = args.force_margin
+        test_info.test_version = PCI_LMT_VERSION
+        test_info.config = str(config)
+
+        for group in config.lmt_groups:
+            test_info.margin_type = group.margin_type
+            test_info.receiver_number = group.receiver_number
+            test_info.annotation = args.annotation if args.annotation else group.name
+
+            # Loop through each step running LMT on all BDFs.
+            for step in group.margin_steps:
+                with reporter.start_step(
+                    name=f"Rcvr:{test_info.receiver_number} Step:{step} Ann:{test_info.annotation}"
+                ):
+                    test_info.step = step
+                    results = collect_lmt_on_bdfs(test_info=test_info, bdf_list=group.bdf_list)
+                    for result in results:
+                        logger.info(result)
+                        reporter.write(result)
---
+++ packages/ocptv-pci-lmt/ocptv_pci_lmt-1.5.2-py3-none-any.whl/pci_lmt/results.py
@@ -0,0 +1,178 @@
+# (c) Meta Platforms, Inc. and affiliates.
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+import dataclasses as dc
+import json
+import typing as ty
+from contextlib import contextmanager, ExitStack
+
+import ocptv.output as tv
+from ocptv.output import TestResult, TestRunError, TestStatus, TestStepError
+from pci_lmt import __version__ as PCI_LMT_VERSION
+from pci_lmt.config import MarginType
+from pci_lmt.host import HostInfo
+from pci_lmt.pcie_lane_margining import LmtDeviceInfo
+
+
+@dc.dataclass
+class LmtTestInfo:  # pylint: disable=too-many-instance-attributes,too-few-public-methods
+    """Class to hold test level info for the LMT test."""
+
+    run_id: str = ""
+    timestamp: int = -1
+    host_id: str = ""
+    hostname: str = ""
+    model_name: str = ""
+    receiver_number: int = -1
+    margin_type: MarginType = MarginType.VOLTAGE_NONE
+    step: int = -1
+    force_margin: bool = False
+    dwell_time_secs: int = -1
+    elapsed_time_secs: float = -1
+    error_count_limit: int = -1
+    test_version: str = ""
+    annotation: str = ""
+    config: str = ""
+
+
+@dc.dataclass
+class LmtLaneResult:  # pylint: disable=too-many-instance-attributes,too-few-public-methods
+    """Class to hold lane level info for the LMT test."""
+
+    test_info: LmtTestInfo = dc.field(default_factory=LmtTestInfo)
+    device_info: LmtDeviceInfo = dc.field(default_factory=LmtDeviceInfo)
+    lane: int = -1
+    receiver_number: int = -1
+    margin_type: str = ""
+    step: int = -1
+    sample_count: int = -1
+    sample_count_bits: int = -1
+    error_count: int = -1
+    ber: float = -1.0
+    error: bool = True
+    error_msg: str = ""
+
+    def to_json(self) -> str:
+        """Converts the object into a JSON string."""
+        return json.dumps(dc.asdict(self))
+
+    def to_csv(self) -> ty.Tuple[str, str]:
+        """Converts the object into a CSV string and returns header and row."""
+        nested_dict = dc.asdict(self)
+        flat_dict = {}
+        for key, value in nested_dict.items():
+            if isinstance(value, dict):
+                for inner_key, val in value.items():
+                    flat_dict[f"{key}.{inner_key}"] = val
+            else:
+                flat_dict[key] = value
+
+        header = ",".join(flat_dict.keys())
+        row = ",".join(str(value) for value in flat_dict.values())
+        return header, row
+
+
+class Reporter:
+    def __init__(self):
+        pass
+
+    @contextmanager
+    def start_run(self, host: HostInfo):
+        # pylint: disable=unused-argument
+        yield
+
+    @contextmanager
+    def start_step(self, name: str):
+        # pylint: disable=unused-argument
+        yield
+
+    def write(self, _result: LmtLaneResult) -> None:
+        pass
+
+
+class JsonStdoutReporter(Reporter):
+    def write(self, result: LmtLaneResult) -> None:
+        print(result.to_json())
+
+
+class CsvStdoutReporter(Reporter):
+    def __init__(self):
+        self.__emitted_header = False
+
+    def write(self, result: LmtLaneResult) -> None:
+        header, row = result.to_csv()
+        if not self.__emitted_header:
+            print(header)
+            self.__emitted_header = True
+
+        print(row)
+
+
+class OcptvReporter(Reporter):
+    def __init__(self):
+        self._run = tv.TestRun(name="pci_lmt", version=PCI_LMT_VERSION)
+        self._step = None
+
+    @contextmanager
+    def start_run(self, host: HostInfo):
+        # TODO(sksekar): Add support for HardwareInfo using PlatformConfig.
+        dut = tv.Dut(id=host.host_id, name=host.hostname)
+        try:
+            yield self._run.start(dut=dut)
+        except TestRunError as e:
+            self._run.end(status=e.status, result=e.result)
+        else:
+            self._run.end(status=TestStatus.COMPLETE, result=TestResult.PASS)
+
+    @contextmanager
+    def start_step(self, name: str):
+        if self._step:
+            raise RuntimeError("Cannot start a new step before ending the previous one.")
+
+        # TODO(sksekar): Add support for validators.
+        self._step = self._run.add_step(name=name)
+        try:
+            yield self._step.start()
+        except TestStepError as e:
+            self._step.end(status=e.status)
+        else:
+            self._step.end(status=TestStatus.COMPLETE)
+        finally:
+            # Step context cannot be re-used outside the current scope.
+            self._step = None
+
+    def write(self, result: LmtLaneResult) -> None:
+        if not self._step:
+            raise RuntimeError("Cannot write results before starting a step.")
+
+        meas_name = f"BDF:{result.device_info.bdf} Lane:{result.lane}"
+        self._step.add_measurement(name=meas_name, value=result.ber)
+
+
+class AggregatedReporter(Reporter):
+    def __init__(self, reporters: ty.List[Reporter]):
+        self._reporters = reporters
+
+    @contextmanager
+    def start_run(self, host: HostInfo):
+        stack = ExitStack()
+        for reporter in self._reporters:
+            stack.enter_context(reporter.start_run(host))
+        try:
+            yield
+        finally:
+            stack.close()
+
+    @contextmanager
+    def start_step(self, name: str):
+        stack = ExitStack()
+        for reporter in self._reporters:
+            stack.enter_context(reporter.start_step(name))
+        yield
+        stack.close()
+
+    def write(self, result: LmtLaneResult) -> None:
+        for reporter in self._reporters:
+            reporter.write(result)
