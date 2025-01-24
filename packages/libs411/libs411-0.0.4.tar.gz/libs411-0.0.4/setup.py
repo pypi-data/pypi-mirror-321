e67a902b6973646a4c32371c578625b8920ec514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:04:32 +0000

    Add package libs411/0.0.4/libs411-0.0.4.tar.gz

---
+++ packages/libs411/libs411-0.0.4.tar.gz/libs411-0.0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your-library-name',
+    version='0.1.0',
+    author='your name',
+    description='Your library description',
+    packages=find_packages()
+)
