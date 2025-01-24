139e52c9547e141b8b3e5b33195a091b267d825e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:04:59 +0000

    Add package invenio-communities/17.8.1/invenio-communities-17.8.1.tar.gz

---
+++ packages/invenio-communities/invenio-communities-17.8.1.tar.gz/invenio-communities-17.8.1/invenio_communities/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2016-2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio digital library framework."""
+
+from .ext import InvenioCommunities
+from .proxies import current_communities
+
+__version__ = "17.8.1"
+
+__all__ = ("InvenioCommunities", "current_communities")
