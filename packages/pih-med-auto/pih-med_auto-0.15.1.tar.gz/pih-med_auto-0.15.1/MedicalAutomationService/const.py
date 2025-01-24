93b6351301fbe36d4ac13906be498a46ecd4a61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:47 +0000

    Add package pih-med-auto/0.15.1/pih-med_auto-0.15.1.tar.gz

---
+++ packages/pih-med-auto/pih-med_auto-0.15.1.tar.gz/pih-med_auto-0.15.1/MedicalAutomationService/const.py
@@ -0,0 +1,27 @@
+import ipih
+
+from pih import A
+from pih.collections.service import ServiceDescription
+
+NAME: str = "MedicalAutomation"
+
+HOST = A.CT_H.BACKUP_WORKER
+
+VERSION: str = "0.15.1"
+
+PACKAGES: tuple[str, ...] = (
+    A.PTH_FCD_DIST.NAME(A.CT_SR.MOBILE_HELPER.standalone_name),  # type: ignore
+)
+
+SD: ServiceDescription = ServiceDescription(
+    name=NAME,
+    description="Medical Automation service",
+    host=HOST.NAME,
+    version=VERSION,
+    standalone_name="med_auto",
+    use_standalone=True,
+    packages=PACKAGES,
+)
+
+
+VALENTA_SOURCE_HOST: str = A.CT_H.WS816.NAME
