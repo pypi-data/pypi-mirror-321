0b942dc69d9c24391409dcaab024f83f5cc097ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:01:45 +0000

    Add package MobileInventoryCLI/0.4.885/mobileinventorycli-0.4.885.tar.gz

---
+++ packages/MobileInventoryCLI/mobileinventorycli-0.4.885.tar.gz/mobileinventorycli-0.4.885/MobileInventoryCLI/CodeProcessing/RecordCodesAndBarcodes/Run.py
@@ -0,0 +1,4 @@
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.DB.PrintLogging import prinL as print
+#!/usr/bin/env python3
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes import RecordMyCodes as rmc
+rmc.quikRn()
