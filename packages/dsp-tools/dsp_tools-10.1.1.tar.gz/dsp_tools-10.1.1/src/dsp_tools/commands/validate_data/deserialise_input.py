3b6092750d1380fd7ddb200e78be15560125f7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:03:18 +0000

    Add package dsp-tools/10.1.1/dsp_tools-10.1.1.tar.gz

---
+++ packages/dsp-tools/dsp_tools-10.1.1.tar.gz/dsp_tools-10.1.1/src/dsp_tools/commands/validate_data/deserialise_input.py
@@ -0,0 +1,178 @@
+from lxml import etree
+
+from dsp_tools.commands.validate_data.constants import AUDIO_SEGMENT_RESOURCE
+from dsp_tools.commands.validate_data.constants import REGION_RESOURCE
+from dsp_tools.commands.validate_data.constants import VIDEO_SEGMENT_RESOURCE
+from dsp_tools.commands.validate_data.constants import XML_TAG_TO_VALUE_TYPE_MAPPER
+from dsp_tools.commands.validate_data.models.data_deserialised import AbstractFileValueDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import BitstreamDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import DataDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import IIIFUriDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import KnoraValueType
+from dsp_tools.commands.validate_data.models.data_deserialised import ProjectDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import ProjectInformation
+from dsp_tools.commands.validate_data.models.data_deserialised import PropertyObject
+from dsp_tools.commands.validate_data.models.data_deserialised import ResourceDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import TripleObjectType
+from dsp_tools.commands.validate_data.models.data_deserialised import TriplePropertyType
+from dsp_tools.commands.validate_data.models.data_deserialised import ValueInformation
+from dsp_tools.models.exceptions import BaseError
+
+
+def deserialise_xml(root: etree._Element) -> ProjectDeserialised:
+    """
+    Takes the root of an XML
+    Extracts the data of the project and transforms all its resources.
+
+    Args:
+        root: root of an xml with qnames and comments removed
+
+    Returns:
+        Class instance with the information reformatted
+    """
+    shortcode = root.attrib["shortcode"]
+    default_ontology = root.attrib["default-ontology"]
+    project_info = ProjectInformation(shortcode, default_ontology)
+    data_deserialised = _deserialise_all_resources(root)
+    return ProjectDeserialised(project_info, data_deserialised)
+
+
+def _deserialise_all_resources(root: etree._Element) -> DataDeserialised:
+    all_res: list[ResourceDeserialised] = []
+    for res in root.iterdescendants(tag="resource"):
+        dsp_type = None
+        res_type = res.attrib["restype"]
+        if res_type == REGION_RESOURCE:
+            dsp_type = REGION_RESOURCE
+        elif res_type == VIDEO_SEGMENT_RESOURCE:
+            dsp_type = VIDEO_SEGMENT_RESOURCE
+        elif res_type == AUDIO_SEGMENT_RESOURCE:
+            dsp_type = AUDIO_SEGMENT_RESOURCE
+        if dsp_type:
+            all_res.append(_deserialise_one_in_built(res, dsp_type))
+        else:
+            all_res.append(_deserialise_one_resource(res))
+    file_values = _deserialise_file_value(root)
+    return DataDeserialised(all_res, file_values)
+
+
+def _deserialise_one_in_built(resource: etree._Element, res_type: str) -> ResourceDeserialised:
+    lbl = PropertyObject(TriplePropertyType.RDFS_LABEL, resource.attrib["label"], TripleObjectType.STRING)
+    rdf_type = PropertyObject(TriplePropertyType.RDF_TYPE, res_type, TripleObjectType.IRI)
+    return ResourceDeserialised(
+        res_id=resource.attrib["id"],
+        property_objects=[rdf_type, lbl],
+        values=[],
+    )
+
+
+def _deserialise_one_resource(resource: etree._Element) -> ResourceDeserialised:
+    values = []
+    for val in resource.iterchildren():
+        values.extend(_deserialise_one_property(val))
+    lbl = PropertyObject(TriplePropertyType.RDFS_LABEL, resource.attrib["label"], TripleObjectType.STRING)
+    rdf_type = PropertyObject(TriplePropertyType.RDF_TYPE, resource.attrib["restype"], TripleObjectType.IRI)
+    return ResourceDeserialised(
+        res_id=resource.attrib["id"],
+        property_objects=[rdf_type, lbl],
+        values=values,
+    )
+
+
+def _deserialise_one_property(prop_ele: etree._Element) -> list[ValueInformation]:
+    match prop_ele.tag:
+        case (
+            (
+                "boolean-prop"
+                | "color-prop"
+                | "date-prop"
+                | "decimal-prop"
+                | "geoname-prop"
+                | "integer-prop"
+                | "resptr-prop"
+                | "time-prop"
+                | "uri-prop"
+            ) as prop_tag
+        ):
+            return _extract_generic_value_information(prop_ele, XML_TAG_TO_VALUE_TYPE_MAPPER[prop_tag])
+        case "list-prop":
+            return _extract_list_value_information(prop_ele)
+        case "text-prop":
+            return _extract_text_value_information(prop_ele)
+        case _:
+            return []
+
+
+def _extract_generic_value_information(prop: etree._Element, value_type: KnoraValueType) -> list[ValueInformation]:
+    prop_name = prop.attrib["name"]
+    return [
+        ValueInformation(
+            user_facing_prop=prop_name,
+            user_facing_value=x.text,
+            knora_type=value_type,
+            value_metadata=[],
+        )
+        for x in prop.iterchildren()
+    ]
+
+
+def _extract_list_value_information(prop: etree._Element) -> list[ValueInformation]:
+    prop_name = prop.attrib["name"]
+    list_name = prop.attrib["list"]
+    all_vals = []
+    for val in prop.iterchildren():
+        found_value = f"{list_name} / {val.text}" if val.text else None
+        all_vals.append(
+            ValueInformation(
+                user_facing_prop=prop_name,
+                user_facing_value=found_value,
+                knora_type=KnoraValueType.LIST_VALUE,
+                value_metadata=[],
+            )
+        )
+    return all_vals
+
+
+def _extract_text_value_information(prop: etree._Element) -> list[ValueInformation]:
+    prop_name = prop.attrib["name"]
+    all_vals = []
+    for val in prop.iterchildren():
+        encoding = val.attrib["encoding"]
+        match encoding:
+            case "utf8":
+                val_type = KnoraValueType.SIMPLETEXT_VALUE
+            case "xml":
+                val_type = KnoraValueType.RICHTEXT_VALUE
+            case _:
+                raise BaseError(f"Unknown encoding: {encoding}.")
+        all_vals.append(
+            ValueInformation(
+                user_facing_prop=prop_name,
+                user_facing_value=_get_text_as_string(val),
+                knora_type=val_type,
+                value_metadata=[],
+            )
+        )
+    return all_vals
+
+
+def _get_text_as_string(value: etree._Element) -> str | None:
+    if len(value):
+        text_list = []
+        if found := value.text:
+            text_list = [found]
+        text_list.extend([etree.tostring(child, encoding="unicode", method="xml") for child in value])
+        return "".join(text_list).strip()
+    else:
+        return value.text
+
+
+def _deserialise_file_value(root: etree._Element) -> list[AbstractFileValueDeserialised]:
+    file_values: list[AbstractFileValueDeserialised] = []
+    for res in root.iterchildren(tag="resource"):
+        res_id = res.attrib["id"]
+        if (bitstream := res.find("bitstream")) is not None:
+            file_values.append(BitstreamDeserialised(res_id, bitstream.text))
+        elif (iiif_uri := res.find("iiif-uri")) is not None:
+            file_values.append(IIIFUriDeserialised(res_id, iiif_uri.text))
+    return file_values
---
+++ packages/dsp-tools/dsp_tools-10.1.1.tar.gz/dsp_tools-10.1.1/src/dsp_tools/commands/validate_data/make_data_rdf.py
@@ -0,0 +1,104 @@
+from uuid import uuid4
+
+from rdflib import RDF
+from rdflib import XSD
+from rdflib import Graph
+from rdflib import Literal
+from rdflib import URIRef
+
+from dsp_tools.commands.validate_data.constants import DATA
+from dsp_tools.commands.validate_data.constants import KNORA_API
+from dsp_tools.commands.validate_data.constants import TRIPLE_OBJECT_TYPE_TO_XSD
+from dsp_tools.commands.validate_data.constants import TRIPLE_PROP_TYPE_TO_IRI_MAPPER
+from dsp_tools.commands.validate_data.constants import VALUE_INFO_TO_RDF_MAPPER
+from dsp_tools.commands.validate_data.constants import VALUE_INFO_TRIPLE_OBJECT_TYPE
+from dsp_tools.commands.validate_data.models.data_deserialised import AbstractFileValueDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import DataDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import IIIFUriDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import KnoraValueType
+from dsp_tools.commands.validate_data.models.data_deserialised import ResourceDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import TripleObjectType
+from dsp_tools.commands.validate_data.models.data_deserialised import ValueInformation
+from dsp_tools.commands.xmlupload.make_rdf_graph.constants import IIIF_URI_VALUE
+from dsp_tools.commands.xmlupload.make_rdf_graph.make_file_value import get_file_type_info
+from dsp_tools.commands.xmlupload.models.rdf_models import RDFPropTypeInfo
+from dsp_tools.models.exceptions import BaseError
+
+
+def make_data_rdf(data_deserialised: DataDeserialised) -> Graph:
+    """
+    Transforms the deserialised data into instances that can produce a RDF graph.
+
+    Args:
+        data_deserialised: Deserialised Data
+
+    Returns:
+        Graph with the data
+    """
+    g = Graph()
+    for r in data_deserialised.resources:
+        g += _make_one_resource(r)
+    for f in data_deserialised.file_values:
+        g += _make_file_value(f)
+    return g
+
+
+def _make_one_resource(res: ResourceDeserialised) -> Graph:
+    res_iri = DATA[res.res_id]
+    g = Graph()
+    for trpl in res.property_objects:
+        object_val = _make_one_rdflib_object(trpl.object_value, trpl.object_type)
+        g.add((res_iri, TRIPLE_PROP_TYPE_TO_IRI_MAPPER[trpl.property_type], object_val))
+    for v in res.values:
+        g += _make_one_value(v, res_iri)
+    return g
+
+
+def _make_one_value(val: ValueInformation, res_iri: URIRef) -> Graph:
+    if val.knora_type == KnoraValueType.LINK_VALUE:
+        link_val = val.user_facing_value if val.user_facing_value else ""
+        triple_object: Literal | URIRef = DATA[link_val]
+    else:
+        triple_object = _make_one_rdflib_object(val.user_facing_value, VALUE_INFO_TRIPLE_OBJECT_TYPE[val.knora_type])
+    prop_type_info = VALUE_INFO_TO_RDF_MAPPER[val.knora_type]
+
+    g = Graph()
+    val_iri = DATA[str(uuid4())]
+    g.add((res_iri, URIRef(val.user_facing_prop), val_iri))
+    g.add((val_iri, RDF.type, prop_type_info.knora_type))
+    g.add((val_iri, prop_type_info.knora_prop, triple_object))
+    return g
+
+
+def _make_one_rdflib_object(object_value: str | None, object_type: TripleObjectType) -> Literal | URIRef:
+    if not object_value:
+        return Literal("", datatype=XSD.string)
+    if object_type == TripleObjectType.IRI:
+        return URIRef(object_value)
+    return Literal(object_value, datatype=TRIPLE_OBJECT_TYPE_TO_XSD[object_type])
+
+
+def _make_file_value(val: AbstractFileValueDeserialised) -> Graph:
+    if val.value is None:
+        return Graph()
+    if isinstance(val, IIIFUriDeserialised):
+        return _make_file_value_graph(val, IIIF_URI_VALUE, KNORA_API.stillImageFileValueHasExternalUrl)
+    try:
+        file_type = get_file_type_info(val.value)
+        return _make_file_value_graph(val, file_type)
+    except BaseError:
+        return Graph()
+
+
+def _make_file_value_graph(
+    val: AbstractFileValueDeserialised,
+    prop_type_info: RDFPropTypeInfo,
+    prop_to_value: URIRef = KNORA_API.fileValueHasFilename,
+) -> Graph:
+    g = Graph()
+    res_iri = DATA[val.res_id]
+    val_iri = DATA[str(uuid4())]
+    g.add((res_iri, prop_type_info.knora_prop, val_iri))
+    g.add((val_iri, RDF.type, prop_type_info.knora_type))
+    g.add((val_iri, prop_to_value, Literal(val.value, datatype=prop_type_info.xsd_type)))
+    return g
