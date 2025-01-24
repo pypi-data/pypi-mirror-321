7c35014e0705e859128125750ce4e132f93d4455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:04:55 +0000

    Add package ratelimit-io/0.5.0/ratelimit_io-0.5.0-py3-none-any.whl

---
+++ packages/ratelimit-io/ratelimit_io-0.5.0-py3-none-any.whl/ratelimit_io/__init__.py
@@ -0,0 +1,9 @@
+from ratelimit_io.rate_limit import LimitSpec
+from ratelimit_io.rate_limit import RatelimitIO
+from ratelimit_io.rate_limit import RatelimitIOError
+
+__all__ = [
+    "RatelimitIO",
+    "LimitSpec",
+    "RatelimitIOError",
+]
