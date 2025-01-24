c564610147f4f348127ceae015a6d46297cd78e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:04:25 +0000

    Add package pyufunc/0.3.8/pyufunc-0.3.8-py3-none-any.whl

---
+++ packages/pyufunc/pyufunc-0.3.8-py3-none-any.whl/pyufunc/util_data_processing/__init__.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Sunday, July 9th 2023
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+from ._dict import (dict_split_by_chunk,
+                    dict_delete_keys)
+from ._list import (list_split_by_equal_sublist,
+                    list_split_by_fixed_length,
+                    list_flatten_nested)
+from ._str import (str_strip,
+                   cvt_digit_str_to_int,
+                   cvt_digit_str_to_float)
+from ._dataclass import (create_dataclass,
+                         create_dataclass_from_dict,
+                         merge_dataclass,
+                         extend_dataclass,
+                         dataclass_dict_wrapper)
+from ._float import is_float
+from ._data_cleaning import get_layer_boundary
+from ._int_to_alpha import cvt_int_to_alpha
+
+
+__all__ = [
+    # _data_cleaning
+    "get_layer_boundary",
+
+    # _dict
+    # "split_dict_by_chunk",
+    "dict_split_by_chunk",
+    # "delete_dict_keys",
+    "dict_delete_keys",
+
+    # _int_to_alpha
+    "cvt_int_to_alpha",
+
+    # _list
+    "list_split_by_equal_sublist",
+    "list_split_by_fixed_length",
+    "list_flatten_nested",
+
+    # _float
+    "is_float",
+
+    # _str
+    "str_strip",
+    "cvt_digit_str_to_int",
+    "cvt_digit_str_to_float",
+
+    # dataclass
+    "create_dataclass",
+    "create_dataclass_from_dict",
+    "merge_dataclass",
+    "extend_dataclass",
+    "dataclass_dict_wrapper",
+]
---
+++ packages/pyufunc/pyufunc-0.3.8-py3-none-any.whl/pyufunc/util_pathio/__init__.py
@@ -0,0 +1,80 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Sunday, July 9th 2023
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+from ._argparse import with_argparse
+from ._io import (get_file_size,
+                  get_dir_size,
+                  create_tempfile,
+                  remove_file,
+                  add_dir_to_env,
+                  pickle_save,
+                  pickle_load,
+                  find_duplicate_files,
+                  remove_duplicate_files,
+                  )
+from ._path import (path2linux,
+                    path2uniform,
+                    get_filenames_by_ext,
+                    get_files_by_ext,
+                    check_files_in_dir,
+                    check_filename,
+                    check_file_existence,
+                    generate_unique_filename,
+                    create_unique_filename,
+                    show_dir_in_tree,
+                    add_pkg_to_sys_path,
+                    find_executable_from_PATH_on_win,
+                    find_fn_from_PATH_on_win,
+                    )
+from ._platform import (check_platform,
+                        is_windows,
+                        is_linux,
+                        is_mac,
+                        get_terminal_width,
+                        get_terminal_height,
+                        )
+
+
+__all__ = [
+    # argparse
+    "with_argparse",
+
+    # io
+    "get_file_size",
+    "get_dir_size",
+    "create_tempfile",
+    "remove_file",
+    "add_dir_to_env",
+    "pickle_save",
+    "pickle_load",
+    "find_duplicate_files",
+    "remove_duplicate_files",
+
+    # path
+    'path2linux',
+    'path2uniform',
+    "get_filenames_by_ext",
+    "get_files_by_ext",
+    "check_files_in_dir",
+    "check_filename",
+    "check_file_existence",
+    "generate_unique_filename",
+    "create_unique_filename",
+    "show_dir_in_tree",
+    "add_pkg_to_sys_path",
+    "find_executable_from_PATH_on_win",
+    "find_fn_from_PATH_on_win",
+
+    # platform
+    "check_platform",
+    "is_windows",
+    "is_linux",
+    "is_mac",
+    "get_terminal_width",
+    "get_terminal_height",
+
+]
