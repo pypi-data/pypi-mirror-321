7c2e2c5fd1baf9898f28a69c3d6ad6617f8b6ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:08:27 +0000

    Add package Grindr/0.3.0.post4/grindr-0.3.0.post4.tar.gz

---
+++ packages/Grindr/grindr-0.3.0.post4.tar.gz/grindr-0.3.0.post4/Grindr/client/web/routes/fetch/fetch_genders.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+from Grindr.client.web.web_base import ClientRoute, URLTemplate
+from Grindr.client.web.web_settings import GRINDR_PUBLIC_V2
+
+
+class Gender(BaseModel):
+    genderId: int | None = None
+    gender: str | None = None
+    displayGroup: int | None = None
+    sortProfile: int | None = None
+    sortFilter: int | None = None
+    genderPlural: str | None = None
+    excludeOnProfileSelection: bool | None = None
+    excludeOnFilterSelection: list[int] | None = None
+    alsoClassifiedAs: list[int] | None = None
+
+
+type FetchGendersRouteResponse = list[Gender]
+
+
+class FetchGendersRoute(
+    ClientRoute[
+        "GET",
+        URLTemplate(GRINDR_PUBLIC_V2, "/genders"),
+        None,
+        None,
+        FetchGendersRouteResponse
+    ]
+):
+    pass
