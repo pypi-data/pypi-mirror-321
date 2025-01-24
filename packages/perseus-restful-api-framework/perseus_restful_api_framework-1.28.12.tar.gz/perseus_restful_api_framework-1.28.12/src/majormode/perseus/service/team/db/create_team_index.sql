3064aaa8c4d8c1e218840a1c5fe1900845fd51df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 17:05:26 +0000

    Add package perseus-restful-api-framework/1.28.12/perseus_restful_api_framework-1.28.12.tar.gz

---
+++ packages/perseus-restful-api-framework/perseus_restful_api_framework-1.28.12.tar.gz/perseus_restful_api_framework-1.28.12/src/majormode/perseus/service/team/db/create_team_index.sql
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C) 2021 Majormode.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Majormode or one of its subsidiaries.  You shall not disclose this
+ * confidential information and shall use it only in accordance with the
+ * terms of the license agreement or other applicable agreement you
+ * entered into with Majormode.
+ *
+ * MAJORMODE MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY
+ * OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT.  MAJORMODE SHALL NOT BE LIABLE FOR ANY
+ * LOSSES OR DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+ * OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+*/
+
+CREATE UNIQUE INDEX idx_team_name
+  ON team (lower(name));
+
+
+CREATE UNIQUE INDEX idx_team_contact
+  ON team_contact (
+    lower(property_value),
+    property_name
+  );
