33cf69ff205872741eb8cc6c79ab1a1c2cd775a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 16:04:26 +0000

    Add package huaweicloudsdkelb/3.1.132/huaweicloudsdkelb-3.1.132-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkelb/huaweicloudsdkelb-3.1.132-py2.py3-none-any.whl/huaweicloudsdkelb/v3/model/count_preoccupy_ip_num_request.py
@@ -0,0 +1,231 @@
+# coding: utf-8
+
+import six
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class CountPreoccupyIpNumRequest:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'l7_flavor_id': 'str',
+        'ip_target_enable': 'bool',
+        'ip_version': 'int',
+        'loadbalancer_id': 'str',
+        'availability_zone_id': 'list[str]'
+    }
+
+    attribute_map = {
+        'l7_flavor_id': 'l7_flavor_id',
+        'ip_target_enable': 'ip_target_enable',
+        'ip_version': 'ip_version',
+        'loadbalancer_id': 'loadbalancer_id',
+        'availability_zone_id': 'availability_zone_id'
+    }
+
+    def __init__(self, l7_flavor_id=None, ip_target_enable=None, ip_version=None, loadbalancer_id=None, availability_zone_id=None):
+        """CountPreoccupyIpNumRequest
+
+        The model defined in huaweicloud sdk
+
+        :param l7_flavor_id: 负载均衡器七层规格的ID。传入该字段表示计算创建该规格的LB，或变更LB的原七层规格到该规格所需要的预占IP。  适用场景：创建负LB，变更LB规格。  [不支持传入l7_flavor_id](tag:hcso,hk_vdf,fcs,fcs_vm,mix,hcso_g42,hcso_g42_b)
+        :type l7_flavor_id: str
+        :param ip_target_enable: 是否开启跨VPC转发。  取值true表示计算创建或变更为开启跨VPC转发的LB的预占IP。  取值false表示计算创建或变更为不开启跨VPC转发的LB的预占IP。不传等价false。  适用场景：创建LB，变更LB规格。  [荷兰region不支持该字段，请勿使用。](tag:dt,dt_test)
+        :type ip_target_enable: bool
+        :param ip_version: 负载均衡器IP地址类型，取值4，6 。  取值4表示计算创建支持IPv4地址的LB的预占IP。  取值6表示计算创建支持IPv6地址的LB的预占IP。  适用场景：创建LB。  [不支持IPv6，请勿设置为6。](tag:dt,dt_test)
+        :type ip_version: int
+        :param loadbalancer_id: 负载均衡器ID。计算LB规格变更或创建LB中的第一个七层监听器的预占IP。  适用场景：变更LB规格，创建LB中的第一个七层监听器。
+        :type loadbalancer_id: str
+        :param availability_zone_id: 计算创建AZ列表为availability_zone_id的负载局衡器实例的预占IP。  适用场景：创建LB。  使用说明：传入loadbalancer_id时，该参数无效。
+        :type availability_zone_id: list[str]
+        """
+        
+        
+
+        self._l7_flavor_id = None
+        self._ip_target_enable = None
+        self._ip_version = None
+        self._loadbalancer_id = None
+        self._availability_zone_id = None
+        self.discriminator = None
+
+        if l7_flavor_id is not None:
+            self.l7_flavor_id = l7_flavor_id
+        if ip_target_enable is not None:
+            self.ip_target_enable = ip_target_enable
+        if ip_version is not None:
+            self.ip_version = ip_version
+        if loadbalancer_id is not None:
+            self.loadbalancer_id = loadbalancer_id
+        if availability_zone_id is not None:
+            self.availability_zone_id = availability_zone_id
+
+    @property
+    def l7_flavor_id(self):
+        """Gets the l7_flavor_id of this CountPreoccupyIpNumRequest.
+
+        负载均衡器七层规格的ID。传入该字段表示计算创建该规格的LB，或变更LB的原七层规格到该规格所需要的预占IP。  适用场景：创建负LB，变更LB规格。  [不支持传入l7_flavor_id](tag:hcso,hk_vdf,fcs,fcs_vm,mix,hcso_g42,hcso_g42_b)
+
+        :return: The l7_flavor_id of this CountPreoccupyIpNumRequest.
+        :rtype: str
+        """
+        return self._l7_flavor_id
+
+    @l7_flavor_id.setter
+    def l7_flavor_id(self, l7_flavor_id):
+        """Sets the l7_flavor_id of this CountPreoccupyIpNumRequest.
+
+        负载均衡器七层规格的ID。传入该字段表示计算创建该规格的LB，或变更LB的原七层规格到该规格所需要的预占IP。  适用场景：创建负LB，变更LB规格。  [不支持传入l7_flavor_id](tag:hcso,hk_vdf,fcs,fcs_vm,mix,hcso_g42,hcso_g42_b)
+
+        :param l7_flavor_id: The l7_flavor_id of this CountPreoccupyIpNumRequest.
+        :type l7_flavor_id: str
+        """
+        self._l7_flavor_id = l7_flavor_id
+
+    @property
+    def ip_target_enable(self):
+        """Gets the ip_target_enable of this CountPreoccupyIpNumRequest.
+
+        是否开启跨VPC转发。  取值true表示计算创建或变更为开启跨VPC转发的LB的预占IP。  取值false表示计算创建或变更为不开启跨VPC转发的LB的预占IP。不传等价false。  适用场景：创建LB，变更LB规格。  [荷兰region不支持该字段，请勿使用。](tag:dt,dt_test)
+
+        :return: The ip_target_enable of this CountPreoccupyIpNumRequest.
+        :rtype: bool
+        """
+        return self._ip_target_enable
+
+    @ip_target_enable.setter
+    def ip_target_enable(self, ip_target_enable):
+        """Sets the ip_target_enable of this CountPreoccupyIpNumRequest.
+
+        是否开启跨VPC转发。  取值true表示计算创建或变更为开启跨VPC转发的LB的预占IP。  取值false表示计算创建或变更为不开启跨VPC转发的LB的预占IP。不传等价false。  适用场景：创建LB，变更LB规格。  [荷兰region不支持该字段，请勿使用。](tag:dt,dt_test)
+
+        :param ip_target_enable: The ip_target_enable of this CountPreoccupyIpNumRequest.
+        :type ip_target_enable: bool
+        """
+        self._ip_target_enable = ip_target_enable
+
+    @property
+    def ip_version(self):
+        """Gets the ip_version of this CountPreoccupyIpNumRequest.
+
+        负载均衡器IP地址类型，取值4，6 。  取值4表示计算创建支持IPv4地址的LB的预占IP。  取值6表示计算创建支持IPv6地址的LB的预占IP。  适用场景：创建LB。  [不支持IPv6，请勿设置为6。](tag:dt,dt_test)
+
+        :return: The ip_version of this CountPreoccupyIpNumRequest.
+        :rtype: int
+        """
+        return self._ip_version
+
+    @ip_version.setter
+    def ip_version(self, ip_version):
+        """Sets the ip_version of this CountPreoccupyIpNumRequest.
+
+        负载均衡器IP地址类型，取值4，6 。  取值4表示计算创建支持IPv4地址的LB的预占IP。  取值6表示计算创建支持IPv6地址的LB的预占IP。  适用场景：创建LB。  [不支持IPv6，请勿设置为6。](tag:dt,dt_test)
+
+        :param ip_version: The ip_version of this CountPreoccupyIpNumRequest.
+        :type ip_version: int
+        """
+        self._ip_version = ip_version
+
+    @property
+    def loadbalancer_id(self):
+        """Gets the loadbalancer_id of this CountPreoccupyIpNumRequest.
+
+        负载均衡器ID。计算LB规格变更或创建LB中的第一个七层监听器的预占IP。  适用场景：变更LB规格，创建LB中的第一个七层监听器。
+
+        :return: The loadbalancer_id of this CountPreoccupyIpNumRequest.
+        :rtype: str
+        """
+        return self._loadbalancer_id
+
+    @loadbalancer_id.setter
+    def loadbalancer_id(self, loadbalancer_id):
+        """Sets the loadbalancer_id of this CountPreoccupyIpNumRequest.
+
+        负载均衡器ID。计算LB规格变更或创建LB中的第一个七层监听器的预占IP。  适用场景：变更LB规格，创建LB中的第一个七层监听器。
+
+        :param loadbalancer_id: The loadbalancer_id of this CountPreoccupyIpNumRequest.
+        :type loadbalancer_id: str
+        """
+        self._loadbalancer_id = loadbalancer_id
+
+    @property
+    def availability_zone_id(self):
+        """Gets the availability_zone_id of this CountPreoccupyIpNumRequest.
+
+        计算创建AZ列表为availability_zone_id的负载局衡器实例的预占IP。  适用场景：创建LB。  使用说明：传入loadbalancer_id时，该参数无效。
+
+        :return: The availability_zone_id of this CountPreoccupyIpNumRequest.
+        :rtype: list[str]
+        """
+        return self._availability_zone_id
+
+    @availability_zone_id.setter
+    def availability_zone_id(self, availability_zone_id):
+        """Sets the availability_zone_id of this CountPreoccupyIpNumRequest.
+
+        计算创建AZ列表为availability_zone_id的负载局衡器实例的预占IP。  适用场景：创建LB。  使用说明：传入loadbalancer_id时，该参数无效。
+
+        :param availability_zone_id: The availability_zone_id of this CountPreoccupyIpNumRequest.
+        :type availability_zone_id: list[str]
+        """
+        self._availability_zone_id = availability_zone_id
+
+    def to_dict(self):
+        """Returns the model properties as a dict"""
+        result = {}
+
+        for attr, _ in six.iteritems(self.openapi_types):
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        if six.PY2:
+            import sys
+            reload(sys)
+            sys.setdefaultencoding("utf-8")
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, CountPreoccupyIpNumRequest):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
---
+++ packages/huaweicloudsdkelb/huaweicloudsdkelb-3.1.132-py2.py3-none-any.whl/huaweicloudsdkelb/v3/model/list_flavors_request.py
@@ -0,0 +1,289 @@
+# coding: utf-8
+
+import six
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class ListFlavorsRequest:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'marker': 'str',
+        'limit': 'int',
+        'page_reverse': 'bool',
+        'id': 'list[str]',
+        'name': 'list[str]',
+        'type': 'list[str]',
+        'shared': 'bool'
+    }
+
+    attribute_map = {
+        'marker': 'marker',
+        'limit': 'limit',
+        'page_reverse': 'page_reverse',
+        'id': 'id',
+        'name': 'name',
+        'type': 'type',
+        'shared': 'shared'
+    }
+
+    def __init__(self, marker=None, limit=None, page_reverse=None, id=None, name=None, type=None, shared=None):
+        """ListFlavorsRequest
+
+        The model defined in huaweicloud sdk
+
+        :param marker: 参数解释：上一页最后一条记录的ID。  约束限制： - 必须与limit一起使用。 - 不指定时表示查询第一页。 - 该字段不允许为空或无效的ID。
+        :type marker: str
+        :param limit: 参数解释：每页返回的个数。  取值范围：0-2000  默认取值：2000
+        :type limit: int
+        :param page_reverse: 参数解释：是否反向查询。  约束限制： - 必须与limit一起使用。 - 当page_reverse&#x3D;true时，若要查询上一页，marker取值为当前页返回值的previous_marker。  取值范围： - true：查询上一页。 - false：查询下一页，默认。
+        :type page_reverse: bool
+        :param id: 参数解释：规格ID。  支持多值查询，查询条件格式：*id&#x3D;xxx&amp;id&#x3D;xxx*。
+        :type id: list[str]
+        :param name: 参数解释：规格名称。  支持多值查询，查询条件格式：*name&#x3D;xxx&amp;name&#x3D;xxx*。
+        :type name: list[str]
+        :param type: 参数解释：规格类别。  取值范围： - L4和L7 表示四层网络型和七层应用型flavor。 [- gateway 表示网关型LB的flavor，目前只支持弹性计费类型。当前仅支持欧洲局点。](tag:hws_eu) - L4_elastic和L7_elastic 表示弹性扩缩容实例的下限规格。 - L4_elastic_max、L7_elastic_max[和gateway_elastic_max](tag:hws_eu) 表示弹性扩缩容实例的上限规格。  支持多值查询，查询条件格式：*type&#x3D;xxx&amp;type&#x3D;xxx*。
+        :type type: list[str]
+        :param shared: 参数解释：是否查询公共规格。  取值范围：true表示公共规格，所有租户可见。false表示私有规格，为当前租户所有。
+        :type shared: bool
+        """
+        
+        
+
+        self._marker = None
+        self._limit = None
+        self._page_reverse = None
+        self._id = None
+        self._name = None
+        self._type = None
+        self._shared = None
+        self.discriminator = None
+
+        if marker is not None:
+            self.marker = marker
+        if limit is not None:
+            self.limit = limit
+        if page_reverse is not None:
+            self.page_reverse = page_reverse
+        if id is not None:
+            self.id = id
+        if name is not None:
+            self.name = name
+        if type is not None:
+            self.type = type
+        if shared is not None:
+            self.shared = shared
+
+    @property
+    def marker(self):
+        """Gets the marker of this ListFlavorsRequest.
+
+        参数解释：上一页最后一条记录的ID。  约束限制： - 必须与limit一起使用。 - 不指定时表示查询第一页。 - 该字段不允许为空或无效的ID。
+
+        :return: The marker of this ListFlavorsRequest.
+        :rtype: str
+        """
+        return self._marker
+
+    @marker.setter
+    def marker(self, marker):
+        """Sets the marker of this ListFlavorsRequest.
+
+        参数解释：上一页最后一条记录的ID。  约束限制： - 必须与limit一起使用。 - 不指定时表示查询第一页。 - 该字段不允许为空或无效的ID。
+
+        :param marker: The marker of this ListFlavorsRequest.
+        :type marker: str
+        """
+        self._marker = marker
+
+    @property
+    def limit(self):
+        """Gets the limit of this ListFlavorsRequest.
+
+        参数解释：每页返回的个数。  取值范围：0-2000  默认取值：2000
+
+        :return: The limit of this ListFlavorsRequest.
+        :rtype: int
+        """
+        return self._limit
+
+    @limit.setter
+    def limit(self, limit):
+        """Sets the limit of this ListFlavorsRequest.
+
+        参数解释：每页返回的个数。  取值范围：0-2000  默认取值：2000
+
+        :param limit: The limit of this ListFlavorsRequest.
+        :type limit: int
+        """
+        self._limit = limit
+
+    @property
+    def page_reverse(self):
+        """Gets the page_reverse of this ListFlavorsRequest.
+
+        参数解释：是否反向查询。  约束限制： - 必须与limit一起使用。 - 当page_reverse=true时，若要查询上一页，marker取值为当前页返回值的previous_marker。  取值范围： - true：查询上一页。 - false：查询下一页，默认。
+
+        :return: The page_reverse of this ListFlavorsRequest.
+        :rtype: bool
+        """
+        return self._page_reverse
+
+    @page_reverse.setter
+    def page_reverse(self, page_reverse):
+        """Sets the page_reverse of this ListFlavorsRequest.
+
+        参数解释：是否反向查询。  约束限制： - 必须与limit一起使用。 - 当page_reverse=true时，若要查询上一页，marker取值为当前页返回值的previous_marker。  取值范围： - true：查询上一页。 - false：查询下一页，默认。
+
+        :param page_reverse: The page_reverse of this ListFlavorsRequest.
+        :type page_reverse: bool
+        """
+        self._page_reverse = page_reverse
+
+    @property
+    def id(self):
+        """Gets the id of this ListFlavorsRequest.
+
+        参数解释：规格ID。  支持多值查询，查询条件格式：*id=xxx&id=xxx*。
+
+        :return: The id of this ListFlavorsRequest.
+        :rtype: list[str]
+        """
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        """Sets the id of this ListFlavorsRequest.
+
+        参数解释：规格ID。  支持多值查询，查询条件格式：*id=xxx&id=xxx*。
+
+        :param id: The id of this ListFlavorsRequest.
+        :type id: list[str]
+        """
+        self._id = id
+
+    @property
+    def name(self):
+        """Gets the name of this ListFlavorsRequest.
+
+        参数解释：规格名称。  支持多值查询，查询条件格式：*name=xxx&name=xxx*。
+
+        :return: The name of this ListFlavorsRequest.
+        :rtype: list[str]
+        """
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        """Sets the name of this ListFlavorsRequest.
+
+        参数解释：规格名称。  支持多值查询，查询条件格式：*name=xxx&name=xxx*。
+
+        :param name: The name of this ListFlavorsRequest.
+        :type name: list[str]
+        """
+        self._name = name
+
+    @property
+    def type(self):
+        """Gets the type of this ListFlavorsRequest.
+
+        参数解释：规格类别。  取值范围： - L4和L7 表示四层网络型和七层应用型flavor。 [- gateway 表示网关型LB的flavor，目前只支持弹性计费类型。当前仅支持欧洲局点。](tag:hws_eu) - L4_elastic和L7_elastic 表示弹性扩缩容实例的下限规格。 - L4_elastic_max、L7_elastic_max[和gateway_elastic_max](tag:hws_eu) 表示弹性扩缩容实例的上限规格。  支持多值查询，查询条件格式：*type=xxx&type=xxx*。
+
+        :return: The type of this ListFlavorsRequest.
+        :rtype: list[str]
+        """
+        return self._type
+
+    @type.setter
+    def type(self, type):
+        """Sets the type of this ListFlavorsRequest.
+
+        参数解释：规格类别。  取值范围： - L4和L7 表示四层网络型和七层应用型flavor。 [- gateway 表示网关型LB的flavor，目前只支持弹性计费类型。当前仅支持欧洲局点。](tag:hws_eu) - L4_elastic和L7_elastic 表示弹性扩缩容实例的下限规格。 - L4_elastic_max、L7_elastic_max[和gateway_elastic_max](tag:hws_eu) 表示弹性扩缩容实例的上限规格。  支持多值查询，查询条件格式：*type=xxx&type=xxx*。
+
+        :param type: The type of this ListFlavorsRequest.
+        :type type: list[str]
+        """
+        self._type = type
+
+    @property
+    def shared(self):
+        """Gets the shared of this ListFlavorsRequest.
+
+        参数解释：是否查询公共规格。  取值范围：true表示公共规格，所有租户可见。false表示私有规格，为当前租户所有。
+
+        :return: The shared of this ListFlavorsRequest.
+        :rtype: bool
+        """
+        return self._shared
+
+    @shared.setter
+    def shared(self, shared):
+        """Sets the shared of this ListFlavorsRequest.
+
+        参数解释：是否查询公共规格。  取值范围：true表示公共规格，所有租户可见。false表示私有规格，为当前租户所有。
+
+        :param shared: The shared of this ListFlavorsRequest.
+        :type shared: bool
+        """
+        self._shared = shared
+
+    def to_dict(self):
+        """Returns the model properties as a dict"""
+        result = {}
+
+        for attr, _ in six.iteritems(self.openapi_types):
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        if six.PY2:
+            import sys
+            reload(sys)
+            sys.setdefaultencoding("utf-8")
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, ListFlavorsRequest):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
