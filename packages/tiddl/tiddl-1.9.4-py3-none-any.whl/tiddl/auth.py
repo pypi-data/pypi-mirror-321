f397b24f21e6189e8988552eda734f8f133086e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:58:44 +0000

    Add package tiddl/1.9.4/tiddl-1.9.4-py3-none-any.whl

---
+++ packages/tiddl/tiddl-1.9.4-py3-none-any.whl/tiddl/auth.py
@@ -0,0 +1,42 @@
+from requests import request
+from .types.auth import AuthDeviceResponse, AuthResponse, AuthResponseWithRefresh
+
+AUTH_URL = "https://auth.tidal.com/v1/oauth2"
+CLIENT_ID = "zU4XHVVkc2tDPo4t"
+CLIENT_SECRET = "VJKhDFqJPqvsPVNBV6ukXTJmwlvbttP7wlMlrc72se4="
+
+
+def getDeviceAuth() -> AuthDeviceResponse:
+    return request(
+        "POST",
+        f"{AUTH_URL}/device_authorization",
+        data={"client_id": CLIENT_ID, "scope": "r_usr+w_usr+w_sub"},
+    ).json()
+
+
+def getToken(device_code: str) -> AuthResponseWithRefresh:
+    return request(
+        "POST",
+        f"{AUTH_URL}/token",
+        data={
+            "client_id": CLIENT_ID,
+            "device_code": device_code,
+            "grant_type": "urn:ietf:params:oauth:grant-type:device_code",
+            "scope": "r_usr+w_usr+w_sub",
+        },
+        auth=(CLIENT_ID, CLIENT_SECRET),
+    ).json()
+
+
+def refreshToken(refresh_token: str) -> AuthResponse:
+    return request(
+        "POST",
+        f"{AUTH_URL}/token",
+        data={
+            "client_id": CLIENT_ID,
+            "refresh_token": refresh_token,
+            "grant_type": "refresh_token",
+            "scope": "r_usr+w_usr+w_sub",
+        },
+        auth=(CLIENT_ID, CLIENT_SECRET),
+    ).json()
