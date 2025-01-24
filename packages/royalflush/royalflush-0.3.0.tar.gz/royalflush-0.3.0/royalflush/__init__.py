df158bc7447f0b3697786aaeab0f363bc8cf07e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:56:13 +0000

    Add package royalflush/0.3.0/royalflush-0.3.0.tar.gz

---
+++ packages/royalflush/royalflush-0.3.0.tar.gz/royalflush-0.3.0/royalflush/__init__.py
@@ -0,0 +1,17 @@
+"""Top-level package for Royal FLush."""
+
+__version__ = "0.3.0"
+
+from . import agent, behaviour, dataset, datatypes, log, message, nn, similarity, utils
+
+__all__ = [
+    "agent",
+    "behaviour",
+    "dataset",
+    "datatypes",
+    "log",
+    "message",
+    "nn",
+    "utils",
+    "similarity",
+]
