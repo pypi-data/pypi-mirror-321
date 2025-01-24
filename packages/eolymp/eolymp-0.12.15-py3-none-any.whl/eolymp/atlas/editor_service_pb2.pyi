0ed6f04031c4ac38db929df62bdb2dc3d29ffb59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 18:05:54 +0000

    Add package eolymp/0.12.15/eolymp-0.12.15-py3-none-any.whl

---
+++ packages/eolymp/eolymp-0.12.15-py3-none-any.whl/eolymp/atlas/editor_service_pb2.pyi
@@ -0,0 +1,48 @@
+from eolymp.annotations import http_pb2 as _http_pb2
+from eolymp.annotations import ratelimit_pb2 as _ratelimit_pb2
+from eolymp.annotations import scope_pb2 as _scope_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class DescribeStateInput(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class DescribeStateOutput(_message.Message):
+    __slots__ = ["runtime", "source_code", "input_data"]
+    RUNTIME_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_CODE_FIELD_NUMBER: _ClassVar[int]
+    INPUT_DATA_FIELD_NUMBER: _ClassVar[int]
+    runtime: str
+    source_code: str
+    input_data: str
+    def __init__(self, runtime: _Optional[str] = ..., source_code: _Optional[str] = ..., input_data: _Optional[str] = ...) -> None: ...
+
+class UpdateStateInput(_message.Message):
+    __slots__ = ["runtime", "source_code", "input_data"]
+    RUNTIME_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_CODE_FIELD_NUMBER: _ClassVar[int]
+    INPUT_DATA_FIELD_NUMBER: _ClassVar[int]
+    runtime: str
+    source_code: str
+    input_data: str
+    def __init__(self, runtime: _Optional[str] = ..., source_code: _Optional[str] = ..., input_data: _Optional[str] = ...) -> None: ...
+
+class UpdateStateOutput(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class PrintCodeInput(_message.Message):
+    __slots__ = ["runtime", "source_code"]
+    RUNTIME_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_CODE_FIELD_NUMBER: _ClassVar[int]
+    runtime: str
+    source_code: str
+    def __init__(self, runtime: _Optional[str] = ..., source_code: _Optional[str] = ...) -> None: ...
+
+class PrintCodeOutput(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
