218e1157b3c00c041dd2cac2824a649163e66612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:02:25 +0000

    Add package knify/1.8.35/knify-1.8.35.tar.gz

---
+++ packages/knify/knify-1.8.35.tar.gz/knify-1.8.35/knify/help.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+
+def get_pip_name():
+    return 'knify'
+
+
+def get_version():
+    return '1.8.35'
+
+
+def print_version():
+    print('''Knify %s
+Development tools for python.''' % get_version())
---
+++ packages/knify/knify-1.8.35.tar.gz/knify-1.8.35/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+from setuptools import setup, find_packages
+
+from knify import help
+
+setup(
+    name=help.get_pip_name(),
+    version=help.get_version(),
+    keywords=help.get_pip_name(),
+    description='Development tools for python',
+    license='MIT License',
+    url='https://github.com/qicongsheng/%s' % help.get_pip_name(),
+    author='qicongsheng',
+    author_email='qicongsheng@outlook.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'loguru',
+        'urllib3'
+    ]
+)
