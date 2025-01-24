c672bd5476a90bc7335007d8963d437635c32ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 23:04:50 +0000

    Add package django-spellbook/0.1.6/django_spellbook-0.1.6.tar.gz

---
+++ packages/django-spellbook/django_spellbook-0.1.6.tar.gz/django_spellbook-0.1.6/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-spellbook',
+    version='0.1.6',  # Increment version
+    author='Mathew Storm',
+    author_email='mathewstormdev@gmail.com',
+    description='A Django library for creating and managing content blocks in markdown for developers and bloggers',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/django-spellbook',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'django_spellbook': [
+            'templates/*.html',
+            'templates/*/*.html',
+            'templates/*/*/*.html',
+            'static/*',
+            'static/*/*',
+        ],
+    },
+    install_requires=[
+        'django>=5.0',
+        'markdown>=3.0',
+        'pyyaml>=6.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Framework :: Django :: 5.0',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    python_requires='>=3.10',
+    project_urls={
+        'Bug Reports': 'https://github.com/smattymatty/django_spellbook/issues',
+        'Source': 'https://github.com/smattymatty/django_spellbook',
+    },
+)
