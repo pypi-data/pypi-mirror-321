b12e43a8bb8906666ef51fe54e8f7f267912a838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:42 +0000

    Add package hg-oap/0.1.11/hg_oap-0.1.11.tar.gz

---
+++ packages/hg-oap/hg_oap-0.1.11.tar.gz/hg_oap-0.1.11/src/hg_oap/instruments/physical.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from enum import Enum
+
+from hg_oap.assets.commodities import Commodity
+from hg_oap.instruments.instrument import Instrument
+from hg_oap.units import Quantity
+
+
+@dataclass(frozen=True)
+class PhysicalCommodity(Instrument):
+    asset: Commodity
+
+    unit_conversion_factors: tuple[Quantity, ...] = lambda self: self.asset.unit_conversion_factors
+
+
+class PhysicalCommodities(Enum):
+    ...
+    # MAL = PhysicalCommodity("MAL", asset=BaseMetals.MAL)
