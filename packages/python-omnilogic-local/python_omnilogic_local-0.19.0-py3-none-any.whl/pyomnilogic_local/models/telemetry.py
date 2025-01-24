c8022da91d79403fc21e83f3685cd4b8f6067baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:27 +0000

    Add package python-omnilogic-local/0.19.0/python_omnilogic_local-0.19.0-py3-none-any.whl

---
+++ packages/python-omnilogic-local/python_omnilogic_local-0.19.0-py3-none-any.whl/pyomnilogic_local/models/telemetry.py
@@ -0,0 +1,264 @@
+from __future__ import annotations
+
+from typing import Any, SupportsInt, TypeAlias, TypeVar, cast, overload
+
+from pydantic.v1 import BaseModel, Field, ValidationError
+from xmltodict import parse as xml_parse
+
+from ..exceptions import OmniParsingException
+from ..omnitypes import (
+    BackyardState,
+    ChlorinatorOperatingMode,
+    ColorLogicBrightness,
+    ColorLogicPowerState,
+    ColorLogicShow,
+    ColorLogicSpeed,
+    CSADMode,
+    FilterState,
+    FilterValvePosition,
+    FilterWhyOn,
+    HeaterMode,
+    HeaterState,
+    OmniType,
+    PumpState,
+    RelayState,
+    ValveActuatorState,
+)
+
+# Example telemetry XML data:
+#
+# <?xml version="1.0" encoding="UTF-8" ?>
+# <STATUS version="1.11">
+#     <Backyard systemId="0" statusVersion="11" airTemp="77" state="1" ConfigChksum="2211028" mspVersion="R0408000" />
+#     <BodyOfWater systemId="7" waterTemp="-1" flow="255" />
+#     <Filter systemId="8" filterState="0" filterSpeed="0" valvePosition="1" whyFilterIsOn="0" fpOverride="0" reportedFilterSpeed="0" power="0" lastSpeed="50" /> # noqa: E501
+#     <ValveActuator systemId="9" valveActuatorState="0" whyOn="0" />
+#     <ColorLogic-Light systemId="10" lightState="6" currentShow="0" speed="4" brightness="4" specialEffect="0" />
+#     <ValveActuator systemId="13" valveActuatorState="0" whyOn="0" />
+#     <ValveActuator systemId="14" valveActuatorState="0" whyOn="0" />
+#     <VirtualHeater systemId="18" Current-Set-Point="85" enable="1" SolarSetPoint="90" Mode="0" SilentMode="0" whyHeaterIsOn="1" />
+#     <Heater systemId="19" heaterState="0" temp="74" enable="1" priority="254" maintainFor="24" />
+#     <Group systemId="21" groupState="0" />
+# </STATUS>
+
+
+class TelemetryBackyard(BaseModel):
+    omni_type: OmniType = OmniType.BACKYARD
+    system_id: int = Field(alias="@systemId")
+    status_version: int = Field(alias="@statusVersion")
+    air_temp: int = Field(alias="@airTemp")
+    state: BackyardState | int = Field(alias="@state")
+    # The below two fields are only available for telemetry with a status_version >= 11
+    config_checksum: int | None = Field(alias="@ConfigChksum")
+    msp_version: str | None = Field(alias="@mspVersion")
+
+
+class TelemetryBoW(BaseModel):
+    omni_type: OmniType = OmniType.BOW
+    system_id: int = Field(alias="@systemId")
+    water_temp: int = Field(alias="@waterTemp")
+    flow: int = Field(alias="@flow")
+
+
+class TelemetryChlorinator(BaseModel):
+    omni_type: OmniType = OmniType.CHLORINATOR
+    system_id: int = Field(alias="@systemId")
+    status_raw: int = Field(alias="@status")
+    instant_salt_level: int = Field(alias="@instantSaltLevel")
+    avg_salt_level: int = Field(alias="@avgSaltLevel")
+    chlr_alert: int = Field(alias="@chlrAlert")
+    chlr_error: int = Field(alias="@chlrError")
+    sc_mode: int = Field(alias="@scMode")
+    operating_state: int = Field(alias="@operatingState")
+    timed_percent: int | None = Field(alias="@Timed-Percent")
+    operating_mode: ChlorinatorOperatingMode | int = Field(alias="@operatingMode")
+    enable: bool = Field(alias="@enable")
+
+    # Still need to do a bit more work to determine if a chlorinator is actively chlorinating
+    # @property
+    # def active(self) -> bool:
+    #     return self.status_raw & 4 == 4 # Check if bit 4 is set, which means the chlorinator is currently chlorinating
+
+
+class TelemetryCSAD(BaseModel):
+    omni_type: OmniType = OmniType.CSAD
+    system_id: int = Field(alias="@systemId")
+    status_raw: int = Field(alias="@status")
+    ph: float = Field(alias="@ph")
+    orp: int = Field(alias="@orp")
+    mode: CSADMode | int = Field(alias="@mode")
+
+
+class TelemetryColorLogicLight(BaseModel):
+    omni_type: OmniType = OmniType.CL_LIGHT
+    system_id: int = Field(alias="@systemId")
+    state: ColorLogicPowerState | int = Field(alias="@lightState")
+    show: ColorLogicShow | int = Field(alias="@currentShow")
+    speed: ColorLogicSpeed | int = Field(alias="@speed")
+    brightness: ColorLogicBrightness | int = Field(alias="@brightness")
+    special_effect: int = Field(alias="@specialEffect")
+
+
+class TelemetryFilter(BaseModel):
+    omni_type: OmniType = OmniType.FILTER
+    system_id: int = Field(alias="@systemId")
+    state: FilterState | int = Field(alias="@filterState")
+    speed: int = Field(alias="@filterSpeed")
+    valve_position: FilterValvePosition | int = Field(alias="@valvePosition")
+    why_on: FilterWhyOn | int = Field(alias="@whyFilterIsOn")
+    reported_speed: int = Field(alias="@reportedFilterSpeed")
+    power: int = Field(alias="@power")
+    last_speed: int = Field(alias="@lastSpeed")
+
+
+class TelemetryGroup(BaseModel):
+    omni_type: OmniType = OmniType.GROUP
+    system_id: int = Field(alias="@systemId")
+    state: int = Field(alias="@groupState")
+
+
+class TelemetryHeater(BaseModel):
+    omni_type: OmniType = OmniType.HEATER
+    system_id: int = Field(alias="@systemId")
+    state: HeaterState | int = Field(alias="@heaterState")
+    temp: int = Field(alias="@temp")
+    enabled: bool = Field(alias="@enable")
+    priority: int = Field(alias="@priority")
+    maintain_for: int = Field(alias="@maintainFor")
+
+
+class TelemetryPump(BaseModel):
+    omni_type: OmniType = OmniType.PUMP
+    system_id: int = Field(alias="@systemId")
+    state: PumpState | int = Field(alias="@pumpState")
+    speed: int = Field(alias="@pumpSpeed")
+    last_speed: int = Field(alias="@lastSpeed")
+    why_on: int = Field(alias="@whyOn")
+
+
+class TelemetryRelay(BaseModel):
+    omni_type: OmniType = OmniType.RELAY
+    system_id: int = Field(alias="@systemId")
+    state: RelayState | int = Field(alias="@relayState")
+    why_on: int = Field(alias="@whyOn")
+
+
+class TelemetryValveActuator(BaseModel):
+    omni_type: OmniType = OmniType.VALVE_ACTUATOR
+    system_id: int = Field(alias="@systemId")
+    state: ValveActuatorState | int = Field(alias="@valveActuatorState")
+    why_on: int = Field(alias="@whyOn")
+
+
+class TelemetryVirtualHeater(BaseModel):
+    omni_type: OmniType = OmniType.VIRT_HEATER
+    system_id: int = Field(alias="@systemId")
+    current_set_point: int = Field(alias="@Current-Set-Point")
+    enabled: bool = Field(alias="@enable")
+    solar_set_point: int = Field(alias="@SolarSetPoint")
+    mode: HeaterMode | int = Field(alias="@Mode")
+    silent_mode: int = Field(alias="@SilentMode")
+    why_on: int = Field(alias="@whyHeaterIsOn")
+
+
+TelemetryType: TypeAlias = (
+    TelemetryBackyard
+    | TelemetryBoW
+    | TelemetryChlorinator
+    | TelemetryColorLogicLight
+    | TelemetryFilter
+    | TelemetryGroup
+    | TelemetryHeater
+    | TelemetryPump
+    | TelemetryRelay
+    | TelemetryValveActuator
+    | TelemetryVirtualHeater
+)
+
+
+class Telemetry(BaseModel):
+    version: str = Field(alias="@version")
+    backyard: TelemetryBackyard = Field(alias="Backyard")
+    bow: list[TelemetryBoW] = Field(alias="BodyOfWater")
+    chlorinator: list[TelemetryChlorinator] | None = Field(alias="Chlorinator")
+    colorlogic_light: list[TelemetryColorLogicLight] | None = Field(alias="ColorLogic-Light")
+    csad: list[TelemetryCSAD] | None = Field(alias="CSAD")
+    filter: list[TelemetryFilter] | None = Field(alias="Filter")
+    group: list[TelemetryGroup] | None = Field(alias="Group")
+    heater: list[TelemetryHeater] | None = Field(alias="Heater")
+    pump: list[TelemetryPump] | None = Field(alias="Pump")
+    relay: list[TelemetryRelay] | None = Field(alias="Relay")
+    valve_actuator: list[TelemetryValveActuator] | None = Field(alias="ValveActuator")
+    virtual_heater: list[TelemetryVirtualHeater] | None = Field(alias="VirtualHeater")
+
+    class Config:
+        orm_mode = True
+
+    @staticmethod
+    def load_xml(xml: str) -> Telemetry:
+        TypeVar("KT")
+        TypeVar("VT", SupportsInt, Any)
+
+        @overload
+        def xml_postprocessor(path: Any, key: Any, value: SupportsInt) -> tuple[Any, SupportsInt]: ...
+
+        @overload
+        def xml_postprocessor(path: Any, key: Any, value: Any) -> tuple[Any, Any]: ...
+
+        def xml_postprocessor(path: Any, key: Any, value: SupportsInt | Any) -> tuple[Any, SupportsInt | Any]:
+            """Post process XML to attempt to convert values to int.
+
+            Pydantic can coerce values natively, but the Omni API returns values as strings of numbers (I.E. "2", "5", etc) and we need them
+            coerced into int enums.  Pydantic only seems to be able to handle one coercion, so it could coerce an int into an Enum, but it
+            cannot coerce a string into an int and then into the Enum. We help it out a little bit here by pre-emptively coercing any
+            string ints into an int, then pydantic handles the int to enum coercion if necessary.
+            """
+            newvalue: SupportsInt | Any
+
+            try:
+                newvalue = int(value)
+            except (ValueError, TypeError):
+                newvalue = value
+
+            return key, newvalue
+
+        data = xml_parse(
+            xml,
+            postprocessor=xml_postprocessor,
+            # Some things will be lists or not depending on if a pool has more than one of that piece of equipment.  Here we are coercing
+            # everything into lists to make the parsing more consistent. This does mean that some things that would normally never be lists
+            # will become lists (I.E.: Backyard, VirtualHeater), but the upside is that we need far less conditional code to deal with the
+            # "maybe list maybe not" devices.
+            force_list=(
+                OmniType.BOW,
+                OmniType.CHLORINATOR,
+                OmniType.CSAD,
+                OmniType.CL_LIGHT,
+                OmniType.FILTER,
+                OmniType.GROUP,
+                OmniType.HEATER,
+                OmniType.PUMP,
+                OmniType.RELAY,
+                OmniType.VALVE_ACTUATOR,
+                OmniType.VIRT_HEATER,
+            ),
+        )
+        try:
+            return Telemetry.parse_obj(data["STATUS"])
+        except ValidationError as exc:
+            raise OmniParsingException(f"Failed to parse Telemetry: {exc}") from exc
+
+    def get_telem_by_systemid(self, system_id: int) -> TelemetryType | None:
+        for field_name, value in self:
+            if field_name == "version" or value is None:
+                continue
+            if isinstance(value, list):
+                for model in value:
+                    cast_model = cast(TelemetryType, model)
+                    if cast_model.system_id == system_id:
+                        return cast_model
+            else:
+                cast_model = cast(TelemetryType, value)
+                if cast_model.system_id == system_id:
+                    return cast_model
+        return None
---
+++ packages/python-omnilogic-local/python_omnilogic_local-0.19.0-py3-none-any.whl/pyomnilogic_local/omnitypes.py
@@ -0,0 +1,391 @@
+from enum import Enum, IntEnum
+
+from .util import PrettyEnum
+
+
+# OmniAPI Enums
+class MessageType(Enum):
+    XML_ACK = 0000
+    REQUEST_CONFIGURATION = 1
+    SET_FILTER_SPEED = 9
+    SET_HEATER_COMMAND = 11
+    SET_SUPERCHLORINATE = 15
+    REQUEST_LOG_CONFIG = 31
+    SET_SOLAR_SET_POINT_COMMAND = 40
+    SET_HEATER_MODE_COMMAND = 42
+    SET_CHLOR_ENABLED = 121
+    SET_HEATER_ENABLED = 147
+    SET_CHLOR_PARAMS = 155
+    SET_EQUIPMENT = 164
+    CREATE_SCHEDULE = 230
+    DELETE_SCHEDULE = 231
+    GET_TELEMETRY = 300
+    GET_ALARM_LIST = 304
+    SET_STANDALONE_LIGHT_SHOW = 308
+    SET_SPILLOVER = 311
+    RUN_GROUP_CMD = 317
+    RESTORE_IDLE_STATE = 340
+    GET_FILTER_DIAGNOSTIC_INFO = 386
+    HANDSHAKE = 1000
+    ACK = 1002
+    MSP_TELEMETRY_UPDATE = 1004
+    MSP_CONFIGURATIONUPDATE = 1003
+    MSP_ALARM_LIST = 1304
+    MSP_LEADMESSAGE = 1998
+    MSP_BLOCKMESSAGE = 1999
+
+
+class ClientType(Enum):
+    XML = 0
+    SIMPLE = 1
+    OMNI = 3
+
+
+class OmniType(str, Enum):
+    BACKYARD = "Backyard"
+    BOW = "BodyOfWater"
+    BOW_MSP = "Body-of-water"
+    CHLORINATOR = "Chlorinator"
+    CHLORINATOR_EQUIP = "Chlorinator-Equipment"
+    CSAD = "CSAD"
+    CL_LIGHT = "ColorLogic-Light"
+    FAVORITES = "Favorites"
+    FILTER = "Filter"
+    GROUP = "Group"
+    GROUPS = "Groups"
+    HEATER = "Heater"
+    HEATER_EQUIP = "Heater-Equipment"
+    PUMP = "Pump"
+    RELAY = "Relay"
+    SCHE = "sche"
+    SCHEDULE = "Schedule"
+    SENSOR = "Sensor"
+    SYSTEM = "System"
+    VALVE_ACTUATOR = "ValveActuator"
+    VIRT_HEATER = "VirtualHeater"
+
+    def __str__(self) -> str:
+        return OmniType[self.name].value
+
+
+# Backyard/BoW
+class BackyardState(PrettyEnum):
+    OFF = 0
+    ON = 1
+    SERVICE_MODE = 2
+    CONFIG_MODE = 3
+    TIMED_SERVICE_MODE = 4
+
+
+class BodyOfWaterState(PrettyEnum):
+    NO_FLOW = 0
+    FLOW = 1
+
+
+class BodyOfWaterType(str, PrettyEnum):
+    POOL = "BOW_POOL"
+    SPA = "BOW_SPA"
+
+
+# Chlorinators
+# Chlorinator status is a bitmask that we still need to figure out
+# class ChlorinatorStatus(str,Enum):
+#     pass
+
+
+# I have seen one pool that had an operatingMode of 3, I am not sure what that means, perhaps that is an OFF mode
+class ChlorinatorOperatingMode(IntEnum):
+    DISABLED = 0
+    TIMED = 1
+    ORP = 2
+    OFF = 3
+
+
+class ChlorinatorDispenserType(str, PrettyEnum):
+    SALT = "SALT_DISPENSING"
+    LIQUID = "LIQUID_DISPENSING"
+
+
+class ChlorinatorCellType(PrettyEnum):
+    UNKNOWN = "CELL_TYPE_UNKNOWN"
+    T3 = "CELL_TYPE_T3"
+    T5 = "CELL_TYPE_T5"
+    T9 = "CELL_TYPE_T9"
+    T15 = "CELL_TYPE_T15"
+    T15_LS = "CELL_TYPE_T15_LS"
+    TCELLS315 = "CELL_TYPE_TCELLS315"
+    TCELLS325 = "CELL_TYPE_TCELLS325"
+    TCELLS340 = "CELL_TYPE_TCELLS340"
+    LIQUID = "CELL_TYPE_LIQUID"
+    TABLET = "CELL_TYPE_TABLET"
+
+    # There is probably an easier way to do this
+    def __int__(self) -> int:
+        return ChlorinatorCellInt[self.name].value
+
+
+class ChlorinatorCellInt(IntEnum):
+    UNKNOWN = 0
+    T3 = 1
+    T5 = 2
+    T9 = 3
+    T15 = 4
+    T15_LS = 5
+    TCELLS315 = 6
+    TCELLS325 = 7
+    TCELLS340 = 8
+    LIQUID = 9
+    TABLET = 10
+
+
+# Lights
+class ColorLogicSpeed(PrettyEnum):
+    ONE_SIXTEENTH = 0
+    ONE_EIGHTH = 1
+    ONE_QUARTER = 2
+    ONE_HALF = 3
+    ONE_TIMES = 4
+    TWO_TIMES = 5
+    FOUR_TIMES = 6
+    EIGHT_TIMES = 7
+    SIXTEEN_TIMES = 8
+
+
+class ColorLogicBrightness(PrettyEnum):
+    TWENTY_PERCENT = 0
+    FOURTY_PERCENT = 1
+    SIXTY_PERCENT = 2
+    EIGHTY_PERCENT = 3
+    ONE_HUNDRED_PERCENT = 4
+
+
+class ColorLogicShow(PrettyEnum):
+    VOODOO_LOUNGE = 0
+    DEEP_BLUE_SEA = 1
+    ROYAL_BLUE = 2
+    AFTERNOON_SKY = 3
+    AQUA_GREEN = 4
+    EMERALD = 5
+    CLOUD_WHITE = 6
+    WARM_RED = 7
+    FLAMINGO = 8
+    VIVID_VIOLET = 9
+    SANGRIA = 10
+    TWILIGHT = 11
+    TRANQUILITY = 12
+    GEMSTONE = 13
+    USA = 14
+    MARDI_GRAS = 15
+    COOL_CABARET = 16
+    #### The below options only work on lights that support OmniDirect / V2-Active in MSPConfig
+    YELLOW = 17
+    ORANGE = 18
+    GOLD = 19
+    MINT = 20
+    TEAL = 21
+    BURNT_ORANGE = 22
+    PURE_WHITE = 23
+    CRISP_WHITE = 24
+    WARM_WHITE = 25
+    BRIGHT_YELLOW = 26
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class ColorLogicPowerState(PrettyEnum):
+    OFF = 0
+    POWERING_OFF = 1
+    CHANGING_SHOW = 3
+    FIFTEEN_SECONDS_WHITE = 4
+    ACTIVE = 6
+    COOLDOWN = 7
+
+
+class ColorLogicLightType(str, PrettyEnum):
+    UCL = "COLOR_LOGIC_UCL"
+    FOUR_ZERO = "COLOR_LOGIC_4_0"
+    TWO_FIVE = "COLOR_LOGIC_2_5"
+
+    def __str__(self) -> str:
+        return ColorLogicLightType[self.name].value
+
+
+class CSADType(str, PrettyEnum):
+    ACID = "ACID"
+    CO2 = "CO2"
+
+
+# Chemistry Sense and Dispense
+class CSADStatus(PrettyEnum):
+    NOT_DISPENSING = 0
+    DISPENSING = 1
+
+
+class CSADMode(PrettyEnum):
+    OFF = 0
+    AUTO = 1
+    FORCE_ON = 2
+    MONITORING = 3
+    DISPENSING_OFF = 4
+
+
+# Filters
+class FilterState(PrettyEnum):
+    OFF = 0
+    ON = 1
+    PRIMING = 2
+    WAITING_TURN_OFF = 3
+    WAITING_TURN_OFF_MANUAL = 4
+    HEATER_EXTEND = 5
+    COOLDOWN = 6
+    SUSPEND = 7
+    CSAD_EXTEND = 8
+    FILTER_SUPERCHLORINATE = 9
+    FILTER_FORCE_PRIMING = 10
+    FILTER_WAITING_TURN_OFF = 11
+
+
+class FilterType(str, PrettyEnum):
+    VARIABLE_SPEED = "FMT_VARIABLE_SPEED_PUMP"
+    DUAL_SPEED = "FMT_DUAL_SPEED"
+    SINGLE_SPEED = "FMT_SINGLE_SPEED"
+
+
+class FilterValvePosition(PrettyEnum):
+    POOL_ONLY = 1
+    SPA_ONLY = 2
+    SPILLOVER = 3
+    LOW_PRIO_HEAT = 4
+    HIGH_PRIO_HEAT = 5
+
+
+class FilterWhyOn(PrettyEnum):
+    OFF = 0
+    NO_WATER_FLOW = 1
+    COOLDOWN = 2
+    PH_REDUCE_EXTEND = 3
+    HEATER_EXTEND = 4
+    PAUSED = 5
+    VALVE_CHANGING = 6
+    FORCE_HIGH_SPEED = 7
+    OFF_EXTERNAL_INTERLOCK = 8
+    SUPER_CHLORINATE = 9
+    COUNTDOWN = 10
+    MANUAL_ON = 11
+    MANUAL_SPILLOVER = 12
+    TIMER_SPILLOVER = 13
+    TIMER_ON = 14
+    FREEZE_PROTECT = 15
+    UNKNOWN_16 = 16
+    UNKNOWN_17 = 17
+    UNKNOWN_18 = 18
+
+
+# Heaters
+class HeaterState(PrettyEnum):
+    OFF = 0
+    ON = 1
+    PAUSE = 2
+
+
+class HeaterType(str, PrettyEnum):
+    GAS = "HTR_GAS"
+    HEAT_PUMP = "HTR_HEAT_PUMP"
+    SOLAR = "HTR_SOLAR"
+    ELECTRIC = "HTR_ELECTRIC"
+    GEOTHERMAL = "HTR_GEOTHERMAL"
+    SMART = "HTR_SMART"
+
+
+class HeaterMode(PrettyEnum):
+    HEAT = 0
+    COOL = 1
+    AUTO = 2
+
+
+# Pumps
+class PumpState(PrettyEnum):
+    OFF = 0
+    ON = 1
+
+
+class PumpType(str, PrettyEnum):
+    SINGLE_SPEED = "PMP_SINGLE_SPEED"
+    DUAL_SPEED = "PMP_DUAL_SPEED"
+    VARIABLE_SPEED = "PMP_VARIABLE_SPEED_PUMP"
+
+
+class PumpFunction(str, PrettyEnum):
+    PUMP = "PMP_PUMP"
+    WATER_FEATURE = "PMP_WATER_FEATURE"
+    CLEANER = "PMP_CLEANER"
+    WATER_SLIDE = "PMP_WATER_SLIDE"
+    WATERFALL = "PMP_WATERFALL"
+    LAMINARS = "PMP_LAMINARS"
+    FOUNTAIN = "PMP_FOUNTAIN"
+    JETS = "PMP_JETS"
+    BLOWER = "PMP_BLOWER"
+    ACCESSORY = "PMP_ACCESSORY"
+    CLEANER_PRESSURE = "PMP_CLEANER_PRESSURE"
+    CLEANER_SUCTION = "PMP_CLEANER_SUCTION"
+    CLEANER_ROBOTIC = "PMP_CLEANER_ROBOTIC"
+    CLEANER_IN_FLOOR = "PMP_CLEANER_IN_FLOOR"
+
+
+# Relays
+class RelayFunction(str, PrettyEnum):
+    WATER_FEATURE = "RLY_WATER_FEATURE"
+    LIGHT = "RLY_LIGHT"
+    BACKYARD_LIGHT = "RLY_BACKYARD_LIGHT"
+    POOL_LIGHT = "RLY_POOL_LIGHT"
+    CLEANER = "RLY_CLEANER"
+    WATER_SLIDE = "RLY_WATER_SLIDE"
+    WATERFALL = "RLY_WATERFALL"
+    LAMINARS = "RLY_LAMINARS"
+    FOUNTAIN = "RLY_FOUNTAIN"
+    FIREPIT = "RLY_FIREPIT"
+    JETS = "RLY_JETS"
+    BLOWER = "RLY_BLOWER"
+    ACCESSORY = "RLY_ACCESSORY"
+    CLEANER_PRESSURE = "RLY_CLEANER_PRESSURE"
+    CLEANER_SUCTION = "RLY_CLEANER_SUCTION"
+    CLEANER_ROBOTIC = "RLY_CLEANER_ROBOTIC"
+    CLEANER_IN_FLOOR = "RLY_CLEANER_IN_FLOOR"
+
+
+class RelayState(PrettyEnum):
+    OFF = 0
+    ON = 1
+
+
+class RelayType(str, PrettyEnum):
+    VALVE_ACTUATOR = "RLY_VALVE_ACTUATOR"
+    HIGH_VOLTAGE = "RLY_HIGH_VOLTAGE_RELAY"
+    LOW_VOLTAGE = "RLY_LOW_VOLTAGE_RELAY"
+
+
+# Sensors
+class SensorType(str, PrettyEnum):
+    AIR_TEMP = "SENSOR_AIR_TEMP"
+    SOLAR_TEMP = "SENSOR_SOLAR_TEMP"
+    WATER_TEMP = "SENSOR_WATER_TEMP"
+    FLOW = "SENSOR_FLOW"
+    ORP = "SENSOR_ORP"
+    EXT_INPUT = "SENSOR_EXT_INPUT"
+
+
+class SensorUnits(str, PrettyEnum):
+    FAHRENHEIT = "UNITS_FAHRENHEIT"
+    CELSIUS = "UNITS_CELSIUS"
+    PPM = "UNITS_PPM"
+    GRAMS_PER_LITER = "UNITS_GRAMS_PER_LITER"
+    MILLIVOLTS = "UNITS_MILLIVOLTS"
+    NO_UNITS = "UNITS_NO_UNITS"
+    ACTIVE_INACTIVE = "UNITS_ACTIVE_INACTIVE"
+
+
+# Valve Actuators
+class ValveActuatorState(PrettyEnum):
+    OFF = 0
+    ON = 1
