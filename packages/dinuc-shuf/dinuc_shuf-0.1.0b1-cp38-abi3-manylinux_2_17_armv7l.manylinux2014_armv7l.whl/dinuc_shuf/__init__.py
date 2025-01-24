cc7777f171bf7a2d2f26f435ac361f1af517ceb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 22:05:00 +0000

    Add package dinuc-shuf/0.1.0b1/dinuc_shuf-0.1.0b1-cp38-abi3-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/dinuc-shuf/dinuc_shuf-0.1.0b1-cp38-abi3-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/dinuc_shuf/__init__.py
@@ -0,0 +1,11 @@
+"""
+This module provides a method `shuffle` to dinucleotide shuffle one-hot encoded sequences.
+
+For installation and usage instructions, check out the [GitHub repository](https://github.com/austintwang/dinuc_shuf).
+"""
+
+from .main import shuffle
+
+__all__ = ['shuffle']
+
+__docformat__ = 'numpy'
