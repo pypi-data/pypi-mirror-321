f2ebe8206c29fea5512013adb3f6bf5f161b2055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 18:05:41 +0000

    Add package volcengine-python-sdk/1.0.121/volcengine-python-sdk-1.0.121.tar.gz

---
+++ packages/volcengine-python-sdk/volcengine-python-sdk-1.0.121.tar.gz/volcengine-python-sdk-1.0.121/volcenginesdkarkruntime/resources/__init__.py
@@ -0,0 +1,25 @@
+from .chat import Chat, AsyncChat
+from .embeddings import Embeddings, AsyncEmbeddings
+from .tokenization import Tokenization, AsyncTokenization
+from .classification import Classification, AsyncClassification
+from .bot import BotChat, AsyncBotChat
+from .context import Context, AsyncContext
+from .content_generation import ContentGeneration, AsyncContentGeneration
+from .batch_chat import BatchChat, AsyncBatchChat
+
+__all__ = [
+    "Chat",
+    "BotChat",
+    "AsyncChat",
+    "AsyncBotChat",
+    "Embeddings",
+    "AsyncEmbeddings",
+    "Tokenization",
+    "AsyncTokenization",
+    "Context",
+    "AsyncContext",
+    "ContentGeneration",
+    "AsyncContentGeneration"
+    "BatchChat",
+    "AsyncBatchChat"
+]
