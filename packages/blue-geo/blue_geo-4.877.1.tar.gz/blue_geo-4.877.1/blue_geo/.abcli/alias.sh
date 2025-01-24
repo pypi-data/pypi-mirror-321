e7fe2a32b9a5a8e8f028bccad6678a1bea71beb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:56:55 +0000

    Add package blue-geo/4.877.1/blue_geo-4.877.1.tar.gz

---
+++ packages/blue-geo/blue_geo-4.877.1.tar.gz/blue_geo-4.877.1/blue_geo/.abcli/alias.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+alias @catalog=blue_geo_catalog
+alias @datacube=blue_geo_datacube
+
+alias @gdal=blue_geo_gdal
+
+alias @geo=blue_geo
+
+alias @maxar=blue_geo_maxar_open_data
+
+alias @targets=blue_geo_watch_targets
+
+alias QGIS=blue_geo_QGIS
---
+++ packages/blue-geo/blue_geo-4.877.1.tar.gz/blue_geo-4.877.1/blue_geo/.abcli/watch/map.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+function blue_geo_watch_map() {
+    local options=$1
+
+    local algo=$(abcli_option "$options" algo modality)
+    local do_dryrun=$(abcli_option_int "$options" dryrun 0)
+    local do_download=$(abcli_option_int "$options" download $(abcli_not $do_dryrun))
+
+    local query_object_name=$(abcli_clarify_object $2 .)
+    [[ "$do_download" == 1 ]] &&
+        abcli_download - $query_object_name
+
+    abcli_log "🌐 @geo watch map $query_object_name: $options -> @algo"
+
+    abcli_eval - \
+        blue_geo_watch_algo_${algo}_map "$@"
+}
