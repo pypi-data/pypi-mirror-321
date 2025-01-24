edafe393477635cf4f2cb6ac007d4f049e51963b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:02:46 +0000

    Add package nanosurf/1.11.0/nanosurf-1.11.0.tar.gz

---
+++ packages/nanosurf/nanosurf-1.11.0.tar.gz/nanosurf-1.11.0/nanosurf/lib/datatypes/__init__.py
@@ -0,0 +1,1 @@
+import nanosurf.lib.datatypes.prop_val as prop_val
---
+++ packages/nanosurf/nanosurf-1.11.0.tar.gz/nanosurf-1.11.0/nanosurf/lib/util/__init__.py
@@ -0,0 +1,11 @@
+
+"""Copyright (C) Nanosurf AG - All Rights Reserved (2021)
+License - MIT"""
+
+import nanosurf.lib.util.dataexport as dataexport
+import nanosurf.lib.util.fileutil as fileutil
+import nanosurf.lib.util.nhf_reader as nhf_reader
+import nanosurf.lib.util.nid_reader as nid_reader
+import nanosurf.lib.util.gwy_export as gwy_export
+
+
