df231fb723183b62165909dcdf53ef19a6c315d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 13:03:00 +0000

    Add package skylog-sdk/0.1.15/skylog_sdk-0.1.15.tar.gz

---
+++ packages/skylog-sdk/skylog_sdk-0.1.15.tar.gz/skylog_sdk-0.1.15/skylog/integration/abc.py
@@ -0,0 +1,84 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from skylog.integration import AlertingProvider
+from skylog.integration.utils import BaseClient, RetryMixin
+
+
+class BaseAlertingSkyLogClient(RetryMixin, BaseClient, ABC):
+    """
+    client to send alert to skylog
+    """
+
+    @abstractmethod
+    def fire_alert(
+        self,
+        description: str,
+        *,
+        instance_name: str = None,
+        summery=None,
+        provider: Optional[AlertingProvider] = None,
+        notify_on_duplicate: bool = False,
+    ) -> bool:
+        """
+        firing an alert cause to add a record in triggered list in the panel and Immediately notice messages are sent
+         to related endpoints.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def stop_alert(
+        self,
+        description: str,
+        *,
+        instance_name: str = None,
+        summery: str = None,
+        provider: Optional[AlertingProvider] = None,
+        notify_on_duplicate: bool = False,
+    ) -> bool:
+        """
+        Stopping a fired alert cause remove its record in triggered list furthermore
+        it's solving message are sent to endpoints.
+        This procedure can be done manually in the panel or automatically by api call.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def notify(
+        self,
+        description: str,
+        instance_name: str = None,
+        summery: str = None,
+        *,
+        provider: Optional[AlertingProvider] = None,
+    ) -> bool:
+        """
+        Stopping a fired alert cause remove its record in triggered list furthermore
+        it's solving issue that sent to endpoints.
+        This procedure can be done manually in the panel or automatically by api call.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def _request(
+        self,
+        description: str,
+        instance_name: str,
+        summery: str,
+        path: str,
+        *,
+        provider: Optional[AlertingProvider] = None,
+        notify_on_duplicate: bool = False,
+    ) -> bool:
+        """
+        Parameter:
+            alert_name: (required) the alert_name of created alert_rule.
+            Instance: (required) instance of a group alert.
+                In detail instance use to determine a specific part of a universal alert.
+                For example, in "Database Down" alert rule instance can be "DB_Slave1" or for "low SMS credit"
+                instance can be "Mobtakeran"
+            description: all description need to explain the situation more.
+            summery: summery to explain the situation more.
+
+        """
+        raise NotImplementedError
---
+++ packages/skylog-sdk/skylog_sdk-0.1.15.tar.gz/skylog_sdk-0.1.15/skylog/integration/config.py
@@ -0,0 +1,50 @@
+import dataclasses
+from typing import Type
+
+from decouple import config
+
+
+class ClientSettings:
+    PROXY_USERNAME: str = None
+    PROXY_PASSWORD: str = None
+    PROXY_IP: str = None
+    PROXY_PORT: str = None
+    SKY_LOG_ALERTING_TELEGRAM_ALERT_NAME: str = None
+    SKY_LOG_ALERTING_PHONE_CALL_ALERT_NAME: str = None
+    SKY_LOG_ALERTING_SMS_ALERT_NAME: str = None
+    SKY_LOG_ALERTING_BASE_URL: str = None
+    SKY_LOG_ALERTING_TOKEN: str = None
+
+    def _setup(self):
+        self.PROXY_USERNAME = config("PROXY_USERNAME", "")
+        self.PROXY_PASSWORD = config("PROXY_PASSWORD", "")
+        self.PROXY_IP = config("PROXY_IP", "")
+        self.PROXY_PORT = config("PROXY_PORT", "")
+        self.DEFAULT_SKY_LOG_ALERTING_TELEGRAM_ALERT_NAME = config("DEFAULT_SKY_LOG_ALERTING_TELEGRAM_ALERT_NAME")
+        self.DEFAULT_SKY_LOG_ALERTING_PHONE_CALL_ALERT_NAME = config("DEFAULT_SKY_LOG_ALERTING_PHONE_CALL_ALERT_NAME")
+        self.DEFAULT_SKY_LOG_ALERTING_SMS_ALERT_NAME = config("DEFAULT_SKY_LOG_ALERTING_SMS_ALERT_NAME")
+        self.SKY_LOG_ALERTING_BASE_URL = config("SKY_LOG_ALERTING_BASE_URL")
+        self.SKY_LOG_ALERTING_TOKEN = config("SKY_LOG_ALERTING_TOKEN")
+
+
+class LazySettings:
+    _wrapped = None
+    client_settings_class: Type[ClientSettings]
+
+    def __init__(self, client_settings_class: Type[ClientSettings] = ClientSettings):
+        self.client_settings_class = client_settings_class
+
+    def _setup(self):
+        if self._wrapped is None:
+            setting = dataclasses.dataclass(self.client_settings_class)()
+            setting._setup()
+            self._wrapped = setting
+        return self._wrapped
+
+    def __getattr__(self, item):
+        if self._wrapped is None:
+            self._setup()
+        return getattr(self._wrapped, item)
+
+
+settings = LazySettings()
