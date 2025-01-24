0ef7fb3700a975881b2399b292b1f1bffecb5571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 15:06:27 +0000

    Add package streamlit-toggle-diy/1.1.3/streamlit_toggle_diy-1.1.3-py3-none-any.whl

---
+++ packages/streamlit-toggle-diy/streamlit_toggle_diy-1.1.3-py3-none-any.whl/streamlit_toggle_diy/frontend/build/precache-manifest.b7df6085566f07f8f27b94ee8ed2d302.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd8a0a2ff8e6d15a7bca2c89c97e6ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "20f10c03f835c549ab55",
+    "url": "./static/js/2.9720bf9e.chunk.js"
+  },
+  {
+    "revision": "fbabe484400fd249a0740ba2c0d294dd",
+    "url": "./static/js/2.9720bf9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51e5da4a5a242279f81",
+    "url": "./static/js/main.9f3c3eed.chunk.js"
+  },
+  {
+    "revision": "459827b3e50cfc060dc2",
+    "url": "./static/js/runtime-main.cc20bba3.js"
+  }
+]);
