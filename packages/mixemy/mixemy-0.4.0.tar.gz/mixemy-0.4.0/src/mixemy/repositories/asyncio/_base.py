6f78dc601ca4ad555a2fef3fda42bb096108eb4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 12:57:11 +0000

    Add package mixemy/0.4.0/mixemy-0.4.0.tar.gz

---
+++ packages/mixemy/mixemy-0.4.0.tar.gz/mixemy-0.4.0/src/mixemy/repositories/asyncio/_base.py
@@ -0,0 +1,93 @@
+from abc import ABC
+from collections.abc import Sequence
+from typing import Any
+
+from sqlalchemy import Select, func, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from mixemy.repositories._base import BaseRepository
+from mixemy.schemas import InputSchema
+from mixemy.types import (
+    BaseModelType,
+    PaginationSchemaType,
+)
+
+
+class BaseAsyncRepository(BaseRepository[BaseModelType], ABC):
+    async def create(
+        self, db_session: AsyncSession, db_object: BaseModelType
+    ) -> BaseModelType:
+        return await self._add_refreshing(db_session=db_session, db_object=db_object)
+
+    async def read_multi(
+        self,
+        db_session: AsyncSession,
+        after_filter: PaginationSchemaType | None = None,
+        before_filter: InputSchema | None = None,
+    ) -> Sequence[BaseModelType]:
+        return await self._execute_returning_all(
+            db_session=db_session,
+            statement=self._add_after_filter(
+                statement=self._add_before_filter(
+                    statement=select(self.model), filter=before_filter
+                ),
+                filter=after_filter,
+            ),
+        )
+
+    async def update(
+        self,
+        db_session: AsyncSession,
+        db_object: BaseModelType,
+        object_in: InputSchema,
+    ) -> BaseModelType:
+        self._update_db_object(db_object=db_object, object_in=object_in)
+        return await self._add_refreshing(db_session=db_session, db_object=db_object)
+
+    async def delete(self, db_session: AsyncSession, db_object: BaseModelType) -> None:
+        await self._delete_refreshing(db_session=db_session, db_object=db_object)
+
+    async def count(
+        self,
+        db_session: AsyncSession,
+        before_filter: InputSchema | None = None,
+    ) -> int:
+        return await self._execute_returning_one(
+            db_session=db_session,
+            statement=self._add_before_filter(
+                statement=select(func.count()).select_from(self.model),
+                filter=before_filter,
+            ),
+        )
+
+    @staticmethod
+    async def _execute_returning_all(
+        db_session: AsyncSession, statement: Select[Any]
+    ) -> Sequence[BaseModelType]:
+        res = await db_session.execute(statement)
+        return res.scalars().all()
+
+    @staticmethod
+    async def _execute_returning_one(
+        db_session: AsyncSession, statement: Select[Any]
+    ) -> Any:
+        res = await db_session.execute(statement=statement)
+        return res.scalar_one()
+
+    @staticmethod
+    async def _add_refreshing(
+        db_session: AsyncSession, db_object: BaseModelType
+    ) -> BaseModelType:
+        db_session.add(db_object)
+        await db_session.commit()
+        await db_session.refresh(db_object)
+
+        return db_object
+
+    @staticmethod
+    async def _delete_refreshing(
+        db_session: AsyncSession, db_object: BaseModelType
+    ) -> None:
+        await db_session.delete(db_object)
+        await db_session.commit()
+        await db_session.refresh(db_object)
---
+++ packages/mixemy/mixemy-0.4.0.tar.gz/mixemy-0.4.0/src/mixemy/utils/_convertors.py
@@ -0,0 +1,19 @@
+from typing import Any, TypeVar
+
+from mixemy.models import BaseModel
+from mixemy.schemas import BaseSchema
+
+TO_MODEL = TypeVar("TO_MODEL", bound=BaseModel)
+TO_SCHEMA = TypeVar("TO_SCHEMA", bound=BaseSchema)
+
+
+def unpack_schema(schema: BaseSchema) -> dict[str, Any]:
+    return schema.model_dump(exclude_unset=True)
+
+
+def to_model(model: type[TO_MODEL], schema: BaseSchema) -> TO_MODEL:
+    return model(**unpack_schema(schema=schema))
+
+
+def to_schema(model: BaseModel, schema: type[TO_SCHEMA]) -> TO_SCHEMA:
+    return schema.model_validate(model)
