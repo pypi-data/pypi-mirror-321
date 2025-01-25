fc65f8404dc3e89a689053a763f7202f42062e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:08:37 +0000

    Add package pybase3/1.96.1/pybase3-1.96.1.tar.gz

---
+++ packages/pybase3/pybase3-1.96.1.tar.gz/pybase3-1.96.1/pybase3/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.96.01"
+__author__ = "Domingo E. Savoretti"
+__email__ = "esavoretti@gmail.com"
+__license__ = "MIT"
+__url__ = "https://github.com/sandy98/pybase3"
+__description__ = "A simple library to read and write dbase III files."
+
