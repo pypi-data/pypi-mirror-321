f3ca7a9b9ba4c05e6c67d1b4d33a2a9e2e8545a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:08:52 +0000

    Add package iinfer/0.10.5.2/iinfer-0.10.5.2.tar.gz

---
+++ packages/iinfer/iinfer-0.10.5.2.tar.gz/iinfer-0.10.5.2/iinfer/app/features/web/iinfer_web_gui.py
@@ -0,0 +1,9 @@
+from cmdbox.app.features.web import cmdbox_web_gui
+from iinfer import version
+
+
+class Gui(cmdbox_web_gui.Gui):
+    def __init__(self, appcls, ver):
+        super().__init__(appcls, ver)
+        self.version_info.append(dict(tabid='versions_iinfer', title=version.__appid__,
+                                      icon=f'assets/iinfer/icon.png', url='versions_iinfer'))
