e9a14a03308bb68b1edd0297fa8b057bc3eafb5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:06:12 +0000

    Add package spectrumx/0.1.4/spectrumx-0.1.4-py3-none-any.whl

---
+++ packages/spectrumx/spectrumx-0.1.4-py3-none-any.whl/spectrumx/ops/files.py
@@ -0,0 +1,167 @@
+"""File operations for SpectrumX SDK."""
+
+import mimetypes
+import uuid
+from collections.abc import Generator
+from datetime import datetime
+from datetime import timedelta
+from pathlib import Path
+
+from spectrumx.models import File
+from spectrumx.utils import log_user
+from spectrumx.utils import log_user_warning
+
+_tz = datetime.now().astimezone().tzinfo
+
+
+def get_file_media_type(file_path: Path) -> str:
+    """Returns the media type of a file.
+
+    Args:
+        file_path: The path to the file.
+
+    https://www.iana.org/assignments/media-types/media-types.xhtml#application
+    https://developer.mozilla.org/en-US/docs/Web/HTTP/MIME_types
+    """
+    mime_type, _encoding = mimetypes.guess_type(file_path)
+    return mime_type or "application/octet-stream"
+
+
+def get_file_permissions(file_path: Path) -> str:
+    """Returns the permissions of a file."""
+    permissions = file_path.stat().st_mode
+    binary_rep = f"{permissions:09b}"[-9:]
+    permission_map = ["r", "w", "x"]
+    return "".join(
+        permission_map[i % 3] if binary_rep[i] == "1" else "-" for i in range(9)
+    )
+
+
+def get_file_created_at(file_path: Path) -> datetime:
+    """Returns the creation timestamp of a file."""
+    return datetime.fromtimestamp(file_path.stat().st_ctime, tz=_tz)
+
+
+def get_file_updated_at(file_path: Path) -> datetime:
+    """Returns the last update timestamp of a file."""
+    return datetime.fromtimestamp(file_path.stat().st_mtime, tz=_tz)
+
+
+def construct_file(file_path: Path, sds_path: Path) -> File:
+    """Constructs a file instance from a local file. File has to exist on disk."""
+    file_path = Path(file_path)
+    if not file_path.exists():
+        msg = f"File not found: {file_path}"
+        raise FileNotFoundError(msg)
+    return File(
+        name=file_path.name,
+        expiration_date=None,
+        local_path=file_path,
+        size=file_path.stat().st_size,
+        directory=sds_path,
+        media_type=get_file_media_type(file_path),
+        created_at=get_file_created_at(file_path),
+        updated_at=get_file_updated_at(file_path),
+        permissions=get_file_permissions(file_path),
+    )
+
+
+def is_valid_file(file_path: Path) -> tuple[bool, list[str]]:
+    """Returns True if the path is a valid file.
+    A similar check is also performed at the server side.
+
+    Returns:
+        True/False whether the file is valid.
+        Reasons for invalidity otherwise.
+    """
+    file_mime = get_file_media_type(file_path)
+    disallowed_mimes = [
+        "application/octet-stream",  # generic binary
+        "application/x-msdownload",  # .exe
+        "application/x-msdos-program",  # .com
+        "application/x-msi",  # .msi
+    ]
+    is_valid_mime = file_mime not in disallowed_mimes
+    reasons: list[str] = []
+    if not is_valid_mime:
+        reasons.append(f"Invalid MIME type: {file_mime}")
+    if not file_path.is_file():
+        reasons.append("Not a file")
+    if file_path.stat().st_size == 0:
+        reasons.append("Empty file")
+    final_decision = (
+        file_path.is_file() and file_path.stat().st_size > 0 and is_valid_mime
+    )
+    return final_decision, reasons
+
+
+def get_valid_files(local_path: Path, *, warn_skipped: bool = False) -> Generator[File]:
+    """Yields valid SDS files in the given directory.
+
+    Args:
+        local_path: The path to the directory.
+    Yields:
+        File instances.
+    """
+    recursive_file_list = local_path.rglob("*")
+    successful_files = 0
+    ignored_files = 0
+    for file_path in recursive_file_list:
+        if not file_path.is_file():
+            continue
+        is_valid, reasons = is_valid_file(file_path)
+        if not is_valid:
+            ignored_files += 1
+            if warn_skipped:
+                log_user_warning(f"Skipping {file_path}: {', '.join(reasons)}")
+            continue
+        try:
+            successful_files += 1
+            local_rel_path = file_path.relative_to(local_path).parent
+            yield construct_file(file_path=file_path, sds_path=local_rel_path)
+        except FileNotFoundError:
+            continue
+    log_user(
+        f"Discovered {successful_files:,}/{successful_files + ignored_files:,} "
+        "valid files"
+    )
+    if warn_skipped:
+        if ignored_files:
+            log_user_warning(f"Ignored {ignored_files} invalid files in '{local_path}'")
+        else:
+            log_user(f"No invalid files found in '{local_path}'")
+
+
+def generate_sample_file(uuid_to_set: uuid.UUID) -> File:
+    """Generates a sample file for dry-run mode."""
+    tz = datetime.now().astimezone().tzinfo
+    created_at = datetime.now(tz=tz)
+    updated_at = created_at
+    expiration_date = datetime.now(tz=tz) + timedelta(days=30)
+    trailing_uuid_hex = uuid_to_set.hex[-6:]
+
+    sample_file = File(
+        uuid=uuid_to_set,
+        name=f"dry-run-{trailing_uuid_hex}.txt",
+        media_type="text/plain",
+        size=888,
+        directory=Path("./sds-files/dry-run/"),
+        permissions="rw-rw-r--",
+        created_at=created_at,
+        updated_at=updated_at,
+        expiration_date=expiration_date,
+        is_sample=True,  # always True for sample files
+    )
+    assert (
+        sample_file.is_sample is True  # pyright: ignore[reportPrivateUsage]
+    ), "SDS internal error: generated file is not a sample"
+    return sample_file
+
+
+def generate_random_files(num_files: int) -> list[File]:
+    """Calls generate_sample_file() to create a list of fabricated files."""
+    sample_files = []
+    for _ in range(num_files):
+        file_obj = generate_sample_file(uuid.uuid4())
+        sample_files.append(file_obj)
+    return sample_files
---
+++ packages/spectrumx/spectrumx-0.1.4-py3-none-any.whl/spectrumx/ops/network.py
@@ -0,0 +1,30 @@
+"""Network operations for SpectrumX SDK."""
+
+from http import HTTPStatus
+
+import requests
+
+from spectrumx import errors
+
+
+def success_or_raise(
+    response: requests.Response,
+    ContextException: type[errors.SDSError] = errors.SDSError,  # noqa: N803
+) -> None:
+    """Checks a response is successful, raising a contextual error if not."""
+    status = HTTPStatus(response.status_code)
+    if status.is_success:
+        return
+
+    try:
+        error_details = response.json().get("detail")
+    except requests.exceptions.JSONDecodeError:
+        error_details = response.text
+
+    if status in {HTTPStatus.UNAUTHORIZED, HTTPStatus.FORBIDDEN}:
+        raise errors.AuthError(message=error_details)
+    if status.is_client_error:
+        raise ContextException(message=error_details)
+    if status.is_server_error:
+        raise errors.ServiceError(message=error_details)
+    raise errors.SDSError(message=error_details)
