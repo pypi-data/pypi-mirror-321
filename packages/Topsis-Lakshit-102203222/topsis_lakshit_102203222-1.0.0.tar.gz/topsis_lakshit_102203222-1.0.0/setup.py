cf26c5daf5a0660c6eeac858545a95484a47d5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:09:22 +0000

    Add package Topsis-Lakshit-102203222/1.0.0/topsis_lakshit_102203222-1.0.0.tar.gz

---
+++ packages/Topsis-Lakshit-102203222/topsis_lakshit_102203222-1.0.0.tar.gz/topsis_lakshit_102203222-1.0.0/setup.py
@@ -0,0 +1,39 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="Topsis-Lakshit-102203222",
+    version="1.0.0",
+    description="It does the mathematics of topsis",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lakshit78/Topsis-Lakshit-102203222",
+    author="Lakshit Gupta",
+    author_email="lakshitajaygupta@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    packages=["topsis"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.__main__:main",
+        ]
+    },
+)
