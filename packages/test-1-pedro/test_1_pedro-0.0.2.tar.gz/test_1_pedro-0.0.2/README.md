a200c833e7edba3211d08b33862c53d340cc92d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:08:41 +0000

    Add package test-1-pedro/0.0.2/test_1_pedro-0.0.2.tar.gz

---
+++ packages/test-1-pedro/test_1_pedro-0.0.2.tar.gz/test_1_pedro-0.0.2/README.md
@@ -0,0 +1,9 @@
+# READ ME
+
+This is an example package.
+
+Sources: 
+
+* https://packaging.python.org/en/latest/tutorials/packaging-projects/
+* https://earthly.dev/blog/create-python-package/
+* https://pypi.org/help/
---
+++ packages/test-1-pedro/test_1_pedro-0.0.2.tar.gz/test_1_pedro-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+setup(
+
+    name= "test_1_pedro",
+    version= "0.0.2",
+    packages=find_packages(), install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy >= 1.11.1'
+    ],
+    long_description=description,
+    long_description_content_type='text/markdown',
+)
+
