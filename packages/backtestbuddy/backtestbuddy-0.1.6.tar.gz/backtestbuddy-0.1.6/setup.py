75967ae0870e2b2efed2dc8d861ba54dbe7f870d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:05:40 +0000

    Add package backtestbuddy/0.1.6/backtestbuddy-0.1.6.tar.gz

---
+++ packages/backtestbuddy/backtestbuddy-0.1.6.tar.gz/backtestbuddy-0.1.6/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="backtestbuddy",
+    version="0.1.6",
+    author="Sebastian Pachl",
+    description="A flexible backtesting framework for trading and betting strategies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Pacman1984/backtestbuddy",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    install_requires=[
+        "numpy>=1.13.3",
+        "pandas>=0.25",
+        "matplotlib>=3.0.0",
+        "scipy>=1.0.0",
+        "plotly>=4.14.0",
+        "scikit-learn>=0.24.0",
+        "nbformat>=4.2.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov>=2.0",
+            "flake8>=3.9",
+            "mypy>=0.910",
+            "black>=21.5b1",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "backtestbuddy=backtestbuddy.cli:main",
+        ],
+    },
+    zip_safe=False,
+)
