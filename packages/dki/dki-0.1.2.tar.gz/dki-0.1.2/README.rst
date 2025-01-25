af989df2f9ced1f3d0afc3f7301f6cb9b2f3c515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:22:13 +0000

    Add package dki/0.1.2/dki-0.1.2.tar.gz

---
+++ packages/dki/dki-0.1.2.tar.gz/dki-0.1.2/README.rst
@@ -0,0 +1,16 @@
+dki
+==========
+
+Docker Image.
+
+快速构建和拉取不同位置的镜像，以及构建缺失的镜像。
+
+Install
+-------
+
+``pipx install dki``
+
+Usage
+-----
+
+dki build --template python_base --name svtter/python_base --tag latest
---
+++ packages/dki/dki-0.1.2.tar.gz/dki-0.1.2/src/dki/libs.py
@@ -0,0 +1,72 @@
+import pathlib
+import sys
+import os
+import subprocess
+
+
+default_path = pathlib.Path("~/.config/deployments").expanduser()
+
+
+def has_deployments() -> bool:
+  return default_path.exists()
+
+
+def build_image(template: str, name: str):
+  """considering context is the current directory"""
+  if name.find(':') == -1:
+    image_name = f"{name}:latest"
+  else:
+    image_name = name
+
+  assert isinstance(template, str), "template must be a string"
+  template_dir = default_path / template
+  dockerfile = template_dir / "Dockerfile"
+  context = "."
+
+  if (template_dir / "check.py").exists():
+    res = subprocess.run(
+      [
+        "python",
+        template_dir / "check.py",
+      ],
+      cwd=context,
+    )
+    if res.returncode != 0:
+      print('Check files failed, please check the files in the context directory.')
+      sys.exit(1)
+
+  subprocess.run(
+    [
+      "docker",
+      "build",
+      "-t",
+      image_name,
+      "-f",
+      dockerfile,
+      context,
+    ],
+    cwd=context,
+  )
+
+
+def download_deployment():
+  subprocess.run(
+    [
+      "git",
+      "clone",
+      "https://github.com/svtter/deployments.git",
+      default_path,
+    ]
+  )
+
+
+def pull_deployment(branch: str = "main"):
+  subprocess.run(
+    [
+      "git",
+      "pull",
+      "origin",
+      branch,
+    ],
+    cwd=default_path,
+  )
