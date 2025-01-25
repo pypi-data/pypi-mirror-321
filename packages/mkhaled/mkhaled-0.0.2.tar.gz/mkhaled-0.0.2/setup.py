543c735fb95653d3c836364fe0605147ce4dae7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 11:09:13 +0000

    Add package mkhaled/0.0.2/mkhaled-0.0.2.tar.gz

---
+++ packages/mkhaled/mkhaled-0.0.2.tar.gz/mkhaled-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mkhaled",
+    version="0.0.2",
+    author="Hassan",
+    author_email="your.email@example.com",
+    description="A library to store and manage scikit-learn code examples",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/mkhaled",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'scikit-learn',
+        'numpy',
+        'pandas'
+    ],
+)
