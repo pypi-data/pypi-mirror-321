14f0340a38b9b41df2d0a250caa40c3b1d8c3689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:07:29 +0000

    Add package douroucoulis/0.1.3/douroucoulis-0.1.3-py3-none-any.whl

---
+++ packages/douroucoulis/douroucoulis-0.1.3-py3-none-any.whl/douroucoulis/__init__.py
@@ -0,0 +1,5 @@
+# Import the ModSelection class from the mod_selection module
+from douroucoulis.douroucoulis_v0_1 import ModSelection
+
+# Create an instance of ModSelection
+mod_sel = ModSelection()
