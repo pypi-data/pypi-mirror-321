63fd955ab431f248b875beba00c6beed270d1a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:05:36 +0000

    Add package arrendatools.actualiza-renta/1.0.1/arrendatools.actualiza_renta-1.0.1-py3-none-any.whl

---
+++ packages/arrendatools.actualiza-renta/arrendatools.actualiza_renta-1.0.1-py3-none-any.whl/arrendatools/actualiza_renta/tipos/porcentaje.py
@@ -0,0 +1,44 @@
+from decimal import Decimal, ROUND_HALF_UP
+from arrendatools.actualiza_renta.actualizacion_renta import ActualizacionRenta
+
+
+class Porcentaje(ActualizacionRenta):
+    """Implementación de actualización por porcentaje."""
+
+    def calcular(
+        self,
+        cantidad: Decimal,
+        dato: Decimal = None,
+        mes: int = None,
+        anyo_inicial: int = None,
+        anyo_final: int = None,
+    ) -> dict:
+        self.validar_datos(cantidad, dato, mes, anyo_inicial, anyo_final)
+        cantidad = Decimal(cantidad)  # Convertir explícitamente a Decimal
+        cantidad_actualizada = cantidad + (cantidad * Decimal(dato)).quantize(
+            Decimal("0.01"), rounding=ROUND_HALF_UP
+        )
+        return {
+            "cantidad": cantidad,
+            "dato": dato,
+            "cantidad_actualizada": cantidad_actualizada,
+            "tasa_variacion": dato,
+        }
+
+    def validar_datos(
+        self,
+        cantidad: Decimal,
+        dato: Decimal = None,
+        mes: int = None,
+        anyo_inicial: int = None,
+        anyo_final: int = None,
+    ) -> None:
+        """Valida los datos de entrada."""
+        super().validar_datos(cantidad, dato, mes, anyo_inicial, anyo_final)
+        if dato is None:
+            raise ValueError("Debes proporcionar el campo 'dato'.")
+        if not (Decimal("-1.0") <= dato <= Decimal("1.0")):
+            raise ValueError(
+                "El dato debe ser un porcentaje entre -1 (-100%) y 1 (100%)."
+            )
+        return None
