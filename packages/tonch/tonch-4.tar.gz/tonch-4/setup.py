6bdbf6b1f9a680c5f4c4b5b07eb9043b3f6c2684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:05:50 +0000

    Add package tonch/4/tonch-4.tar.gz

---
+++ packages/tonch/tonch-4.tar.gz/tonch-4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='tonch',
+      version='4',
+      description='none',
+      packages=['tonch'],
+      author_email='antoha123_3@mail.ru',
+      zip_safe=False)
