e0aae51c86b8d2cee6ecdc0233287c03b7b39808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:21:53 +0000

    Add package 1-0-9-local/0.0.0.dev0/1_0_9_local-0.0.0.dev0-py3-none-any.whl

---
+++ packages/1-0-9-local/1_0_9_local-0.0.0.dev0-py3-none-any.whl/1_0_9_local/__init__.py
@@ -0,0 +1,5 @@
+from 1_0_9_local.core import *
+from 1_0_9_local.tests import *
+
+if __name__ == '__main__':
+    main()
---
+++ packages/1-0-9-local/1_0_9_local-0.0.0.dev0-py3-none-any.whl/1_0_9_local/__main__.py
@@ -0,0 +1,4 @@
+from 1_0_9_local import main
+
+if __name__ == '__main__':
+    main()
