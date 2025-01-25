d9217bc6402dc232caffc713933851dd2590e24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:22:03 +0000

    Add package scepy/0.1.2/scepy-0.1.2.tar.gz

---
+++ packages/scepy/scepy-0.1.2.tar.gz/scepy-0.1.2/scepy/__init__.py
@@ -0,0 +1,32 @@
+"""
+SCEPY: Single-Case Experiment Python Package
+"""
+
+from .combined import *
+from .pnd import *
+from .prepare_sced import *
+from .readdata import *
+from .sced import *
+from .select_case import *
+from .variable_management import *
+
+# Version check on import
+from ._version import version as __version__
+
+import requests
+import warnings
+
+def check_new_version():
+    try:
+        response = requests.get("https://pypi.org/pypi/scepy/json", timeout=3)
+        response.raise_for_status()
+        latest_version = response.json()["info"]["version"]
+        if latest_version != __version__:
+            warnings.warn(
+                f"A newer version ({latest_version}) of SCEPY is available. "
+                f"Please update using 'pip install --upgrade scepy'."
+            )
+    except Exception:
+        pass  # Ignore any errors during the version check
+
+check_new_version()
