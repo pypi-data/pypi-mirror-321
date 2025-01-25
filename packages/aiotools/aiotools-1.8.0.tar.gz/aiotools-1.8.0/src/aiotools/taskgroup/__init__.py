4e41f867c7b8477ada2de51377e13f7c59f413dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:06:52 +0000

    Add package aiotools/1.8.0/aiotools-1.8.0.tar.gz

---
+++ packages/aiotools/aiotools-1.8.0.tar.gz/aiotools-1.8.0/src/aiotools/taskgroup/__init__.py
@@ -0,0 +1,35 @@
+import asyncio
+
+from .types import MultiError, TaskGroupError
+
+if hasattr(asyncio, "TaskGroup"):
+    from . import persistent
+    from .base import *  # noqa
+    from .persistent import *  # noqa
+
+    __all__ = [
+        "MultiError",
+        "TaskGroup",
+        "TaskGroupError",
+        "current_taskgroup",
+        *persistent.__all__,
+    ]
+else:
+    from . import persistent_compat
+    from .base_compat import *  # type: ignore  # noqa
+    from .base_compat import has_contextvars
+    from .persistent_compat import *  # type: ignore  # noqa
+
+    __all__ = [  # type: ignore  # noqa
+        "MultiError",
+        "TaskGroup",
+        "TaskGroupError",
+        *persistent_compat.__all__,
+    ]
+    if has_contextvars:
+        __all__.append("current_taskgroup")
+
+
+from .utils import as_completed_safe  # noqa
+
+__all__.append("as_completed_safe")
