18aaec580a7a29418f034b8bc72266189618c9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:05:50 +0000

    Add package megaprofiler/1.0.0/megaprofiler-1.0.0-py3-none-any.whl

---
+++ packages/megaprofiler/megaprofiler-1.0.0-py3-none-any.whl/megaprofiler/__init__.py
@@ -0,0 +1,11 @@
+from .profiling import *
+from .outliers import *
+from .analysis import *
+from .modeling import *
+from .preprocessing import *
+from .visualization import *
+from .utils import *
+from .text_processing import *
+
+
+__version__ = '1.0.0'
