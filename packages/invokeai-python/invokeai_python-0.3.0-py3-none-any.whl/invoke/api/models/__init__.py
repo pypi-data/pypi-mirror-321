58cdf66db291079e7283f85106b780c133728c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:05:59 +0000

    Add package invokeai-python/0.3.0/invokeai_python-0.3.0-py3-none-any.whl

---
+++ packages/invokeai-python/invokeai_python-0.3.0-py3-none-any.whl/invoke/api/models/__init__.py
@@ -0,0 +1,40 @@
+# Path: invoke\api\models\__init__.py
+from .models_api import ModelsApi, BaseModels, ModelType
+from .schema import (
+    Submodel,
+    ModelRecord,
+    ValidationErrorDetail,
+    ScannedModel,
+    HuggingFaceModelResponse,
+    DownloadPart,
+    SourceMetadata,
+    Source,
+    ModelInstallJob,
+    CachePerformanceStats,
+    Dependency,
+    StarterModel,
+    StarterModelsResponse,
+    HFTokenStatus,
+)
+
+__all__ = [
+    "ModelsApi",
+    "BaseModels",
+    "ModelType",
+    "Model",
+    "Models",
+    "Submodel",
+    "ModelRecord",
+    "ValidationErrorDetail",
+    "ScannedModel",
+    "HuggingFaceModelResponse",
+    "DownloadPart",
+    "SourceMetadata",
+    "Source",
+    "ModelInstallJob",
+    "CachePerformanceStats",
+    "Dependency",
+    "StarterModel",
+    "StarterModelsResponse",
+    "HFTokenStatus",
+]
