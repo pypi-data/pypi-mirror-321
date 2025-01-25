c64d057a31120b3ac98ad220133c54282aa4be97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:22:12 +0000

    Add package tablelocking-nonprod/0.2.2/tablelocking_nonprod-0.2.2.tar.gz

---
+++ packages/tablelocking-nonprod/tablelocking_nonprod-0.2.2.tar.gz/tablelocking_nonprod-0.2.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+name="tablelocking_nonprod",
+version="0.2.2",
+packages=find_packages(),
+author="Javad Javadzade",
+author_email="JavadJavadzade@LiveNation.com",
+install_requires=[],  # Dependencies are listed in requirements.txt
+)
