48b692005152450942e322eebdc99d213c451cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:05:10 +0000

    Add package epubedit/0.1.1/epubedit-0.1.1-py3-none-any.whl

---
+++ packages/epubedit/epubedit-0.1.1-py3-none-any.whl/epubedit/__init__.py
@@ -0,0 +1,9 @@
+from .epubedit import Epubedit
+
+"""
+epubedit: // a python package to read ePub infos and edit it.
+"""
+
+__title__ = "epubedit"
+__version__ = "0.1.1"
+__authors__ = "childeyouyu (有语)"
