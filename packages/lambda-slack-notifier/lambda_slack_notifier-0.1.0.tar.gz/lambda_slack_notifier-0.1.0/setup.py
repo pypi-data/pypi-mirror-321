1889d0b4c4b0f4a94f1569b97fd5be022b603d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:07:38 +0000

    Add package lambda-slack-notifier/0.1.0/lambda_slack_notifier-0.1.0.tar.gz

---
+++ packages/lambda-slack-notifier/lambda_slack_notifier-0.1.0.tar.gz/lambda_slack_notifier-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lambda-slack-notifier",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        'boto3>=1.26.0'
+    ],
+    author="Vikas",
+    author_email="vikasingawale16@gmail.com",
+    description="A package to send Slack notifications via AWS Lambda",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    #url="https://github.com/yourusername/aws-slack-notifier",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
