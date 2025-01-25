949ee5a5c46024be79aedbfc0fb9ad1ab4039c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 07:05:38 +0000

    Add package SNPfilter/0.0.5/SNPfilter-0.0.5-py3-none-any.whl

---
+++ packages/SNPfilter/SNPfilter-0.0.5-py3-none-any.whl/snpfilter/versions.py
@@ -0,0 +1,38 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.5",
+        "features": [
+            "1. add max_depth option for qcfilter",
+        ],
+    },
+    {
+        "number": "0.0.4",
+        "features": [
+            "1. update README.md",
+        ],
+    },
+    {
+        "number": "0.0.3",
+        "features": [
+            "1. change default --min-MQ to 20 and --min-BQ to 20",
+        ],
+    },
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. update README.md",
+            "2. add change site in output file",
+            "3. debug",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. build base framework",
+        ],
+    },
+]
