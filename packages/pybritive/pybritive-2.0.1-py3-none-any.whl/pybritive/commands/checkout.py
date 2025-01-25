08b61b82fae8dfcddf35eee7adcf4680847f94f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:05:06 +0000

    Add package pybritive/2.0.1/pybritive-2.0.1-py3-none-any.whl

---
+++ packages/pybritive/pybritive-2.0.1-py3-none-any.whl/pybritive/commands/checkout.py
@@ -0,0 +1,60 @@
+import click
+
+from ..helpers.build_britive import build_britive
+from ..helpers.profile_argument_decorator import click_smart_profile_argument
+from ..options.britive_options import britive_options
+
+
+@click.command()
+@build_britive
+@britive_options(
+    names='alias,blocktime,console,justification,otp,mode,maxpolltime,silent,force_renew,aws_credentials_file,'
+    'gcloud_key_file,verbose,extend,profile_type,tenant,token,passphrase,federation_provider'
+)
+@click_smart_profile_argument
+def checkout(  # noqa: PLR0913
+    ctx,
+    alias,
+    blocktime,
+    console,
+    justification,
+    otp,
+    mode,
+    maxpolltime,
+    silent,
+    force_renew,
+    aws_credentials_file,
+    gcloud_key_file,
+    verbose,
+    extend,
+    profile_type,
+    tenant,
+    token,
+    passphrase,
+    federation_provider,
+    profile,
+):
+    """Checkout a profile.
+
+    This command takes 1 required argument `PROFILE`. This should be a string representation of the profile
+    that should be checked out. Format is `application name/environment name/profile name`.
+    """
+
+    # silent will get passed in via @build_britive
+    ctx.obj.britive.checkout(
+        alias=alias,
+        blocktime=blocktime,
+        console=console,
+        justification=justification,
+        otp=otp,
+        mode=mode,
+        maxpolltime=maxpolltime,
+        profile=profile,
+        passphrase=passphrase,
+        force_renew=force_renew,
+        aws_credentials_file=aws_credentials_file,
+        gcloud_key_file=gcloud_key_file,
+        verbose=verbose,
+        extend=extend,
+        profile_type=profile_type,
+    )
---
+++ packages/pybritive/pybritive-2.0.1-py3-none-any.whl/pybritive/options/britive_options.py
@@ -0,0 +1,90 @@
+import click
+
+from ..options.alias import option as alias
+from ..options.aws_console_duration import option as aws_console_duration
+from ..options.aws_credentials_file import option as aws_credentials_file
+from ..options.aws_profile import option as aws_profile
+from ..options.blocktime import option as blocktime
+from ..options.browser import option as browser
+from ..options.checked_out import option as checked_out
+from ..options.configure_alias import option as configure_alias
+from ..options.configure_backend import option as configure_backend
+from ..options.configure_prompt import option as configure_prompt
+from ..options.configure_tenant import option as configure_tenant
+from ..options.console import option as console
+from ..options.extend import option as extend
+from ..options.federation_provider import option as federation_provider
+from ..options.file import option as file
+from ..options.force_renew import option as force_renew
+from ..options.gcloud_key_file import option as gcloud_key_file
+from ..options.justification import option as justification
+from ..options.maxpolltime import option as maxpolltime
+from ..options.mode import option as mode
+from ..options.otp import option as otp
+from ..options.output_format import option as output_format
+from ..options.passphrase import option as passphrase
+from ..options.profile_type import option as profile_type
+from ..options.query import option as query
+from ..options.silent import option as silent
+from ..options.ssh_hostname import option as ssh_hostname
+from ..options.ssh_key_source import option as ssh_key_source
+from ..options.ssh_port import option as ssh_port
+from ..options.ssh_push_public_key import option as ssh_push_public_key
+from ..options.ssh_username import option as ssh_username
+from ..options.tenant import option as tenant
+from ..options.token import option as token
+from ..options.verbose import option as verbose
+from ..options.version import option as version
+
+options_map = {
+    'tenant': tenant,
+    'token': token,
+    'format': output_format,
+    'output_format': output_format,
+    'alias': alias,
+    'blocktime': blocktime,
+    'mode': mode,
+    'maxpolltime': maxpolltime,
+    'version': version,
+    'configure_tenant': configure_tenant,
+    'configure_alias': configure_alias,
+    'configure_prompt': configure_prompt,
+    'justification': justification,
+    'otp': otp,
+    'silent': silent,
+    'console': console,
+    'checked_out': checked_out,
+    'file': file,
+    'configure_backend': configure_backend,
+    'passphrase': passphrase,
+    'force_renew': force_renew,
+    'aws_credentials_file': aws_credentials_file,
+    'federation_provider': federation_provider,
+    'gcloud_key_file': gcloud_key_file,
+    'verbose': verbose,
+    'query': query,
+    'ssh_hostname': ssh_hostname,
+    'ssh_username': ssh_username,
+    'ssh_push_public_key': ssh_push_public_key,
+    'ssh_port': ssh_port,
+    'ssh_key_source': ssh_key_source,
+    'aws_profile': aws_profile,
+    'aws_console_duration': aws_console_duration,
+    'browser': browser,
+    'extend': extend,
+    'profile_type': profile_type,
+}
+
+
+def britive_options(*args, **kwargs):
+    def inner(f):
+        names = [n.strip() for n in kwargs['names'].split(',')]
+        names.reverse()
+        for name in names:
+            option = options_map.get(name)
+            if not name:
+                raise click.ClickException(f'Invalid option {name} provided.')
+            f = option(f)
+        return f
+
+    return inner
