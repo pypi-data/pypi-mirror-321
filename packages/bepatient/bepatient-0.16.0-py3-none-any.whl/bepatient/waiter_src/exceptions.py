39dd8301c55b99e658ab55b4616ca47dec755973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:06:20 +0000

    Add package bepatient/0.16.0/bepatient-0.16.0-py3-none-any.whl

---
+++ packages/bepatient/bepatient-0.16.0-py3-none-any.whl/bepatient/waiter_src/exceptions.py
@@ -0,0 +1,12 @@
+class ExecutorIsNotReady(Exception):
+    def __init__(self, message: str = "The condition has not yet been checked."):
+        super().__init__(message)
+
+
+class WaiterIsNotReady(Exception):
+    def __init__(self, message: str = "Not all required attributes have been set!"):
+        super().__init__(message)
+
+
+class WaiterConditionWasNotMet(Exception):
+    pass
