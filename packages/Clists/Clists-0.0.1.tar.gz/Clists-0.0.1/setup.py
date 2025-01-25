6586e79d24f3890eef986c4bb0d7183afc825e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:08:49 +0000

    Add package Clists/0.0.1/Clists-0.0.1.tar.gz

---
+++ packages/Clists/Clists-0.0.1.tar.gz/Clists-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Operating System :: Microsoft :: Windows :: Windows 11',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+]
+
+setup(
+    name='Clists',
+    version='0.0.1',
+    description='A simple package to hyper-optimise list operations by caching.',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    author='Seth Brockman',
+    author_email='brockman.seth@google.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords=['list caching','caching','list','list optimisation', 'list operations'],
+    packages=find_packages(),
+    install_requires=['os'],
+)
