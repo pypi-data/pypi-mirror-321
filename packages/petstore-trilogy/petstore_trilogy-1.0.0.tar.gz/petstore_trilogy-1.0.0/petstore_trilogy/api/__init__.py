0ec1b6db2bc822bbfbd025261ad826c4eb2a576d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 01:31:55 +0000

    Add package petstore-trilogy/1.0.0/petstore_trilogy-1.0.0.tar.gz

---
+++ packages/petstore-trilogy/petstore_trilogy-1.0.0.tar.gz/petstore_trilogy-1.0.0/petstore_trilogy/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from petstore_trilogy.api.pet_api import PetApi
+from petstore_trilogy.api.store_api import StoreApi
+from petstore_trilogy.api.user_api import UserApi
+
