973fa42ca4c5dadff025dee1f27739bcc6e0181b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:06:18 +0000

    Add package topsis-102217115/0.1/topsis_102217115-0.1.tar.gz

---
+++ packages/topsis-102217115/topsis_102217115-0.1.tar.gz/topsis_102217115-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="topsis_102217115",
+      version="0.1",
+      description="This is a package for TOPSIS score calculation",
+      long_description="TOPSIS (Technique for Order of Preference by Similarity to Ideal Solution) is a method used to evaluate and rank alternatives by comparing them to an ideal solution. It calculates how close each option is to the best possible outcome and how far it is from the worst. By considering multiple criteria and their importance, TOPSIS helps make clear and logical decisions, often used in areas like project selection, resource allocation, and performance evaluation. Its simplicity and effectiveness make it a popular choice for solving complex problems.",
+      author="Gurleen",
+      packages=['topsis_102217115']
+      )
