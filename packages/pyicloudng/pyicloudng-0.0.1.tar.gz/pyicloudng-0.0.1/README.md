9dfc4e8b262cb10f1d854814a8a583558945c0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:06:06 +0000

    Add package pyicloudng/0.0.1/pyicloudng-0.0.1.tar.gz

---
+++ packages/pyicloudng/pyicloudng-0.0.1.tar.gz/pyicloudng-0.0.1/README.md
@@ -0,0 +1,17 @@
+
+# PYICLOUDNG
+
+Python libraries to interact with Apple icloud. This is a fork from PYICLOUD library 
+
+## Description 
+
+Python libraries to interact with Apple icloud 
+
+## Author
+
+
+Name: JordiRedondo 
+Email: redcorjo@gmail.com
+
+
+Version: 0.0.1
---
+++ packages/pyicloudng/pyicloudng-0.0.1.tar.gz/pyicloudng-0.0.1/src/pyicloudng/services/__init__.py
@@ -0,0 +1,9 @@
+"""Services."""
+from pyicloudng.services.calendar import CalendarService
+from pyicloudng.services.findmyiphone import FindMyiPhoneServiceManager
+from pyicloudng.services.ubiquity import UbiquityService
+from pyicloudng.services.contacts import ContactsService
+from pyicloudng.services.reminders import RemindersService
+from pyicloudng.services.photos import PhotosService
+from pyicloudng.services.account import AccountService
+from pyicloudng.services.drive import DriveService
