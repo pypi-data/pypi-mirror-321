5a49dd213a95e4253386c630992bd1785bbe625d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:07:32 +0000

    Add package crimson-agent/1.0.0/crimson_agent-1.0.0.tar.gz

---
+++ packages/crimson-agent/crimson_agent-1.0.0.tar.gz/crimson_agent-1.0.0/crimson_agent/__init__.py
@@ -0,0 +1,6 @@
+from .agent import CrimsonAgent
+from .tools import AgentTools
+from .social import SocialMediaTools
+
+__version__ = "0.1.0"
+__all__ = ["CrimsonAgent", "AgentTools", "SocialMediaTools"]
