d508036dbdbccbab0cedf89f708c3d7413bfbaa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:05:51 +0000

    Add package Topsis-ShrihanTayal-102203460/1.0.0/topsis_shrihantayal_102203460-1.0.0.tar.gz

---
+++ packages/Topsis-ShrihanTayal-102203460/topsis_shrihantayal_102203460-1.0.0.tar.gz/topsis_shrihantayal_102203460-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Topsis-ShrihanTayal-102203460',
+    version='1.0.0',
+    author='Shrihan Tayal',
+    description='A Python package for TOPSIS',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy'],
+)
