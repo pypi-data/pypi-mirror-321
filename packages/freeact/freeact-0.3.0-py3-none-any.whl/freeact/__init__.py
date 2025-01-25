ce5ea3940cac40d3d7751e50a30d149f37e290b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 00:07:43 +0000

    Add package freeact/0.3.0/freeact-0.3.0-py3-none-any.whl

---
+++ packages/freeact/freeact-0.3.0-py3-none-any.whl/freeact/__init__.py
@@ -0,0 +1,14 @@
+from freeact.agent import CodeActAgent, CodeActAgentResponse, CodeActAgentTurn, CodeExecution, CodeExecutionResult
+from freeact.executor import CodeExecutionContainer, CodeExecutionEnvironment, CodeExecutor, execution_environment
+from freeact.model import (
+    Claude,
+    ClaudeModelName,
+    CodeActModel,
+    CodeActModelResponse,
+    CodeActModelTurn,
+    Gemini,
+    GeminiLive,
+    GeminiModelName,
+    GenericModel,
+    QwenCoder,
+)
