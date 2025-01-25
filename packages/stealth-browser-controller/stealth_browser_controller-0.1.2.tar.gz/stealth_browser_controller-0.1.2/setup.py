26c6847248b36d993897bf1b2d566fef6cd6a4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:06:07 +0000

    Add package stealth-browser-controller/0.1.2/stealth_browser_controller-0.1.2.tar.gz

---
+++ packages/stealth-browser-controller/stealth_browser_controller-0.1.2.tar.gz/stealth_browser_controller-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1.2"
+
+setup(
+    name="stealth-browser-controller",
+    version=VERSION,
+    description="A stealthy browser automation tool with only screen and mouse",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Sarper AVCI",
+    author_email="sarperavci20@gmail.com",
+    url="https://github.com/sarperavci/stealth-browser-controller",
+    package_dir={"": "stealth_browser_controller"},
+    packages=find_packages(where="stealth_browser_controller"),
+    install_requires=["pyautogui", "human-mouse"],
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+) 
