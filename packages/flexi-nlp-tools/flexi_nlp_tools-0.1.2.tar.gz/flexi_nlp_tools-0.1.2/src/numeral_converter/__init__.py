5e69ed45f1d78a4553a392d406d782994c10c32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 01:31:33 +0000

    Add package flexi-nlp-tools/0.1.2/flexi_nlp_tools-0.1.2.tar.gz

---
+++ packages/flexi-nlp-tools/flexi_nlp_tools-0.1.2.tar.gz/flexi_nlp_tools-0.1.2/src/numeral_converter/__init__.py
@@ -0,0 +1,6 @@
+from .numeral_converter import numeral2int, int2numeral, int2numerals
+from .numeral_converter_loader import get_available_languages
+from .text import convert_numerical_in_text
+
+
+__all__ = ('numeral2int', 'int2numeral', 'convert_numerical_in_text', 'int2numerals', 'get_available_languages')
