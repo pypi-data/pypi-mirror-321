0b278bc77d2af31a4518c3685795a3e05951d7ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 01:32:06 +0000

    Add package odoo-addon-stock-picking-report-valued-copy-unvalued/16.0.1.0.0.3/odoo_addon_stock_picking_report_valued_copy_unvalued-16.0.1.0.0.3-py3-none-any.whl

---
+++ packages/odoo-addon-stock-picking-report-valued-copy-unvalued/odoo_addon_stock_picking_report_valued_copy_unvalued-16.0.1.0.0.3-py3-none-any.whl/odoo/addons/stock_picking_report_valued_copy_unvalued/models/stock_picking.py
@@ -0,0 +1,24 @@
+# Copyright 2023 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    unvalued_copy = fields.Boolean(
+        help="Print an unvalued picking copy",
+    )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            vals.update(
+                {
+                    "unvalued_copy": self.env["res.partner"]
+                    .browse(vals["partner_id"])
+                    .unvalued_picking_copy
+                }
+            )
+        return super().create(vals_list)
