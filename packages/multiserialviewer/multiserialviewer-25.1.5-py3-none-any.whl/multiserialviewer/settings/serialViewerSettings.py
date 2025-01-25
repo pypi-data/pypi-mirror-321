cc97c5cf78618d8427be544859304e46dc66f488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:06:06 +0000

    Add package multiserialviewer/25.1.5/multiserialviewer-25.1.5-py3-none-any.whl

---
+++ packages/multiserialviewer/multiserialviewer-25.1.5-py3-none-any.whl/multiserialviewer/settings/serialViewerSettings.py
@@ -0,0 +1,19 @@
+from typing import Optional, List
+from PySide6.QtCore import QSize, QPoint, QByteArray
+
+from multiserialviewer.settings.serialConnectionSettings import SerialConnectionSettings
+from multiserialviewer.settings.counterSettings import CounterSettings
+
+
+class SerialViewerSettings:
+    def __init__(self):
+        self.title: str = ''
+        self.size: Optional[QSize] = None
+        self.position: Optional[QPoint] = None
+        self.splitterState: Optional[QByteArray] = None
+        self.currentTabName: Optional[str] = None
+        self.autoscrollActive: bool = True
+        self.autoscrollReactivate: bool = True
+        self.counters: List[CounterSettings] = []
+        self.connection: SerialConnectionSettings = SerialConnectionSettings('')
+        self.showNonPrintableCharsAsHex = True
