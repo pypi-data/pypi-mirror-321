29671705a5e82656e37c0827419d9424fa36c74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:07:33 +0000

    Add package TOPSIS-DishavSingla-102217004/0.2/TOPSIS-DishavSingla-102217004-0.2.tar.gz

---
+++ packages/TOPSIS-DishavSingla-102217004/TOPSIS-DishavSingla-102217004-0.2.tar.gz/TOPSIS-DishavSingla-102217004-0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TOPSIS-DishavSingla-102217004",
+    version="0.2",
+    author="Dishav",
+    author_email="dsingla2_be22@thapar.edu",
+    description="A Python package for implementing the TOPSIS decision-making method.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/DISHAVSINGLA/Topsis-Dishav-102217004",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.topsis:main", 
+        ],
+    },
+)
