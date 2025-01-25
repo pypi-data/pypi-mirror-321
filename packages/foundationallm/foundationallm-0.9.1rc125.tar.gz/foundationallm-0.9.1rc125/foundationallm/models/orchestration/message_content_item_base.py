5a0143827fd0e4e5b68b4c873edb1049dfec3b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:06:39 +0000

    Add package foundationallm/0.9.1rc125/foundationallm-0.9.1rc125.tar.gz

---
+++ packages/foundationallm/foundationallm-0.9.1rc125.tar.gz/foundationallm-0.9.1rc125/foundationallm/models/orchestration/message_content_item_base.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from .message_content_item_types import MessageContentItemTypes
+from foundationallm.models.constants import AgentCapabilityCategories
+
+class MessageContentItemBase(BaseModel):
+    """Base message content item model."""
+    agent_capability_category: Optional[AgentCapabilityCategories] = Field(None, description="The category of capability assigned to the agent.")
+    
+    class Config:
+        use_enum_values = True
+        populate_by_name = True
+        extra = "forbid"
