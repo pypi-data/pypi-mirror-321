3aaa170523578fe782f69615c863cde93b73465b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:33 +0000

    Add package MorphoPreText/0.1.1/MorphoPreText-0.1.1-py3-none-any.whl

---
+++ packages/MorphoPreText/MorphoPreText-0.1.1-py3-none-any.whl/morphopretext/__init__.py
@@ -0,0 +1,6 @@
+from .character_word_count import WordCharacterCount
+from .english_text_preprocessor import EnglishTextPreprocessor
+from .persian_text_preprocessor import PersianTextPreprocessor
+from .Dictionaries_En import english_dict
+from .Dictionaries_Fa import arabic_dict
+from .separate_train_test_validation import main as split_datasets
