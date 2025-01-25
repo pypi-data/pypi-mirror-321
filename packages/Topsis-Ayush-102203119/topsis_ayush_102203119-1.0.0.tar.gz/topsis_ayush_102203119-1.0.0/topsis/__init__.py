63a496706ca71dfe3e4eb6bcf86581bcb638fe7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:21:54 +0000

    Add package Topsis-Ayush-102203119/1.0.0/topsis_ayush_102203119-1.0.0.tar.gz

---
+++ packages/Topsis-Ayush-102203119/topsis_ayush_102203119-1.0.0.tar.gz/topsis_ayush_102203119-1.0.0/topsis/__init__.py
@@ -0,0 +1,13 @@
+ 
+"""
+TOPSIS Package
+==============
+
+This package provides functionality to perform multi-criteria decision-making
+using the Technique for Order of Preference by Similarity to Ideal Solution (TOPSIS).
+
+Modules:
+    - topsis: Contains the main implementation of the TOPSIS method.
+"""
+
+from .topsis import topsis
