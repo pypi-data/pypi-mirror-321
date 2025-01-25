64f3e7b5f514e970bba0a555d60c0f7fc1578f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:06:23 +0000

    Add package aiq-platform-api/0.1.7/aiq_platform_api-0.1.7.tar.gz

---
+++ packages/aiq-platform-api/aiq_platform_api-0.1.7.tar.gz/aiq_platform_api-0.1.7/aiq_platform_api/integration_use_cases.py
@@ -0,0 +1,36 @@
+import itertools
+
+from .common_utils import AttackIQRestClient, AttackIQLogger, ConnectorUtils
+from .env import ATTACKIQ_API_TOKEN, ATTACKIQ_PLATFORM_URL
+
+logger = AttackIQLogger.get_logger(__name__)
+
+
+def fetch_and_log_connectors(client: AttackIQRestClient, limit: int):
+    logger.info(f"Fetching and processing up to {limit} company connectors...")
+    connector_count = 0
+    connectors = ConnectorUtils.get_connectors(client)
+
+    for connector in itertools.islice(connectors, limit):
+        connector_count += 1
+        logger.info(f"Connector {connector_count}:")
+        logger.info(f"  ID: {connector.get('id')}")
+        logger.info(f"  Name: {connector.get('name')}")
+        logger.info(f"  Type: {connector.get('type')}")
+        logger.info(f"  Status: {connector.get('status')}")
+        logger.info("---")
+
+    if connector_count == 0:
+        logger.error("Failed to retrieve any company connectors.")
+    else:
+        logger.info(f"Successfully processed {connector_count} company connectors.")
+
+
+def main():
+    client = AttackIQRestClient(ATTACKIQ_PLATFORM_URL, ATTACKIQ_API_TOKEN)
+
+    fetch_and_log_connectors(client, limit=5)
+
+
+if __name__ == "__main__":
+    main()
