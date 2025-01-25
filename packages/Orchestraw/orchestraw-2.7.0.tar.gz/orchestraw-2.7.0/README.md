256747f24a294a95fe208b4424a8c85055a0afdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:05:59 +0000

    Add package Orchestraw/2.7.0/orchestraw-2.7.0.tar.gz

---
+++ packages/Orchestraw/orchestraw-2.7.0.tar.gz/orchestraw-2.7.0/README.md
@@ -0,0 +1,61 @@
+# Orchestraw 「稻草乐队」
+Orchestraw: Download Netease Cloud Music with Metadata
+下载网易云音乐音频与元数据
+
+# Installation 安装
+1. 通过 pip 安装
+在终端执行以下命令:
+```
+pip install orchestraw
+```
+
+2. 通过[Releases页](https://github.com/crillerium/orchestraw/releases)下载 *.whl* 或 *.tar.gz* 安装文件
+下载完成后，在终端执行以下命令:
+```
+pip install path/to/your_downloaded.whl
+或
+pip install path/to/your_downloaded.tar.gz
+```
+
+# Usage 使用
+## 下载音频、元数据与歌词
+介绍: 下载网易云音乐音频、元数据与歌词  
+在终端执行以下命令:  
+```
+python -m orchestraw
+```
+或  
+```
+ost
+```
+## 元数据编辑器
+介绍: 为已有音频添加元数据与歌词 *(前提是网易云音乐中有对应的音乐)* ，现在也支持手动输入！  
+在终端执行以下命令:  
+```
+python -m orchestraw.metaeditor <待添加元数据文件.mp3>
+```
+或  
+```
+ostm <待添加元数据文件.mp3>
+```
+
+## 歌词下载器
+介绍: 单独为已有音频下载歌词 *(前提是网易云音乐中有对应的歌词)*  
+在终端执行以下命令:  
+```
+python -m orchestraw.lyric <待下载歌词文件.mp3>
+```
+或  
+```
+ostl <待下载歌词文件.mp3>
+```
+
+
+# Notice 注意事项  
+1. 此项目使用其他网站提供的API接口，因此稳定性不高，但是能用！  
+(◦˙▽˙◦)  
+2. 可正常下载的音乐只能是非会员歌曲，会员歌曲只能下载片段，但是可以通过第三方网站下载+元数据编辑器搭配使用。  
+3. 元数据编辑器与歌词下载器不受网易云会员限制，请放心食用！  
+4. 元数据编辑器的手动输入模式无法获取歌词  
+5. Orchestraw 只能处理mp3格式文件，flac等其他格式的文件请使用ffmpeg等第三方工具转换格式  
+6. Orchestraw 所有功能造成的更改均**不可逆**,使用前**请做好备份**！  
---
+++ packages/Orchestraw/orchestraw-2.7.0.tar.gz/orchestraw-2.7.0/src/orchestraw/metaeditor.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+import requests
+import sys
+from mutagen.id3 import ID3, APIC, TIT2, TPE1, TALB
+
+def search_for_songs():
+    # 1. 获取用户输入
+    query = input("请输入查询关键词: ")
+    
+    # 2. 发送请求到API
+    url = "https://api.csm.sayqz.com/search"
+    params = {'keywords': query}
+    response = requests.get(url, params=params)
+    
+    # 3. 将获取的数据转为字典
+    responseMap = response.json()
+
+    # 4. 遍历responseMap中的'result'下的'songs'列表
+    if 'result' in responseMap and 'songs' in responseMap['result']:
+        songs = responseMap['result']['songs']
+        music_list = {}
+        i = 1
+        for song in songs:
+            music_list[i] = [song['id'],song['name'],song['artists'][0]['name'],song['album']['name'],song['album']['id']]
+            print("["+str(i)+"]", "歌名:", music_list[i][1], "歌手:", music_list[i][2], "专辑:", music_list[i][3])
+            i+=1
+        return music_list
+    else:
+        print("抱歉，没有找到歌曲信息。")
+        sys.exit()
+
+def set_song_metadata(filename, song_name, singer, album, cover):
+    #获取音乐内容
+    audio = ID3(filename)
+    #print(audio)
+    audio.update_to_v23()  # 把可能存在的旧版本升级为2.3
+    audio['APIC'] = APIC(  # 插入专辑图片
+        encoding=0,
+        mime='image/jpeg',
+        type=3,
+        # desc=u'Cover',
+        data=cover
+    )
+    audio['TIT2'] = TIT2(  # 插入歌名
+        encoding=3,
+        text=song_name
+    )
+    audio['TPE1'] = TPE1(  # 插入第一演奏家、歌手、等
+        encoding=3,
+        text=singer
+    )
+    audio['TALB'] = TALB(  # 插入专辑名称
+        encoding=3,
+        text=album
+    )
+    audio.save() 
+
+def set_song_metadata_by_hand():
+    #获取音乐内容
+    audio = ID3(sys.argv[1])
+    #print(audio)
+    audio.update_to_v23()  # 把可能存在的旧版本升级为2.3
+    pic = input('请输入本地封面路径(留空则不更改):')
+    if pic != "":
+        with open(pic,"rb") as f:
+            cover = f.read()
+        audio['APIC'] = APIC(  # 插入专辑图片
+            encoding=0,
+            mime='image/jpeg',
+            type=3,
+            # desc=u'Cover',
+            data=cover
+        )
+    song_name = input('请输入歌曲名称(留空则不更改):')
+    if song_name != "":
+        audio['TIT2'] = TIT2(  # 插入歌名
+            encoding=3,
+            text=song_name
+        )
+    singer = input("请输入歌手名称(留空则不更改):")
+    if  singer != "":
+        audio['TPE1'] = TPE1(  # 插入第一演奏家、歌手、等
+            encoding=3,
+            text=singer
+        )
+    album = input("请输入专辑名称(留空则不更改):")
+    if album != "":
+        audio['TALB'] = TALB(  # 插入专辑名称
+            encoding=3,
+            text=album
+        )
+    audio.save()  
+    print("歌曲元数据信息修改完毕")
+
+def detect_lyric(id,filename):
+    response = requests.get("https://api.csm.sayqz.com/lyric",params={"id":id})
+    if response.status_code == 200:
+        data = response.json()
+        if "lrc" in data and "lyric" in data['lrc']:
+            opt = input("检测到此歌曲存在歌词，是否下载？[y/N]")
+            if opt == "y" or opt == "Y":
+                with open(filename+".lrc","w") as f:
+                    f.write(data['lrc']['lyric'].replace("\n","\n"))
+                    print(f"歌词已下载并保存为：{filename}.lrc")
+
+def get_filename(filename):
+    # 检查文件名是否以.mp3结尾
+    if filename.endswith('.mp3'):
+        new_filename = filename[:-4]
+        return new_filename
+
+def download(music_list):
+    order = int(input('请输入序号:'))
+    with requests.get('https://api.csm.sayqz.com/album',params={'id': music_list[order][4]}) as res:
+        # 获取封面
+        if res.status_code == 200:
+            picUrl = res.json()['album']['picUrl']
+            picUrlStr = res.json()['album']['picId_str']
+            for song in res.json()['songs']:
+                if song['id'] == music_list[order][0]:
+                    pic = song['al']['pic_str']
+                    picUrl = picUrl.replace(picUrlStr,pic)
+            with requests.get(picUrl) as r:
+                global cover
+                cover = r.content
+        else:
+            print('封面下载失败')
+            sys.exit()
+    filename = sys.argv[1]
+    set_song_metadata(filename, music_list[order][1], music_list[order][2], music_list[order][3], cover)
+    print(f"文件元数据修改成功并保存为：{filename}")
+    detect_lyric(music_list[order][0],get_filename(filename))
+
+def main():
+    try:
+        print("欢迎使用Orchestraw 元数据编辑器!")
+        print("请选择元数据来源:\n[1]网易云音乐\n[2]手动输入")
+        opt = input("请输入序号: ")
+        if opt == "1":
+            music_list = search_for_songs()
+            download(music_list)
+        elif opt == "2":
+            set_song_metadata_by_hand()
+    except KeyboardInterrupt:
+        print('脚本已退出')
+    except EOFError:
+        print('脚本已退出')
+    except Exception as e:
+        print('脚本意外退出: ',e)
+
+if __name__ == '__main__':
+    main()
