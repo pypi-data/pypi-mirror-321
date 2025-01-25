8953edfdd82a7031db6cbeab055d56eef96b8b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:06:12 +0000

    Add package pypharm/1.4.1/pypharm-1.4.1.tar.gz

---
+++ packages/pypharm/pypharm-1.4.1.tar.gz/pypharm-1.4.1/PyPharm/constants.py
@@ -0,0 +1,56 @@
+MODEL_CONST = {
+    'human':{
+                  'adipose': {'V':143, 'Q':3.7},
+                  'bone': {'V':124, 'Q': 3.6} ,
+                  'brain': {'V':20.7, 'Q': 10} ,
+                  'gut': {'V':23.6, 'Q': 13} ,
+                  'heart': {'V':3.8, 'Q': 2.14},
+                  'kidney': {'V':4.4, 'Q': 15.7} ,
+                  'liver': {'V':24.1, 'Q': 21} ,
+                  'lung': {'V':16.7, 'Q': 71} ,
+                  'muscle': {'V':429, 'Q': 10.7} ,
+                  'pancreas': {'V':1.2, 'Q': 1.9} ,
+                  'skin': {'V':111, 'Q': 4.3} ,
+                  'spleen': {'V':2.7, 'Q': 1.1},
+                  'stomach': {'V':2.2, 'Q':0.56},
+                  'teaster': {'V':0.51, 'Q':0.04},
+                  'arterial_blood': {'V':25.7} ,
+                  'venous_blood': {'V':51.4}
+               },
+               'rat':{
+                  'adipose': {'V':40, 'Q':1.6},
+                  'bone': {'V':53.2, 'Q': 10.12},
+                  'brain': {'V':6.8, 'Q': 5.32}  ,
+                  'gut': {'V':40, 'Q': 52}  ,
+                  'heart': {'V':3.2, 'Q': 15.68},
+                  'kidney': {'V':9.2, 'Q': 36.92},
+                  'liver': {'V':41.2, 'Q': 80}  ,
+                  'lung': {'V':4, 'Q': 203.2}   ,
+                  'muscle': {'V':487.6, 'Q': 30} ,
+                  'pancreas': {'V':5.2, 'Q': 4}  ,
+                  'skin': {'V':160, 'Q': 20}   ,
+                  'spleen': {'V':2.4, 'Q': 5}  ,
+                  'stomach': {'V':4.4, 'Q':8.2} ,
+                  'teaster': {'V':10, 'Q':1.8}   ,
+                  'arterial_blood': {'V':22.4, 'Q':10.8}  ,
+                  'venous_blood': {'V':45.2}
+               }
+}
+
+class ORGAN_NAMES:
+
+    LUNG = 'lung'
+    HEART = 'heart'
+    BRAIN = 'brain'
+    MUSCLE = 'muscle'
+    ADIPOSE = 'adipose'
+    SKIN = 'skin'
+    BONE = 'bone'
+    KIDNEY = 'kidney'
+    LIVER = 'liver'
+    GUT = 'gut'
+    SPLEEN = 'spleen'
+    STOMACH = 'stomach'
+    PANCREAS = 'pancreas'
+    VENOUS = 'venous_blood'
+    ARTERIAL = 'arterial_blood'
