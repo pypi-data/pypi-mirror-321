f70897f4996140dd3c939f72692be5957e477c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:07:09 +0000

    Add package diff-1c/6.6.8/diff_1c-6.6.8.tar.gz

---
+++ packages/diff-1c/diff_1c-6.6.8.tar.gz/diff_1c-6.6.8/src/diff_1c/__about__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Информация о пакете"""
+
+
+import importlib.metadata
+
+APP_AUTHOR = "util-1c"
+APP_NAME = "diff-1c"
+
+__version__ = importlib.metadata.version(__package__)
