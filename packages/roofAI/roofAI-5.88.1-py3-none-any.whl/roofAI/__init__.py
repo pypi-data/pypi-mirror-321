60b6757072613641dfa60195a3766ec8d39c65c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 00:06:48 +0000

    Add package roofAI/5.88.1/roofAI-5.88.1-py3-none-any.whl

---
+++ packages/roofAI/roofAI-5.88.1-py3-none-any.whl/roofAI/__init__.py
@@ -0,0 +1,17 @@
+NAME = "roofAI"
+
+ICON = "🏛️"
+
+DESCRIPTION = f"{ICON} everything AI about roofs."
+
+VERSION = "5.88.1"
+
+MARQUEE = "https://github.com/kamangir/assets/blob/main/2023-10-28-16-28-36-88493-predict.gif?raw=true"
+
+REPO_NAME = NAME
+
+ALIAS = NAME
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"
