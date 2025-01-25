960b9d43be69176ca45e9a1c1dd7d652bc79d6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:07:54 +0000

    Add package VectorCode/0.1.1/vectorcode-0.1.1-py3-none-any.whl

---
+++ packages/VectorCode/vectorcode-0.1.1-py3-none-any.whl/vectorcode/subcommands/__init__.py
@@ -0,0 +1,7 @@
+from vectorcode.subcommands.drop import drop
+from vectorcode.subcommands.init import init
+from vectorcode.subcommands.ls import ls
+from vectorcode.subcommands.query import query
+from vectorcode.subcommands.vectorise import vectorise
+
+__all__ = ["drop", "init", "query", "ls", "vectorise"]
