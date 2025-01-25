db077b35cb3ff66735b3c7a9baa9c775ae1b4169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:06:51 +0000

    Add package tj0vtj0v-API-Client/1.2/tj0vtj0v_api_client-1.2.tar.gz

---
+++ packages/tj0vtj0v-API-Client/tj0vtj0v_api_client-1.2.tar.gz/tj0vtj0v_api_client-1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='tj0vtj0v_API_Client',
+    packages=['API_Client'],
+    version='1.2',
+    license='GNU',
+    description='API Client, which automatically authenticates and refreshes',
+    author='tj0vtj0v',
+    author_email='burdorftjorven@gmail.com',
+    url='https://github.com/tj0vtj0v/API_Client',
+    keywords=['Connector', 'REST', 'CRUD', 'Private'],
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
