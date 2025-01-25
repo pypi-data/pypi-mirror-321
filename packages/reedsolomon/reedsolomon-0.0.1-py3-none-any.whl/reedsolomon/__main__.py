b417db89cb3417d96df828c080c83c925ce82caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:08:14 +0000

    Add package reedsolomon/0.0.1/reedsolomon-0.0.1-py3-none-any.whl

---
+++ packages/reedsolomon/reedsolomon-0.0.1-py3-none-any.whl/reedsolomon/__main__.py
@@ -0,0 +1,11 @@
+"""A file to configure calling in the prompt line"""
+
+import sys
+
+
+def main():
+    print("Not implemented for the moment.")
+
+
+if __name__ == '__main__':
+    sys.exit(main())
