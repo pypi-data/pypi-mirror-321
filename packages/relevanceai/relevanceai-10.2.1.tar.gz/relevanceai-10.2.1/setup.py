5b8cf044dc4df584754609210bc0b3e7bba0148e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 00:07:15 +0000

    Add package relevanceai/10.2.1/relevanceai-10.2.1.tar.gz

---
+++ packages/relevanceai/relevanceai-10.2.1.tar.gz/relevanceai-10.2.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="relevanceai",
+    version="10.2.1",
+    url="https://relevanceai.com/",
+    author="Relevance AI",
+    author_email="jacky@relevanceai.com",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    setup_requires=["wheel"],
+    install_requires=[
+        "pydantic==2.8.2",
+        "requests==2.32.3",
+        "httpx==0.27.2",
+        "pytest==8.3.4"
+    ],
+    package_data={"": ["*.ini"]},
+    extras_require=dict(),
+)
