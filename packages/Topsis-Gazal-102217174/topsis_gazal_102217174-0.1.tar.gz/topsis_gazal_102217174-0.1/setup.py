cdfdb2343a7097753295374949039ca3a986f3f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:06:10 +0000

    Add package Topsis-Gazal-102217174/0.1/topsis_gazal_102217174-0.1.tar.gz

---
+++ packages/Topsis-Gazal-102217174/topsis_gazal_102217174-0.1.tar.gz/topsis_gazal_102217174-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Topsis-Gazal-102217174',
+    version='0.1',
+    author='Gazal',
+    author_email='2580gazal@gmail.com',
+    description='A Python implementation of the TOPSIS method.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy'],
+    entry_points={
+        'console_scripts': [
+            'topsis=topsis.topsis:main',
+        ],
+    },
+)
