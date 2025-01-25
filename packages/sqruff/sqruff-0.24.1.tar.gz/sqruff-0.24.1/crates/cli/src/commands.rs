bb7971cdaa1e88d43f55ca7c06d35b37e54c3fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:21:47 +0000

    Add package sqruff/0.24.1/sqruff-0.24.1.tar.gz

---
+++ packages/sqruff/sqruff-0.24.1.tar.gz/sqruff-0.24.1/crates/cli/src/commands.rs
@@ -0,0 +1,68 @@
+use std::path::PathBuf;
+use strum_macros::Display;
+
+use clap::{Parser, Subcommand, ValueEnum};
+
+use crate::github_action::is_in_github_action;
+
+#[derive(Debug, Parser)]
+#[command(name = "sqruff")]
+#[command(about = "sqruff is a sql formatter and linter", long_about = None, version=env!("CARGO_PKG_VERSION")
+)]
+pub(crate) struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+    /// Path to a configuration file.
+    #[arg(long, global = true)]
+    pub config: Option<String>,
+    /// Show parse errors.
+    #[arg(long, global = true, default_value = "false")]
+    pub parsing_errors: bool,
+}
+
+#[derive(Debug, Subcommand)]
+pub(crate) enum Commands {
+    #[command(name = "lint", about = "lint files")]
+    Lint(LintArgs),
+    #[command(name = "fix", about = "fix files")]
+    Fix(FixArgs),
+    #[command(name = "lsp", about = "Run an LSP server")]
+    Lsp,
+}
+
+#[derive(Debug, Parser)]
+pub(crate) struct LintArgs {
+    /// Files or directories to fix. Use `-` to read from stdin.
+    pub paths: Vec<PathBuf>,
+    #[arg(default_value_t, short, long)]
+    pub format: Format,
+}
+
+#[derive(Debug, Parser)]
+pub(crate) struct FixArgs {
+    /// Files or directories to fix. Use `-` to read from stdin.
+    pub paths: Vec<PathBuf>,
+    /// Skip the confirmation prompt and go straight to applying fixes.
+    #[arg(long)]
+    pub force: bool,
+    #[arg(default_value_t, short, long)]
+    pub format: Format,
+}
+
+#[derive(Debug, Clone, Copy, ValueEnum, Display)]
+#[strum(serialize_all = "kebab-case")]
+pub(crate) enum Format {
+    Human,
+    GithubAnnotationNative,
+    Json,
+}
+
+impl Default for Format {
+    fn default() -> Self {
+        if is_in_github_action() {
+            Format::GithubAnnotationNative
+        } else {
+            Format::Human
+        }
+    }
+}
