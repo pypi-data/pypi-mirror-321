5176bd4b6e8cc3c100af04a9986716802b66f306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:54 +0000

    Add package automock25/0.2.2/automock25-0.2.2-py3-none-any.whl

---
+++ packages/automock25/automock25-0.2.2-py3-none-any.whl/automock25/main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from automock25.logset import setup_logging
+from automock25.recordJob import jobStart
+from   automock25 import controller  # 导入 controller.py 文件
+app = Flask(__name__)
+
+
+def main():
+    # 设置日志
+    logger = setup_logging
+    logger.info("Starting App")
+    # 启动定时任务
+    jobStart()
+    # 初始化路由
+    controller.init_routes(app)
+    app.run(host='127.0.0.1', port=5000)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
