78b0b1a2507b81b0b39807cc1d254112a46c37ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:09:22 +0000

    Add package elastic-structlog/0.0.4/elastic_structlog-0.0.4.tar.gz

---
+++ packages/elastic-structlog/elastic_structlog-0.0.4.tar.gz/elastic_structlog-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Extension / Wrapper for structlog library.'
+LONG_DESCRIPTION = (Path(__file__).parent.parent / 'README.md').read_text()
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="elastic_structlog",
+    version=VERSION,
+    author="Nuriel Gadilov",
+    author_email="nurielprivet@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["elasticsearch", "structlog"],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'structlog', 'es', "elastic"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
