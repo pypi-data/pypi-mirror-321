82cd6f180e54cf6fc3a4e35191028156e4b85727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:06:17 +0000

    Add package PBI-dashboard-creator/1.0.7/pbi_dashboard_creator-1.0.7-py3-none-any.whl

---
+++ packages/PBI-dashboard-creator/pbi_dashboard_creator-1.0.7-py3-none-any.whl/PBI_dashboard_creator/create_blank_dashboard.py
@@ -0,0 +1,47 @@
+# Create a new blank .pbir dashboard
+import os
+from pathlib import Path
+import uuid
+from importlib import resources
+import shutil
+
+
+
+def create_new_dashboard(parent_dir, report_name):
+
+
+	#create a new logical id field
+	# see this for explanation of what a UUID is: https://stackoverflow.com/a/534847 
+	logical_id = str(uuid.uuid4())
+
+	# define page name
+	page1_name = "page1"
+
+
+	# Outer level directory --------------------------------------------------------------------------------
+	project_folder_path = os.path.join(parent_dir, report_name)
+
+
+
+
+	# check to make sure parent directory exists
+	if not os.path.exists(parent_dir):
+		raise ValueError("The parent directory doesn't exist! Please create it and try again!")
+
+
+	# make sure a report folder doesn't already exist
+	if os.path.exists(project_folder_path):
+		raise ValueError("Sorry a report with that name already exists! Please use a different report name or parent directory and try again")
+	
+	
+	traversable = resources.files("PBI-dashboard-creator.dashboard_resources")
+	
+	with resources.as_file(traversable) as path:
+		shutil.copytree(path, project_folder_path)
+
+
+
+
+
+
+
