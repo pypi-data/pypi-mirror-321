efccf918ae3ed44baeec29811a2a3d3a2d4b91c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:05:55 +0000

    Add package covvi-eci/1.1.5/covvi_eci-1.1.5.tar.gz

---
+++ packages/covvi-eci/covvi_eci-1.1.5.tar.gz/covvi_eci-1.1.5/eci/__init__.py
@@ -0,0 +1,9 @@
+
+from eci.utils      import *
+from eci.enums      import *
+from eci.primitives import *
+from eci.messages   import *
+from eci.interfaces import *
+from eci.sockets    import *
+
+from eci.__version__ import __version__
