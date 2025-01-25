28e42020186ba0902200cacc7bdf1d89e8334b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:22:03 +0000

    Add package blissdata/2.0.0rc3/blissdata-2.0.0rc3.tar.gz

---
+++ packages/blissdata/blissdata-2.0.0rc3.tar.gz/blissdata-2.0.0rc3/src/blissdata/redis_engine/encoding/json.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the bliss project
+#
+# Copyright (c) Beamline Control Unit, ESRF
+# Distributed under the GNU LGPLv3. See LICENSE for more info.
+import json
+from . import EncodedBatch, StreamEncoder
+
+
+class JsonStreamEncoder(StreamEncoder):
+    def info(self):
+        return {"type": "json"}
+
+    @classmethod
+    def from_info(cls, info):
+        assert info["type"] == "json"
+        return cls()
+
+    def encode(self, data):
+        return EncodedBatch(json.dumps(data).encode())
+
+    def decode(self, batches):
+        if isinstance(batches, EncodedBatch):
+            batches = (batches,)
+        return [json.loads(batch.payload) for batch in batches]
