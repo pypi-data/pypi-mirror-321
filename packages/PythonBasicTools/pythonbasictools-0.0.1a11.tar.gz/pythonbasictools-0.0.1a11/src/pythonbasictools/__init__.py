ebe2b7b7d6a53e5f0483c40f504cd135a7b9e9ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:05:56 +0000

    Add package PythonBasicTools/0.0.1a11/pythonbasictools-0.0.1a11.tar.gz

---
+++ packages/PythonBasicTools/pythonbasictools-0.0.1a11.tar.gz/pythonbasictools-0.0.1a11/src/pythonbasictools/__init__.py
@@ -0,0 +1,20 @@
+__author__ = "Jérémie Gince"
+__email__ = "gincejeremie@gmail.com"
+__copyright__ = "Copyright 2021, Jérémie Gince"
+__license__ = "Apache 2.0"
+__url__ = "https://github.com/JeremieGince/PythonBasicTools"
+__version__ = "0.0.1-alpha.11"
+
+from .logging_tools import logs_file_setup
+from .device import DeepLib, log_device_setup
+from .decorators import log_func
+from .multiprocessing_tools import worker_init, multiprocess_logger_init, apply_func_multiprocess
+from .slurm import SlurmHostServer, send_slurm_cmd, generate_slurm_cmd
+from . import docstring
+from . import google_drive
+from . import lock
+from . import discord
+from .simple_tts import say
+from . import cmds
+from .run_output_file import RunOutputFile
+from .hash_tools import hash_dict
