50144b8ffef32d987a8d0dbd85c5b4bf75372922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:05:17 +0000

    Add package lldb-rust-pretty/0.1.0/lldb_rust_pretty-0.1.0-py3-none-any.whl

---
+++ packages/lldb-rust-pretty/lldb_rust_pretty-0.1.0-py3-none-any.whl/lldb_rust_pretty/logger.py
@@ -0,0 +1,23 @@
+import logging
+from datetime import datetime, timezone
+from logging import DEBUG, LogRecord, Logger, StreamHandler, getLogger
+from typing import Optional
+
+NAME = 'lldb_rust_pretty'
+
+class Formatter(logging.Formatter):
+    def formatTime(self, record: LogRecord, datefmt: Optional[str] = None) -> str:
+        return datetime.fromtimestamp(record.created, timezone.utc).astimezone().isoformat(sep="T", timespec="milliseconds")
+
+def get_logger() -> Logger:
+    return getLogger(NAME)
+
+def _init():
+    handler = StreamHandler()
+    handler.setLevel(DEBUG)
+    handler.setFormatter(Formatter('%(asctime)s %(levelname)-5s %(name)s: %(message)s'))
+    logger = get_logger()
+    logger.setLevel(DEBUG)
+    logger.addHandler(handler)
+
+_init()
