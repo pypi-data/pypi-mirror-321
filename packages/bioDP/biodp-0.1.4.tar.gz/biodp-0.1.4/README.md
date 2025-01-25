8d03aaebd633dfe5e4a545b90544156af6d4634c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:42 +0000

    Add package bioDP/0.1.4/biodp-0.1.4.tar.gz

---
+++ packages/bioDP/biodp-0.1.4.tar.gz/biodp-0.1.4/README.md
@@ -0,0 +1,25 @@
+# bioDP
+
+A package for biological data processing, especially related to metabodirect
+## Installation
+
+You can install this package using pip:
+```bash
+pip install bioDP
+```
+## Usage
+
+### Command
+
+```bash
+bioDP-metabodirect [input_folder] [output_folder and fileName]
+```  
+ eg.`bioDP-metabodirect C:\data C:\dispose\report.csv`  
+
+`bioDP-metabodirect --help`
+
+### Import Package
+
+```python
+from bioDP.metabodirect import excel_process
+```
---
+++ packages/bioDP/biodp-0.1.4.tar.gz/biodp-0.1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bioDP',
+    version='0.1.4',
+    description='pi',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='bit15k',
+    author_email='i@logdict.com',
+    url='https://github.com/bit15k/bioDP',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'openpyxl',
+        # 其他依赖项
+    ],
+    entry_points={
+        'console_scripts': [
+            'bioDP-metabodirect=bloDP.metabodirect.cli:main',  # 假设 cli.py 中有一个 main 函数作为命令行入口
+        ],
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+
+    ],
+)
