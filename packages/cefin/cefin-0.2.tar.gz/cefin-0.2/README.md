ea78042c6b198c459efea3e5e24f85d57735de1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:06:12 +0000

    Add package cefin/0.2/cefin-0.2.tar.gz

---
+++ packages/cefin/cefin-0.2.tar.gz/cefin-0.2/README.md
@@ -0,0 +1,11 @@
+# cefin
+We are Insper's Finance Unit. This repository holds code and data used in our studies.
+
+# Installation
+```commandline
+pip install cefin
+```
+
+# Data Feeder
+All of our data can be retrieved through the `CefinData` class. The available methods are:
+- `titulos_publicos()`: Brazilian Public Bonds secondary market. Source is from the [Brazilian Central Bank website](https://www4.bcb.gov.br/pom/demab/negociacoes/apresentacao.asp?frame=1). Our data has been cleaned and strucured.
---
+++ packages/cefin/cefin-0.2.tar.gz/cefin-0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.2'
+DESCRIPTION = "Repository of Insper's Finance Unit"
+
+# Setting up
+setup(
+    name="cefin",
+    version=VERSION,
+    author="Insper's Finance Unit",
+    maintainer="Insper's Finance Unit",
+    maintainer_email="cefim@insper.edu.br",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'requests',
+    ],
+    keywords=[
+        'finance',
+        'financial data',
+    ],
+)
