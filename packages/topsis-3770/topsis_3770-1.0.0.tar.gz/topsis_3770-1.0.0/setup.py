a4b9f6a663104f2927720c690decaab7c2dbecbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:06:15 +0000

    Add package topsis-3770/1.0.0/topsis_3770-1.0.0.tar.gz

---
+++ packages/topsis-3770/topsis_3770-1.0.0.tar.gz/topsis_3770-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="topsis-3770",
+    version="1.0.0",
+    author="Sarabjeet Singh",
+    author_email="ssingh20_be22@thapar.edu",
+    description="A Python package implementing TOPSIS method for Multi-Criteria Decision Making (MCDM)",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SARAB0297/Topsis_3770",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pandas>=1.0.0',
+        'numpy>=1.19.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'topsis=topsis.topsis:main',
+        ],
+    }
+)
