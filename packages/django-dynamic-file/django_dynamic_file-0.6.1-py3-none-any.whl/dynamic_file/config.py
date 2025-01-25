dd5d322a52747a1474ecdbc48d8146b98965b07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:09:39 +0000

    Add package django-dynamic-file/0.6.1/django_dynamic_file-0.6.1-py3-none-any.whl

---
+++ packages/django-dynamic-file/django_dynamic_file-0.6.1-py3-none-any.whl/dynamic_file/config.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+if not hasattr(settings, 'DYNAMIC_FILE_UPLOADED_BY_MODEL'):
+    setattr(settings, 'DYNAMIC_FILE_UPLOADED_BY_MODEL', settings.AUTH_USER_MODEL)
+
+if not hasattr(settings, 'DYNAMIC_FILE_STORAGE_LOCATION'):
+    setattr(settings, 'DYNAMIC_FILE_STORAGE_LOCATION', 'files')
+
+if not hasattr(settings, 'DYNAMIC_FILE_SERVE_LOCATION'):
+    setattr(settings, 'DYNAMIC_FILE_SERVE_LOCATION', getattr(settings, 'DYNAMIC_FILE_STORAGE_LOCATION'))
+
+if not hasattr(settings, 'DYNAMIC_FILE_UPLOADED_BY_MIGRATION_DEPENDENCY'):
+    setattr(settings, 'DYNAMIC_FILE_UPLOADED_BY_MIGRATION_DEPENDENCY', '__first__')
