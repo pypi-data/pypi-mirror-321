ad0dcd49b1544a23c484462e826dbf0a3346b42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:07:53 +0000

    Add package Molara/0.1.1/Molara-0.1.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/Molara/Molara-0.1.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/molara/__init__.py
@@ -0,0 +1,11 @@
+"""Init file for molara package."""
+
+from __future__ import annotations
+
+__copyright__ = "Copyright 2024, Molara"
+__version__ = "0.1.1"
+"""This variable is used as a single source of truth for the version.
+
+All other parts that need the version such as pyproject.toml and the sphinx
+documentation should import it from here.
+"""
