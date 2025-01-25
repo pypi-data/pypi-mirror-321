4323c5de245d914535891e57fa5cf2e13a78af34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:22:15 +0000

    Add package lingmopyui/1.0.4/lingmopyui-1.0.4.tar.gz

---
+++ packages/lingmopyui/lingmopyui-1.0.4.tar.gz/lingmopyui-1.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools  # 导入setuptools打包工具
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lingmopyui",  
+    version="1.0.4", 
+    author="Admibrill",  
+    author_email="admibrill@outlook.com", 
+    description="A Lingmo GUI Library based on PySide6.QtWidgets",  # 包的简述
+    long_description=long_description,  # 包的详细介绍，一般在README.md文件内
+    long_description_content_type="text/markdown",
+    url="https://github.com/LingmoOS/LingmoPyUI",  # 自己项目地址，比如github的项目地址
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    license='GPLv3',
+    python_requires='>=3.6',  # 对python的最低版本要求
+)
