a2e0bea522a28b845e62b30965576e26f4585d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:05:11 +0000

    Add package deeptensor/0.3.0/deeptensor-0.3.0-pp310-pypy310_pp73-macosx_10_15_x86_64.whl

---
+++ packages/deeptensor/deeptensor-0.3.0-pp310-pypy310_pp73-macosx_10_15_x86_64.whl/deeptensor/__init__.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from ._core import (
+    SGD,
+    AdaGrad,
+    Adam,
+    FeedForwardLayer,
+    GeLu,
+    LeakyReLu,
+    Model,
+    Momentum,
+    ReLu,
+    RMSprop,
+    Sigmoid,
+    SoftMax,
+    Tanh,
+    Tensor,
+    Value,
+    __doc__,
+    cross_entropy,
+    mean_squared_error,
+)
+
+__all__ = [
+    "SGD",
+    "AdaGrad",
+    "Adam",
+    "FeedForwardLayer",
+    "GeLu",
+    "LeakyReLu",
+    "Model",
+    "Momentum",
+    "RMSprop",
+    "ReLu",
+    "Sigmoid",
+    "SoftMax",
+    "Tanh",
+    "Tensor",
+    "Value",
+    "__doc__",
+    "cross_entropy",
+    "mean_squared_error",
+]
