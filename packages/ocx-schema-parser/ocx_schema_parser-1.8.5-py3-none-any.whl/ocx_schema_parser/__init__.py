522afd86f70903dc7077f2193e3f5a8612a43b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:22:19 +0000

    Add package ocx-schema-parser/1.8.5/ocx_schema_parser-1.8.5-py3-none-any.whl

---
+++ packages/ocx-schema-parser/ocx_schema_parser-1.8.5-py3-none-any.whl/ocx_schema_parser/__init__.py
@@ -0,0 +1,24 @@
+#  Copyright (c) 2023-2024. OCX Consortium https://3docx.org. See the LICENSE
+
+__version__ = "1.8.5"
+
+# Third party imports
+from loguru import logger
+
+# Application imports
+from ocx_schema_parser.config import config
+
+SCHEMA_FOLDER = config.get("SchemaParserSettings", "schema_folder")
+TMP_FOLDER = config.get("SchemaParserSettings", "tmp_folder")
+WORKING_DRAFT = config.get("SchemaParserSettings", "working_draft")
+DEFAULT_SCHEMA = config.get("SchemaParserSettings", "schema_url")
+keys = config.get("SchemaParserSettings", "w3c_schema_builtin_keys").split()
+values = config.get("SchemaParserSettings", "w3c_schema_builtin_values").split()
+W3C_SCHEMA_BUILT_IN_TYPES = dict(zip(keys, values))
+PROCESS_SCHEMA_TYPES = config.get(
+    "SchemaParserSettings", "process_schema_types"
+).split()
+ALLOWED_WORDS = config.get("SchemaParserSettings", "known_word_list").split()
+OCX_NAME_EXCEPTIONS = config.get("SchemaParserSettings", "ocx_name_exceptions").split()
+
+logger.disable(__name__)
