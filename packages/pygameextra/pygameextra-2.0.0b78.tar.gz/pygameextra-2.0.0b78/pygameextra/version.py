3d8cdfb053652cc32469c3e821b0aeceef2c128f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 11:07:47 +0000

    Add package pygameextra/2.0.0b78/pygameextra-2.0.0b78.tar.gz

---
+++ packages/pygameextra/pygameextra-2.0.0b78.tar.gz/pygameextra-2.0.0b78/pygameextra/version.py
@@ -0,0 +1,9 @@
+VERSION = '2.0.0'
+revision = 78
+alpha = False
+beta = True
+release_candidate = False
+
+
+def get():
+    return f'{VERSION}{"b" if beta else "a" if alpha else "rc" if release_candidate else "."}{revision}'
