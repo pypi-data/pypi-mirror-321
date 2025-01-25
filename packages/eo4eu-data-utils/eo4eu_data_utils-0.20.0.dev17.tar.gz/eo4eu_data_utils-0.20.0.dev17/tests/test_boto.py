750ba16411509a0f04a4917fc2d679451f892837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:21:42 +0000

    Add package eo4eu-data-utils/0.20.0.dev17/eo4eu_data_utils-0.20.0.dev17.tar.gz

---
+++ packages/eo4eu-data-utils/eo4eu_data_utils-0.20.0.dev17.tar.gz/eo4eu_data_utils-0.20.0.dev17/tests/test_boto.py
@@ -0,0 +1,28 @@
+import os
+import time
+import boto3
+import logging
+from pathlib import Path
+from pprint import pprint
+
+
+logger = logging.getLogger("test")
+logging.basicConfig()
+logger.setLevel(logging.DEBUG)
+
+
+boto_config = {
+    "region_name": "us-east-1",
+    "endpoint_url": os.environ["S3_ENDPOINT_URL"],
+    "aws_access_key_id": os.environ["AWS_ACCESS_KEY_ID"],
+    "aws_secret_access_key": os.environ["AWS_SECRET_ACCESS_KEY"],
+}
+
+
+if __name__ == "__main__":
+    resource = boto3.resource("s3", **boto_config)
+    bucket = resource.Bucket("test_pre_pro-base")
+    bucket.put_object(
+        Key = "logs/dummy.txt",
+        Body = bytes("dummy", "utf-8"),
+    )
