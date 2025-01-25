e1e0f8eb43bdc8bf0d962ea21c1202c56e6dd4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:06:01 +0000

    Add package webviewpy/1.5.1/webviewpy-1.5.1.tar.gz

---
+++ packages/webviewpy/webviewpy-1.5.1.tar.gz/webviewpy-1.5.1/README.md
@@ -0,0 +1,43 @@
+# webviewpy
+
+A tiny webview library for Python.
+
+Based on [webview](https://github.com/webview/webview)
+
+
+# Getting started
+
+### Install
+
+
+``` bash
+pip install webviewpy
+```
+
+
+### Usage
+
+[Build a webview based app](example/test.py)
+
+[Embed a webview in PyQt](example/testpyqt.py)
+
+[Embed a webview in tkinter](example/testtkinter.py)
+
+[Embed a webview in win32 window](example/win32.py)
+
+[Build a webview based app with react and PyQt](example/fluent/main.py)
+
+
+### Specify library path
+
+
+Wheel includes pre-built libraries of Windows/Linux/Macos, and libraries are placed at platform/{sys.platform}/{x86|x64}/webview. 
+
+To specify a self-built library or use other platform's library, use api ```declare_library_path``` like this:
+
+``` python
+from webviewpy import declare_library_path
+declare_library_path(
+    r"C:\build\library\webview.dll"
+)
+```
---
+++ packages/webviewpy/webviewpy-1.5.1.tar.gz/webviewpy-1.5.1/cibuildlib.py
@@ -0,0 +1,69 @@
+import sys, subprocess, shutil, os
+
+print(sys.platform)
+
+
+def listdir():
+    for f in os.walk("."):
+        _dir, _, _fs = f
+        for _f in _fs:
+            print(os.path.abspath(os.path.join(_dir, _f)))
+
+
+if len(sys.argv) >= 2 and sys.argv[1] == "release":
+    os.makedirs("./wheel", exist_ok=True)
+    listdir()
+    filepath = os.path.dirname(__file__)
+    os.chdir(filepath)
+    copy = (
+        lambda f, d: print(os.path.exists(f), os.path.abspath(f), os.path.abspath(d))
+        or os.makedirs(d, exist_ok=True)
+        or shutil.copy(f, d)
+    )
+
+    copy("./build/windows-2022/x86/bin/Release/webview.dll", "./webviewpy/platform/win32/x86")
+    copy("./build/windows-2022/x64/bin/Release/webview.dll", "./webviewpy/platform/win32/x64")
+    # copy('./build/ubuntu-20.04x86/library/libwebview.so','./webviewpy/platform/linux/x86')
+    copy(
+        "./build/ubuntu-22.04/x64/lib/libwebview.so",
+        "./webviewpy/platform/linux/x64",
+    )
+    # copy('./build/macos-11x86/library/libwebview.dylib','./webviewpy/platform/darwin/x86')
+    copy(
+        "./build/macos-14/x64/lib/libwebview.dylib",
+        "./webviewpy/platform/darwin/x64",
+    )
+    os.system("python -m build")
+    listdir()
+    exit()
+
+curr = os.getcwd()
+print(curr)
+if sys.platform == "win32":
+        
+    url = "https://github.com/Chuyu-Team/YY-Thunks/releases/download/v1.1.5/YY-Thunks-1.1.5-Objs.zip"
+    os.system(rf"curl -SLo YY-Thunks-1.1.5-Objs.zip " + url)
+    os.system(rf"7z x -y YY-Thunks-1.1.5-Objs.zip -oYY-Thunks")
+    os.system("cmake -A win32 -T host=x86 -B ./build/x86")
+    os.system("cmake --build ./build/x86 --config Release --target ALL_BUILD")
+
+    os.system("cmake -A x64 -T host=x64 -B ./build/x64")
+    os.system("cmake --build ./build/x64 --config Release --target ALL_BUILD")
+elif sys.platform == "linux":
+    os.system(
+        """
+sudo update-alternatives --force --install /usr/bin/gcc gcc /usr/bin/gcc-12 1200 --slave /usr/bin/g++ g++ /usr/bin/g++-12
+sudo update-alternatives --remove-all clang
+sudo update-alternatives --remove-all clang++
+sudo update-alternatives --remove-all clang-format
+sudo update-alternatives --remove-all clang-tidy
+sudo update-alternatives --force --install /usr/bin/clang clang /usr/bin/clang-15 1500 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-15 --slave /usr/bin/clang-format clang-format /usr/bin/clang-format-15 --slave /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-15
+sudo apt-get update && sudo apt-get install libwebkit2gtk-4.0-dev xvfb -y
+"""
+    )
+    os.system("cmake -B ./build/x64")
+    os.system("cmake --build ./build/x64 --config Release")
+elif sys.platform == "darwin":
+    os.system("cmake -B ./build/x64")
+    os.system("cmake --build ./build/x64 --config Release")
+os.chdir(curr)
