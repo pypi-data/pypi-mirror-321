be9239caf29486c431eaf6204129938065468235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 00:06:53 +0000

    Add package upbge-stubs/0.3.1.27.dev12821309372/upbge-stubs-0.3.1.27.dev12821309372.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev12821309372.tar.gz/upbge-stubs-0.3.1.27.dev12821309372/sequence_sound_modifier_type_items/__init__.pyi
@@ -0,0 +1,14 @@
+"""
+
+
+Sequence Sound Modifier Type Items
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:SOUND_EQUALIZER: 
+  Sound Equalizer.
+
+.. _rna-enum-sequence-sound-modifier-type-items:
+
+"""
+
+import typing
