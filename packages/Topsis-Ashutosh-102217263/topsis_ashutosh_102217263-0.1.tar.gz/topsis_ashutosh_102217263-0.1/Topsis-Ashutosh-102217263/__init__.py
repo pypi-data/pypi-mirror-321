a7c140564b36b9b07de05997eab812e266a4aa53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:06:01 +0000

    Add package Topsis-Ashutosh-102217263/0.1/topsis_ashutosh_102217263-0.1.tar.gz

---
+++ packages/Topsis-Ashutosh-102217263/topsis_ashutosh_102217263-0.1.tar.gz/topsis_ashutosh_102217263-0.1/Topsis-Ashutosh-102217263/__init__.py
@@ -0,0 +1,2 @@
+from .calculator import add, subtract, multiply, divide
+
---
+++ packages/Topsis-Ashutosh-102217263/topsis_ashutosh_102217263-0.1.tar.gz/topsis_ashutosh_102217263-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Topsis-Ashutosh-102217263',          # Package name
+    version='0.1',                     # Version number
+    packages=find_packages(),          # Automatically find packages
+    description='A simple calculator package for basic operations',
+    author='Ashutosh kumar swarnakar',                # Your name
+    author_email='ashutosh.swarnakar06@gmail.com',  # Your email
+    license='MIT',                     # License type
+    keywords='calculator math simple', # Keywords for PyPI
+)
