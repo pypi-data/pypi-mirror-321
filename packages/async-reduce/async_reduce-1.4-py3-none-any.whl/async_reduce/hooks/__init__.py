57233cb49ddaf6f07952635786cedf08d984bdba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:05:48 +0000

    Add package async-reduce/1.4/async_reduce-1.4-py3-none-any.whl

---
+++ packages/async-reduce/async_reduce-1.4-py3-none-any.whl/async_reduce/hooks/__init__.py
@@ -0,0 +1,11 @@
+from async_reduce.hooks.debug import DebugHooks
+from async_reduce.hooks.statistics import (
+    StatisticsOverallHooks,
+    StatisticsDetailHooks,
+)
+
+__all__ = (
+    'DebugHooks',
+    'StatisticsOverallHooks',
+    'StatisticsDetailHooks',
+)
