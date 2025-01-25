dc1e6abf317093bfeb6caabb38987d7a6532bd80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 00:07:50 +0000

    Add package licomp-osadl/0.4.1/licomp_osadl-0.4.1-py2.py3-none-any.whl

---
+++ packages/licomp-osadl/licomp_osadl-0.4.1-py2.py3-none-any.whl/licomp_osadl/config.py
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+licomp_osadl_version = '0.4.1'
+my_supported_api_version = '0.5'
+
+cli_name = 'licomp-osadl'
+module_name = 'licomp_osadl'
+module_url = 'https://github.com/hesa/licomp-osadl'
+osadl_checklist_url = 'https://www.osadl.org/Access-to-raw-data.oss-compliance-raw-data-access.0.html'
+
+
+disclaimer = 'The checklists and particularly the license compatibility data have been assembled with maximum diligence and care; however, the authors do not warrant nor can be held liable in any way for its correctness, usefulness, merchantibility or fitness for a particular purpose as far as permissible by applicable law. Anyone who uses the information does this on his or her sole responsibility. For any individual legal advice, it is recommended to contact a lawyer. (source: https://www.osadl.org/Access-to-raw-data.oss-compliance-raw-data-access.0.html)'
+
+description = """
+Simple command line tool to check compatibility between two licenses, given context
+"""
+
+epilog = """
+
+AUTHOR
+  Henrik Sandklef
+
+PROJECT SITE
+  https://github.com/hesa/licomp-osadl
+
+REPORTING BUGS
+  File a ticket at https://github.com/hesa/licomp-osadl/issues
+
+COPYRIGHT
+  Copyright (c) 2024 Henrik Sandklef<hesa@sandklef.com>.
+  License GPL-3.0-or-later
+
+ATTRIBUTION
+  licomp-osadl is using the license compatibility matrix from osadl.org.
+
+
+"""
