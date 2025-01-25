bd6ae533650c3f8ea52514ee02f6b7d5da5c378d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 11:09:58 +0000

    Add package svlearn-core/0.1.4/svlearn_core-0.1.4.tar.gz

---
+++ packages/svlearn-core/svlearn_core-0.1.4.tar.gz/svlearn_core-0.1.4/src/hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from svlearn-core!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/svlearn-core/svlearn_core-0.1.4.tar.gz/svlearn_core-0.1.4/src/svlearn/common/sv_types.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------------------
+#  Copyright (c) 2024.  SupportVectors AI Lab
+#
+#  This code is part of the training material, and therefore part of the intellectual property.
+#  It may not be reused or shared without the explicit, written permission of SupportVectors.
+#
+#  Use is limited to the duration and purpose of the training at SupportVectors.
+#
+#  Author: SupportVectors AI Training
+#-------------------------------------------------------------------------------------------
+
+
+# Simply a way of indicating that the value is expected to be a url string.
+Url = str
+
+# Specification of the classifier label.
+ClassifierLabel = str
+
+# Classifier name
+ClassifierName = str
+
