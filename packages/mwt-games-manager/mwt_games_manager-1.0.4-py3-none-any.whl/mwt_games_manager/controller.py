692e661b9f77ef609b52a82f4371d8ef81f6424d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:45 +0000

    Add package mwt-games-manager/1.0.4/mwt_games_manager-1.0.4-py3-none-any.whl

---
+++ packages/mwt-games-manager/mwt_games_manager-1.0.4-py3-none-any.whl/mwt_games_manager/controller.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, Response, request, jsonify
+import managers
+import utils
+
+mwt_game_manager = Blueprint('mwt-game-manager', __name__)
+
+
+@mwt_game_manager.route('/get-all-user-history', methods=['GET'])
+def get_all_user_history():
+    if not managers.client:
+        raise Exception("module has not been setup properly")
+
+    username = request.args.get("username")
+    if not username:
+        return Response(response="No username provided", status=400)
+
+    game_histories = list(managers.client.collection("users").document(username).collection("game-data").document(
+        managers.default_game_name).collection("history").stream())
+    game_histories = [utils.stringify_game_history_dict(game_history.to_dict()) for game_history in game_histories]
+    return jsonify(game_histories)
