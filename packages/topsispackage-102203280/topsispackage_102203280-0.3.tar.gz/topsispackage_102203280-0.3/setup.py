037cca91af1fd883fce2600d4e01641c218fb58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:33 +0000

    Add package topsispackage-102203280/0.3/topsispackage_102203280-0.3.tar.gz

---
+++ packages/topsispackage-102203280/topsispackage_102203280-0.3.tar.gz/topsispackage_102203280-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh: 
+    long_description = fh.read()
+setup(
+    name='topsispackage_102203280',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+    author='Bhumika Tandon',
+    author_email='bhumii2114@gmail.com',
+    description='A TOPSIS implementation package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/bhumiii2114/topsis_package',
+)
