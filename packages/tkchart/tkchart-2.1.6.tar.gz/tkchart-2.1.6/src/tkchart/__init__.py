e435550995e5405a59ef48e03a5dc8777cf15c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:05:27 +0000

    Add package tkchart/2.1.6/tkchart-2.1.6.tar.gz

---
+++ packages/tkchart/tkchart-2.1.6.tar.gz/tkchart-2.1.6/src/tkchart/__init__.py
@@ -0,0 +1,21 @@
+from .LineChart import LineChart
+from .Line import Line
+
+ENABLED: str = "enabled"
+DISABLED: str = "disabled"
+
+NORMAL: str = "normal"
+DASHED: str = "dashed"
+DOTTED: str = "dotted"
+
+TOP: str = "top"
+SIDE: str = "side"
+
+AUTO: str = "auto"
+
+"""
+tkchart: a library for create live update chart for tkinter guis.
+"""
+__title__ = "tkchart"
+__version__ = "2.1.6"
+__authors__ = ("Thisal Dilmith", "childeyouyu (有语)")
