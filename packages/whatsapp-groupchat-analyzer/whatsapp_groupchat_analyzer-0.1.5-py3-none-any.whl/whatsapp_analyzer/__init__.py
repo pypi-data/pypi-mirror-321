f83f71deff654ae97df6942a005a5f0251bab635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:05:34 +0000

    Add package whatsapp-groupchat-analyzer/0.1.5/whatsapp_groupchat_analyzer-0.1.5-py3-none-any.whl

---
+++ packages/whatsapp-groupchat-analyzer/whatsapp_groupchat_analyzer-0.1.5-py3-none-any.whl/whatsapp_analyzer/__init__.py
@@ -0,0 +1,5 @@
+# whatsapp_analyzer/__init__.py
+# You can add any package-level initialization here if needed.
+# For example, downloading NLTK data:
+import nltk
+nltk.download('stopwords', quiet=True)
