77122d090ccb0b1845e54589cd80731c16c6dbfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:43 +0000

    Add package logbless/0.1.3/logbless-0.1.3-py3-none-any.whl

---
+++ packages/logbless/logbless-0.1.3-py3-none-any.whl/logbless/constants.py
@@ -0,0 +1,11 @@
+CONFIG_FILENAME = "logbless_conf.yaml"
+
+
+BASE_CONFIG = {
+    "host": "127.0.0.1",
+    "port": 8070,
+    "path": "/logs",
+    "log_filename": "logs.log",
+    "title": "Logbless Log viewer",
+    "authentication": {"login": "admin", "password": "admin"},
+}
