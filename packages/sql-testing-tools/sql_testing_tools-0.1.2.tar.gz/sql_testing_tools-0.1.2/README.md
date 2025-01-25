dd3a4b392f8523b83e8282e86b836b423a18f450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:05:36 +0000

    Add package sql-testing-tools/0.1.2/sql_testing_tools-0.1.2.tar.gz

---
+++ packages/sql-testing-tools/sql_testing_tools-0.1.2.tar.gz/sql_testing_tools-0.1.2/README.md
@@ -0,0 +1,3 @@
+[Beta in Development]
+
+A library that normalizes simple SQL queries and compares them first by equality of the normalized string and then using the cosette API. 
---
+++ packages/sql-testing-tools/sql_testing_tools-0.1.2.tar.gz/sql_testing_tools-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+
+
+setup(
+    name='sql_testing_tools',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'sqlparse>=0.5.1',
+        'requests>=2.32.3'
+    ],
+    package_data={
+        'sql_testing_tools': ['databases/*.db'],
+    },
+    include_package_data=True,
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
