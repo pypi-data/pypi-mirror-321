18961696caf9f45c69beeac6ba8a6927ee5eae5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:05:16 +0000

    Add package tinyrml/0.3.3/tinyrml-0.3.3.tar.gz

---
+++ packages/tinyrml/tinyrml-0.3.3.tar.gz/tinyrml-0.3.3/src/tinyrml/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2022-2023, Ora Lassila & So Many Aircraft
+# All rights reserved.
+#
+# See LICENSE for licensing information
+#
+
+from tinyrml.tinyrml import Mapper, RR, RML, RRE
+
+__all__ = [
+    "Mapper",
+    "RR",
+    "RML",
+    "RRE"
+]
