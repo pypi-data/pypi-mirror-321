000ce7af534e4f150242bebe5d9d2582bc16c0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 07:05:16 +0000

    Add package KeyisBClient/1.4.0.1.32/keyisbclient-1.4.0.1.32.tar.gz

---
+++ packages/KeyisBClient/keyisbclient-1.4.0.1.32.tar.gz/keyisbclient-1.4.0.1.32/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+name = 'KeyisBClient'
+
+setup(
+    name=name,
+    version='1.4.0.1.32',
+    author="KeyisB",
+    author_email="keyisb.pip@gmail.com",
+    description=name,
+    long_description = 'GW and MMB Project libraries',
+    long_description_content_type= 'text/plain',
+    url=f"https://github.com/KeyisB/libs/tree/main/{name}",
+    include_package_data=True,
+    package_data = {'KeyisBClient.gw_certs': ['*']}, # type: ignore
+    package_dir={'': f'{name}'.replace('-','_')},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    license="MMB License v1.0",
+    install_requires = ['KeyisBLogging', 'httpx', 'KeyisBClient-httpx', 'KeyisBClient-mmbp'],
+)
