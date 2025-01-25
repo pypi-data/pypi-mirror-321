c8e83dfc709c377bac0328131240cc5eebea3848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:06:34 +0000

    Add package animethemes-batch-encoder/2.2.3/animethemes_batch_encoder-2.2.3-py3-none-any.whl

---
+++ packages/animethemes-batch-encoder/animethemes_batch_encoder-2.2.3-py3-none-any.whl/batch_encoder/_seek.py
@@ -0,0 +1,19 @@
+# The seek information for our encode
+class Seek:
+    def __init__(self, source_file, ss, to, output_name, new_audio_filter):
+        self.source_file = source_file
+        self.ss = ss
+        self.to = to
+        self.output_name = output_name
+        self.new_audio_filter = new_audio_filter
+
+    # The seek string arguments for our encode
+    def get_seek_string(self) -> str:
+        if len(self.ss) > 0 and len(self.to) > 0:
+            return f'-ss {self.ss} -to {self.to} -i "{self.source_file.file}"'
+        elif len(self.ss) > 0:
+            return f'-ss {self.ss} -i "{self.source_file.file}"'
+        elif len(self.to) > 0:
+            return f'-i "{self.source_file.file}" -to {self.to}'
+        else:
+            return f'-i "{self.source_file.file}"'
