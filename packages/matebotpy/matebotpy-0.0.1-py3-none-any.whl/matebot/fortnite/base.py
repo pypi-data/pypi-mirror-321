bd40631fa1e6cc51e34fd0cd7ef634fce0bbbd63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:05:51 +0000

    Add package matebotpy/0.0.1/matebotpy-0.0.1-py3-none-any.whl

---
+++ packages/matebotpy/matebotpy-0.0.1-py3-none-any.whl/matebot/fortnite/base.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from typing import List, Optional, Any, Dict, Union
+from matebot.fortnite import Character, Cosmetic, CarCosmetic, Instrument, CosmeticVariantToken, CosmeticVehicleVariant, Juno
+
+@dataclass
+class PrimarySecondaryColor:
+    primary: str
+    secondary: str
+
+@dataclass
+class WebsocketEventData:
+    languages: List[str]
+
+@dataclass
+class WebsocketEvent:
+    type: str
+    data: WebsocketEventData
+    timestamp: int
+
+@dataclass
+class StatsTrack:
+    gameId: str
+    trackguid: str
+    accountId: str
+    rankingType: str
+    lastUpdated: str
+    currentDivision: float
+    highestDivision: float
+    promotionProgress: float
+    currentPlayerRanking: Optional[float]
+
+@dataclass
+class Stats:
+    accountId: str
+    stats: Dict[str, Any]
+    ranks: List[StatsTrack]
+
+Definition = Union[Character, Cosmetic, CarCosmetic, Instrument, CosmeticVariantToken, CosmeticVehicleVariant, Juno]
