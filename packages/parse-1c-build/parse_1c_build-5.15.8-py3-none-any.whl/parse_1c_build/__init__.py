b7d9e79a5b9e88da029d05a1336e131e720edda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:07:09 +0000

    Add package parse-1c-build/5.15.8/parse_1c_build-5.15.8-py3-none-any.whl

---
+++ packages/parse-1c-build/parse_1c_build-5.15.8-py3-none-any.whl/parse_1c_build/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Для импорта"""
+
+
+from loguru import logger
+
+from parse_1c_build.__about__ import __version__
+from parse_1c_build.build import Builder
+from parse_1c_build.parse import Parser
