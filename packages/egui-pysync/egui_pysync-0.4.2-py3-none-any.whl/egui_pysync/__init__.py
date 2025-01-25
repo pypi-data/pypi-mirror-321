409f4f2d8a11276394d20d3142b99436e0d6bad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:21:28 +0000

    Add package egui-pysync/0.4.2/egui_pysync-0.4.2-py3-none-any.whl

---
+++ packages/egui-pysync/egui_pysync-0.4.2-py3-none-any.whl/egui_pysync/__init__.py
@@ -0,0 +1,7 @@
+"""TODO: Doc string."""
+
+from egui_pysync import structures, version
+from egui_pysync.server import StateServer
+from egui_pysync.version import __version__
+
+__all__ = ["version", "__version__", "StateServer", "structures"]
