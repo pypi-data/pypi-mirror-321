29555ca3271657949611b8722daaa1b52dd105ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 07:05:33 +0000

    Add package rpycdec/0.1.6/rpycdec-0.1.6-py3-none-any.whl

---
+++ packages/rpycdec/rpycdec-0.1.6-py3-none-any.whl/renpy/atl.py
@@ -0,0 +1,169 @@
+from . import util
+from .object import Object
+
+
+class Context(object):
+    pass
+
+
+class ATLTransformBase(Object):
+    pass
+
+
+class RawStatement(object):
+    pass
+
+
+class Statement(Object):
+    pass
+
+
+class RawBlock(RawStatement):
+    def get_code(self, **kwargs) -> str:
+        if self.animation:
+            raise NotImplementedError
+        return util.get_code(self.statements, **kwargs)
+
+
+class Block(Statement):
+    pass
+
+
+class RawMultipurpose(RawStatement):
+    def get_code(self, **kwargs) -> str:
+        rv = ""
+        properties = self.properties.copy()
+        if self.duration != "0":
+            properties = [(self.warper, self.duration)] + properties
+        if self.circles != "0":
+            raise NotImplementedError
+        for k, v in self.expressions:
+            if v:
+                v = f"with {v}"
+            properties.append((k, v))
+        if self.splines:
+            for name, exprs in self.splines:
+                rv += f" {name}"
+                for expr in exprs:
+                    rv += f" knot {expr}"
+        if self.warp_function:
+            raise NotImplementedError
+        if self.revolution:
+            raise NotImplementedError
+        property = util.get_code_properties(properties)
+        return property + rv
+
+
+class RawContainsExpr(RawStatement):
+    pass
+
+
+# This allows us to have multiple ATL transforms as children.
+class RawChild(RawStatement):
+    def get_code(self, **kwargs) -> str:
+        return util.get_code(self.children, **kwargs)
+
+
+# This changes the child of this statement, optionally with a transition.
+class Child(Statement):
+    pass
+
+
+# This causes interpolation to occur.
+class Interpolation(Statement):
+    pass
+
+
+# https://www.renpy.org/doc/html/atl.html#repeat-statement
+class RawRepeat(RawStatement):
+    pass
+
+    # atl_repeat ::=  "repeat" (simple_expression)?
+    def get_code(self, **kwargs) -> str:
+        if self.repeats:
+            return f"repeat {util.get_code(self.repeats, **kwargs)}"
+        return "repeat"
+
+
+class Repeat(Statement):
+    pass
+
+
+# Parallel statement.
+
+
+class RawParallel(RawStatement):
+    def get_code(self, **kwargs) -> str:
+        return util.get_code(self.blocks, **kwargs)
+
+
+class Parallel(Statement):
+    pass
+
+
+class RawChoice(RawStatement):
+    def get_code(self, **kwargs) -> str:
+        rv = []
+        for text, stmt in self.choices:
+            rv.append(f"choice {text}:")
+            rv.append(util.indent(util.get_code(stmt, **kwargs)))
+        return "\n".join(rv)
+
+
+class Choice(Statement):
+    pass
+
+
+class RawTime(RawStatement):
+    # https://www.renpy.org/doc/html/atl.html#time-statement
+    #
+    # atl_time ::=  "time" (simple_expression)?
+    def get_code(self, **kwargs) -> str:
+        if getattr(self, "time", None):
+            return f"time {self.time}"
+        return "time"
+
+
+class Time(Statement):
+    pass
+
+
+# https://www.renpy.org/doc/html/atl.html#on-statement
+class RawOn(RawStatement):
+    pass
+
+    # atl_on ::=  "on" name [ "," name ] * ":"
+    #   atl_block
+    #
+    # on show:
+    #     alpha 0.0
+    #     linear .5 alpha 1.0
+    # on hide:
+    #     linear .5 alpha 0.0
+    def get_code(self, **kwargs) -> str:
+        rv = []
+        for text, stmt in self.handlers.items():
+            rv.append(f"on {text}:")
+            rv.append(util.indent(util.get_code(stmt, **kwargs)))
+        return "\n".join(rv)
+
+
+class On(Statement):
+    pass
+
+
+class RawEvent(RawStatement):
+    pass
+
+
+class Event(Statement):
+    pass
+
+
+class RawFunction(RawStatement):
+    def get_code(self, **kwargs) -> str:
+        return "function " + self.expr
+
+
+class Function(Statement):
+    pass
---
+++ packages/rpycdec/rpycdec-0.1.6-py3-none-any.whl/renpy/util.py
@@ -0,0 +1,123 @@
+from . import ast
+
+IDENT_CHAR = "    "
+
+
+def indent(code: str, level: int = 1) -> str:
+    return "".join(
+        map(
+            lambda x: f"{IDENT_CHAR * level}{x}",
+            filter(lambda x: x.strip(), code.splitlines(keepends=True)),
+        )
+    )
+
+
+def get_code_properties(props: tuple | dict, newline: bool = False) -> str:
+    """
+    :param keyword: tuple | dict
+    :param newline: bool
+    :return: str
+
+    >>> get_code_properties((("a", 1), ("b", 2)))
+    "a 1 b 2"
+    >>> get_code_properties((("a", 1), (None, b)), newline=True)
+    "a 1\\nb"
+    """
+    list = []
+    if isinstance(props, dict):
+        props = props.items()
+    for prop in props:
+        prop_str = " ".join([str(x) for x in prop if x is not None])
+        list.append(prop_str)
+    return ("\n" if newline else " ").join(list)
+
+
+def __append_first_line(text, add) -> str:
+    lines = text.splitlines()
+    if lines:
+        v = lines[0]
+        i = len(v) - 1 if ":" in v else len(v)
+        lines[0] = v[:i] + add + v[i:]
+    return "\n".join(lines)
+
+
+def get_code(node, **kwargs) -> str:
+    """
+    Parameters
+    ----------
+    node : ast.Node
+    kwargs : dict
+        indent : int
+            space indent level
+        modifier : Callable[[ast.Node], ast.Node]
+            modify node before get code
+
+    Returns
+    -------
+    str
+        generated code
+
+    Raises
+    ------
+    NotImplementedError
+        if node type is not implemented or some attributes unable to handle.
+
+    """
+    if isinstance(node, list):
+        rv = []
+        skip_next = 0
+        for idx, item in enumerate(node):
+            if skip_next > 0:
+                skip_next -= 1
+                continue
+
+            prev = node[idx - 1] if idx > 0 else None
+            next = node[idx + 1] if idx < len(node) - 1 else None
+
+            # TODO: it's a hack, fix it later
+            if (
+                isinstance(item, ast.Say)
+                and not item.interact
+                and isinstance(next, ast.Menu)
+            ):
+                continue
+            if isinstance(item, ast.Label) and isinstance(next, ast.Menu):
+                if next.statement_start == item:
+                    continue  # skip label before menu
+            if isinstance(item, ast.With):
+                if item.paired:
+                    continue
+                prevprev = node[idx - 2] if idx - 2 >= 0 else None
+                if isinstance(prevprev, ast.With) and prevprev.paired == item.expr:
+                    rv[-1] = __append_first_line(rv[-1], f" with {item.expr}")
+                    continue
+            if isinstance(item, ast.Label) and isinstance(prev, ast.Call):
+                rv[-1] = __append_first_line(rv[-1], f" from {item.name}")
+                if isinstance(next, ast.Pass):
+                    # skip pass after call
+                    skip_next += 1
+                continue
+            rv.append(get_code(item, **kwargs))
+        return "\n".join(rv)
+
+    # modify node before get code
+    modifier = kwargs.get("modifier")
+    if modifier:
+        modifier(node, **kwargs)
+    return node.get_code(**kwargs)
+
+
+def get_block_code(node, **kwargs) -> str:
+    """
+    https://www.renpy.org/doc/html/layeredimage.html#layeredimage
+    """
+    if isinstance(node, list):
+        return "\n".join(map(lambda x: get_block_code(x, **kwargs), node))
+    lines = []
+    if isinstance(node, tuple) and len(node) >= 4:
+        _, _, code, block = node
+        lines.append(code)
+        lines.append(indent(get_block_code(block, **kwargs)))
+    else:
+        raise NotImplementedError
+    return "\n".join(lines)
