9fa464aab1491a536cc3c17bf66f67c8c6033e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 00:07:31 +0000

    Add package pnadium/0.1.6/pnadium-0.1.6.tar.gz

---
+++ packages/pnadium/pnadium-0.1.6.tar.gz/pnadium-0.1.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pnadium', 
+    version='0.1.6',  
+    description='Pacote para download e processamento dos microdados da PNAD Contínua do IBGE.',
+    long_description=open('README.md', encoding='utf-8').read(),  # Certifique-se de que o README.md existe
+    long_description_content_type='text/markdown',
+    author='Gustavo G. Ximenez',
+    author_email='ggximenez@gmail.com',
+    url='https://github.com/ggximenez/pnadium',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'unidecode',
+        # Adicione outras dependências necessárias
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License', 
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
