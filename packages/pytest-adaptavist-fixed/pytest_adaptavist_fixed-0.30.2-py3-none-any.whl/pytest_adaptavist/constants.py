87bb463ca31e5531709507338bfde59aa42bbe37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:21:29 +0000

    Add package pytest-adaptavist-fixed/0.30.2/pytest_adaptavist_fixed-0.30.2-py3-none-any.whl

---
+++ packages/pytest-adaptavist-fixed/pytest_adaptavist_fixed-0.30.2-py3-none-any.whl/pytest_adaptavist/constants.py
@@ -0,0 +1,13 @@
+"""Constants used by the module."""
+
+COLORMAP = {
+    "passed": {"green": True, "bold": True},
+    "failed": {"red": True, "bold": True},
+    "blocked": {"blue": True, "bold": True},
+    "skipped": {"yellow": True, "bold": True},
+}
+
+META_BLOCK_TIMEOUT = 600
+
+TEST_CYCLE_NAME_DEFAULT = "%(project_key) %(test_run_suffix)"
+TEST_PLAN_NAME_DEFAULT = "%(project_key) %(test_plan_suffix)"
