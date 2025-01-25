9d0842c8f27b6f6eeb8723baba5e1d089a53b439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:06:19 +0000

    Add package sourav0407/0.3/sourav0407-0.3.tar.gz

---
+++ packages/sourav0407/sourav0407-0.3.tar.gz/sourav0407-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sourav0407",  # Update this to a unique name
+    version='0.3',
+    packages=find_packages(),
+    description='A custom message module',
+    author='Sourav Roy',
+    author_email='sourav.roy0407@gmail.com',
+    url='https://git@github.com:Souravroy0407/mymodule.git',  # Replace with your GitHub repo
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
