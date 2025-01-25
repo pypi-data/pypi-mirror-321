c2c758bc7a2882a404155d3f3a513dcf976882a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 11:08:49 +0000

    Add package UEML/0.5/ueml-0.5.tar.gz

---
+++ packages/UEML/ueml-0.5.tar.gz/ueml-0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UEML',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[],
+    author='juanvel400',
+    author_email='juanvel400@proton.me',
+    description='Official UEML Parser',
+    url='https://github.com/juanvel4000/ueml', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
