bb3aa67dffad5758222d07a27cb17c602d1289bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:05:48 +0000

    Add package svnm/0.1.7.2/svnm-0.1.7.2-py3-none-any.whl

---
+++ packages/svnm/svnm-0.1.7.2-py3-none-any.whl/svnm/config.py
@@ -0,0 +1,39 @@
+modelinfo={
+    "Cifar10":{
+        "filename":"cifar10.keras",
+        "repoid":"svnmurali1/cifar10-80",
+        "classes":{0: 'airplane', 
+                    1: 'automobile', 
+                    2: 'bird', 
+                    3: 'cat', 
+                    4: 'deer', 
+                    5: 'dog', 
+                    6: 'frog', 
+                    7: 'horse', 
+                    8: 'ship', 
+                    9: 'truck'},
+        "metrics":{"accuracy":"80%"}
+    },
+    "GenderDetection":{
+        "repoid":"svnmurali1/genderdetection",
+        "filename":"genderdetection.keras",
+        "classes":{
+            0:"female",
+            1:"male"
+        },
+        "metrics":{
+            "accuracy":"95%"
+        }
+    },
+    "FaceDetectionYolo":{
+        "repoid":"svnmurali1/facedetectionyolo",
+        "filename":"best.pt",
+        "classes":{
+            0:"face"
+        },
+        "metrics":{
+            "accuracy":"90%"
+        }
+    }
+    
+}
