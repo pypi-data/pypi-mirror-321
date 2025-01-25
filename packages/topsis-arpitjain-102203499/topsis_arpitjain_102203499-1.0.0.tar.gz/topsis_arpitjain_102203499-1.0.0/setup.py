9c22b69edbcf2ff76d1f8473999c9a3f126a2542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:06:11 +0000

    Add package topsis-arpitjain-102203499/1.0.0/topsis_arpitjain_102203499-1.0.0.tar.gz

---
+++ packages/topsis-arpitjain-102203499/topsis_arpitjain_102203499-1.0.0.tar.gz/topsis_arpitjain_102203499-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='topsis_arpitjain_102203499',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[],  # Add your dependencies here
+    description='A Python package for TOPSIS analysis',
+    author='Arpit Jain',
+    author_email='ajain1_be22@thapar.edu',
+    url='https://github.com/your-username/Topsis-ArpitJain-102203499',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
