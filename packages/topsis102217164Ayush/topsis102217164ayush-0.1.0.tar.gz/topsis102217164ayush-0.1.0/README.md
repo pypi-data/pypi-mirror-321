ef037418cf4c2df7b26c47a8b110d8e4a051a2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:07:36 +0000

    Add package topsis102217164Ayush/0.1.0/topsis102217164ayush-0.1.0.tar.gz

---
+++ packages/topsis102217164Ayush/topsis102217164ayush-0.1.0.tar.gz/topsis102217164ayush-0.1.0/README.md
@@ -0,0 +1,10 @@
+# topsis102217164Ayush
+
+This is a basic implementation of the TOPSIS method for decision analysis, written in Python. The package provides a simple way to calculate the TOPSIS score and rank alternatives based on their performance with respect to multiple criteria.
+
+## Installation
+
+You can install the package using pip:
+
+```bash
+pip install topsis102217164Ayush
---
+++ packages/topsis102217164Ayush/topsis102217164ayush-0.1.0.tar.gz/topsis102217164ayush-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from setuptools import setup, find_packages
+
+# Read the README file with UTF-8 encoding
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='topsis102217164Ayush',
+    version='0.1.0',
+    description='A basic TOPSIS package for decision analysis.',
+    author='Ayush',
+    author_email='ayushmahajan3997@gmail.com',
+    url='https://github.com/ayushmahajan-31/Topsis-package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
