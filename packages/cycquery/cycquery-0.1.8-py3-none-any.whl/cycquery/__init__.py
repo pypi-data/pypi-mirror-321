7694e7cfa781b30907bc218a5d20289b3101eeae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:05:43 +0000

    Add package cycquery/0.1.8/cycquery-0.1.8-py3-none-any.whl

---
+++ packages/cycquery/cycquery-0.1.8-py3-none-any.whl/cycquery/__init__.py
@@ -0,0 +1,17 @@
+"""The ``query`` API provides classes to query EHR databases."""
+
+from cycquery.base import DatasetQuerier
+from cycquery.eicu import EICUQuerier
+from cycquery.gemini import GEMINIQuerier
+from cycquery.mimiciii import MIMICIIIQuerier
+from cycquery.mimiciv import MIMICIVQuerier
+from cycquery.omop import OMOPQuerier
+
+__all__ = [
+    "DatasetQuerier",
+    "EICUQuerier",
+    "GEMINIQuerier",
+    "MIMICIIIQuerier",
+    "MIMICIVQuerier",
+    "OMOPQuerier",
+]
