e24e83d0581c77274c5fa46b117f947ae380ec5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:37 +0000

    Add package pytest-logging-strict/0.2.0/pytest_logging_strict-0.2.0-py3-none-any.whl

---
+++ packages/pytest-logging-strict/pytest_logging_strict-0.2.0-py3-none-any.whl/pytest_logging_strict/plugin_shared.py
@@ -0,0 +1,12 @@
+"""
+.. moduleauthor:: Dave Faulkmore <https://mastodon.social/@msftcangoblowme>
+
+.. py:data:: item_marker
+   :type: str
+   :value: "logging_package_name"
+
+   Name of a pytest marker. Takes one argument, a dotted path logger
+   handlers package name
+"""
+
+item_marker = "logging_package_name"
