5714e7df4d8d2cd96b870f31bf18da2543eb8b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 07:05:42 +0000

    Add package qwinq/0.1.0/qwinq-0.1.0-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/qwinq/qwinq-0.1.0-cp312-cp312-macosx_11_0_arm64.whl/qwinq/__init__.py
@@ -0,0 +1,5 @@
+from qfetch import *
+from qdatac import *
+from qstrategy import *
+
+from qwinq.qwinq import *
---
+++ packages/qwinq/qwinq-0.1.0-cp312-cp312-macosx_11_0_arm64.whl/qwinq/qwinq.pyi
@@ -0,0 +1,6 @@
+from typing import List, Dict, Optional, Set
+from datetime import date
+
+
+def hello() -> str:
+    pass
