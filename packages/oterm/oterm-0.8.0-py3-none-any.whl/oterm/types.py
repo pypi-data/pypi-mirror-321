69a62e4d3d83f7b8980078f733c2ee42a63330fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:07:50 +0000

    Add package oterm/0.8.0/oterm-0.8.0-py3-none-any.whl

---
+++ packages/oterm/oterm-0.8.0-py3-none-any.whl/oterm/types.py
@@ -0,0 +1,19 @@
+from enum import Enum
+from typing import Awaitable, Callable, TypedDict
+
+from ollama._types import Image, Tool  # noqa
+
+
+class Author(Enum):
+    USER = "me"
+    OLLAMA = "ollama"
+
+
+class ToolDefinition(TypedDict):
+    tool: Tool
+    callable: Callable | Awaitable
+
+
+class ExternalToolDefinition(TypedDict):
+    tool: str
+    callable: str
