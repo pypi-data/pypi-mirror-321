832dae2e6878a1fb6f3396326d368985763bafe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:40 +0000

    Add package SKPHPPRAC/1.0.0/skphpprac-1.0.0.tar.gz

---
+++ packages/SKPHPPRAC/skphpprac-1.0.0.tar.gz/skphpprac-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SKPHPPRAC',
+    version='1.0.0',
+    description='A library of predefined PHP practicals',
+    author='SK',
+    author_email='temp.shabbirkovadiya@gmail.com',
+    url='https://github.com/shabbirkovadiya/SKPHPPRAC',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/SKPHPPRAC/skphpprac-1.0.0.tar.gz/skphpprac-1.0.0/skphpprac/__init__.py
@@ -0,0 +1,7 @@
+from .prac1 import prac1
+from .prac2 import prac2
+
+
+
+
+# Add imports for more practicals as needed
