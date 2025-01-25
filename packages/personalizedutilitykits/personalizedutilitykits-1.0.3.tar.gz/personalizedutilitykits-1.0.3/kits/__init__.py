9fd9043846f81679afc0932a8a6277feb8c5258d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 01:31:34 +0000

    Add package personalizedutilitykits/1.0.3/personalizedutilitykits-1.0.3.tar.gz

---
+++ packages/personalizedutilitykits/personalizedutilitykits-1.0.3.tar.gz/personalizedutilitykits-1.0.3/kits/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+"""
+@Time        : 2023/3/2
+@File        : __init__.py
+@Author      : lyz
+@version     : python 3
+@Description : 初始化程序
+"""
+
+from . import *
+
+"""
+__all__
+:param : 文件名称(不带.py)
+"""
+__all__ = ["util_log"]
