ffa3b853599dbefa343d54c206f9a6d8cd1aec95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:22:35 +0000

    Add package moatless/0.0.4/moatless-0.0.4-py3-none-any.whl

---
+++ packages/moatless/moatless-0.0.4-py3-none-any.whl/moatless/value_function/base.py
@@ -0,0 +1,15 @@
+import logging
+from abc import ABC
+from typing import Optional, Tuple
+
+from pydantic import BaseModel
+
+from moatless.completion.model import Completion
+from moatless.node import Node, Reward
+
+logger = logging.getLogger(__name__)
+
+
+class BaseValueFunction(BaseModel, ABC):
+    def get_reward(self, node: Node) -> Tuple[Reward, Optional[Completion]]:
+        pass
