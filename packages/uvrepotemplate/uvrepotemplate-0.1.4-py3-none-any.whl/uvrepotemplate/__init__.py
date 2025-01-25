571cffbb23bf767f04d12e6bd8c9949af9df4502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:06:02 +0000

    Add package uvrepotemplate/0.1.4/uvrepotemplate-0.1.4-py3-none-any.whl

---
+++ packages/uvrepotemplate/uvrepotemplate-0.1.4-py3-none-any.whl/uvrepotemplate/__init__.py
@@ -0,0 +1,22 @@
+# Wheel names will be generated according to this value. Do not manually modify this value; instead
+# update it according to committed changes by running this command from the root of the repository:
+#
+#   uv run python -m AutoGitSemVer.scripts.UpdatePythonVersion ./src/uvrepotemplate/__init__.py ./src
+#
+__version__ = "0.1.4"
+
+# BugBug: Testing! 4
+
+
+def Add(
+    a: int,
+    b: int,
+) -> int:
+    return a + b
+
+
+def Subtract(
+    a: int,
+    b: int,
+) -> int:
+    return a - b
