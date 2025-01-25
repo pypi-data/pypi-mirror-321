2fdec4d313fe63c0b4a867da3064bdeb74ceb68a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:05:36 +0000

    Add package genvarloader/0.7.0/genvarloader-0.7.0.tar.gz

---
+++ packages/genvarloader/genvarloader-0.7.0.tar.gz/genvarloader-0.7.0/docs/source/conf.py
@@ -0,0 +1,77 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import genvarloader
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "GenVarLoader"
+copyright = "2024, David Laub"
+author = "David Laub"
+release = genvarloader.__version__
+# short X.Y verison
+version = ".".join(release.split(".")[:2])
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "myst_parser",
+    "nbsphinx",
+]
+
+napoleon_use_param = True
+napoleon_type_aliases = {
+    "ArrayLike": ":term:`array_like`",
+    "NDArray": ":ref:`NDArray <numpy.typing.NDArray>`",
+}
+napoleon_use_rtype = True
+
+autodoc_typehints = "both"
+autodoc_type_aliases = {"ArrayLike": "ArrayLike"}
+autodoc_default_options = {"private-members": False}
+autodoc_member_order = "bysource"
+
+myst_enable_extensions = ["colon_fence"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "numba": ("https://numba.readthedocs.io/en/stable/", None),
+    "polars": ("https://docs.pola.rs/py-polars/html", None),
+    "torch": ("https://pytorch.org/docs/stable/", None),
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+html_theme_options = {
+    "home_page_in_toc": True,
+    "repository_url": "https://github.com/mcvickerlab/GenVarLoader",
+    "use_repository_button": True,
+    "pygments_light_style": "tango",
+    "pygments_dark_style": "material",
+}
+html_logo = "_static/gvl_logo.png"
+html_favicon = "_static/favicon.ico"
+html_title = f"GenVarLoader v{version}"
+html_sidebars = {
+    "**": [
+        "navbar-logo.html",
+        "icon-links.html",
+        "search-button-field.html",
+        "sbt-sidebar-nav.html",
+    ]
+}
