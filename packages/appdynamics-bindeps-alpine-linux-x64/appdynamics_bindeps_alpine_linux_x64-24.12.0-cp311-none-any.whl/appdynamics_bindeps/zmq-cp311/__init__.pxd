13ab2e185684eada62b4d1b5f5b2403f33374ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:05:44 +0000

    Add package appdynamics-bindeps-alpine-linux-x64/24.12.0/appdynamics_bindeps_alpine_linux_x64-24.12.0-cp311-none-any.whl

---
+++ packages/appdynamics-bindeps-alpine-linux-x64/appdynamics_bindeps_alpine_linux_x64-24.12.0-cp311-none-any.whl/appdynamics_bindeps/zmq-cp311/__init__.pxd
@@ -0,0 +1,5 @@
+# 2022-07-07, Cisco Systems, Inc.
+from appdynamics_bindeps.zmq.backend.cython.context cimport Context
+from appdynamics_bindeps.zmq.backend.cython.socket cimport Socket
+from appdynamics_bindeps.zmq.backend.cython.message cimport Frame
+from appdynamics_bindeps.zmq.backend.cython cimport libzmq
---
+++ packages/appdynamics-bindeps-alpine-linux-x64/appdynamics_bindeps_alpine_linux_x64-24.12.0-cp311-none-any.whl/appdynamics_bindeps/zmq-cp311/tests/test_etc.py
@@ -0,0 +1,21 @@
+# 2022-07-07, Cisco Systems, Inc.
+# Copyright (c) PyZMQ Developers.
+# Distributed under the terms of the Modified BSD License.
+
+import sys
+
+import appdynamics_bindeps.zmq as zmq
+
+from pytest import mark
+
+@mark.skipif('appdynamics_bindeps.zmq.zmq_version_info() < (4,1)')
+def test_has():
+    assert not zmq.has('something weird')
+    has_ipc = zmq.has('ipc')
+    not_windows = not sys.platform.startswith('win')
+    assert has_ipc == not_windows
+
+@mark.skipif(not hasattr(zmq, '_libzmq'), reason="bundled libzmq")
+def test_has_curve():
+    """bundled libzmq has curve support"""
+    assert zmq.has('curve')
