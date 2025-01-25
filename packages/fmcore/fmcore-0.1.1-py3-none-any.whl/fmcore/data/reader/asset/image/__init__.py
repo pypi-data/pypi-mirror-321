b74cc0397dedba6dc086ba90d4b6b364569a62d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:06:00 +0000

    Add package fmcore/0.1.1/fmcore-0.1.1-py3-none-any.whl

---
+++ packages/fmcore/fmcore-0.1.1-py3-none-any.whl/fmcore/data/reader/asset/image/__init__.py
@@ -0,0 +1,2 @@
+from fmcore.data.reader.asset.image.ImageReader import *
+from fmcore.data.reader.asset.image.ImageIOReader import *
---
+++ packages/fmcore/fmcore-0.1.1-py3-none-any.whl/fmcore/data/writer/__init__.py
@@ -0,0 +1,4 @@
+from fmcore.data.writer.Writer import *
+from fmcore.data.writer.config import *
+from fmcore.data.writer.dataframe import *
+from fmcore.data.writer.binary import *
