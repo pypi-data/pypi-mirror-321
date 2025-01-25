a681dc09b1d2f38b673e94a72f66edb6218226d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:07:06 +0000

    Add package django-pwned/1.2.2/django_pwned-1.2.2.tar.gz

---
+++ packages/django-pwned/django_pwned-1.2.2.tar.gz/django_pwned-1.2.2/setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import find_packages, setup
+
+from django_pwned import __version__
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="UTF-8") as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+dev_requirements = ["pre-commit", "pytest", "pytest-cov", "pytest-django", "responses"]
+
+setup(
+    name="django-pwned",
+    version=__version__,
+    description="A Django password validator using the Pwned Passwords API to check for compromised passwords.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Mohammad Javad Naderi",
+    url="https://github.com/QueraTeam/django-pwned",
+    download_url="https://pypi.org/project/django-pwned/",
+    packages=find_packages(".", include=("django_pwned", "django_pwned.*")),
+    include_package_data=True,
+    install_requires=["Django>=3.2", "requests"],
+    extras_require={"dev": dev_requirements},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Internet :: WWW/HTTP :: Session",
+        "Topic :: Security",
+    ],
+)
