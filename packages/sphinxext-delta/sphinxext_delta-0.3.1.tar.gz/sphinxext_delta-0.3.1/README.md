828fbb40e6d31b87d1c001c7aa9b040d03cd3849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:06:28 +0000

    Add package sphinxext-delta/0.3.1/sphinxext_delta-0.3.1.tar.gz

---
+++ packages/sphinxext-delta/sphinxext_delta-0.3.1.tar.gz/sphinxext_delta-0.3.1/README.md
@@ -0,0 +1,36 @@
+# sphinxext-delta
+
+Sphinx extension to generate a page of changed articles on ReadTheDocs.
+
+## Installation
+
+```
+python -m pip install sphinxext-delta
+```
+
+## Requirements
+
+- Sphinx >= 5
+- ReadTheDocs
+- GitHub
+
+No other environments are supported.
+
+## Usage
+
+Add `sphinxext.delta` to your extensions list in your `conf.py`
+
+```
+extensions = [
+    sphinxext.delta,
+]
+```
+
+## Options
+
+There are 2 required and 1 optional configurations:
+
+- `delta_doc_path`
+  - REQUIRED: Relative path to your articles. IE: `source/docs`
+- `delta_inject_location`
+  - OPTIONAL: Relative location for the toctree to be injected. Defaults to `master_doc`.
---
+++ packages/sphinxext-delta/sphinxext_delta-0.3.1.tar.gz/sphinxext_delta-0.3.1/setup.py
@@ -0,0 +1,54 @@
+import setuptools
+import subprocess
+
+version = "9999"
+try:
+    ret = subprocess.run(
+        "git describe --tags --abbrev=0",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=True,
+        shell=True,
+    )
+    version = ret.stdout.decode("utf-8").strip()
+except:
+    pass
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sphinxext-delta",
+    version=version,
+    author="Vasista Vovveti, Dalton Smith",
+    author_email="daltzsmith@gmail.com",
+    description="Sphinx extension for listing changed Sphinx articles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wpilibsuite/sphinxext-delta",
+    install_requires=["sphinx>=5.0", "six", "requests"],
+    packages=["sphinxext"],
+    classifiers=[
+        "Environment :: Plugins",
+        "Environment :: Web Environment",
+        "Framework :: Sphinx :: Extension",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python",
+        "Topic :: Documentation :: Sphinx",
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Text Processing",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3.8",
+)
