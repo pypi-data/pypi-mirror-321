82f546d4c76c654ac44e7d6210122f493610844e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:05:57 +0000

    Add package sports-betting-scorer/0.0.3/sports_betting_scorer-0.0.3-py3-none-any.whl

---
+++ packages/sports-betting-scorer/sports_betting_scorer-0.0.3-py3-none-any.whl/sports_betting_module/__init__.py
@@ -0,0 +1,20 @@
+"""
+-----------------------------------------------------
+Sports Betting Algorithm (PyPy-ready)
+-----------------------------------------------------
+This module contains a number of helper functions
+to numerically and semantically manage sports
+score betting.
+-----------------------------------------------------
+"""
+
+from .sports_betting_scorer import (
+    simulate_stadium_noise_levels,
+    assess_coach_press_conference_mood,
+    calculate_roster_adjustment_factor,
+    generate_ticket_sales_projection,
+    forecast_matchday_weather_conditions,
+    calculate_halftime_entertainment_budget,
+    simulate_player_fatigue_over_season,
+    sport_score_spreading,
+)
