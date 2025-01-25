1928de96e69c7b80ace4c6a6d7d57fef812e16f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:05:20 +0000

    Add package tamga/0.1.3/tamga-0.1.3.tar.gz

---
+++ packages/tamga/tamga-0.1.3.tar.gz/tamga-0.1.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tamga",
+    version="0.1.3",
+    author="Doğukan Ürker",
+    author_email="dogukanurker@icloud.com",
+    description="A modern, async-capable logging utility with multiple output formats and colorful console output",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dogukanurker/tamga",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: System :: Logging",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "aiofiles>=0.8.0",
+        "aiosqlite>=0.17.0",
+        "motor>=3.0.0",
+        "aiohttp>=3.8.0",
+        "python-dotenv>=1.0.0",
+        "pymongo[srv]>=4.6.1",
+        "bson>=0.5.10",
+        "requests>=2.31.0"
+    ],
+    keywords="logging, async, mongodb, colorful, console, file, json, sql, email",
+)
