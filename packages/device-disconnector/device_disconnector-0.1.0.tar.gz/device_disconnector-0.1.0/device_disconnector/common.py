8635a482bb0f8ceb64b02298e809cc5fb167e82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:22:21 +0000

    Add package device-disconnector/0.1.0/device_disconnector-0.1.0.tar.gz

---
+++ packages/device-disconnector/device_disconnector-0.1.0.tar.gz/device_disconnector-0.1.0/device_disconnector/common.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Common helper functions"""
+import logging
+
+LOG_LEVELS = {
+    0: logging.CRITICAL,
+    1: logging.ERROR,
+    2: logging.WARNING,
+    3: logging.INFO,
+    4: logging.DEBUG,
+}
