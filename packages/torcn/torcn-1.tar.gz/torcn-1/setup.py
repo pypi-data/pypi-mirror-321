9a9477c3cf5c2957f6f0cb64403a87fd3580fb76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:05:37 +0000

    Add package torcn/1/torcn-1.tar.gz

---
+++ packages/torcn/torcn-1.tar.gz/torcn-1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='torcn',
+      version='1',
+      description='none',
+      packages=['torcn'],
+      author_email='antoha123_3@mail.ru',
+      zip_safe=False)
