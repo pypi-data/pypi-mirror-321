c7937b1fefd4266c88124385ac91c52df865e5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:44 +0000

    Add package fm-comfyui-bridge/0.7.0/fm_comfyui_bridge-0.7.0.tar.gz

---
+++ packages/fm-comfyui-bridge/fm_comfyui_bridge-0.7.0.tar.gz/fm_comfyui_bridge-0.7.0/src/fm_comfyui_bridge/comfy_api.py
@@ -0,0 +1,5 @@
+NEGATIVE = """
+((worst quality, low quality)), normal quality, ((poorly drawn face)), poorly drawn hands, ugly, bad anatomy, (bad hands), (missing fingers), disfigured, mutation, mutated, (extra limb),missing limbs, floating limbs, disconnected limbs, signature, watermark, username, blurry, cropped
+"""
+
+WORKFLOW = "Workflow"
