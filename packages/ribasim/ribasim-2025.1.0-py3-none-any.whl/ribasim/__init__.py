9f2b31b2fb4fbc25520dca95a81ae5a296301b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:07:58 +0000

    Add package ribasim/2025.1.0/ribasim-2025.1.0-py3-none-any.whl

---
+++ packages/ribasim/ribasim-2025.1.0-py3-none-any.whl/ribasim/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "2025.1.0"
+# Keep synced write_schema_version in ribasim_qgis/core/geopackage.py
+__schema_version__ = 3
+
+from ribasim.config import Allocation, Logging, Node, Solver
+from ribasim.geometry.edge import EdgeTable
+from ribasim.model import Model
+
+__all__ = ["EdgeTable", "Allocation", "Logging", "Model", "Solver", "Node"]
---
+++ packages/ribasim/ribasim-2025.1.0-py3-none-any.whl/ribasim/geometry/__init__.py
@@ -0,0 +1,5 @@
+from ribasim.geometry.area import BasinAreaSchema
+from ribasim.geometry.edge import EdgeTable
+from ribasim.geometry.node import NodeTable
+
+__all__ = ["BasinAreaSchema", "EdgeTable", "NodeTable"]
