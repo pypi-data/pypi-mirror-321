19f64cb94281a056bdf6df9c594c452818b9677f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:06:04 +0000

    Add package USDOLLAR/0.0.0/USDOLLAR-0.0.0-py3-none-any.whl

---
+++ packages/USDOLLAR/USDOLLAR-0.0.0-py3-none-any.whl/USDOLLAR/__init_.py
@@ -0,0 +1,14 @@
+
+class USdollar:
+    def __init__(self, num1):
+        self.num1 = num1
+        def rupee(self):
+            return self.num1*85
+        def euro(self):
+            return self.num1*0.97
+        def yen(self):
+            return self.num1*157
+        def yuan(self):
+            return self.num1*6
+        
+
