dc5d38357f86f1d662d8967d75a078259bdfef93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:09:26 +0000

    Add package Topsis-Vivek-102203871/1.0.1/topsis_vivek_102203871-1.0.1.tar.gz

---
+++ packages/Topsis-Vivek-102203871/topsis_vivek_102203871-1.0.1.tar.gz/topsis_vivek_102203871-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-Vivek-102203871",
+    version="1.0.1",
+    author="Vivek",
+    author_email="atrivivek001@gmail.com",
+    description="A Python package for implementing the Topsis method",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/VivekAttri01/Topsis-Vivek-102203871",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "pandas",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
