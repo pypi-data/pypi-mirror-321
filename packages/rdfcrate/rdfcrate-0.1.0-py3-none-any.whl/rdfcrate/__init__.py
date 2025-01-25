e6899de8b1a00a6784e3ac79755769da6d8f6e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 00:07:14 +0000

    Add package rdfcrate/0.1.0/rdfcrate-0.1.0-py3-none-any.whl

---
+++ packages/rdfcrate/rdfcrate-0.1.0-py3-none-any.whl/rdfcrate/__init__.py
@@ -0,0 +1,4 @@
+from rdfcrate.wrapper import AttachedCrate, DetatchedCrate
+from rdfcrate import uris, spec_version, bioschemas
+
+__all__ = ["DetatchedCrate", "AttachedCrate", "uris", "spec_version", "bioschemas"]
