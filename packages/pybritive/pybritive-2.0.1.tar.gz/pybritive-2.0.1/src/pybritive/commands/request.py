1b2150219208afff87ffd0c0fed26bcf7c292d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:05:07 +0000

    Add package pybritive/2.0.1/pybritive-2.0.1.tar.gz

---
+++ packages/pybritive/pybritive-2.0.1.tar.gz/pybritive-2.0.1/src/pybritive/commands/request.py
@@ -0,0 +1,71 @@
+import click
+
+from ..helpers.build_britive import build_britive
+from ..helpers.profile_argument_decorator import click_smart_profile_argument
+from ..options.britive_options import britive_options
+
+
+@click.group()
+def request():
+    """Provides functionality related to requesting approval to checkout a profile."""
+    pass
+
+
+@request.command()
+@build_britive
+@britive_options(names='justification,tenant,token,silent,passphrase,federation_provider')
+@click_smart_profile_argument
+def submit(ctx, justification, tenant, token, silent, passphrase, federation_provider, profile):
+    """Submit a request to checkout a profile.
+
+    Only applicable for profiles which require approval. This command will NOT block/wait until the request is
+    approved or rejected. If you want to wait for the request to be approved, run the `checkout` command which will
+    wait until the request is approved and then checkout the profile.
+
+    This command takes 1 required argument `PROFILE`. This should be a string representation of the profile
+    that should be checked out. Format is `application name/environment name/profile name`.
+    """
+
+    ctx.obj.britive.request_submit(profile=profile, justification=justification)
+
+
+@request.command()
+@build_britive
+@britive_options(names='tenant,token,silent,passphrase,federation_provider')
+@click_smart_profile_argument
+def withdraw(ctx, tenant, token, silent, passphrase, federation_provider, profile):
+    """Withdraw a request to checkout a profile.
+
+    Only applicable for profiles which require approval.
+
+    This command takes 1 required argument `PROFILE`. This should be a string representation of the profile
+    that should be checked out. Format is `application name/environment name/profile name`.
+    """
+
+    ctx.obj.britive.request_withdraw(profile=profile)
+
+
+@request.command()
+@build_britive
+@britive_options(names='tenant,token,silent,passphrase,federation_provider')
+@click.argument('request-id')
+def approve(ctx, tenant, token, silent, passphrase, federation_provider, request_id):
+    """Approve a request to checkout a profile.
+
+    This command takes 1 required argument `request-id`. Find the `request-id` via `ls approvals`.
+    """
+
+    ctx.obj.britive.request_disposition(request_id=request_id, decision='approve')
+
+
+@request.command()
+@build_britive
+@britive_options(names='tenant,token,silent,passphrase,federation_provider')
+@click.argument('request-id')
+def reject(ctx, tenant, token, silent, passphrase, federation_provider, request_id):
+    """Reject a request to checkout a profile.
+
+    This command takes 1 required argument `request-id`. Find the `request-id` via `ls approvals`.
+    """
+
+    ctx.obj.britive.request_disposition(request_id=request_id, decision='reject')
---
+++ packages/pybritive/pybritive-2.0.1.tar.gz/pybritive-2.0.1/src/pybritive/helpers/aws_credential_process.py
@@ -0,0 +1,117 @@
+from sys import argv, exit
+
+
+def get_args():
+    from getopt import getopt  # lazy load
+
+    options = getopt(
+        argv[1:],
+        't:T:p:f:P:F:hv',
+        ['tenant=', 'token=', 'passphrase=', 'force-renew=', 'profile=', 'federation-provider=help', 'version'],
+    )[0]
+
+    args = {
+        'tenant': None,
+        'token': None,
+        'passphrase': None,
+        'force_renew': None,
+        'profile': None,
+        'federation_provider': None,
+    }
+
+    for opt, arg in options:
+        if opt in ('-t', '--tenant'):
+            args['tenant'] = arg
+        if opt in ('-T', '--token'):
+            args['token'] = arg
+        if opt in ('-p', '--passphrase'):
+            args['passphrase'] = arg
+        if opt in ('-f', '--force-renew'):
+            args['force_renew'] = int(arg)
+        if opt in ('-P', '--profile'):
+            args['profile'] = arg
+        if opt in ('-F', '--federation-provider'):
+            args['federation_provider'] = arg
+        if opt in ('-h', '--help'):
+            usage()
+        if opt in ('-v', '--version'):
+            from importlib.metadata import version
+            from platform import platform, python_version  # lazy load
+
+            cli_version = version('pybritive')
+            print(f'pybritive: {cli_version} / platform: {platform()} / python: {python_version()}')
+            exit(0)
+
+    return args
+
+
+def usage():
+    print(
+        f'Usage : {argv[0]} --profile <profile> [-t/--tenant, -T/--token, -p/--passphrase, -f/--force-renew, '
+        f'-F/--federation-provider]'
+    )
+    exit(0)
+
+
+def main():
+    args = get_args()
+    if not args['profile']:
+        print('-P/--profile is required')
+        usage()
+
+    creds = None
+    if not args['force_renew']:  # if force renew let's defer to that the full package vs. this helper
+        from .cache import Cache  # lazy load
+
+        creds = Cache(passphrase=args['passphrase']).get_credentials(
+            profile_name=args['profile'], mode='awscredentialprocess'
+        )
+        if creds:
+            from datetime import datetime  # lazy load
+
+            expiration = datetime.fromisoformat(creds['expirationTime'].replace('Z', ''))
+            now = datetime.utcnow()
+            if now > expiration:  # creds have expired so set to none so new one get checked out
+                creds = None
+            else:  # not importing json library on purpose to keep imports down for speed
+                json = '{'
+                json += f'"AccessKeyId": "{creds["accessKeyID"]}",'
+                json += f'"SecretAccessKey": "{creds["secretAccessKey"]}",'
+                json += f'"SessionToken": "{creds["sessionToken"]}",'
+                json += f'"Expiration": "{creds["expirationTime"]}",'
+                json += '"Version": 1}'
+                print(json)
+                raise SystemExit()
+    if not creds:
+        from ..britive_cli import BritiveCli  # lazy load for performance purposes
+
+        b = BritiveCli(
+            tenant_name=args['tenant'],
+            token=args['token'],
+            passphrase=args['passphrase'],
+            federation_provider=args['federation_provider'],
+            silent=True,
+            from_helper_console_script=True,
+        )
+        b.config.get_tenant()  # have to load the config here as that work is generally done
+        b.checkout(
+            alias=None,
+            blocktime=None,
+            console=False,
+            justification=None,
+            mode='awscredentialprocess',
+            maxpolltime=None,
+            profile=args['profile'],
+            passphrase=args['passphrase'],
+            force_renew=args['force_renew'],
+            aws_credentials_file=None,
+            gcloud_key_file=None,
+            verbose=None,
+            extend=False,
+            otp=None,
+        )
+        raise SystemExit()
+
+
+if __name__ == '__main__':
+    main()
