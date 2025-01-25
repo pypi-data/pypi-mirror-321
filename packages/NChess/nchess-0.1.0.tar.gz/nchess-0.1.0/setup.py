ea391412a08fc3fb9569da88c6fb649924d72f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:05:52 +0000

    Add package NChess/0.1.0/nchess-0.1.0.tar.gz

---
+++ packages/NChess/nchess-0.1.0.tar.gz/nchess-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nchess',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.18.0',
+    ],
+    author='MNMoslem',
+    author_email='normoslem256@gmail.com',
+    description='chess library written in c',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MNourMoslem/NChess',  # Replace with your repository URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+) 
