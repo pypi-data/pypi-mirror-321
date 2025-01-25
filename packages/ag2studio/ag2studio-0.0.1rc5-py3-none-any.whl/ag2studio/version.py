7554c42ffb388db5721e06317b06c708ce68e3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:05:56 +0000

    Add package ag2studio/0.0.1rc5/ag2studio-0.0.1rc5-py3-none-any.whl

---
+++ packages/ag2studio/ag2studio-0.0.1rc5-py3-none-any.whl/ag2studio/version.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2023 - 2025, Owners of https://github.com/ag2ai
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Portions derived from  https://github.com/microsoft/autogen are under the MIT License.
+# SPDX-License-Identifier: MIT
+VERSION = "0.0.1-rc5"
+__version__ = VERSION
+APP_NAME = "ag2studio"
+AUTHOR = "Chi Wang & Qingyun Wu"
+LICENSE = "Apache-2.0"
