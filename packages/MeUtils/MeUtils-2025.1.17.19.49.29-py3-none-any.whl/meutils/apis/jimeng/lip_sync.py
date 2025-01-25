2837be9b0522596ecc892c68776e8eba33fae555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:21:53 +0000

    Add package MeUtils/2025.1.17.19.49.29/MeUtils-2025.1.17.19.49.29-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2025.1.17.19.49.29-py3-none-any.whl/meutils/apis/jimeng/lip_sync.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : lip_sync
+# @Time         : 2025/1/3 16:17
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
