8d39f91a66399a79a90b843e5a396ef32dd64c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:05:52 +0000

    Add package DLexam/0.0.2/dlexam-0.0.2.tar.gz

---
+++ packages/DLexam/dlexam-0.0.2.tar.gz/dlexam-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from io import open
+from setuptools import setup
+
+version = '0.0.2'
+
+setup(
+    name = 'DLexam',
+    version=version,
+
+    author='OlegSh',
+    author_email='9753124680sh@gmail.com',
+
+    license='OlegSh' ,
+    
+    include_package_data=True,
+    
+    packages= ['DLexam']
+)
