babe744dd2bd8d24c5a604bb41a6426cea8c6891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:05:49 +0000

    Add package appdynamics-bindeps-alpine-linux-x64/24.12.0/appdynamics_bindeps_alpine_linux_x64-24.12.0-cp39-none-any.whl

---
+++ packages/appdynamics-bindeps-alpine-linux-x64/appdynamics_bindeps_alpine_linux_x64-24.12.0-cp39-none-any.whl/appdynamics_bindeps/zmq-cp39/eventloop/__init__.py
@@ -0,0 +1,6 @@
+# 2022-07-07, Cisco Systems, Inc.
+"""Tornado eventloop integration for pyzmq"""
+
+from appdynamics_bindeps.zmq.eventloop.ioloop import IOLoop
+
+__all__ = ['IOLoop']
---
+++ packages/appdynamics-bindeps-alpine-linux-x64/appdynamics_bindeps_alpine_linux_x64-24.12.0-cp39-none-any.whl/appdynamics_bindeps/zmq-cp39/tests/test_ssh.py
@@ -0,0 +1,9 @@
+# 2022-07-07, Cisco Systems, Inc.
+from appdynamics_bindeps.zmq.ssh.tunnel import select_random_ports
+
+def test_random_ports():
+    for i in range(4096):
+        ports = select_random_ports(10)
+        assert len(ports) == 10
+        for p in ports:
+            assert ports.count(p) == 1
