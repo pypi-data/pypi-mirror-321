5c4ab3fc3853a47b9d682b3073fc0c77b2e5f8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:07:35 +0000

    Add package crystalpy-barno/1.1.16/crystalpy_barno-1.1.16.tar.gz

---
+++ packages/crystalpy-barno/crystalpy_barno-1.1.16.tar.gz/crystalpy_barno-1.1.16/crystalpy_barno/entry_point.py
@@ -0,0 +1,8 @@
+from crystalpy_barno.reports.report_factory import ReportFactory
+from crystalpy_barno.enums import FileTypes
+
+def generate_report(report_type, filename, output_path, parameters, formulas, file_type=FileTypes.PDF):
+    report = ReportFactory.create_report(report_type, filename, output_path, parameters, formulas)
+    export_format = ReportFactory.get_export_format(file_type)
+    report.export(export_format)
+    print(f"Report exported to {output_path} in {file_type.upper()} format.")
