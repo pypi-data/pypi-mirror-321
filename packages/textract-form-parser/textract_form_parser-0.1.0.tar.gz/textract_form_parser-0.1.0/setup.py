f9cf841390371ed6f1efe330b1f0b42b10c8d51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:06:11 +0000

    Add package textract-form-parser/0.1.0/textract_form_parser-0.1.0.tar.gz

---
+++ packages/textract-form-parser/textract_form_parser-0.1.0.tar.gz/textract_form_parser-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="textract-form-parser",
+    version="0.1.0",
+    author="Yogeshvar Senthilkumar",
+    author_email="yogeshvar@icloud.com",
+    description="A Python library for parsing AWS Textract form output",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yogeshvar/text-extractor",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "boto3>=1.26.0",
+        "pandas>=1.3.0",
+        "numpy>=1.19.0",
+    ]
+) 
