5d372c2b298cf9f136aa0e0467442116bc35574a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:07:49 +0000

    Add package anygen/1.0.4/anygen-1.0.4.tar.gz

---
+++ packages/anygen/anygen-1.0.4.tar.gz/anygen-1.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="anygen",
+    version="1.0.4",
+    author="Abdul Waheed",
+    author_email="abdulwaheed1513@gmail.com",
+    description="A unified interface for text generation using Hugging Face, OpenAI, and Gemini models.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/macabdul9/AnyGen",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "transformers",
+        "google-generativeai",
+        "requests",
+        "openai",
+    ],
+)
