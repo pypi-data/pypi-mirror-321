58a7c6fc8e2f9dfd6a1abd495b9d15b9495878e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:07:50 +0000

    Add package pixeltable/0.3.0/pixeltable-0.3.0.tar.gz

---
+++ packages/pixeltable/pixeltable-0.3.0.tar.gz/pixeltable-0.3.0/pixeltable/exec/exec_node.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+import abc
+from typing import TYPE_CHECKING, Iterable, Iterator, Optional, TypeVar
+
+import pixeltable.exprs as exprs
+
+from .data_row_batch import DataRowBatch
+from .exec_context import ExecContext
+
+
+class ExecNode(abc.ABC):
+    """Base class of all execution nodes"""
+    output_exprs: Iterable[exprs.Expr]
+    row_builder: exprs.RowBuilder
+    input: Optional[ExecNode]
+    flushed_img_slots: list[int]  # idxs of image slots of our output_exprs dependencies
+    stored_img_cols: list[exprs.ColumnSlotIdx]
+    ctx: Optional[ExecContext]
+    __iter: Optional[Iterator[DataRowBatch]]
+
+    def __init__(
+            self, row_builder: exprs.RowBuilder, output_exprs: Iterable[exprs.Expr],
+            input_exprs: Iterable[exprs.Expr], input: Optional[ExecNode] = None):
+        self.output_exprs = output_exprs
+        self.row_builder = row_builder
+        self.input = input
+        # we flush all image slots that aren't part of our output but are needed to create our output
+        output_slot_idxs = {e.slot_idx for e in output_exprs}
+        output_dependencies = row_builder.get_dependencies(output_exprs, exclude=input_exprs)
+        self.flushed_img_slots = [
+            e.slot_idx for e in output_dependencies
+            if e.col_type.is_image_type() and e.slot_idx not in output_slot_idxs
+        ]
+        self.stored_img_cols = []
+        self.ctx = None  # all nodes of a tree share the same context
+        self.__iter = None
+
+    def set_ctx(self, ctx: ExecContext) -> None:
+        self.ctx = ctx
+        if self.input is not None:
+            self.input.set_ctx(ctx)
+
+    def set_stored_img_cols(self, stored_img_cols: list[exprs.ColumnSlotIdx]) -> None:
+        self.stored_img_cols = stored_img_cols
+        # propagate batch size to the source
+        if self.input is not None:
+            self.input.set_stored_img_cols(stored_img_cols)
+
+    # TODO: make this an abstractmethod when __next__() is removed
+    def __iter__(self) -> Iterator[DataRowBatch]:
+        return self
+
+    # TODO: remove this and switch every subclass over to implementing __iter__
+    def __next__(self) -> DataRowBatch:
+        if self.__iter is None:
+            self.__iter = iter(self)
+        return next(self.__iter)
+
+    def open(self) -> None:
+        """Bottom-up initialization of nodes for execution. Must be called before __next__."""
+        if self.input is not None:
+            self.input.open()
+        self._open()
+
+    def close(self) -> None:
+        """Frees node resources top-down after execution. Must be called after final __next__."""
+        self._close()
+        if self.input is not None:
+            self.input.close()
+
+    def _open(self) -> None:
+        pass
+
+    def _close(self) -> None:
+        pass
+
+    T = TypeVar('T', bound='ExecNode')
+
+    def get_node(self, node_class: type[T]) -> Optional[T]:
+        if isinstance(self, node_class):
+            return self
+        if self.input is not None:
+            return self.input.get_node(node_class)
+        return None
+
+    def set_limit(self, limit: int) -> None:
+        """Default implementation propagates to input"""
+        if self.input is not None:
+            self.input.set_limit(limit)
---
+++ packages/pixeltable/pixeltable-0.3.0.tar.gz/pixeltable-0.3.0/pixeltable/functions/anthropic.py
@@ -0,0 +1,154 @@
+"""
+Pixeltable [UDFs](https://pixeltable.readme.io/docs/user-defined-functions-udfs)
+that wrap various endpoints from the Anthropic API. In order to use them, you must
+first `pip install anthropic` and configure your Anthropic credentials, as described in
+the [Working with Anthropic](https://pixeltable.readme.io/docs/working-with-anthropic) tutorial.
+"""
+
+from typing import TYPE_CHECKING, Any, Callable, Optional, TypeVar, Union
+
+import tenacity
+
+import pixeltable as pxt
+import pixeltable.exceptions as excs
+from pixeltable import env, exprs
+from pixeltable.func import Tools
+from pixeltable.utils.code import local_public_names
+
+if TYPE_CHECKING:
+    import anthropic
+
+
+@env.register_client('anthropic')
+def _(api_key: str) -> 'anthropic.Anthropic':
+    import anthropic
+    return anthropic.Anthropic(api_key=api_key)
+
+
+def _anthropic_client() -> 'anthropic.Anthropic':
+    return env.Env.get().get_client('anthropic')
+
+
+def _retry(fn: Callable) -> Callable:
+    import anthropic
+    return tenacity.retry(
+        retry=tenacity.retry_if_exception_type(anthropic.RateLimitError),
+        wait=tenacity.wait_random_exponential(multiplier=1, max=60),
+        stop=tenacity.stop_after_attempt(20),
+    )(fn)
+
+
+@pxt.udf
+def messages(
+    messages: list[dict[str, str]],
+    *,
+    model: str,
+    max_tokens: int = 1024,
+    metadata: Optional[dict[str, Any]] = None,
+    stop_sequences: Optional[list[str]] = None,
+    system: Optional[str] = None,
+    temperature: Optional[float] = None,
+    tool_choice: Optional[dict] = None,
+    tools: Optional[list[dict]] = None,
+    top_k: Optional[int] = None,
+    top_p: Optional[float] = None,
+) -> dict:
+    """
+    Create a Message.
+
+    Equivalent to the Anthropic `messages` API endpoint.
+    For additional details, see: <https://docs.anthropic.com/en/api/messages>
+
+    __Requirements:__
+
+    - `pip install anthropic`
+
+    Args:
+        messages: Input messages.
+        model: The model that will complete your prompt.
+
+    For details on the other parameters, see: <https://docs.anthropic.com/en/api/messages>
+
+    Returns:
+        A dictionary containing the response and other metadata.
+
+    Examples:
+        Add a computed column that applies the model `claude-3-haiku-20240307`
+        to an existing Pixeltable column `tbl.prompt` of the table `tbl`:
+
+        >>> msgs = [{'role': 'user', 'content': tbl.prompt}]
+        ... tbl['response'] = messages(msgs, model='claude-3-haiku-20240307')
+    """
+    if tools is not None:
+        # Reformat `tools` into Anthropic format
+        tools = [
+            {
+                'name': tool['name'],
+                'description': tool['description'],
+                'input_schema': {
+                    'type': 'object',
+                    'properties': tool['parameters']['properties'],
+                    'required': tool['required'],
+                },
+            }
+            for tool in tools
+        ]
+
+    tool_choice_: Optional[dict] = None
+    if tool_choice is not None:
+        if tool_choice['auto']:
+            tool_choice_ = {'type': 'auto'}
+        elif tool_choice['required']:
+            tool_choice_ = {'type': 'any'}
+        else:
+            assert tool_choice['tool'] is not None
+            tool_choice_ = {'type': 'tool', 'name': tool_choice['tool']}
+        if not tool_choice['parallel_tool_calls']:
+            tool_choice_['disable_parallel_tool_use'] = True
+
+    return _retry(_anthropic_client().messages.create)(
+        messages=messages,
+        model=model,
+        max_tokens=max_tokens,
+        metadata=_opt(metadata),
+        stop_sequences=_opt(stop_sequences),
+        system=_opt(system),
+        temperature=_opt(temperature),
+        tool_choice=_opt(tool_choice_),
+        tools=_opt(tools),
+        top_k=_opt(top_k),
+        top_p=_opt(top_p),
+    ).dict()
+
+
+def invoke_tools(tools: Tools, response: exprs.Expr) -> exprs.InlineDict:
+    """Converts an Anthropic response dict to Pixeltable tool invocation format and calls `tools._invoke()`."""
+    return tools._invoke(_anthropic_response_to_pxt_tool_calls(response))
+
+
+@pxt.udf
+def _anthropic_response_to_pxt_tool_calls(response: dict) -> Optional[dict]:
+    anthropic_tool_calls = [r for r in response['content'] if r['type'] == 'tool_use']
+    if len(anthropic_tool_calls) > 0:
+        return {
+            tool_call['name']: {
+                'args': tool_call['input']
+            }
+            for tool_call in anthropic_tool_calls
+        }
+    return None
+
+
+_T = TypeVar('_T')
+
+
+def _opt(arg: _T) -> Union[_T, 'anthropic.NotGiven']:
+    import anthropic
+    return arg if arg is not None else anthropic.NOT_GIVEN
+
+
+__all__ = local_public_names(__name__)
+
+
+def __dir__():
+    return __all__
