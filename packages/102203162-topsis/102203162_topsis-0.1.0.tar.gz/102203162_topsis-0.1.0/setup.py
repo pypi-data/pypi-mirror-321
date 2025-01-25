abac789a2e354bcc8617a6b1bab4b0f45cdad1d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 04:05:37 +0000

    Add package 102203162-topsis/0.1.0/102203162_topsis-0.1.0.tar.gz

---
+++ packages/102203162-topsis/102203162_topsis-0.1.0.tar.gz/102203162_topsis-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="102203162-topsis",
+    version="0.1.0",
+    author="Raghav Manchanda",
+    author_email="rmanchanda_be22@thapar.edu",
+    description="A Python library for the TOPSIS decision-making method.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/102203162-topsis",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
