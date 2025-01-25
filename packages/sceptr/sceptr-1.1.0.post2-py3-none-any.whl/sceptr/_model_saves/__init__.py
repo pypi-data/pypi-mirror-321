970a14696238a41233d71f4864a0bd8ff42d956a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:07:32 +0000

    Add package sceptr/1.1.0.post2/sceptr-1.1.0.post2-py3-none-any.whl

---
+++ packages/sceptr/sceptr-1.1.0.post2-py3-none-any.whl/sceptr/_model_saves/__init__.py
@@ -0,0 +1,30 @@
+from importlib import resources
+import json
+from sceptr.model import Sceptr
+from libtcrlm.config_reader import ConfigReader
+import torch
+
+
+def load_variant(model_name: str) -> Sceptr:
+    model_save_dir = resources.files(__name__) / model_name
+
+    with (model_save_dir / "config.json").open("r") as f:
+        config = json.load(f)
+
+    with (model_save_dir / "state_dict.pt").open("rb") as f:
+        state_dict = torch.load(f, weights_only=True)
+
+    config_reader = ConfigReader(config)
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+
+    name = config_reader.get_model_name()
+    tokeniser = config_reader.get_tokeniser()
+    bert = config_reader.get_bert()
+    bert.load_state_dict(state_dict)
+    bert.to(device)
+
+    return Sceptr(name=name, tokeniser=tokeniser, bert=bert, device=device)
