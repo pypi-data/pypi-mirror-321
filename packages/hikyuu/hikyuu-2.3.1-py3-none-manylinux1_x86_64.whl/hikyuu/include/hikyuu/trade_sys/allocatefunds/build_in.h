252f473a752f680385b16cca00aca87c39f38ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:06:47 +0000

    Add package hikyuu/2.3.1/hikyuu-2.3.1-py3-none-manylinux1_x86_64.whl

---
+++ packages/hikyuu/hikyuu-2.3.1-py3-none-manylinux1_x86_64.whl/hikyuu/include/hikyuu/trade_sys/allocatefunds/build_in.h
@@ -0,0 +1,16 @@
+/*
+ * build_in.h
+ *
+ *  Created on: 2018年2月1日
+ *      Author: fasiondog
+ */
+
+#pragma once
+#ifndef TRADE_SYS_ALLOCATEFUNDS_BUILD_IN_H_
+#define TRADE_SYS_ALLOCATEFUNDS_BUILD_IN_H_
+
+#include "crt/AF_EqualWeight.h"
+#include "crt/AF_FixedWeight.h"
+#include "crt/AF_MultiFactor.h"
+
+#endif /* TRADE_SYS_ALLOCATEFUNDS_BUILD_IN_H_ */
---
+++ packages/hikyuu/hikyuu-2.3.1-py3-none-manylinux1_x86_64.whl/hikyuu/include/hikyuu/version.h
@@ -0,0 +1,24 @@
+/*
+ * version.h
+ *
+ *  Copyright (c) 2020, hikyuu.org
+ *
+ *  Created on: 2020-4-19
+ *      Author: fasiondog
+ */
+
+#pragma once
+#ifndef HKU_VERSION_H
+#define HKU_VERSION_H
+
+// clang-format off
+#define HKU_VERSION "2.3.1"
+#define HKU_VERSION_MAJOR 2
+#define HKU_VERSION_MINOR 3
+#define HKU_VERSION_ALTER 1
+#define HKU_VERSION_BUILD 202501181731
+#define HKU_VERSION_MODE "RELEASE"
+#define HKU_VERSION_GIT "2.3.1 release.ad6263fc (RELEASE)"
+// clang-format on
+
+#endif /* HKU_VERSION_H */
