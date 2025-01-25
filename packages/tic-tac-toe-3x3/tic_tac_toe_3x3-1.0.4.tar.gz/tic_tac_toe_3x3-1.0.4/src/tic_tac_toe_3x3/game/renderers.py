f4b693ec4b8340a64f2ef8b66e0665c0fbaf4826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:05:41 +0000

    Add package tic-tac-toe-3x3/1.0.4/tic_tac_toe_3x3-1.0.4.tar.gz

---
+++ packages/tic-tac-toe-3x3/tic_tac_toe_3x3-1.0.4.tar.gz/tic_tac_toe_3x3-1.0.4/src/tic_tac_toe_3x3/game/renderers.py
@@ -0,0 +1,12 @@
+import abc
+
+from tic_tac_toe_3x3.logic.models import GameState
+
+
+class Renderer(metaclass=abc.ABCMeta):
+    """Abstract class for creating specific renderers - that match the chosen usage.
+    The renderer is responsible for displaying the game state.
+    """
+    @abc.abstractmethod
+    def render(self, game_state: GameState) -> None:
+        """Render the current game state."""
