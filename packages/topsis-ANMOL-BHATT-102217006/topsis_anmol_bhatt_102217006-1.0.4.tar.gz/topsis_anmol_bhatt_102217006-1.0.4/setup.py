49e69121d798d2eb567f65b334fdcdc162385feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:08:51 +0000

    Add package topsis-ANMOL-BHATT-102217006/1.0.4/topsis_anmol_bhatt_102217006-1.0.4.tar.gz

---
+++ packages/topsis-ANMOL-BHATT-102217006/topsis_anmol_bhatt_102217006-1.0.4.tar.gz/topsis_anmol_bhatt_102217006-1.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="topsis-ANMOL-BHATT-102217006",  
+    version="1.0.4",
+    author="ANMOL BHATT",
+    description="A Python package for implementing the TOPSIS multi-criteria decision analysis method.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",  
+    packages=find_packages(),  
+    install_requires=[
+        "numpy",
+        "pandas"
+    ],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.topsis:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    license="MIT",  
+)
