28bea84694e3e0e15b9345df746f7b433c95a5e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 02:22:13 +0000

    Add package lingmopyui/1.0.4/lingmopyui-1.0.4-py3-none-any.whl

---
+++ packages/lingmopyui/lingmopyui-1.0.4-py3-none-any.whl/LingmoPyUI/LingmoApp.py
@@ -0,0 +1,33 @@
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+
+import sys
+import os
+
+from . import LingmoIconDef
+appid='com.lingmo.pyui.1.0'
+windowIcon='./LingmoPyUI/Image/icon.png'
+locale=QLocale()
+launcher=QObject()
+useSystemAppBar=False
+_app=QApplication([])
+_translator=QTranslator(QApplication.instance())
+QApplication.installTranslator(_translator)
+uiLanguages=locale.uiLanguages()
+for i in uiLanguages:
+    basename='lingmoui_'+QLocale(i).name()
+    if _translator.load(os.path.dirname(sys.argv[0])+basename):
+        _app.translate()
+        break
+
+def run():
+    _app.exec()
+
+def iconData(keyword):
+    arr=QJsonArray()
+    for i in LingmoIconDef.__dict__:
+        if (keyword in i) or keyword=='':
+            value=QJsonValue({'name':i,'icon':LingmoIconDef.__dict__[i]})
+            arr.append(value)
+            
