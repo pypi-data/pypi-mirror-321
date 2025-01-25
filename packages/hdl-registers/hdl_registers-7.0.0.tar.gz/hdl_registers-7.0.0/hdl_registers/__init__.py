e1b18d80910f4915f1cca2459857c51c7e67b6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:06:51 +0000

    Add package hdl-registers/7.0.0/hdl_registers-7.0.0.tar.gz

---
+++ packages/hdl-registers/hdl_registers-7.0.0.tar.gz/hdl_registers-7.0.0/hdl_registers/__init__.py
@@ -0,0 +1,25 @@
+# --------------------------------------------------------------------------------------------------
+# Copyright (c) Lukas Vik. All rights reserved.
+#
+# This file is part of the hdl-registers project, an HDL register generator fast enough to run
+# in real time.
+# https://hdl-registers.com
+# https://github.com/hdl-registers/hdl-registers
+# --------------------------------------------------------------------------------------------------
+
+# Standard libraries
+from pathlib import Path
+
+# Local folder libraries
+from .about import get_short_slogan
+
+REPO_ROOT = Path(__file__).parent.parent.resolve()
+
+HDL_REGISTERS_PATH = REPO_ROOT / "hdl_registers"
+HDL_REGISTERS_DOC = REPO_ROOT / "doc"
+HDL_REGISTERS_GENERATED = REPO_ROOT / "generated"
+HDL_REGISTERS_TESTS = REPO_ROOT / "tests"
+HDL_REGISTERS_TOOLS = REPO_ROOT / "tools"
+
+__version__ = "7.0.0"
+__doc__ = get_short_slogan()  # pylint: disable=redefined-builtin
