43a66fef42de04e0736c58d4819b1423e730ceb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:05:14 +0000

    Add package rasa-pro/3.11.3a1.dev6/rasa_pro-3.11.3a1.dev6.tar.gz

---
+++ packages/rasa-pro/rasa_pro-3.11.3a1.dev6.tar.gz/rasa_pro-3.11.3a1.dev6/rasa/core/actions/constants.py
@@ -0,0 +1,13 @@
+DEFAULT_SELECTIVE_DOMAIN = False
+SELECTIVE_DOMAIN = "enable_selective_domain"
+
+SSL_CLIENT_CERT_FIELD = "ssl_client_cert"
+SSL_CLIENT_KEY_FIELD = "ssl_client_key"
+
+TEXT = "text"
+ELEMENTS = "elements"
+QUICK_REPLIES = "quick_replies"
+BUTTONS = "buttons"
+ATTACHMENT = "attachment"
+IMAGE = "image"
+CUSTOM = "custom"
