0b3b10e6ee40fc19015860ae98c20b8acfced483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 01:32:14 +0000

    Add package lowkernels/0.1.0/lowkernels-0.1.0.tar.gz

---
+++ packages/lowkernels/lowkernels-0.1.0.tar.gz/lowkernels-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lowkernels",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Toolkit for creating low-level operating systems",
+    author="lowkernels",
+    license="MIT",
+)
