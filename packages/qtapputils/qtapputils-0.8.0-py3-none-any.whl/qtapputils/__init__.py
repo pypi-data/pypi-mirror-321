449bbcd4b896acdd981f7926401dc8da4bf1bf2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:06:36 +0000

    Add package qtapputils/0.8.0/qtapputils-0.8.0-py3-none-any.whl

---
+++ packages/qtapputils/qtapputils-0.8.0-py3-none-any.whl/qtapputils/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © QtAppUtils Project Contributors
+# https://github.com/jnsebgosselin/apputils
+#
+# This file is part of QtAppUtils.
+# Licensed under the terms of the MIT License.
+# -----------------------------------------------------------------------------
+
+version_info = (0, 8, 0)
+__version__ = '.'.join(map(str, version_info))
+__date__ = '17/01/2025'
+__project_url__ = "https://github.com/jnsebgosselin/qtapputils"
