a5763c41efef1d82ef943a5ff54bf97f2f0d2ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:49 +0000

    Add package comp1010/1.0.0/comp1010-1.0.0-py3-none-any.whl

---
+++ packages/comp1010/comp1010-1.0.0-py3-none-any.whl/comp1010/__main__.py
@@ -0,0 +1,13 @@
+"""
+# COMP1010 Metapackage
+
+This library includes all required COMP1010 dependencies.
+"""
+
+
+def main():
+    print("COMP1010 Metapackage installed successfully!")
+
+
+if __name__ == '__main__':
+    main()
