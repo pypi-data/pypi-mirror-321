66db11f10812316a05c125df8824b490d834a2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:05:43 +0000

    Add package ourprojects/1.0.1/ourprojects-1.0.1-py2.py3-none-any.whl

---
+++ packages/ourprojects/ourprojects-1.0.1-py2.py3-none-any.whl/ourprojects/__init__.py
@@ -0,0 +1,35 @@
+"""Manage projects & teams.
+
+Install and mount `ourprojects` in a new instance:
+
+>>> pip install ourprojects
+>>> lamin init --storage ./test-ourprojects --schema ourprojects
+
+Import the package:
+
+>>> import ourprojects as ops
+
+The `Reference` registry:
+
+.. autosummary::
+   :toctree: .
+
+    Person
+    Project
+    Reference
+"""
+
+__version__ = "1.0.1"  # denote a pre-release for 0.1.0 with 0.1rc1
+
+from lamindb_setup import _check_instance_setup
+
+
+def __getattr__(name):
+    if name != "models":
+        _check_instance_setup(from_module="ourprojects")
+    return globals()[name]
+
+
+if _check_instance_setup():
+    del __getattr__  # delete so that imports work out
+    from .models import Person, Project, Reference
