a5803608268834cbde37984b73e2760ce4d7d9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 07:05:19 +0000

    Add package zoldics-service-utils/0.0.1/zoldics_service_utils-0.0.1-py3-none-any.whl

---
+++ packages/zoldics-service-utils/zoldics_service_utils-0.0.1-py3-none-any.whl/zoldics_service_utils/clients/http_client.py
@@ -0,0 +1,86 @@
+import requests
+from decouple import config
+import uuid
+from typing import Optional, Dict
+from service_utils.logging.base_logger import APP_LOGGER
+
+
+class HttpClient:
+    def __init__(
+        self,
+        headers: Dict[str, str],
+        path: str,
+        body=None,
+        correlationid: Optional[str] = None,
+        path_params: Optional[Dict[str, str]] = None,
+        query_params: Optional[Dict[str, str]] = None,
+        interServiceCall: bool = False,
+        localhostCall: bool = False,
+        portNumber: str = str(config("PORT")),
+    ) -> None:
+        self.interServiceCall = interServiceCall
+        self.localhostCall = localhostCall
+        self.__request_id = str(uuid.uuid4())
+        self.__environment: str = str(config("ENVIRONMENT"))
+        self.__domain: str = str(config("DOMAIN"))
+        self.__portnumber: str = portNumber
+        self.__headers = headers
+        self.__path = path
+        self.__body = body
+        self.__path_params = path_params
+        self.__query_params = query_params
+        if correlationid is None:
+            self.__correlationid = str(uuid.uuid4())
+        else:
+            self.__correlationid = correlationid
+        self.__headers["correlationid"] = self.__correlationid
+
+    def __construct_url(self) -> str:
+        base_url = (
+            f"https://{self.__environment}{self.__domain}/"
+            if self.interServiceCall
+            else f"http://localhost:{self.__portnumber}/"
+        )
+        url = base_url + self.__path
+        if self.__path_params:
+            url = url.format(**self.__path_params)
+        return url
+
+    def __send_request(self, method: str) -> requests.Response:
+        url: str = self.__construct_url()
+        APP_LOGGER.info(
+            f"Sending {method} request to {url} with correlationId = {self.__correlationid} and requestid = {self.__request_id}"
+        )
+        try:
+            response = requests.request(
+                method=method,
+                url=url,
+                headers=self.__headers,
+                params=self.__query_params,
+                json=self.__body,
+            )
+            response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+            APP_LOGGER.info(
+                f"Received response with status code {response.status_code} with correlationid = {self.__correlationid} and requestid  = {self.__request_id}"
+            )
+            return response
+        except requests.exceptions.RequestException as e:
+            APP_LOGGER.error(
+                f"Request failed with correlationid = {self.__correlationid} and requestid = {self.__request_id}. Error: {str(e)}"
+            )
+            raise
+
+    def get(self) -> requests.Response:
+        return self.__send_request("GET")
+
+    def post(self) -> requests.Response:
+        return self.__send_request("POST")
+
+    def patch(self) -> requests.Response:
+        return self.__send_request("PATCH")
+
+    def put(self) -> requests.Response:
+        return self.__send_request("PUT")
+
+    def delete(self) -> requests.Response:
+        return self.__send_request("DELETE")
---
+++ packages/zoldics-service-utils/zoldics_service_utils-0.0.1-py3-none-any.whl/zoldics_service_utils/clients/sqs_client/post_message.py
@@ -0,0 +1,84 @@
+import boto3
+import json
+from typing import final, Dict
+from decouple import config
+from service_utils.clients.sqs_client.helpers import Helpers
+from service_utils.clients.sqs_client.queue_config import QUEUE_CONFIG
+from service_utils.logging.base_logger import APP_LOGGER
+from service_utils.context.vars import headers_context
+from typing import TypedDict, Required, Any
+import uuid
+
+
+class Message_Payload_TypeHinter(TypedDict, total=False):
+    action: Required[str]
+    payload: Dict[str, Any]
+    context: Dict[str, Any]
+
+
+DefaultContext = {
+    "instanceid": "not-applicable",
+    "organizationid": "Embedding-Service",
+    "caller": "not-applicable",
+    "ipaddress": "not-applicable",
+    "origin": "not-applicable",
+    "origintype": "not-applicable",
+    "userid": "not-applicable",
+    "sessionid": "not-applicable",
+    "usertype": "not-applicable",
+    "correlationid": str(uuid.uuid4()),
+}
+
+
+@final
+class SyncSQSPusher:
+    aws_access_key_id: str = str(config("AWS_ACCESS_KEY_ID"))
+    aws_secret_access_key: str = str(config("AWS_SECRET_ACCESS_KEY"))
+    aws_region_name: str = str(config("AWS_REGION_NAME"))
+    queue_config: Dict[str, Dict[str, bool]] = QUEUE_CONFIG
+
+    def __construct_message_body(
+        self, action: str, payload: Dict[str, Any]
+    ) -> Message_Payload_TypeHinter:
+        message_context = (
+            headers_context.get().model_dump(mode="json") or DefaultContext
+        )
+        message_payload = Message_Payload_TypeHinter(
+            action=action, context=message_context, payload=payload
+        )
+        return message_payload
+
+    def __init__(self, queue_name: str, action: str, payload: Dict[str, Any]) -> None:
+        self.sqs_client = boto3.client(
+            "sqs",
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key,
+            region_name=self.aws_region_name,
+        )
+        self.aws_account_id = boto3.client(
+            "sts",
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key,
+        ).get_caller_identity()["Account"]
+        self.queue_name = queue_name
+        self.queue_url = Helpers.construct_queue_url(
+            queue_name=queue_name,
+            region_name=self.aws_region_name,
+            params=self.queue_config[queue_name],
+            aws_account_id=self.aws_account_id,
+        )
+        self.message_payload = self.__construct_message_body(
+            action=action, payload=payload
+        )
+
+    def post_message(self) -> None:
+        try:
+            response = self.sqs_client.send_message(
+                QueueUrl=self.queue_url, MessageBody=json.dumps(self.message_payload)
+            )
+            APP_LOGGER.info(
+                f"Message sent successfully to queue '{self.queue_name}' with Message ID: { response['MessageId']} and  ->  message payload : {self.message_payload}"
+            )
+        except Exception as e:
+            APP_LOGGER.error("Error sending message:" + str(e))
+            raise e
