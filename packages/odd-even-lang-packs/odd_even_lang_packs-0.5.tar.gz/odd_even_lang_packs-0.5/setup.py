d65ab74e0354a91def53b76aca852283566c501f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 07:05:30 +0000

    Add package odd-even-lang-packs/0.5/odd_even_lang_packs-0.5.tar.gz

---
+++ packages/odd-even-lang-packs/odd_even_lang_packs-0.5.tar.gz/odd_even_lang_packs-0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="odd_even_lang_packs",
+    version="0.5",
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+    author="Adiel Boanerge Gananputra",
+    author_email="ergegananputra@gmail.com",
+    description="Language packs for the Odd Even application",
+    long_description="This repository contains language packs for the Odd Even application. These language packs provide translations for various languages to enhance the user experience.",
+    url="https://github.com/ergegananputra/odd_even_lang_packs",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
