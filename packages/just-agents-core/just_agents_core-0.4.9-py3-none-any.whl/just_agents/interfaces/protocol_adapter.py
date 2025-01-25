e17230b874e51f3bcbd3360eb51d9550358c85d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:05:33 +0000

    Add package just-agents-core/0.4.9/just_agents_core-0.4.9-py3-none-any.whl

---
+++ packages/just-agents-core/just_agents_core-0.4.9-py3-none-any.whl/just_agents/interfaces/protocol_adapter.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractmethod
+from pydantic import BaseModel
+from typing import Callable, Coroutine, Union, AsyncGenerator, List, Sequence, ClassVar, Type, TypeVar, Generic, Any
+from just_agents.interfaces.streaming_protocol import IAbstractStreamingProtocol
+from just_agents.interfaces.function_call import IFunctionCall
+
+BaseModelResponse = TypeVar('BaseModelResponse', bound=BaseModel)
+BaseModelStreamWrapper = TypeVar('BaseModelStreamWrapper', bound=BaseModel)
+AbstractMessage = TypeVar("AbstractMessage")
+
+ModelResponseCallback=Callable[...,BaseModelResponse]
+MessageUnpackCallback=Callable[[BaseModelResponse], AbstractMessage]
+ExecuteToolCallback=Callable[[Sequence[IFunctionCall]],List[AbstractMessage]]
+
+class IProtocolAdapter(IAbstractStreamingProtocol, ABC, Generic[BaseModelResponse, AbstractMessage, BaseModelStreamWrapper]):
+    """
+    Class that is required to wrap the model protocol
+    """
+    function_convention: ClassVar[Type[IFunctionCall[Any]]]
+    _output_streaming: IAbstractStreamingProtocol
+    execute_function_hook: ExecuteToolCallback[AbstractMessage]
+
+    @abstractmethod
+    def completion(self, *args, **kwargs) -> BaseModelResponse:
+        raise NotImplementedError("You need to implement completion first!")
+
+    async def async_completion(self, *args, **kwargs) \
+            -> Coroutine[Any,Any,Union[BaseModelResponse, AsyncGenerator, Any]]:
+        raise NotImplementedError("You need to implement async_completion first!")
+
+    @abstractmethod
+    def message_from_response(self, response: BaseModelResponse) -> AbstractMessage:
+        raise NotImplementedError("You need to implement message_from_response first!")
+
+    @abstractmethod
+    def delta_from_response(self, response: BaseModelStreamWrapper) -> AbstractMessage:
+        raise NotImplementedError("You need to implement delta_from_response first!")
+
+    @abstractmethod
+    def content_from_delta(self, delta: AbstractMessage) -> str:
+        raise NotImplementedError("You need to implement content_from_delta first!")
+
+    @abstractmethod
+    def tool_calls_from_message(self, message: AbstractMessage) -> List[IFunctionCall[AbstractMessage]]:
+        raise NotImplementedError("You need to implement tool_calls_from_response first!")
+
+    @abstractmethod
+    def response_from_deltas(self, deltas: List[AbstractMessage]) -> BaseModelResponse:
+        raise NotImplementedError("You need to implement message_from_deltas first!")
+
+    def get_chunk(self, index:int, delta:str, options:dict) -> AbstractMessage:
+        return self._output_streaming.get_chunk(index, delta, options)
+
+    def done(self) -> str:
+        return self._output_streaming.done()
---
+++ packages/just-agents-core/just_agents_core-0.4.9-py3-none-any.whl/just_agents/protocols/protocol_factory.py
@@ -0,0 +1,41 @@
+from enum import Enum
+from just_agents.interfaces.protocol_adapter import IProtocolAdapter, ExecuteToolCallback
+
+
+class StreamingMode(str, Enum):
+    openai = "openai"
+    echo = "echo"
+    qwen2 = "qwen2"
+
+    def __new__(cls, value, *args, **kwargs):
+        obj = str.__new__(cls, value)
+        obj._value_ = value
+        return obj
+
+    def __str__(self):
+        return str(self.value)
+
+class ProtocolAdapterFactory:
+    @staticmethod
+    def get_protocol_adapter(
+            mode: StreamingMode,
+            execute_functions: ExecuteToolCallback,
+
+    ) -> IProtocolAdapter:
+        if mode == StreamingMode.openai:
+            from just_agents.protocols.litellm_protocol import LiteLLMAdapter
+            return LiteLLMAdapter(
+                execute_function_hook=execute_functions,
+            )
+        elif mode == StreamingMode.echo:
+            from just_agents.protocols.echo_protocol import EchoProtocolAdapter
+            return EchoProtocolAdapter(
+                execute_function_hook=execute_functions,
+            )
+        elif mode == StreamingMode.qwen2:
+            # todo: implement
+            raise NotImplementedError("Qwen2 streaming is not yet implemented for pydantic-based agents")
+        else:
+            raise ValueError("Unknown streaming method")
+
+
