90393ad1e00ec29062a35b57a6c71cc384a001ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:05:30 +0000

    Add package bepatient/0.15.0/bepatient-0.15.0-py3-none-any.whl

---
+++ packages/bepatient/bepatient-0.15.0-py3-none-any.whl/bepatient/__init__.py
@@ -0,0 +1,31 @@
+"""A library facilitating work with asynchronous APIs"""
+
+import logging
+from logging import NullHandler
+
+from .api import (
+    RequestsWaiter,
+    dict_differences,
+    to_curl,
+    wait_for_value_in_request,
+    wait_for_values_in_request,
+)
+from .retry import retry
+from .waiter_src.checkers import CHECKERS
+from .waiter_src.checkers.checker import Checker
+from .waiter_src.comparators import COMPARATORS
+
+__version__ = "0.15.0"
+__all__ = [
+    "Checker",
+    "CHECKERS",
+    "COMPARATORS",
+    "dict_differences",
+    "retry",
+    "RequestsWaiter",
+    "to_curl",
+    "wait_for_values_in_request",
+    "wait_for_value_in_request",
+]
+
+logging.getLogger(__name__).addHandler(NullHandler())
