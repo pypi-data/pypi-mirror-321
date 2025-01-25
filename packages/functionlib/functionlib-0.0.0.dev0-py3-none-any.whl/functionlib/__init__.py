46ce85b17426c1b881c86ffdb0149b5f1042bda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 13:08:39 +0000

    Add package functionlib/0.0.0.dev0/functionlib-0.0.0.dev0-py3-none-any.whl

---
+++ packages/functionlib/functionlib-0.0.0.dev0-py3-none-any.whl/functionlib/__init__.py
@@ -0,0 +1,5 @@
+from functionlib.core import *
+from functionlib.tests import *
+
+if __name__ == "__main__":
+    main()
---
+++ packages/functionlib/functionlib-0.0.0.dev0-py3-none-any.whl/functionlib/__main__.py
@@ -0,0 +1,4 @@
+from functionlib import main
+
+if __name__ == "__main__":
+    main()
