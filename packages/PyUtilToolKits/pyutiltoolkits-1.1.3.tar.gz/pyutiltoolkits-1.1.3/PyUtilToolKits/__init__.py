269e2a9a9c5341f0eda177a8c5f04c0bfdb35131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 09:05:48 +0000

    Add package PyUtilToolKits/1.1.3/pyutiltoolkits-1.1.3.tar.gz

---
+++ packages/PyUtilToolKits/pyutiltoolkits-1.1.3.tar.gz/pyutiltoolkits-1.1.3/PyUtilToolKits/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+"""
+@Time        : 2023/3/2
+@File        : __init__.py
+@Author      : lyz
+@version     : python 3
+@Description : 文件夹下所包含的文件
+"""
+
+from PyUtilToolKits import *
+
+"""
+__all__ 子文件名称(不带.py)
+:param util_log: 日志
+"""
+__all__ = ["util_log"]
