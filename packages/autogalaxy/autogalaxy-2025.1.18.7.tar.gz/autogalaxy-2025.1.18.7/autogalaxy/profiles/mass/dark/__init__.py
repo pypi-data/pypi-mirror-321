8a1a78bdebbfb2a088ba3e0dd85f5a073b7e7332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 08:05:57 +0000

    Add package autogalaxy/2025.1.18.7/autogalaxy-2025.1.18.7.tar.gz

---
+++ packages/autogalaxy/autogalaxy-2025.1.18.7.tar.gz/autogalaxy-2025.1.18.7/autogalaxy/profiles/mass/dark/__init__.py
@@ -0,0 +1,10 @@
+from .gnfw import gNFW, gNFWSph
+from .gnfw_mcr import gNFWMCRLudlow
+from .gnfw_virial_mass_conc import gNFWVirialMassConcSph
+from .nfw import NFW, NFWSph
+from .nfw_mcr import NFWMCRLudlowSph, NFWMCRDuffySph, NFWMCRLudlow
+from .nfw_mcr_scatter import NFWMCRScatterLudlow, NFWMCRScatterLudlowSph
+from .nfw_truncated import NFWTruncatedSph
+from .nfw_truncated_mcr import NFWTruncatedMCRLudlowSph, NFWTruncatedMCRDuffySph
+from .nfw_truncated_mcr_scatter import NFWTruncatedMCRScatterLudlowSph
+from .nfw_virial_mass_conc import NFWVirialMassConcSph
