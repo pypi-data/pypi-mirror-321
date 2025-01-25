bda84cd840e15b82ddf81a7f2b0b1d8134bea27b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 05:06:00 +0000

    Add package topsis-Akshat-102203644/1.1.0/topsis_akshat_102203644-1.1.0.tar.gz

---
+++ packages/topsis-Akshat-102203644/topsis_akshat_102203644-1.1.0.tar.gz/topsis_akshat_102203644-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="topsis_Akshat_102203644",
+    packages=find_packages(),
+    version="1.1.0",       
+    author="Akshat Khurana",  # Your name
+    author_email="akhurana_be22@thapar.edu",  # Your email
+    description="A Python package to perform TOPSIS (Technique for Order Preference by Similarity to Ideal Solution).",
+    long_description=open("README.md").read(),  # Make sure you have a README.md
+    long_description_content_type="text/markdown",
+    url="https://github.com/Akshatkhurana/TOPSIS-package",  # Replace with your GitHub repo URL
+      # Automatically find packages in the folder
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Specify Python version compatibility
+    install_requires=[
+        "numpy",
+        "pandas" # Add dependencies here (e.g., pandas, numpy, etc.)
+    ],
+)
