0c2744f4710a925866873c927eefdcf69e91b9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 15:06:16 +0000

    Add package paprika-api/0.1.0/paprika_api-0.1.0-py3-none-any.whl

---
+++ packages/paprika-api/paprika_api-0.1.0-py3-none-any.whl/paprika_api/sync.py
@@ -0,0 +1,35 @@
+from typing import (
+    TypedDict,
+    cast,
+)
+
+from paprika_api.exceptions import PaprikaError
+from paprika_api.paprika import PaprikaClient
+
+
+class SyncCounters(TypedDict):
+    categories: int
+    recipes: int
+    photos: int
+    groceries: int
+    grocerylists: int
+    groceryaisles: int
+    groceryingredients: int
+    meals: int
+    mealtypes: int
+    bookmarks: int
+    pantry: int
+    pantrylocations: int
+    menus: int
+    menuitems: int
+
+
+def get_status(client: PaprikaClient) -> SyncCounters:
+    response = client.session.get(
+        f"{client.base_url}/sync/status/",
+        timeout=client.timeout,
+    )
+    response.raise_for_status()
+    if response.json().get("result", False) is False:
+        raise PaprikaError(request=response.request)
+    return cast(SyncCounters, response.json()["result"])
