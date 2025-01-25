9144377db0c6789cba5287827a5776793e67da37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 01:31:52 +0000

    Add package vidizmo-yolox/0.1.0/vidizmo_yolox-0.1.0.tar.gz

---
+++ packages/vidizmo-yolox/vidizmo_yolox-0.1.0.tar.gz/vidizmo_yolox-0.1.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# Copyright (c) Megvii, Inc. and its affiliates. All Rights Reserved
+
+
+import setuptools
+
+
+
+
+setuptools.setup(
+    name="vidizmo-yolox",
+    version="0.1.0",
+    author="Zia Badar",
+    python_requires=">=3.12",
+    packages=setuptools.find_namespace_packages(),
+)
