9beb25a18e1d8bd3221e331c0fbf71ed085dc54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 03:06:00 +0000

    Add package stjames/0.0.49/stjames-0.0.49.tar.gz

---
+++ packages/stjames/stjames-0.0.49.tar.gz/stjames-0.0.49/stjames/base.py
@@ -0,0 +1,42 @@
+from enum import Enum
+from typing import Annotated, Any, Hashable, TypeVar
+
+import numpy as np
+import pydantic
+
+_T = TypeVar("_T")
+
+
+class Base(pydantic.BaseModel):
+    @pydantic.field_validator("*", mode="before")
+    @classmethod
+    def coerce_numpy(cls, val: _T) -> _T | list[Any]:
+        if isinstance(val, np.ndarray):
+            return val.tolist()  # type: ignore [no-any-return, unused-ignore, return-value]
+
+        return val
+
+
+class LowercaseStrEnum(str, Enum):
+    """Enum where hyphens, underscores, and case are ignored."""
+
+    @classmethod
+    def _missing_(cls, value: object) -> str | None:
+        for member in cls:
+            if isinstance(value, str):
+                if member.lower().replace("-", "").replace("_", "") == value.lower().replace("-", "").replace("_", ""):
+                    return member
+        return None
+
+
+# cf. https://github.com/pydantic/pydantic-core/pull/820#issuecomment-1670475909
+_H = TypeVar("_H", bound=Hashable)
+
+
+def _validate_unique_list(v: list[_H]) -> list[_H]:
+    if len(v) != len(set(v)):
+        raise ValueError("this list must be unique, and isn't!")
+    return v
+
+
+UniqueList = Annotated[list[_H], pydantic.AfterValidator(_validate_unique_list)]
---
+++ packages/stjames/stjames-0.0.49.tar.gz/stjames-0.0.49/stjames/workflows/fukui.py
@@ -0,0 +1,12 @@
+from ..types import UUID
+from .workflow import Workflow
+
+
+class FukuiIndexWorkflow(Workflow):
+    # UUID of optimization
+    optimization: UUID | None = None
+
+    global_electrophilicity_index: float | None = None
+    fukui_positive: list[float] | None = None
+    fukui_negative: list[float] | None = None
+    fukui_zero: list[float] | None = None
