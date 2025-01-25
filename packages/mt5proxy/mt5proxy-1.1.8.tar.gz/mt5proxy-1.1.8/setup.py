c604ca89de9e8e98fb2bb77cd1304143e19f9f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:06:06 +0000

    Add package mt5proxy/1.1.8/mt5proxy-1.1.8.tar.gz

---
+++ packages/mt5proxy/mt5proxy-1.1.8.tar.gz/mt5proxy-1.1.8/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+from setuptools import setup, find_packages
+
+from mt5proxy import help
+
+setup(
+    name=help.get_name(),
+    version=help.get_version(),
+    keywords=help.get_name(),
+    description='Mt5 http proxy for python.',
+    license='MIT License',
+    url='https://github.com/qicongsheng/%s' % help.get_name(),
+    author='qicongsheng',
+    author_email='qicongsheng@outlook.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'MetaTrader5',
+        'flask',
+        'pandas',
+        'pytz'
+    ],
+    entry_points={
+        'console_scripts': [
+            'mt5proxy = mt5proxy.__main__:main'
+        ]
+    }
+)
