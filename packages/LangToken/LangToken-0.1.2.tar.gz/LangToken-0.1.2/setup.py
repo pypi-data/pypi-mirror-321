801ad85406bd1b5c7c24d17494b062069cf0ef82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 14:07:39 +0000

    Add package LangToken/0.1.2/LangToken-0.1.2.tar.gz

---
+++ packages/LangToken/LangToken-0.1.2.tar.gz/LangToken-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LangToken",            
+    version="0.1.2",                     
+    author="S Victor Kumar",
+    author_email="victor.myid@gmail.com",
+    description="Word Tokenizer to create each word into token and decode back",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ictorv/word_token",  
+    packages=find_packages(),           
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
