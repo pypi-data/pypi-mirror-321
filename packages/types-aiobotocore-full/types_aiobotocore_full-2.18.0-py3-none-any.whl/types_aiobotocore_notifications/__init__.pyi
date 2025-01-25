5700751d8bb522a05431cc3bf6d9a3d273939602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 06:05:58 +0000

    Add package types-aiobotocore-full/2.18.0/types_aiobotocore_full-2.18.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-full/types_aiobotocore_full-2.18.0-py3-none-any.whl/types_aiobotocore_notifications/__init__.pyi
@@ -0,0 +1,53 @@
+"""
+Main interface for notifications service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_notifications import (
+        Client,
+        ListChannelsPaginator,
+        ListEventRulesPaginator,
+        ListNotificationConfigurationsPaginator,
+        ListNotificationEventsPaginator,
+        ListNotificationHubsPaginator,
+        UserNotificationsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("notifications") as client:
+        client: UserNotificationsClient
+        ...
+
+
+    list_channels_paginator: ListChannelsPaginator = client.get_paginator("list_channels")
+    list_event_rules_paginator: ListEventRulesPaginator = client.get_paginator("list_event_rules")
+    list_notification_configurations_paginator: ListNotificationConfigurationsPaginator = client.get_paginator("list_notification_configurations")
+    list_notification_events_paginator: ListNotificationEventsPaginator = client.get_paginator("list_notification_events")
+    list_notification_hubs_paginator: ListNotificationHubsPaginator = client.get_paginator("list_notification_hubs")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import UserNotificationsClient
+from .paginator import (
+    ListChannelsPaginator,
+    ListEventRulesPaginator,
+    ListNotificationConfigurationsPaginator,
+    ListNotificationEventsPaginator,
+    ListNotificationHubsPaginator,
+)
+
+Client = UserNotificationsClient
+
+__all__ = (
+    "Client",
+    "ListChannelsPaginator",
+    "ListEventRulesPaginator",
+    "ListNotificationConfigurationsPaginator",
+    "ListNotificationEventsPaginator",
+    "ListNotificationHubsPaginator",
+    "UserNotificationsClient",
+)
