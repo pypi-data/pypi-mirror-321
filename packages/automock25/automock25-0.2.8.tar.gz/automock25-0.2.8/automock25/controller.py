0263922b2b267786423e95c901ff8bf6c0ad7988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:55 +0000

    Add package automock25/0.2.8/automock25-0.2.8.tar.gz

---
+++ packages/automock25/automock25-0.2.8.tar.gz/automock25-0.2.8/automock25/controller.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from status import get_config_params, get_job_status
+import  logset
+# 设置日志
+logger = logset.setup_logging
+def init_routes(app):
+
+
+    @app.route('/')
+    def index():
+        logger.info("Index page accessed")
+        return "Flask App is running with job running in the background."
+
+    @app.route('/status')
+    def status():
+        logger.info("Status page accessed")
+        config_params = get_config_params()
+        job_status = get_job_status()
+        return render_template('status.html', config_params=config_params, job_status=job_status)
