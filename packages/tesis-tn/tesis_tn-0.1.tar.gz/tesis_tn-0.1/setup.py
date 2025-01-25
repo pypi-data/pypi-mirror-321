19e5b9c5c335777c44492c1bec0920d3e65f8a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 11:09:56 +0000

    Add package tesis-tn/0.1/tesis_tn-0.1.tar.gz

---
+++ packages/tesis-tn/tesis_tn-0.1.tar.gz/tesis_tn-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tesis_tn',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    license='MIT',
+)
