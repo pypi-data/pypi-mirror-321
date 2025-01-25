6125e021a20b079d1596baee4098712fc76384a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-25 12:05:54 +0000

    Add package feltools/0.0.1/feltools-0.0.1.tar.gz

---
+++ packages/feltools/feltools-0.0.1.tar.gz/feltools-0.0.1/README.md
@@ -0,0 +1,14 @@
+# feltools
+
+feltoos is a simple Python toolbox designed to provide utility functions for various tasks. This package aims to simplify common operations and make your coding experience more efficient.
+
+## Features
+
+general utility
+
+## Installation
+
+You can install feltools using pip:
+
+```bash
+pip install feltools
---
+++ packages/feltools/feltools-0.0.1.tar.gz/feltools-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='feltools',
+    version='0.0.1',
+    author="Felix Reiter",
+    author_email='sbody1113@gmail.com',
+    description='A collection of simple tools in Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    packages=find_packages(),
+    classifiers=[],
+    install_requires=[]
+)
